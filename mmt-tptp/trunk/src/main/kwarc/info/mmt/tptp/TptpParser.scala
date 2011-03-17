7ae416e014fa54a23cdf5aad0bfe41a9b4cf02bc
Dimitar Misev <misev@rasdaman.com>
2011-03-17 17:06:01 +0000

    setup tptp parser
    git-svn-id: https://svn.kwarc.info/repos/MMT/src@49 09069c07-60fa-4895-8316-727242e252e4

---
+++ mmt-tptp/trunk/src/main/kwarc/info/mmt/tptp/TptpParser.scala
@@ -0,0 +1,19 @@
+package kwarc.info.mmt.tptp
+
+import tptp._
+import java.io._
+
+class TptpParser {
+
+  var parser: tptp.TptpParser = null
+  val outputManager = new SimpleTptpParserOutput()
+
+  def this(tptpFile: File) = {
+    this()
+    val in = new FileInputStream(tptpFile)
+    val lexer = new TptpLexer(new DataInputStream(in))
+    parser = new tptp.TptpParser(lexer)
+  }
+
+  def parseNext: TptpParserOutput.TptpInput = parser.topLevelItem(outputManager)
+}
