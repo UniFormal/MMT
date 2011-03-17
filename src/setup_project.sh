823f76d1f394a0c74fbc96bd2d653c64ede54978
dmisev <dmisev@09069c07-60fa-4895-8316-727242e252e4>
2011-03-17 07:58:57 +0000

    setup mmt-tptp project
    git-svn-id: https://svn.kwarc.info/repos/MMT@39 09069c07-60fa-4895-8316-727242e252e4

--- src/setup_project.sh
+++ src/setup_project.sh
@@ -3,5 +3,5 @@
 # usage: setup_project.sh project_name
 
 mkdir $1
-mkdir $1/trunk $1/releases $1/branches
+mkdir -p $1/trunk $1/releases $1/branches $1/trunk/src/main/info/kwarc/mmt/ $1/trunk/src/test/info/kwarc/mmt/ $1/trunk/lib
 svn add $1
