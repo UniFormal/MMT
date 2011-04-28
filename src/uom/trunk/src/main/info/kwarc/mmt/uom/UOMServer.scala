42133996fb36a874c7e40209082e2b1a0aded347
frabe <frabe@09069c07-60fa-4895-8316-727242e252e4>
2011-04-28 15:08:52 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@113 09069c07-60fa-4895-8316-727242e252e4

--- src/uom/trunk/src/main/info/kwarc/mmt/uom/UOMServer.scala
+++ src/uom/trunk/src/main/info/kwarc/mmt/uom/UOMServer.scala
@@ -5,9 +5,9 @@ import java.io._
 import java.util.jar._
 import info.kwarc.mmt.api.objects._
 import info.kwarc.mmt.api._
-import scala.collection.mutable.HashMap
+import scala.collection.mutable.{HashMap,HashSet}
 
-object UOMServer {
+class UOMServer {
 
   val impls = new HashMap[GlobalName, Implementation]
   /** a set of associative operators (semigroup) (flex arity is assumed) */
@@ -15,7 +15,7 @@ object UOMServer {
   /** a set of commutative operators (commutative magma) */
   val commut = new HashSet[GlobalName]
   /** a partial map from associative operators to unit elements (monoid) */
-  val unit   = new HashSet[GlobalName,GlobalName]
+  val unit   = new HashMap[GlobalName,GlobalName]
   /** a partial map from monoidal operators to unary and binary inversion operators (group) */
   val inverse = new HashMap[GlobalName,(Option[GlobalName],Option[GlobalName])]
   /** maps operators that should trigger symbolic simplification (magma operations and inverses)
@@ -23,39 +23,39 @@ object UOMServer {
   val symbolic = new HashMap[GlobalName,GlobalName]
 
   def register(jarFileName : String) {
-    val urlArray = new Array[URL](1)
     val jarFile = new File(jarFileName)
+    
+    val urlArray = new Array[URL](1)
     urlArray(0) = jarFile.toURI.toURL 
     val child = new URLClassLoader(urlArray, this.getClass.getClassLoader)
 
     val jarInput = new JarInputStream(new FileInputStream(jarFileName))
-    while(true) {
-      val jarEntry = jarInput.getNextJarEntry
-      if (jarEntry == null) {
-        return
-      }
-      if ( jarEntry.getName.endsWith(".class") ) {
+    
+    var jarEntry : JarEntry = null // a jar entry is a resource/file in the jar 
+    while ({jarEntry = jarInput.getNextJarEntry; jarEntry != null}) {
+      if (jarEntry.getName.endsWith(".class")) {
         val className = jarEntry.getName()
           .replaceAll("/", "\\.")
           .replaceAll(".class","")
         
-        val classToLoad = Class.forName(className, true, child)
-        classToLoad.getDeclaredMethods.map(method => { 
-          if (method.getReturnType.getName.equals(
-              "info.kwarc.mmt.uom.Implementation")) {
-            val instance = classToLoad.newInstance
+        val classToLoad = Class.forName(className, true, child) // returns an object reflecting the class
+        val instance = classToLoad.newInstance
+        // call each method in instance that returns an Implementation (we really only care about the Scala-generated getter methods of Implementation-fields)
+        // and register the result in impls
+        classToLoad.getDeclaredMethods map {method => 
+          if (method.getReturnType.getName == "info.kwarc.mmt.uom.Implementation") {
             val invokeResult = method.invoke(instance)
             invokeResult match {
-              case impl : Implementation => { 
+              case impl : Implementation => {
                 impls(impl.name) = impl
               }
               case _ => {
                 System.err.println("Wrong return type of method")
                 System.exit(1)
-              } 
+              }
             }
           }
-        })
+        }
       }
     }
   }
@@ -77,26 +77,28 @@ object UOMServer {
         impls.get(p) match {
            case Some(impl) => impl(recargs :_*)
            case None => OMA(OMID(p), recargs)
+        }
      case _ => term
   }
+}
 
+object Test {
   def main (args : Array[String]) {
-    register(args(0))
+    val uom = new UOMServer
+    args.map(uom.register)
     
     /* Test with some example terms */
     val base = DPath(new utils.xml.URI("http://cds.omdoc.org/unsorted/uom.omdoc"))
-    val gname1 = GlobalName(OMMOD(MPath(base, LocalPath(List(
-          "lists")))), LocalName(List(NamedStep("append_*"))))
-    val gname2 = GlobalName(OMMOD(MPath(base, LocalPath(List(
-          "lists_ext")))), LocalName(List(NamedStep("append_many_*"))))
+    val gname1 = OMMOD(base ? "lists")  % LocalName("append_*")
+    val gname2 = OMMOD(base ? "lists_ext") % LocalName("append_many_*")
 
-    val ex = new org.omdoc.cds.unsorted.uom.omdoc.lists
+    val ex  = new org.omdoc.cds.unsorted.uom.omdoc.lists
     val ext = new org.omdoc.cds.unsorted.uom.omdoc.lists_ext
 
-    /* append( [el], [el])  */
-    val c1 = OMA(ex.append, OMA(ex.cons, ex.elem::ex.nil::Nil) :: 
+    /* append( [el], [el]) */
+    val c1 = OMA(ex.append, OMA(ex.cons, ex.elem :: ex.nil::Nil) :: 
         OMA(ex.cons, ex.elem::ex.nil::Nil) ::Nil)
-    val t1 = (simplify(c1))
+    val t1 = (uom.simplify(c1))
     println(t1+"\n\n")
 
     /* append_many([el], [el], [el]) */
@@ -105,23 +107,20 @@ object UOMServer {
       OMA(ex.cons, ex.elem::ex.nil::Nil),
       OMA(ex.cons, ex.elem::ex.nil::Nil)
       )))
-    val t2 = simplify(c2)
+    val t2 = uom.simplify(c2)
     println(t2+"\n\n")
 
     /* append_many(c2)  */
     val c2_equal = OMA(ext.append_many, c2::Nil)
-    if (simplify(c2_equal).equals(t2)) 
+    if (uom.simplify(c2_equal).equals(t2)) 
       println("They are the same as expceted\n\n")
 
     /* append(c1, c2)  */
     val c3 = OMA(ex.append, c1::c2::Nil)
-    println(simplify(c3)+"\n\n")
+    println(uom.simplify(c3)+"\n\n")
 
     /* very composite, result should be a list with 10 el */
     val c4 = OMA(ext.append_many, c3::c2_equal::c1::Nil)
-    println(simplify(c4))
+    println(uom.simplify(c4))
   }
 }
-
-
-
