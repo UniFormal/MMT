35663ae62265b9e4be7e9edf04c115adda6788cd
Florian Rabe <florian.rabe@gmail.com>
2011-05-03 14:58:01 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT/src@122 09069c07-60fa-4895-8316-727242e252e4

--- mmt-api/trunk/src/main/info/kwarc/mmt/api/archives/Archive.scala
+++ mmt-api/trunk/src/main/info/kwarc/mmt/api/archives/Archive.scala
@@ -3,11 +3,12 @@ import info.kwarc.mmt.api._
 import info.kwarc.mmt.api.frontend._
 import info.kwarc.mmt.api.modules._
 
-class Archive(root: java.io.File) {
+class Archive(root: java.io.File, report: Report) {
+    def log(msg: => String) = report("archive", msg)
     def MMTPathToFilePath(m: MPath) : java.io.File = null
     def narrToCont(controller : Controller) {
         val omdocfile = new java.io.File("")
-        // controller.delete(omdocfile)
+        // controller.delete(omdocfile) or controller.clear
         val doc = controller.read(omdocfile)
         controller.getDocument(doc).getModulesResolved(controller.library) foreach put
     }
@@ -20,4 +21,15 @@ class Archive(root: java.io.File) {
          //java.io.writeFile(trg, ... xml)
     }
     def toMar(target: java.io.File) {}
+}
+
+object Test {
+   def main(args: Array[String]) {
+      val report = new frontend.FileReport(new java.io.File("archives.log"))
+      report.groups += "archive"
+      //val checker = new libraries.FoundChecker(libraries.DefaultFoundation)
+      val controller = new Controller(libraries.NullChecker, report)
+      val archive = new Archive(new java.io.File(""), report)
+      archive.narrToCont(controller)
+   }
 }
