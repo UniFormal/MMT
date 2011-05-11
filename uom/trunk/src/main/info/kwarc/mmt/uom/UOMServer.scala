ddfaaaac9ed01bdf4c5b0807540c97b9891aa584
vzamdzhiev <vzamdzhiev@9069c07-60fa-4895-8316-727242e252e4>
2011-05-11 14:40:00 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT/src@144 09069c07-60fa-4895-8316-727242e252e4

--- uom/trunk/src/main/info/kwarc/mmt/uom/UOMServer.scala
+++ uom/trunk/src/main/info/kwarc/mmt/uom/UOMServer.scala
@@ -90,13 +90,13 @@ class UOMServer(report: frontend.Report) {
      case _ => term
   }
 }
-
+/*
 object Test {
   def main (args : Array[String]) {
     val uom = new UOMServer(new frontend.FileReport(new java.io.File("uom.log")))
     args.map(uom.register)
     
-    /* Test with some example terms */
+    // Test with some example terms 
     val base = DPath(new utils.xml.URI("http://cds.omdoc.org/unsorted/uom.omdoc"))
     val gname1 = OMMOD(base ? "lists")  % LocalName("append_*")
     val gname2 = OMMOD(base ? "lists_ext") % LocalName("append_many_*")
@@ -104,14 +104,14 @@ object Test {
     val ex  = new org.omdoc.cds.unsorted.uom.omdoc.lists
     val ext = new org.omdoc.cds.unsorted.uom.omdoc.lists_ext
 
-    /* append( [el], [el]) */
+    // append( [el], [el]) 
     val c1 = OMA(ex.append, OMA(ex.cons, ex.elem :: ex.nil::Nil) :: 
         OMA(ex.cons, ex.elem::ex.nil::Nil) ::Nil)
     println("XML BELOW\n\n" + c1.toNode)
     val t1 = (uom.simplify(c1))
     println(t1+"\n\n")
 
-    /* append_many([el], [el], [el]) */
+    // append_many([el], [el], [el]) 
     val c2 =(OMA(ext.append_many, List(
       OMA(ex.cons, ex.elem::ex.nil::Nil), 
       OMA(ex.cons, ex.elem::ex.nil::Nil),
@@ -120,17 +120,18 @@ object Test {
     val t2 = uom.simplify(c2)
     println(t2+"\n\n")
 
-    /* append_many(c2)  */
+    // append_many(c2)  
     val c2_equal = OMA(ext.append_many, c2::Nil)
     if (uom.simplify(c2_equal).equals(t2)) 
       println("They are the same as expceted\n\n")
 
-    /* append(c1, c2)  */
+    // append(c1, c2)  
     val c3 = OMA(ex.append, c1::c2::Nil)
     println(uom.simplify(c3)+"\n\n")
 
-    /* very composite, result should be a list with 10 el */
+    // very composite, result should be a list with 10 el 
     val c4 = OMA(ext.append_many, c3::c2_equal::c1::Nil)
     println(uom.simplify(c4))
   }
 }
+*/
