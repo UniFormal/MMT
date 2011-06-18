ae399180f069ab031521b490474d5aaa82e88bee
fihorozal <fihorozal@09069c07-60fa-4895-8316-727242e252e4>
2011-06-18 20:29:49 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@219 09069c07-60fa-4895-8316-727242e252e4

--- src/OWLMMT/src/info/kwarc/mmt/owl/Export.scala
+++ src/OWLMMT/src/info/kwarc/mmt/owl/Export.scala
@@ -26,40 +26,42 @@ import info.kwarc.mmt.api.utils._
 import scala.collection.immutable.List 
 
 class Export (manager : OWLOntologyManager , controller : Controller) {
-   /**
+   
+	/**
     * translates all theories in an MMT document to ontologies
     * @param doc the URI of the MMT document
     * @return the list of IRIs of the ontologies
     */
    def documentToOWL(doc: DPath) : List[IRI] = {  
-	   Nil
+	null   
    }
 }
 
 object Export {	
+	
+	
 	def main(args: Array[String]) {
 		val checker = new FoundChecker(DefaultFoundation)
-		val report = new FileReport(new java.io.File("controller.log")) //report("owl", "message")
+		val report = new FileReport(new java.io.File("controller.log")) 
 		val controller = new Controller(checker, report)
 		controller.handle(ExecFile(new java.io.File("startup.mmt"))) 
 		val manager : OWLOntologyManager = OWLManager.createOWLOntologyManager()
 		val exporter = new Export (manager, controller)
-		
 		/*	
 			val source : File = new File(arg(0))
 			val target : File = new File(arg(1))			
 		*/
 		//val file : File = new File("examples\\ex2.owl");
-		val source : File = new File("E:\\Fall10\\CompSem\\Project\\OWLMMT\\Test\\Axioms\\classAxiom.omdoc")		
-		val target : File = new File("E:\\Fall10\\CompSem\\Project\\OWLMMT\\Test\\Axioms\\classAxiomToOWL.owl") 
+		val source : File = new File("E:\\Fall10\\CompSem\\Project\\OWLMMT\\Test\\Axioms\\declarationAxiom.omdoc")		
+		val target : File = new File("E:\\Fall10\\CompSem\\Project\\OWLMMT\\Test\\Axioms\\declarationAxiomToOWL.owl") 
 		
 		val doc : DPath  = controller.read(source)
 		
 		def writeToFile(iri : IRI, trg : File) {
 			val onto = manager.getOntology(iri)
 			val file = new java.io.FileWriter(trg)
-			val ontotarget = new WriterDocumentTarget(file) 
-			manager.saveOntology(onto, ontotarget)
+			val ontoTarget = new WriterDocumentTarget(file) 
+			manager.saveOntology(onto, ontoTarget)
 			file.close
 		}
 		
