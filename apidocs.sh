61bdeeec7bc64af4e40369a313979030508f47dd
Alin Iacob <alin.iub@gmail.com>
2011-06-19 13:23:50 +0000

    added lfcatalog to the apidocs generation
    git-svn-id: https://svn.kwarc.info/repos/MMT/src@222 09069c07-60fa-4895-8316-727242e252e4

--- apidocs.sh
+++ apidocs.sh
@@ -5,10 +5,10 @@
 BASE_URL=https://svn.kwarc.info/repos/MMT/src/
 DEST=../deploy/apidocs/
 # classpath libraries
-LIBS=mmt-api/trunk/lib/tntbase-plugin-api.jar:../deploy/mmt-api/lib/mmt-api.jar:OWLMMT/lib/owlapi-bin.jar:../deploy/uom/lib/uom.jar:mmt-web/trunk/lib/commons-fileupload-1.2.1.jar:mmt-web/trunk/lib/joda-time-1.6.2.jar:mmt-web/trunk/lib/lift-actor_2.8.1-2.2-SNAPSHOT.jar:mmt-web/trunk/lib/lift-common_2.8.1-2.2-SNAPSHOT.jar:mmt-web/trunk/lib/lift-json_2.8.1-2.2-SNAPSHOT.jar:mmt-web/trunk/lib/lift-util_2.8.1-2.2-SNAPSHOT.jar:mmt-web/trunk/lib/lift-webkit_2.8.1-2.2-SNAPSHOT.jar:mmt-web/trunk/lib/log4j-1.2.16.jar:mmt-web/trunk/lib/servlet-api-2.5.jar:mmt-web/trunk/lib/slf4j-api-1.6.1.jar:mmt-web/trunk/lib/slf4j-log4j12-1.6.1.jar
+LIBS=mmt-api/trunk/lib/tntbase-plugin-api.jar:../deploy/mmt-api/lib/mmt-api.jar:OWLMMT/lib/owlapi-bin.jar:../deploy/uom/lib/uom.jar:mmt-web/trunk/lib/commons-fileupload-1.2.1.jar:mmt-web/trunk/lib/joda-time-1.6.2.jar:mmt-web/trunk/lib/lift-actor_2.8.1-2.2-SNAPSHOT.jar:mmt-web/trunk/lib/lift-common_2.8.1-2.2-SNAPSHOT.jar:mmt-web/trunk/lib/lift-json_2.8.1-2.2-SNAPSHOT.jar:mmt-web/trunk/lib/lift-util_2.8.1-2.2-SNAPSHOT.jar:mmt-web/trunk/lib/lift-webkit_2.8.1-2.2-SNAPSHOT.jar:mmt-web/trunk/lib/log4j-1.2.16.jar:mmt-web/trunk/lib/servlet-api-2.5.jar:mmt-web/trunk/lib/slf4j-api-1.6.1.jar:mmt-web/trunk/lib/slf4j-log4j12-1.6.1.jar:../deploy/lfcatalog/lib/lfcatalog.jar
 # scala source directories
 SRC=trunk/src/main
-SOURCES="mmt-api/$SRC mmt-web/$SRC OWLMMT/src/ uom/$SRC"
+SOURCES="mmt-api/$SRC mmt-web/$SRC OWLMMT/src/ uom/$SRC lfcatalog/$SRC"
 
 
 mkdir -p $DEST
