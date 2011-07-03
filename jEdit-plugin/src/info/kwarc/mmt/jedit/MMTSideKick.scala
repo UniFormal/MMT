d733c62b8a63a6ef54de093188ffee9b46a7e7db
Florian Rabe <florian.rabe@gmail.com>
2011-07-03 19:41:32 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT/src@246 09069c07-60fa-4895-8316-727242e252e4

--- jEdit-plugin/src/info/kwarc/mmt/jedit/MMTSideKick.scala
+++ jEdit-plugin/src/info/kwarc/mmt/jedit/MMTSideKick.scala
@@ -6,11 +6,51 @@ import sidekick._
 import info.kwarc.mmt.api._
 import frontend._
 import libraries._
+import javax.swing.tree.DefaultMutableTreeNode
+
+import scala.collection.JavaConversions.asJavaList
+
+case class MyPosition(offset : Int) extends javax.swing.text.Position {
+   def getOffset = offset
+}
+
+object MyNode {
+   // not sure if line is ever used for anything interesting
+   def apply(name: String, line: Int, startOffset: Int, endOffset: Int) : DefaultMutableTreeNode = {
+      val asset = new enhanced.SourceAsset(name, line, MyPosition(startOffset))
+      asset.setEnd(MyPosition(endOffset))
+      new DefaultMutableTreeNode(asset)
+   }
+}
+
+// text is the string that is to be completed, items is the list of completions
+class MyCompletion(view : View, text: String, items: List[String])
+  extends SideKickCompletion(view, text, items) {
+   // override def insert(index: Int) // this methods modifies the textArea after the user selected a completion
+}
 
 class MMTSideKick extends SideKickParser("mmt") {
-   // override def getPanel()
    def parse(buffer: Buffer, errorSource: DefaultErrorSource) : SideKickParsedData = {
-      null
+      val path = buffer.getPath()
+      val tree = new SideKickParsedData(path)
+      val root = tree.root
+      // build a tree with some random data for testing 
+      val child1 = MyNode("child1", 0, 0, 9)
+      root.add(child1)
+      val child2 = MyNode("child2", 0, 10, 20)
+      root.add(child2)
+      val child21 = MyNode("child21", 1, 21, 30)
+      child2.add(child21)
+      val child22 = MyNode("child22", 2, 31, 40)
+      child2.add(child22)
+      
+      // register some errors
+      val error1 = new DefaultErrorSource.DefaultError(errorSource, ErrorSource.WARNING, path, 3,0,0,"a full line warning")
+      val error2 = new DefaultErrorSource.DefaultError(errorSource, ErrorSource.ERROR, path, 4,4,15,"a partial line error")
+      errorSource.addError(error1)
+      errorSource.addError(error2)
+      
+      tree
    }
    // override def stop() 
    // override def getParseTriggers : String = ""
@@ -19,6 +59,10 @@ class MMTSideKick extends SideKickParser("mmt") {
    // override def canCompleteAnywhere = true
    // override def getInstantCompletionTriggers : String = ""
    override def complete(editPane: EditPane, caret : Int) : SideKickCompletion = {
-      null
+      val view = editPane.getView
+      val pd = SideKickParsedData.getParsedData(view) // we have access to the result of the parser
+      val asset = pd.getAssetAtOffset(caret)
+      // return a dummy completion popup
+      new MyCompletion(view, "test", List("compl1", "compl2", asset.getName))
    }
 }
