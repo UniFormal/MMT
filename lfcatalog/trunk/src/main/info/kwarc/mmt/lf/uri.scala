a48b51923cb4ce1f184268280a4c6d836e266d96
Alin Iacob <alin.iub@gmail.com>
2011-07-07 06:52:21 +0000

    fixed URI bug that sometimes disallowed unknown schemes
    git-svn-id: https://svn.kwarc.info/repos/MMT/src@280 09069c07-60fa-4895-8316-727242e252e4

--- lfcatalog/trunk/src/main/info/kwarc/mmt/lf/uri.scala
+++ lfcatalog/trunk/src/main/info/kwarc/mmt/lf/uri.scala
@@ -1,10 +1,10 @@
 package info.kwarc.mmt.lf
+import java.util.regex.{Pattern,Matcher}
 
 case class URI(var uri : java.net.URI) {
     /** Create an valid URI where the illegal or non-ascii character in path, query and fragment are quoted
       * @return a valid, %-encoded URI
-      * @throws java.net.MalformedURLException if the scheme part is unknown. An empty scheme is not an error
-      * @throws java.net.URISyntaxException if the URI violates RFC 2396 
+      * @throws java.net.URISyntaxException if the URI violates RFC 2396 or RFC 3986
       */
     def this(s : String) = this(URI.encode(s))
     
@@ -44,26 +44,13 @@ case class URI(var uri : java.net.URI) {
 object URI {
     /** Create an valid URI where the illegal or non-ascii character in path, query and fragment are quoted
       * @return a valid, %-encoded URI
-      * @throws java.net.MalformedURLException if the scheme part is unknown. An empty scheme is not an error
-      * @throws java.net.URISyntaxException if the URI violates RFC 2396 
+      * @throws java.net.URISyntaxException if the URI violates RFC 2396 or RFC 3986
       */
     private def encode(s : String) : java.net.URI = {
-        // special case for empty string
-        if (s.isEmpty) return new java.net.URI(s)
-        
-        var u : Option[java.net.URL] = None
-        try {
-            // first convert to URL, so that we can use the library extraction methods for its components
-            u = Some(new java.net.URL(s))
-        } catch {
-            // if there is no scheme, add one temporarily so that the conversion to URL works
-            case e : java.net.MalformedURLException => {
-                u = Some(new java.net.URL("http://" + s))
-                // remove the fake scheme here                
-                return new java.net.URI(new java.net.URI(u.get.getProtocol, u.get.getAuthority, u.get.getPath, u.get.getQuery, u.get.getRef).toString.substring("http://".length))
-            }
-        }
-        return new java.net.URI(u.get.getProtocol, u.get.getAuthority, u.get.getPath, u.get.getQuery, u.get.getRef)
+        val m : Matcher = Pattern.compile("^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))?").matcher(s) // pattern taken from RFC 3986
+        if (m.matches == false)
+            throw new java.net.URISyntaxException(s, "malformed URI reference")
+        return new java.net.URI(m.group(2), m.group(4), m.group(5), m.group(7), m.group(9))
     }
     implicit def toMyURI(u : URI) : java.net.URI = u.uri
     implicit def javaURItoMyURI(u : java.net.URI) : URI = new URI(u)
@@ -76,7 +63,7 @@ object testme extends Application {
   //println(URI(u) ? "s")
   //println(u.^ + "\n" + new URI("/a").^ + "\n" + new URI("/").^ + "\n" + new URI("/c:").^ + "\n" + new URI("/c:/a").^)
   val tst = new java.net.URL("http://a?q→")
-  val s = "" //"www.test.org/a^?q #f+a"
+  val s = "www.test.org/a^?q #f+a"
   val u = new info.kwarc.mmt.lf.URI(s)
   println(s + "     " + u.toString)
 }*/
