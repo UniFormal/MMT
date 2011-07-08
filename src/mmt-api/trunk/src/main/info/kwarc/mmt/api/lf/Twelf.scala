7ee69f6dc3bb5556255f7f9191f4b4017adcaf7a
aiacob <aiacob@09069c07-60fa-4895-8316-727242e252e4>
2011-07-08 12:30:07 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@299 09069c07-60fa-4895-8316-727242e252e4

--- src/mmt-api/trunk/src/main/info/kwarc/mmt/api/lf/Twelf.scala
+++ src/mmt-api/trunk/src/main/info/kwarc/mmt/api/lf/Twelf.scala
@@ -3,18 +3,21 @@ import info.kwarc.mmt.lf._
 import info.kwarc.mmt.api.backend._
 
 import java.io.File
+import scala.collections.mutable.HashSet
 
-class Catalog(val port: Int)
 
 class Twelf(path : java.io.File) extends Compiler {
    val kind = "twelf"
-   val catalog = new Catalog(8081)
+   val catalog = new Catalog(new HashSet[String]()+path.getPath, new HashSet[String]+"*.elf", new HashSet[String]+".svn", 8081)
    val catalogURI = "http://localhost:" + catalog.port + "/getText"
+   
+   /** Initialize the catalog
+     * @throws PortUnavailable if the port is in use */
    def init {
-      // start catalog
+      catalog.init
    }
    def destroy {
-      // end catalog
+      catalog.destroy
    }
    def check(in: File, targetdir: File) {
       val proc = new java.lang.ProcessBuilder(path.toString, "-omdoc ", targetdir.toString, "-catalog", catalogURI, in.toString)
