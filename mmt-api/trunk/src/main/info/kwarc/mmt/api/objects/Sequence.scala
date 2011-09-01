f0f9a408603c8be27e979a477a85c46b9480d94b
fhorozal <fhorozal@9069c07-60fa-4895-8316-727242e252e4>
2011-09-01 14:57:25 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT/src@444 09069c07-60fa-4895-8316-727242e252e4

--- mmt-api/trunk/src/main/info/kwarc/mmt/api/objects/Sequence.scala
+++ mmt-api/trunk/src/main/info/kwarc/mmt/api/objects/Sequence.scala
@@ -9,13 +9,6 @@ import Conversions._
 
 import scala.xml.{Node}
 
-/*
-object Succ {
-   def apply(n: Nat) : Nat = n + 1
-   def unapply(n:Nat) : Option[Nat] = if (n > 0) Some(n - 1) else None 
-}
-*/
-
 object FlatSequence {
 	def unapply(s: Sequence) : Option[List[Term]] = s.items match {
 		case Nil => Some(Nil)
@@ -58,59 +51,6 @@ object normalize {
     	case SeqVar(n) => List(sit)
     	case e : Term => List(normalizeTerm(e))
 	  }
-	  
-	 	/*case e : Term =>
-	 	    		s2 match {
-	 	    			case SeqItemList(items) =>
-	 	    				val en = normalizeTerm(e)
-	 	    				items.flatMap(i => normalizeSeqItem(SeqSubst(en,n,i)))
-	 	    			case tm : Term => List(normalizeTerm(e) ^ Substitution(TermSub(n,normalizeTerm(tm))))
-	 	    			case SeqVar(m) => List(SeqSubst(normalizeTerm(e),n,SeqVar(m)))
-	 	    			case SeqSubst(ep,np,sp) => normalizeSeqItem(SeqSubst(e,n,normalizeSeq(SeqSubst(ep,np,sp)))) 	 	  	     
-	 	    			case SeqUpTo(m) =>
-	 	    				val mn = normalizeTerm(m)
-	 	    				val en = normalizeTerm(e)
-	 	    				mn match {
-	 	    					case OMI(i) =>
-	 	    						val l = List.range(1,i.toInt)	  
-	 	    						l.map(x => en ^ Substitution(TermSub(n,OMI(x))))
-	 	    					case _ => List(SeqSubst(en,n,mn))
-	 	    				}	 	  	     
-	 	    		}
-	 			case SeqItemList(items) => 
-	 				val ns2 = normalizeSeq(s2)
-	 					items.flatMap(normalizeSeqItem(_)).map {
-	 						case e : Term => ns2.items.map {
-	 							case x : Term => e ^ Substitution(TermSub(n,x))
-	 							case  _ => e
-	 						}
-	 						case _ => this
-	 					}
-	 			case SeqSubst(s1,n,s2) => List(SeqSubst(normalizeSeq(s1),n,normalizeSeq(s2)))
-	 			case _ => throw Invalid("normalization failed")
-	 			
-	 		}
-	 	*/
-	 	//case SeqUpTo(OMI(n)) => List.range(1,n.toInt).map(i => OMI(i))
-	    //case SeqUpTo(e) => List(SeqUpTo(normalizeTerm(e)))		
-	   /*
-    	case SeqSubst(e,n,SeqSubst(e',m,s)) => 
-    	   normalizeSeqItem(SeqSubst(e',m,s)).map(t => normalizeTerm(e ^ Substitution(Sub(n,t))))
-	 	case SeqSubst(e,n,tm : Term) => List(normalizeTerm(e) ^ Substitution(TermSub(n,normalizeTerm(tm)))	 	
-	 	case SeqSubst(e,n,SeqVar(m)) => List(SeqSubst(normalizeTerm(e),n,SeqVar(m)))
-	 	case SeqSubst(e,n,SeqItemList(items)) => 
-	 	   val en = normalizeTerm(e)
-	 	   items.flatMap(i => normalizeSeqItem(SeqSubst(en,n,i)))
-	 	case SeqSubst(e,n,SeqUpTo(m)) =>
-	 	   val mn = normalizeTerm(m)
-	 	   val en = normalizeTerm(e)
-	 	   mn match {
-	 	  	   case OMI(i) =>
-	 	 	   	  val l = List.range(1,i.toInt)	  
-	 	  	   	  l.map(x => en ^ Substitution(TermSub(n,OMI(x))))
-	 	  	   case _ => List(SeqSubst(en,n,mn))
-	 	   }
-	 	*/  
   }
 
   def normalizeTerm(tm : Term) : Term = {
@@ -180,9 +120,28 @@ object normalize {
      }
   }
   
-  def normalizeNat(t : Term) : Term = {
+  def normalizeNat(t : Term) : Term = {//TODO Complete?
 	  t match {
 	 	  case OMI(n) => OMI(n)
+	 	  /*
+	 	  case Index(seq,ind) => 
+	 	  	val seqN = normalizeSeq(seq)
+	 	  	val indN = normalizeNat(ind)
+	 	  	(seqN,indN) match {
+	 	  		case (FlatSequence(items),OMI(n)) => items(n.toInt)
+	 	  		case _ => Index(seqN,indN)
+	 	  	}
+          case OMA(fn,args) => 
+          	val argsN = args.flatMap(normalizeSeqItem)
+          	if (argsN.isEmpty) normalizeTerm(fn)
+          	else OMA(normalizeTerm(fn),args.flatMap(normalizeSeqItem))
+	 	  case OMBIND(bin,con,bdy) =>
+          	val (conN,sub) = normalizeContext(con)
+          	OMBIND(normalizeTerm(bin), conN, normalizeTerm(bdy ^ sub))	 	  
+	 	  case OMATTR(arg,key,value)=> OMATTR(normalizeTerm(arg),key,normalizeTerm(value)) 
+	 	  case OMM(arg,via) => OMM(normalizeTerm(arg),via)
+	 	  case OME(err, args) => OME(normalizeTerm(err),args.map(normalizeTerm))
+	 	  */
 	 	  case t => t
 	  }
   }
@@ -192,8 +151,7 @@ object normalize {
 	 	  case OMI(n) => Some(n.toInt)
 	 	  case _ => None
 	  }
-  }
-	 
+  }	 
 }  
 
  
