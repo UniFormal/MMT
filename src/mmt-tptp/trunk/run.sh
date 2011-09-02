2851fd1262a6e42e77e1cf0c53081fb5710bdbce
dmisev <dmisev@09069c07-60fa-4895-8316-727242e252e4>
2011-09-02 17:41:50 +0000

    added compiler
    git-svn-id: https://svn.kwarc.info/repos/MMT@450 09069c07-60fa-4895-8316-727242e252e4

---
+++ src/mmt-tptp/trunk/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -cp build/mmt-tptp.jar -jar lib/mmt-api.jar file run.mmt
--- src/mmt-tptp/trunk/src/main/info/kwarc/mmt/tptp/TptpToMMT.scala
+++ src/mmt-tptp/trunk/src/main/info/kwarc/mmt/tptp/TptpToMMT.scala
@@ -3,12 +3,12 @@ package info.kwarc.mmt.tptp
 import scala.collection.mutable._
 import tptp.SimpleTptpParserOutput._
 import tptp.SimpleTptpParserOutput.Formula._
-import info.kwarc.mmt.api.libraries.NullChecker
-import info.kwarc.mmt.api.frontend.{FileReport, Controller}
 import java.io.File
-import info.kwarc.mmt.api.modules.PlainImport
 import tptp.TptpParserOutput.TptpInput
-import info.kwarc.mmt.api.{LocalPath, DPath, MPath, StructuralElement}
+import info.kwarc.mmt.api._
+import info.kwarc.mmt.api.libraries._
+import info.kwarc.mmt.api.frontend._
+import info.kwarc.mmt.api.modules._
 
 class TptpToMMT {
 
@@ -20,56 +20,56 @@ class TptpToMMT {
       imports.add(theory.replaceFirst("\\+\\d+", "+0"))
   }
 
-  def translate(item: TopLevelItem): Option[StructuralElement] = {
-    if (item.getKind().equals(TptpInput.Kind.Formula))
-      translate((AnnotatedFormula) item)
-    else if (item.getKind().equals(TptpInput.Kind.Include))
-      translate((IncludeDirective) item)
-    else
-      error(item)
-  }
-
-  def translate(item: IncludeDirective): Option[StructuralElement] = {
-    val from = item.getFileName().substring(item.getFileName().indexOf("/") + 1);
-    val mfrom = MPath(DPath(xml.URI(item.getFileName)), LocalPath(TptpUtils.removeExtension(from)));
-//    imports.add(mfrom, );
-    PlainImport()
-  }
-
-  def translate(item: AnnotatedFormula): Option[StructuralElement] = {
-
-  }
-
-  def translate(item: Formula): Option[StructuralElement] = {
-
-  }
-
-  def translate(item: Atomic): Option[StructuralElement] = {
-
-  }
-
-  def translate(item: Term): Option[StructuralElement] = {
-
-  }
-
-  def translate(item: Negation): Option[StructuralElement] = {
-
-  }
-
-  def translate(item: Binary): Option[StructuralElement] = {
-
-  }
-
-  def translate(item: Quantified): Option[StructuralElement] = {
-
-  }
-
-  def error(item: Any): Option[StructuralElement] = {
-    println("error", "Error translating " + item.toString)
-    None
-  }
+//  def translate(item: TopLevelItem): Option[StructuralElement] = {
+//    if (item.getKind().equals(TptpInput.Kind.Formula))
+//      translate((AnnotatedFormula) item)
+//    else if (item.getKind().equals(TptpInput.Kind.Include))
+//      translate((IncludeDirective) item)
+//    else
+//      error(item)
+//  }
+//
+//  def translate(item: IncludeDirective): Option[StructuralElement] = {
+//    val from = item.getFileName().substring(item.getFileName().indexOf("/") + 1);
+//    val mfrom = MPath(DPath(xml.URI(item.getFileName)), LocalPath(TptpUtils.removeExtension(from)));
+////    imports.add(mfrom, );
+//    PlainImport()
+//  }
+//
+//  def translate(item: AnnotatedFormula): Option[StructuralElement] = {
+//
+//  }
+//
+//  def translate(item: Formula): Option[StructuralElement] = {
+//
+//  }
+//
+//  def translate(item: Atomic): Option[StructuralElement] = {
+//
+//  }
+//
+//  def translate(item: Term): Option[StructuralElement] = {
+//
+//  }
+//
+//  def translate(item: Negation): Option[StructuralElement] = {
+//
+//  }
+//
+//  def translate(item: Binary): Option[StructuralElement] = {
+//
+//  }
+//
+//  def translate(item: Quantified): Option[StructuralElement] = {
+//
+//  }
+//
+//  def error(item: Any): Option[StructuralElement] = {
+//    println("error", "Error translating " + item.toString)
+//    None
+//  }
 }
 
 object TptpToMMT {
-  val CONTROLLER = new Controller(NullChecker, new FileReport(new File("tptp.log")))
+  val controller = new Controller(NullChecker, new FileReport(new File("tptp.log")))
 }
