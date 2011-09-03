1fd2419beb12859ffb4c4b81d69a2dd7402afec0
dmisev <dmisev@09069c07-60fa-4895-8316-727242e252e4>
2011-09-03 06:54:52 +0000

    update run script
    git-svn-id: https://svn.kwarc.info/repos/MMT@456 09069c07-60fa-4895-8316-727242e252e4

--- src/mmt-tptp/trunk/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -cp build/mmt-tptp.jar -jar lib/mmt-api.jar file run.mmt
--- src/mmt-tptp/trunk/src/main/info/kwarc/mmt/tptp/TptpCompiler.scala
+++ src/mmt-tptp/trunk/src/main/info/kwarc/mmt/tptp/TptpCompiler.scala
@@ -15,28 +15,17 @@ import info.kwarc.mmt.api.presentation._
 /**
  * TPTP Compiler, translates TPTP sources to OMDoc
  */
-object TptpCompiler extends Compiler {
+class TptpCompiler extends Compiler {
 
   def isApplicable(src : String) : Boolean = {
     true
   }
 
   override def compile(in : File, out : File) : List[CompilerError] = {
-    println("Compiling " + in.toJava.getName())
     Nil
   }
 
   override def init(args : List[String] = Nil) {
     println("Initialization...")
-//    TptpToMMT.controller.handle(ExecFile(new java.io.File("run.mmt")))
-    println("Done")
-  }
-  
-  override def destroy {
-    println("Destroying")
-  }
-  
-  def main(args: Array[String]): Unit = {
-		TptpCompiler.init()
   }
 }
