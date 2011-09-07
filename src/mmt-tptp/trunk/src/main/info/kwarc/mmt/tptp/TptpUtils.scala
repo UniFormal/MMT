7943ea422506845de4a8faf06fdf462e68e5f915
dmisev <dmisev@09069c07-60fa-4895-8316-727242e252e4>
2011-09-07 19:38:52 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@479 09069c07-60fa-4895-8316-727242e252e4

--- src/mmt-tptp/trunk/src/main/info/kwarc/mmt/tptp/TptpUtils.scala
+++ src/mmt-tptp/trunk/src/main/info/kwarc/mmt/tptp/TptpUtils.scala
@@ -7,8 +7,19 @@ package info.kwarc.mmt.tptp
 
 import tptp.TptpParserOutput.BinaryConnective._
 import tptp.TptpParserOutput.Quantifier._
+import info.kwarc.mmt.api._
+import info.kwarc.mmt.api.documents._
+import info.kwarc.mmt.api.utils._
+import info.kwarc.mmt.api.frontend._
+import info.kwarc.mmt.api.symbols._
+import info.kwarc.mmt.api.libraries._
+import info.kwarc.mmt.api.modules._
+import info.kwarc.mmt.api.objects._
 
 object TptpUtils {
+  
+  val baseURI = URI("http", "oaff.mathweb.org") / "tptp"
+	val tptpTh = DPath(URI("http", "latin.omdoc.org") / "foundations" / "tptp") ? "tptp"
 
   val PARSE_DIRS = List("Axioms", "Problems")
   val PARSE_EXTS = List("ax", "p")
@@ -44,4 +55,5 @@ object TptpUtils {
 
   def removeExtension(s: String) = s.substring(0, s.lastIndexOf("."))
 
+	def constant(name : String) : Term = OMID(tptpTh ? name)
 }
