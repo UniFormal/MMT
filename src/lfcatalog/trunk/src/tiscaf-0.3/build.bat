d9f1fbed4abf88adac6ba33d9afc4e7cd6d42571
aiacob <aiacob@09069c07-60fa-4895-8316-727242e252e4>
2011-10-08 14:24:53 +0000

    moved to Scala 2.8.2; fixed small bug in tiscaf-0.3;
    git-svn-id: https://svn.kwarc.info/repos/MMT@523 09069c07-60fa-4895-8316-727242e252e4

--- src/lfcatalog/trunk/src/tiscaf-0.3/build.bat
+++ src/lfcatalog/trunk/src/tiscaf-0.3/build.bat
@@ -1,4 +1,4 @@
 rem @echo off
 mkdir classes
 
-scalac -d myclasses src\zgs\httpd\*.scala src\zgs\httpd\let\*.scala src\zgs\utl\*.scala src\zgs\sync\*.scala
+scalac -d classes src\zgs\httpd\*.scala src\zgs\httpd\let\*.scala src\zgs\utl\*.scala src\zgs\sync\*.scala
