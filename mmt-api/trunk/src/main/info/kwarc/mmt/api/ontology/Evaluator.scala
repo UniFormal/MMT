9a21699201ce5cdca6ffa6504aa74fed6a29d168
Florian Rabe <florian.rabe@gmail.com>
2011-10-12 20:44:03 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT/src@553 09069c07-60fa-4895-8316-727242e252e4

--- mmt-api/trunk/src/main/info/kwarc/mmt/api/ontology/Evaluator.scala
+++ mmt-api/trunk/src/main/info/kwarc/mmt/api/ontology/Evaluator.scala
@@ -11,14 +11,14 @@ sealed abstract class QueryResult {
    def toNode : scala.xml.Node
 }
 /** result of an element query */
-case class ElemResult(s: BaseType) extends QueryResult {
-   def toNode : scala.xml.Node = s match {
-      case p: Path => <result path={p.toPath}/>
-      case o: Obj => <result xmlns:om="http://www.openmath.org/OpenMath">{o.toNode}</result>
-   }
+case class ElemResult(l: List[BaseType]) extends QueryResult {
+   def toNode : scala.xml.Node = <result>{l map {
+      case p: Path => <uri path={p.toPath}/>
+      case o: Obj => <object xmlns:om="http://www.openmath.org/OpenMath">{o.toNode}</object>
+   }}</result>
 }
 /** result of a set query */
-case class ESetResult(h: HashSet[BaseType]) extends QueryResult {
+case class ESetResult(h : HashSet[List[BaseType]]) extends QueryResult {
    def toNode : scala.xml.Node =
       <results>{h map {x => ElemResult(x).toNode}}</results>
 }
@@ -38,72 +38,92 @@ class Evaluator(controller: Controller) {
       case ESet(_) => ESetResult(evaluateESet(q)(Nil))
    }
    /** pre: Query.infer(e) = Elem(_) */
-   private def evaluateElem(q: Query)(implicit context: List[BaseType]) : BaseType = evaluateESet(q).head
+   private def evaluateElem(q: Query)(implicit context: List[BaseType]) : List[BaseType] = evaluateESet(q).head
+   /** pre: Query.infer(e) = Elem(List(b)) */
+   private def evaluateElemBase(q: Query)(implicit context: List[BaseType]) : BaseType = evaluateElem(q).head
    /** pre: Query.infer(e) = Elem(PathType) */
-   private def evaluateElemPath(e: Query)(implicit context: List[BaseType]) = evaluateElem(e).asInstanceOf[Path]
+   private def evaluateElemPath(e: Query)(implicit context: List[BaseType]) = evaluateElemBase(e).asInstanceOf[Path]
    /** pre: Query.infer(e) = Elem(ObjectType) */
-   private def evaluateElemObj(e: Query)(implicit context: List[BaseType]) = evaluateElem(e).asInstanceOf[Obj]
-   private def singleton(b: BaseType) = {val res = new HashSet[BaseType]; res += b; res} 
+   private def evaluateElemObj(e: Query)(implicit context: List[BaseType]) = evaluateElemBase(e).asInstanceOf[Obj]
+
+   private class RHashSet extends HashSet[List[BaseType]] {
+      def +=(b: BaseType) {this += List(b)}
+   }
+   private def empty = new RHashSet 
+   private def singleton(b: BaseType) = {val res = empty; res += b; res}
+
    /** sets are evaluated to hash sets of the respective type
     *  pre: Query.infer(e) succeeds 
     */
-   private def evaluateESet(q: Query)(implicit context: List[BaseType]) : HashSet[BaseType] = q match {
-      case Bound(i) => singleton(context(i))
+   private def evaluateESet(q: Query)(implicit context: List[BaseType]) : HashSet[List[BaseType]] = q match {
+      case Bound(i) => singleton(context(i-1))
       case ThePath(p) => singleton(p)
       case TheObject(o) => singleton(o)
       case SubObject(of, pos) => singleton(evaluateElemObj(of).subobject(pos))
       case Component(of, comp) =>
-         val res = new HashSet[BaseType]
+         val res = empty
          evaluateESet(of) foreach {p => 
             lup.get(p.asInstanceOf[Path]).contComponents(comp) match {
-               case obj : Obj => res += obj
+               case obj : Obj => res += List(obj)
                case _ => throw GetError("component exists but does not indicate an object: " + comp)
             }
          }
          res
       case InferedType(of) =>
-         val res = new HashSet[BaseType]
+         val res = empty
          //TODO: find an applicable foundation
          val found = extman.getFoundation(null).getOrElse(throw GetError("no applicable type inference engine defined"))
          evaluateESet(of) foreach {
-            case t: Term => res += found.infer(t, Context())(lup)
+            case t: Term => res += List(found.infer(t, Context())(lup))
             case o => throw GetError("object exists but is not a term: " + o)
          }
          res
       case ThePaths(es @ _*) =>
-         val res = new HashSet[BaseType]
+         val res = empty
          es foreach {res += _}
          res
       case TheObjects(es @ _*) =>
-         val res = new HashSet[BaseType]
+         val res = empty
          es foreach {res += _}
          res
-      case Singleton(e) => singleton(evaluateElem(e))
+      case Singleton(e) =>
+         val res = empty
+         res += evaluateElem(e)
+         res
       case AllThatAre(c) =>
-         val res = new HashSet[BaseType]
+         val res = empty
          rs.getInds(c) foreach {res += _}
          res
       case Union(s,t) => evaluateESet(s) union evaluateESet(t)
       case BigUnion(dom,s) =>
-         val res = new HashSet[BaseType]
+         val res = empty
             evaluateESet(dom) foreach {
-            e => evaluateESet(s)(e::context) foreach {x => res += x}
+            e => evaluateESet(s)(e:::context) foreach {x => res += x}
          }
          res
-      case Comprehension(dom, pred) => evaluateESet(dom) filter {e => evaluate(pred)(e :: context)}
-      case Unifies(wth) => new HashSet[BaseType] //TODO empty for now
+      case Comprehension(dom, pred) => evaluateESet(dom) filter {e => evaluate(pred)(e ::: context)}
+      case Unifies(wth) => empty //TODO empty for now
       case Related(to, by) =>
-         val res = new HashSet[BaseType]
+         val res = empty
          rs.query(evaluateElemPath(to), by)(res += _)
          res
       case Closure(of) =>
          evaluateESet(of) match { 
             case p : MPath =>
-               val res = new HashSet[BaseType]
+               val res = empty
                rs.theoryClosure(p) foreach {res += _}
                res
             case p => throw GetError("must be a module path " + p)
          }
+      case Tuple(l) =>
+         val res = empty
+         res += l map {q => evaluateElemBase(q)}
+         res
+      case Projection(q,i) =>
+        val t = evaluateElem(q)
+        val res = empty
+        res += t(i)
+        res
    }
    /** propositions are evaluated to booleans */
    def evaluate(p: Prop)(implicit context: List[BaseType]) : Boolean = p match {
@@ -114,6 +134,6 @@ class Evaluator(controller: Controller) {
       case Equal(e,f) => evaluateElem(e) == evaluateElem(f)
       case Not(q) => ! evaluate(q)
       case And(q,r) => evaluate(q) && evaluate(r)
-      case Forall(dom, sc) => evaluateESet(dom) forall {e => evaluate(sc)(e::context)}
+      case Forall(dom, sc) => evaluateESet(dom) forall {e => evaluate(sc)(e:::context)}
    }
 }
