5ab610a163e7825fbaa811aed18a2818305187dd
fihorozal <fihorozal@09069c07-60fa-4895-8316-727242e252e4>
2012-03-16 17:17:01 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@670 09069c07-60fa-4895-8316-727242e252e4

---
+++ src/mmt-owl/run.bat
@@ -0,0 +1,4 @@
+java -Xmx1024m -cp ../mmt-api/trunk/build/mmt-api.jar;../../deploy/lib/*;../../deploy/lfcatalog/*;../../deploy/mmt/*;mmt-owl.jar;lib/owlapi-bin.jar info.kwarc.mmt.api.frontend.RunNull
+
+
+
