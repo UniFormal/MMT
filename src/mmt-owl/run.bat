c39474e184168c8b075e2f930b44c1b1f805e4e1
fihorozal <fihorozal@09069c07-60fa-4895-8316-727242e252e4>
2012-03-23 00:56:23 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@695 09069c07-60fa-4895-8316-727242e252e4

--- src/mmt-owl/run.bat
+++ src/mmt-owl/run.bat
@@ -1,3 +1,9 @@
+: the MMT Shell is invoked by java -cp scala/*; mmt-api.jar info.kwarc.mmt.api.RunNull 
+: Xmx1024m increases the memory available
+: -cp Java classpath
+: jar files of the scala library are here ../../deploy/lib/*
+
+: command line paramets are passed to the shell and executed as a command
 java -Xmx1024m -cp ../mmt-api/trunk/build/mmt-api.jar;../../deploy/lib/*;../../deploy/lfcatalog/*;../../deploy/mmt/*;mmt-owl.jar;lib/owlapi-bin.jar info.kwarc.mmt.api.frontend.RunNull
 
 
