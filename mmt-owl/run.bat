1b134ded3822206614ffe9434ac2cf5bf8a91a5b
fihorozal <fihorozal@9069c07-60fa-4895-8316-727242e252e4>
2012-04-29 18:56:00 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT/src@767 09069c07-60fa-4895-8316-727242e252e4

--- mmt-owl/run.bat
+++ mmt-owl/run.bat
@@ -4,7 +4,9 @@
 : jar files of the scala library are here ../../deploy/lib/*
 
 : command line paramets are passed to the shell and executed as a command
-java -Xmx1024m -cp ../mmt-api/trunk/build/mmt-api.jar;../../deploy/lib/*;../../deploy/lfcatalog/*;../../deploy/mmt/*;mmt-owl.jar;lib/owlapi-bin.jar info.kwarc.mmt.api.frontend.RunNull
+: java -Xmx1024m -cp ../mmt-api/trunk/build/mmt-api.jar;../../deploy/lib/*;../../deploy/lfcatalog/*;../../deploy/mmt/*;mmt-owl.jar;lib/owlapi-bin.jar info.kwarc.mmt.api.frontend.RunNull
+
+java -Xmx1024m -cp ../mmt-api/branches/next-version/build/mmt-api.jar;../../deploy/lib/*;../../deploy/lfcatalog/*;../../deploy/mmt/*;mmt-owl.jar;lib/owlapi-bin.jar info.kwarc.mmt.api.frontend.Run
 
 
 
