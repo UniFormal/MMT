4853065a1120cc9557647fa0279231e29b02abf3
fhorozal <fhorozal@09069c07-60fa-4895-8316-727242e252e4>
2012-05-03 16:24:00 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@795 09069c07-60fa-4895-8316-727242e252e4

---
+++ src/mmt-api/branches/next-version/src/main/info/kwarc/mmt/api/patterns/InstanceElaborator.scala
@@ -0,0 +1,54 @@
+package info.kwarc.mmt.api.patterns
+
+import info.kwarc.mmt.api._
+import libraries._
+import frontend._
+import symbols._
+import modules._
+import objects._
+import objects.Conversions._
+import utils._
+import scala.io.Source
+
+/** elaborates Instance declarations
+ * this is also called the pragmatic-to-strict translation
+ */
+class InstanceElaborator(controller: Controller) extends Elaborator {
+   val report = controller.report
+   /**
+   * returns the elaboration of an instance
+   */
+   def apply(e: ContentElement) {e match {
+     case inst : Instance => 
+     	val pt : Pattern = controller.globalLookup.getPattern(inst.pattern)
+     	val lpair = pt.con.map {d => (d.name,d.name / OMID(inst.home % (inst.name / d.name)))} //TODO Check c.c1
+     	val names = lpair.unzip._1
+     	val subs = lpair.unzip._2  
+        def auxSub(x : Term) = {
+     		x ^ (inst.matches ++ Substitution(subs : _*))  
+     	}
+     	pt.con.map {
+     		case VarDecl(n,tp,df,at @ _*) =>
+     			val nname = inst.name / n
+     			report("elaboration", "generating constant " + nname)
+     			val c = new Constant(inst.home,nname,tp.map(auxSub),df.map(auxSub),None,None)
+     			c.setOrigin(InstanceElaboration(inst.path)) //TODO Check InstanceElaboration
+     			controller.add(c)
+     	} 
+     case _ => ()
+   }
+  }
+  
+  /**
+   * elaborates all instances in a theory and inserts the elaborated constants after the respective instance
+   */
+  def elaborate(thy: DeclaredTheory) {
+     thy.valueList foreach {
+        case i : Instance =>
+           i.setOrigin(Elaborated)
+           thy.replace(i.name, i)
+           apply(i)
+        case c @ _ => c 
+     }
+  }
+}
---
+++ src/mmt-api/branches/next-version/src/main/info/kwarc/mmt/api/patterns/PatternSpotter.scala
@@ -0,0 +1,21 @@
+package info.kwarc.mmt.api.patterns
+
+import info.kwarc.mmt.api._
+import libraries._
+import modules._
+import frontend._
+import symbols._
+import objects._
+import objects.Conversions._
+import utils._
+import scala.io.Source
+
+/** elaborates Instance declarations
+ * this is also called the pragmatic-to-strict translation
+ */
+class PatternSpotter(controller: Controller) extends Elaborator {
+   def apply(e: ContentElement) : Unit = e match {
+     case c: Constant =>
+     case _ =>
+   }
+}
