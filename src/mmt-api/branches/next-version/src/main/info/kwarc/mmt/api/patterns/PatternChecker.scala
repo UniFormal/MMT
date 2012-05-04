5058d422e3a1e521f46ecd50555babef3b4a493c
fhorozal <fhorozal@09069c07-60fa-4895-8316-727242e252e4>
2012-05-04 12:09:37 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@801 09069c07-60fa-4895-8316-727242e252e4

--- src/mmt-api/branches/next-version/src/main/info/kwarc/mmt/api/patterns/PatternChecker.scala
+++ src/mmt-api/branches/next-version/src/main/info/kwarc/mmt/api/patterns/PatternChecker.scala
@@ -8,14 +8,41 @@ import symbols._
 import objects._
 import objects.Conversions._
 import utils._
+import utils.MyList._
 import scala.io.Source
 
 /** elaborates Instance declarations
  * this is also called the pragmatic-to-strict translation
  */
 class PatternChecker(controller: Controller) extends Elaborator {
-   def apply(e: ContentElement) : Unit = e match {
+  def getPatterns(home : Term) : List[Pattern] = {     
+     home match {
+       case OMMOD(p) => 
+         val thy = controller.globalLookup(p)
+         thy match {
+           case d : DeclaredTheory => d.meta match {
+             case Some(m) => 
+               val cmeta = controller.globalLookup(m)
+               cmeta match {
+                 case mthy : DeclaredTheory => 
+                   val decls = mthy.valueList
+                   val patts : List[Pattern]= decls.mapPartial{
+                     case p : Pattern => Some(p)
+                     case _ => None}
+                 case _ => Nil //TODO
+               }
+             case None => Nil
+            }
+           case _ => Nil //TODO
+          }
+        case _ => Nil //TODO
+     }
+   }
+  def apply(e: ContentElement) : Unit = e match {
      case c: Constant =>
-     case _ =>
+       val patts = getPatterns(c.home)
+       
+     case _ => 
    }
 }
+
