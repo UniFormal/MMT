fc8a6e20c61705950cf40da4bc509230a67b4a85
Florian Rabe <florian.rabe@gmail.com>
2012-06-26 11:31:19 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT/src@884 09069c07-60fa-4895-8316-727242e252e4

--- setup_project.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# usage: setup_project.sh project_name
-
-mkdir $1
-mkdir -p $1/trunk $1/releases $1/branches $1/trunk/src/main/info/kwarc/mmt/ $1/trunk/src/test/info/kwarc/mmt/ $1/trunk/lib
-svn add $1
