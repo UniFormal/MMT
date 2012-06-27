d2b144e71b74ec996284836b5a22112afc3b0991
Florian Rabe <florian.rabe@gmail.com>
2012-06-27 12:58:22 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT/src@890 09069c07-60fa-4895-8316-727242e252e4

---
+++ hets-mmt/src/info/kwarc/mmt/hets/Main.scala
@@ -0,0 +1,11 @@
+package info.kwarc.mmt.hets
+
+import info.kwarc.mmt.api._
+import utils.File._ //maybe the stuff here is useful
+import backend.TextReader // the parser
+
+object Main {
+   def main(args: Array[String]) {
+     
+   }
+}
