965bcf744cbb3bf4e4c1bdf2a49dec2e719556ba
Florian Rabe <florian.rabe@gmail.com>
2012-07-19 14:39:11 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT/src@948 09069c07-60fa-4895-8316-727242e252e4

---
+++ mmt-mathscheme/run.bat
@@ -0,0 +1,1 @@
+java -Xmx1024m -cp mmt-mathscheme.jar;../../deploy/lib/*;../../deploy/mmt/* info.kwarc.mmt.api.frontend.Run file startup.mmt
--- mmt-mathscheme/src/info/kwarc/mmt/mathscheme/MathScheme.scala
+++ mmt-mathscheme/src/info/kwarc/mmt/mathscheme/MathScheme.scala
@@ -16,16 +16,24 @@ class MathScheme extends Compiler {
    override def includeFile(n: String) : Boolean = n.endsWith(".msl")
    
    var path : File = null
-
-   private def log(msg : => String) {report("twelf", msg)}
+   /** None: we're on unix
+    *  Some("/cygdrive"): we're on windows using cygwin, so file paths are translated into cygwin mounts using a prefix */ 
+   var prefix : Option[String] = None
+   
+   private def log(msg : => String) {report("mathscheme", msg)}
    
    /** 
-    * creates and intializes a Catalog
+    * creates and initializes a Catalog
     * first argument is the location of the twelf-server script
     */
    override def init(rep: frontend.Report, args: List[String]) {
       super.init(rep, Nil)
-      path = File(args(0))
+      val p = args(0)
+      if (p.startsWith("cygwin:")) {
+         prefix = Some(p.substring(7,p.length))
+         path = File(args(1))
+      } else
+         path = File(args(0))
    }
 
    /** 
@@ -35,16 +43,15 @@ class MathScheme extends Compiler {
      */
    def compile(in: File, out: File) : List[SourceError] = {
       File(out.getParent).mkdirs
-      val procBuilder = new java.lang.ProcessBuilder(path.toString, in.toString)
+      def toCygwinIfNeeded(f: File) = prefix match {
+         case None => f.toString
+         case Some(p) => p + f.toString.replace(":", "").replace("\\", "/") //remove the ":" and turn the \'s into /'s; TODO: clean up this HACK 
+      }
+      val inCyg = toCygwinIfNeeded(in)
+      val outCyg = toCygwinIfNeeded(out.setExtension("omdoc"))
+      val procBuilder = new java.lang.ProcessBuilder(path.toString, in.toString, out.setExtension("omdoc").toString)
       //procBuilder.redirectErrorStream()
       val proc = procBuilder.start()
-      //val input = new PrintWriter(proc.getOutputStream(), true)
-      val output = new BufferedReader(new InputStreamReader(proc.getInputStream()))
-      val outFile = File.Writer(out)
-      var line : String = null
-      while ({line = output.readLine; line != null}) {
-         outFile.write(line + "\n")
-      }
       Nil
    }
 }
