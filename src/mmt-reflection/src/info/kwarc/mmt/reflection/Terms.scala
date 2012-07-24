83e3d479fe855dcd367617ed9de61ef27d1c3ceb
sdumbrava <sdumbrava@09069c07-60fa-4895-8316-727242e252e4>
2012-07-24 12:40:01 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@980 09069c07-60fa-4895-8316-727242e252e4

--- src/mmt-reflection/src/info/kwarc/mmt/reflection/Terms.scala
+++ src/mmt-reflection/src/info/kwarc/mmt/reflection/Terms.scala
@@ -7,7 +7,9 @@ import objects._
 import utils._
 import frontend._
 import objects.Conversions._
-import info.kwarc.mmt.lf._
+import scala.collection.immutable.Stack
+
+//import info.kwarc.mmt.lf._
 
 /* Meta-variable names
    s, t: Terms
@@ -27,13 +29,14 @@ import info.kwarc.mmt.lf._
 case class ReflectionError(msg : String) extends java.lang.Throwable(msg)
 
 object Reflection {
-   val base = new DPath(utils.URI("http", "cds.omdoc.org") / "foundations" / "reflection" / "terms.omdoc")
-   val theory = LF.lfbase ? "Terms"
-   def constant(name : String) = OMID(LF.lftheory ? name)
+   val base = new DPath(utils.URI("http", "cds.omdoc.org") / "foundations" / "reflection" / "termReflection.omdoc")
+   val theory = base ? "TermReflection"
+   def constant(name : String) : GlobalName = theory ? name
    val intro = constant("introduction")
    val elim = constant("elimination")
    val rtype = constant("refltype")
    val eval = constant("evaluation")
+   val ttype = constant("type")
 }
 
 /* apply methods and extractor methods for Scala
@@ -43,7 +46,7 @@ object Reflection {
  */
 
 object TermRefl {
-   def apply(thy: MPath, term: Term) = OMA(Reflection.intro, List(OMMOD(thy), term))
+   def apply(thy: MPath, term: Term) = OMA(OMS(Reflection.intro), List(OMMOD(thy), term))
    def unapply(t : Term) : Option[(MPath,Term)] = t match {
 	   case OMA(Reflection.intro, List(OMMOD(thy), tm)) => Some((thy, tm))
 	   case _ => None
@@ -51,7 +54,7 @@ object TermRefl {
 }
 
 object TermEval {
-  def apply(thy: MPath, term: Term) = OMA(Reflection.eval, List(OMMOD(thy), term))
+  def apply(thy: MPath, term: Term) = OMA(OMS(Reflection.eval), List(OMMOD(thy), term))
   def unapply(t : Term) : Option[(MPath,Term)] = t match {
     case OMA(Reflection.elim, List(OMMOD(thy), tm)) => Some((thy, tm))
     case _ => None
@@ -59,8 +62,7 @@ object TermEval {
 }
 
 object ReflType {
-  def apply(thy: MPath, tp: Term)
-          ={ OMA(Reflection.rtype, List(OMMOD(thy), tp))     }
+  def apply(thy: MPath, tp: Term)= OMA(OMS(Reflection.rtype), List(OMMOD(thy), tp))
   def unapply(t : Term) : Option[(MPath,Term)] = t match {
     case OMA(Reflection.rtype, List(OMMOD(thy), tp)) => Some((thy, tp))
     case _ => None
@@ -68,162 +70,149 @@ object ReflType {
 }
 
 object Elim {
-  def apply(t: Term, mor: Term) = OMA(Reflection.elim, List(t, mor))
+  def apply(t: Term, mor: Term) = OMA(OMS(Reflection.elim), List(t, mor))
   def unapply(t : Term) : Option[(Term,Term)] = t match {
     case OMA(Reflection.rtype, List(t, mor)) => Some((t, mor))
     case _ => None
   }
 }
 
-/** @param theory : quoted theory
- * @param context : context
- */
-case class Frame(theory : MPath, context : Context)
+/* Typing Rules for Reflected Terms */
 
+object TermReflectionRule extends InferenceRule(Reflection.intro) {
+   def apply(solver: Solver, tm: Term, frameStack: Stack[Frame]): Option[Term] =
+    tm match {
+       case TermRefl(s,t) => solver.inferTypeRefl(t, frameStack.push(Frame(s, Context())))
+       case _ => None
+     }
+ }
 
-case class Stack(frames : List[Frame]) {
-   def push (f : Frame) = f::frames
-   def top () = frames.last
-}
+ object TypeReflectionRule extends InferenceRule(Reflection.rtype) {
+   def apply(solver: Solver, tm: Term, frameStack: Stack[Frame]): Option[Term] =
+     tm match {
+       case ReflType(s,a) => solver.inferTypeRefl(a, frameStack.push(Frame(s,Context())))
+       case _ => None
+     }
+ }
 
-//TODO: variable capture is not avoided anywhere
-/** The LF foundation. Implements type checking and equality */
-class ReflectionFoundation extends Foundation {
-   private def log(msg : => String) = report("lf", msg)
-   val foundTheory = LF.lftheory
+ object ReflTermEvalRule extends InferenceRule(Reflection.eval){
+   def apply(solver: Solver, tm: Term, frameStack: Stack[Frame]): Option[Term] =
+     tm match {
+       case TermEval(s,q) => if (frameStack.nonEmpty) solver.inferTypeRefl(q, frameStack.pop)
+         else None
+       case _ => None
+     }
+ }
 
-  def typing(tm : Option[Term], tp : Option[Term], G : Context = Context())(implicit fl : FoundationLookup)  = false
-  def refltyping(tm : Option[Term], tp : Option[Term], W : Stack = Frame(thy: MPath, Context()))(implicit fl : FoundationLookup) : Boolean = {
-      log("typing\n" + tm.toString + "\n" + tp.toString)
-      (tm, tp) match {
-         case (Some(s), Some(a)) => check(s, a, W)
-         case (Some(s), None) => infer(s, W) != Univ(2)
-         case (None, Some(a)) => check(a, Univ(1), W) || check(a, Univ(2), W)
-         case (None, None) => false
-      }
+object ElimReflectionRule extends InferenceRule(Reflection.elim){
+  implicit def pCont(p:Path){}
+   def apply(solver: Solver, tm: Term, frameStack: Stack[Frame]) : Option[Term] = {
+     tm match {
+       case Elim(q,mor) => if (frameStack.nonEmpty)
+         solver.inferTypeRefl(q, frameStack) match {
+            case None => None
+            case Some(ReflType(s,a)) => Some(solver.controller.checker.checkMorphism(mor,OMMOD(s),OMMOD(frameStack.last.theory)))
+            case _ => None
+         }
+         else None
+       case _ => None
+     }
    }
-   def equality(tm1 : Term, tm2 : Term)(implicit fl : FoundationLookup) : Boolean = {
-      log("equal\n" + tm1.toString + "\n" + tm2.toString)
-      equal(tm1, tm2, Context())
-   }
-   def inference(tm: Term, context: Context)(implicit lup: Lookup) : Term = infer(tm, context)(new PlainLookup(lup))
- 
-/**
- * @param path the URI of a constant
- * @param lib  Lookup library
- * @return type of the constant
- * */  
-  def lookuptype(path : GlobalName)(implicit fl : FoundationLookup) : Term = fl.getType(path).getOrElse(throw LFError("no type exists for " + path))
-  def lookupdef(path : GlobalName)(implicit fl : FoundationLookup) : Option[Term] = fl.getDefiniens(path)
+ }
 
-   /**
-    * check(s,T,G) iff G |- s : T : U for some U \in {type,kind}
-    * checks whether a term s has type T  
-    */
-   def check(s : Term, T : Term, W : Stack)(implicit fl : FoundationLookup) : Boolean = {
-	   s match {
-       case TermRefl(thy,tm) => T match {
-         case ReflType(thy2,tp) => if (thy == thy2) check(tm, tp, W.push(Frame(thy, Context())))
-           else throw ReflectionError("expected: " + thy + "\nfound: " + thy2)
-         case _ => throw ReflectionError("invalid type")
+/* Computation Rules for Reflected Terms */
+
+object ComputationReflectionRule extends ComputationRule(Reflection.elim){
+ implicit def pCont(p:Path){}
+ def apply(solver: Solver, tm: Term, frameStack: Stack[Frame]) : Option[Term] = {
+   tm match {
+     case Elim(TermRefl(s,t),mor) => if (frameStack.nonEmpty) {
+       try {
+         solver.controller.checker.checkMorphism(mor,OMMOD(s),OMMOD(frameStack.last.theory))
+       } catch {
+         case e:Error => false
        }
-	   }
+       Some(OMA(mor,List(t)))
+     }
+       else None
+     case _ => None
    }
+ }
 }
 
-  /*
-  
-  /**
-   * if G |- tm1 : A and G |- tm2 : A, then equal(tm1,tm2,G) iff G |- tm1 = tm2 : A
-   */
-   def equal (tm1 : Term, tm2 : Term, G : Context)(implicit fl : FoundationLookup) : Boolean = {
-	   (tm1, tm2) match { 
- 	 		case (OMV(x), OMV(y)) => x == y
- 	 		case (Const(c), Const(d)) => if (c == d) true else {
- 	 			lookupdef(c) match {
- 	 				case None => lookupdef(d) match {
- 	 					case None => false
- 	 					case Some(t) => equal(Const(c), t, G)
- 	 				}
- 	 				case Some(t) => equal(Const(d), t, G) //flipping the order so that if both c and d have definitions, d is expanded next 
- 	 			}
- 	 		}
- 	 		case (Lambda(x1,a1,t1), Lambda(x2,a2,t2)) => 
- 	 			val G2 = G ++ OMV(x1) % a1
- 	 			equal(a1, a2, G) && equal(t1, t2^(OMV(x2)/OMV(x1)), G2)
- 	 		case (Pi(x1,a1,t1), Pi(x2,a2,t2)) => 
- 	 			val G2 = G ++ OMV(x1) % a1
- 	 			equal(a1, a2, G) && equal(t1, t2^(OMV(x2)/OMV(x1)), G2)
- 	 		case (Apply(f1,arg1), Apply(f2,arg2)) => {
- 	 			if (equal(f1, f2, G) && equal(arg1, arg2, G)) true else {
-        		//val tm1r = reduce(tm1, Context())  // why empty context?
-        		//val tm2r = reduce(tm2, Context())
- 	 				val tm1r = reduce(tm1, G)
- 	 				val tm2r = reduce(tm2, G)
- 	 				if (tm1r != tm1 || tm2r != tm2) {
- 	 					equal(tm1r, tm2r, G)
- 	 				} else false
- 	 			}
- 	 		}
- 	 		case _ => tm1 == tm2
-	   }
-   }
+object SoundnessReflectionRule extends ComputationRule(Reflection.eval){
+  def apply(solver: Solver, tm: Term, frameStack: Stack[Frame]) : Option[Term] = {
+    tm match {
+      case TermEval(s1,TermRefl(s2,t)) =>
+        if (s1 == s2)
+           Some(t)
+        else
+           None
+      case _ => None
+    }
+  }
+}
 
- /**
-  * if t is a constant or an application of a defined constant, it replaces the constant with its definition.
-  * if t is a lambda expression application, it is beta-reduced 
-  * removes top-level redex, application of defined constant
-  * if t well-formed, then |- reduce(t,G) = t
-  * if |- t = Pi x:A. B for some A,B, then reduce(t) of the form Pi x:A.B (?)
-  */
-   def reduce(t : Term, G : Context)(implicit fl : FoundationLookup) : Term = t match {
-	 // t can also be an arbitrary application
-   		case Apply(Lambda(x,a,t), s) => if (check(s,a,G)) reduce(t ^ (G.id ++ OMV(x)/s), G) else throw LFError("ill-formed")
-   		case Apply(tm1, tm2) =>
-   			val tm1r = reduce(tm1, G)
-   			tm1r match {
-   				case Lambda(x,a,t) => reduce(Apply(tm1r, tm2), G)
-   				case _ => Apply(tm1r, tm2)
-   			}	 	
-   		case ApplySpine(Const(p), args) => lookupdef(p) match {
-   			case None => t
-   			case Some(d) => reduce(ApplySpine(d, args :_*), G)
-   		}
-   		case ApplySpine(_,_) => t
-   		case Const(p) => lookupdef(p) match {
-   		   case Some(d) => reduce(d, G)
-   		   case None => t
-   		}
-   		case t => t
-   }
- 
- /**
-  * if exists A such that G |- s : A, then G |- infer(s,G) = A
-  */
-   def infer(s : Term, G : Context)(implicit fl : FoundationLookup) : Term = {
-	   s match {
-	   		case Univ(1) => Univ(2)
-	   		case Const(path) => lookuptype(path)
-	   		case OMV(name) => G(name).tp.get // modify it as in check
-	   		case Lambda(name, tp, body) =>
-	   			val G2 = G ++ OMV(name) % tp
-	   			Pi(name,tp,infer(body, G2))
-	   		case Pi(name, tp, body) =>
-	   			val G2 = G ++ OMV(name) % tp
-	   			infer(body, G2)
-	   		case Apply(f, arg) =>
-	   			// need to ensure that type of argument matches with type of functions 
-	   			val funtype = infer(f, G)
-	   			val argtype = infer(arg, G)
-	   			val r = reduce(funtype, G)
-	   			r match {
-	   				case Pi(x,a,b) =>
-	   					if (equal(argtype, a, G))	
-	   						b ^ G.id ++ OMV(x)/arg
-	   					else throw LFError("ill-formed")	  
-	   				case _ => throw LFError("ill-formed")
-	   			}
-	   		case _ => throw LFError("ill-formed")
-	 	}
-   }
-*/
+object CompletenessReflectionRule extends ComputationRule(Reflection.intro){
+  def apply(solver: Solver, tm: Term, frameStack: Stack[Frame]) : Option[Term] = {
+    tm match {
+      case TermRefl(s1,TermEval(s2,t)) =>
+        if (s1 == s2) {
+          solver.inferTypeRefl(tm,frameStack) match {
+            case Some(ReflType(s,a)) => s == s2
+            case _ => false
+        }
+        Some(t)
+        }
+        else None
+      case _ => None
+    }
+  }
+}
+
+/* Equality Rule for Reflected Terms */
+
+object ExtensionalityReflectionRule extends EqualityRule(Reflection.intro){
+  def apply(solver: Solver, tm1: Term, tm2: Term, tp: Term, frameStack: Stack[Frame]) : Boolean = {
+    (tm1,tm2) match {
+      case (TermRefl(s1,t1),TermRefl(s2,t2)) =>
+        if (s1 == s2) {
+          solver.checkEqualityRefl(TermEval(s1,t1),TermEval(s2,t2),None,frameStack)
+        }
+        else false
+      case (_,_) => false
+    }
+  }
+}
+
+
+/* Solution Rules for Reflected Terms */
+
+object SolveEvalReflectionRule extends SolutionRule(Reflection.eval){
+  def apply(solver: Solver, tm1: Term, tm2: Term, frameStack: Stack[Frame]) : Boolean = {
+    tm1 match {
+      case TermEval(s,x) => solver.checkEqualityRefl(x,TermRefl(s,tm2),None,frameStack.pop)
+      case _ => false
+    }
+  }
+}
+
+object SolveReflReflectionRule extends SolutionRule(Reflection.eval){
+  def apply(solver: Solver, tm1: Term, tm2: Term, frameStack: Stack[Frame]) : Boolean = {
+    tm1 match {
+      case TermRefl(s,x) => solver.checkEqualityRefl(x,TermEval(s,tm2),None,frameStack.push(Frame(s,Context())))
+      case _ => false
+    }
+  }
+}
+
+/* Type Checking Rules for Reflected Terms */
+
+object ReflTypingRule extends TypingRule(Reflection.rtype){
+  def apply(solver: Solver, tm: Term, tp: Term, frameStack: Stack[Frame]) : Boolean = {
+    tp match {
+      case ReflType(s,a) => solver.checkTypingRefl(TermEval(s,tm),a,frameStack.push(Frame(s,Context())))
+      case _ => false
+    }
+  }
+}
