56b040915228f918a822acaff5e91f3922db02a3
frabe <frabe@09069c07-60fa-4895-8316-727242e252e4>
2012-08-08 14:26:05 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@1008 09069c07-60fa-4895-8316-727242e252e4

---
+++ src/mmt-api/branches/next-version/src/main/info/kwarc/mmt/api/backend/MMTCompiler.scala
@@ -0,0 +1,23 @@
+package info.kwarc.mmt.api.backend
+import info.kwarc.mmt.api._
+import frontend._
+import utils._
+
+/** a Compiler that uses the generic MMT functions */
+class MMTCompiler extends Compiler {
+   def isApplicable(src : String): Boolean = src == "mmt"
+
+   def compile(in: File, out: File) : List[SourceError] = {
+      val dpath = DPath(FileURI(in))
+      val source = scala.io.Source.fromFile(in.toJava, "UTF-8")
+      val controller = new Controller
+      val tr = new TextReader(controller)
+      val (doc, errorList) = tr.readDocument(source, dpath, "mmt")
+      source.close
+      val outFile = out.setExtension("omdoc")
+      outFile.toJava.getParentFile.mkdirs
+      File.write(outFile, doc.toNode.toString)
+      doc.toNode
+      errorList.toList
+   }
+}
