21845e164dddcaacf4d4444c215509020b7a4b9e
fihorozal <fihorozal@9069c07-60fa-4895-8316-727242e252e4>
2012-08-09 21:27:14 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT/src@1026 09069c07-60fa-4895-8316-727242e252e4

--- mmt-owl/src/info/kwarc/mmt/owl/AddImpacts.scala
+++ mmt-owl/src/info/kwarc/mmt/owl/AddImpacts.scala
@@ -13,25 +13,40 @@ import info.kwarc.mmt.api.ontology._
 
 class AddImpacts(memory: ROMemory) extends ImpactPropagator(memory){
 	// path is Man (A), B depends on A
+    // changed constant
 	def dependsOn(path : Path) : Set[Path] = {
-	    val impacts = new mutable.HashSet[Path]() 
-	    memory.ontology.query(path,ToSubject(RefersTo))(p => impacts += p) // B refersTo A
-	    impacts //Bs
+	    var impacts = new mutable.HashSet[Path]()
+	    var whichTerm : Term = null
+	    path match { 
+	       	 case  CPath(parent : GlobalName, comp) =>   
+	       	 	   val whichConstant = memory.content.getConstant(parent)
+	        	   whichTerm = whichConstant.tp match {
+	       	 	      			    					   case Some(t) => t
+	       	 	      			    					   case None => throw Exception("Not a Term!")
+	       	 	   }
+	         case _ => None
+	    }
+	    whichTerm match { 
+	        case OMS(s) =>  memory.ontology.query(path,ToSubject(RefersTo)) (p => impacts += p) // entity // B refersTo A 
+	        case _ =>   memory.ontology.query(path,ToObject(RefersTo)) (p => impacts += p) // axiom 
+	  }	         			
+ 	   impacts //Bs
 	}
 
 	//path is ax168 (B), changes are As which impact B
+	//impacted constant, set of changes that have impact on the constant
 	def propFunc(path : Path, changes : Set[ContentChange]) : Option[StrictChange] = {
 		path match {
-		case CPath(parent: GlobalName, comp) => 
-			val constant = memory.content.getConstant(parent) 
-			val mdatum = changes.map(setMetaDatum)
-	 		constant.metadata.add(mdatum.toList:_*)
+		case CPath(parent : GlobalName, comp) => 
+			val impactedConstant = memory.content.getConstant(parent) 
+			val mdatum = changes.map(c => setMetaDatum(c, impactedConstant)) //val mdatum = changes.map(setMetaDatum)
+	 		impactedConstant.metadata.add(mdatum.toList:_*)
 	 		None
 		case _ => None
 	   }
 	}
 	
-	def setMetaDatum(change: Change) : MetaDatum = {
+	def setMetaDatum(change: Change, impCons: Constant) : MetaDatum = {
 	    val (changeType : String, changePath : GlobalName) = change match {
       		case DeleteDeclaration(d) => ("delete", d.path)
 	      	case AddDeclaration(d)    => ("add", d.path)
@@ -45,7 +60,7 @@ class AddImpacts(memory: ROMemory) extends ImpactPropagator(memory){
 	    //set key for metadatum
 		val name : String = changeType match {
 	    	case "delete" => "delete"
-	    	case "add" => "add"
+	    	case "add"    => "add"
 	    	//case "rename" => "rename"
 	    	case "update" => "update"
 	    }
@@ -53,30 +68,50 @@ class AddImpacts(memory: ROMemory) extends ImpactPropagator(memory){
 	    val dpath = DPath(utils.URI(base))
 	    val key : GlobalName = dpath ? "_" ? name 
 	    //set value for metadatum
-	    val value : Term = ApplySpine(OWL2OMS("OWL2SUB","literal"), OMSTR(setLiteral(changeType, changedConstant)), OWLOMS("OWL1Datatype","string"))
+	    val value : Term = ApplySpine(OWL2OMS("OWL2SUB","literal"), OMSTR(setLiteral(changeType, changedConstant, impCons)), OWLOMS("OWL1Datatype","string"))
 	    val md = new MetaDatum(key, value)
 		md
 	} 
 
-	def setLiteral(changeType: String, changedConstant: Constant) : String = {
+	def setLiteral(changeType : String, changedConstant : Constant, impCons : Constant) : String = {
 	    val changedConstantName : LocalName = changedConstant.name
 	    val term : Term = (changedConstant.tp match { 
 	                                  case Some(t) => t
 	                                  case None => throw Exception("Not a Term!")
 	    				   })
-	    var entityType : String = " "
-	    entityType = term match { 
-	         case OMS(p) => p.name.toString
-	         case _ => entityType 
-	    }
-	    val str = changeType match { 
-	      case "delete" => "This axiom has been impacted by deleting " + entityType + changedConstantName.toString + "." 
-	      case "add"=> "This axiom has been impacted by adding " + entityType + changedConstantName.toString + "."
-	      //case "rename" => This axiom has been impacted by renaming " + entityType + changedConstantName.toString + "."
-	      case "update" => "An axiom with id " + changedConstantName.toString + " including this entity has been updated."
-	    }
+	    var entityType : String = " "				   
+	        entityType = term match { 
+	                          case OMS(p) => p.name.toString
+	                          case _ => entityType
+	    }	    				   
+	            	    				   
+	    impCons.tp match {
+	      		   case Some(t) => t match { 
+	      		     				 case OMS(s) => setLiteralForImpactedConstantSymbol(changeType, changedConstantName)
+	      		     				 case _      => setLiteralForImpactedConstantApplication(changeType, changedConstantName, entityType)
+	      		   				   }
+			       case None => throw Exception("Not a Term!")
+	       	 	}
+	}
+	
+	def setLiteralForImpactedConstantSymbol(chgType : String, chgdConsName : LocalName) : String ={
+	  val str = chgType match { 
+	      case "delete" => "An axiom with ID " + chgdConsName.toString + " that includes this entity has been deleted." 
+	      case "add"=> "An axiom with ID " + chgdConsName.toString + " that includes this entity has been added."
+	      case "update" => "An axiom with ID " + chgdConsName.toString + " that includes this entity has been updated." // think about it
+	      }
+	    str
+	 }
+	
+	def setLiteralForImpactedConstantApplication(chgType : String, chgdConsName : LocalName, entityType : String) : String = {
+	  val str = chgType match { 
+	      case "delete" => "This axiom has been impacted by deleting " +  entityType + chgdConsName.toString + "." 
+	      case "add"=> "This axiom has been impacted by adding " + entityType + chgdConsName.toString + "."
+	      //case "rename" => This axiom has been impacted by renaming " + entityType + chgdConsName.toString + "." // update it
+	     }
 	    str
 	}
+	
 }	
 
 /*
