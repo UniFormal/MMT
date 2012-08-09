1792f0b99fb571bffb28d8767b21466e8c027a5d
Florian Rabe <florian.rabe@gmail.com>
2012-08-09 17:01:25 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT/src@1017 09069c07-60fa-4895-8316-727242e252e4

--- jEdit-mmt/src/info/kwarc/mmt/jedit/MouseAdapter.scala
+++ jEdit-mmt/src/info/kwarc/mmt/jedit/MouseAdapter.scala
@@ -4,10 +4,14 @@ import org.gjt.sp.jedit._
 
 import java.awt.event._
 
+/** a MouseAdapter that the MMTPlugin adds to a TextAreaPainter in order to control mouse interaction */
 class MMTMouseAdapter(editPane: EditPane) extends MouseAdapter {
+   private val textArea = editPane.getTextArea
    override def mouseClicked(e: MouseEvent) {
       if (e.getButton() == MouseEvent.BUTTON1 && e.getClickCount == 2) {
-         editPane.getTextArea.selectToMatchingBracket
+         textArea.selectToMatchingBracket
+         textArea
       }
+      
    }
 }
