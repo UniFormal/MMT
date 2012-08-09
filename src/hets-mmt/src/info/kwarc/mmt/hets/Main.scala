559531a3cd1a1cbd6f48863958fdb0dd34d729cc
ajakubauskas <ajakubauskas@09069c07-60fa-4895-8316-727242e252e4>
2012-08-09 15:11:23 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@1011 09069c07-60fa-4895-8316-727242e252e4

--- src/hets-mmt/src/info/kwarc/mmt/hets/Main.scala
+++ src/hets-mmt/src/info/kwarc/mmt/hets/Main.scala
@@ -5,12 +5,25 @@ import info.kwarc.mmt.api._
 import utils.File._ //maybe the stuff here is useful
 import backend.TextReader // the parser
 
+import info.kwarc.mmt.lf.compile._
+
 import scala.util.control.Exception._
 
 // for exception handling
 import java.io.FileNotFoundException
 import java.io.IOException
+import utils._
+import utils.MyList._
+import modules._
+import patterns._
+import libraries._
+import frontend._
+import symbols._
+import objects._
+import objects.Conversions._
+//import utils.MyList
 
+import scala.sys.process._
 
 //TODO 
 /*	should be able to read in twelf files
@@ -23,33 +36,75 @@ import java.io.IOException
 
 object Main {
   
+   val base = "http://latin.omdoc.org/logics/syntax"
+  
    def main(args: Array[String]) {
      
      if (args.length == 0) {
-    	 println("No arguments given. \n Terminating.")
-     } else {
+    	 println("No arguments given. Expecting filepath argument.\nTerminating.")
+    	 return
+     } 
+     
        try {         
          val filename = args(0) // take first argument - file name of the source
          val controller = new frontend.Controller
-         val uri = new utils.URI(None,None,List(filename))
+         val uri = new utils.URI(None,None,List(base + "?" + filename))
          
          val source = scala.io.Source.fromFile(new java.io.File(filename),"UTF-8")       
-         val (doc, err) = controller.textReader.readDocument(source, DPath(uri))
+         val (doc, err) = controller.textReader.readDocument(source, DPath(uri),"")
          source.close()
-         println("doc: ")
-         println(controller.globalLookup.getTheory(DPath(uri) ? "Base") )
-         //TODO 
-         /* for starters: give document back to hets
-          * give data structures? back to hets?
-          */
+//         println("doc: ")      
+         val fileout = "/home/aivaras/Desktop/Hets/hets-mmt-output.txt"
+           val output = controller.library.toString()
+           println(output)
+//         "echo" + " " + output #> new java.io.File(fileout) ! 
+//         val items = doc.getItems
+//         println(controller.globalLookup.getTheory(DPath(uri) ? "Logic") )
+//         println(controller.library.toString())
+//         val patt = controller.localLookup.getPattern(controller.library.getAllPaths.head ? "dqq" )
+//         println("printing patter:")
+//         println(patt) 
+//         val t = controller.get(controller.library.getAllPaths.head)
+//         println(t)
+//         
+////         val pc = new PatternChecker(controller)
+////         val home = OMMOD(controller.library.getAllPaths.head)
+////         println(pc.getPatterns(home)(2))
+//         
+//         println(t.components.map(x => x.getClass()))
+//         val comps = t.components.filter(x => x match {
+//           case x : Constant => true
+//           case _ => false
+//         })                 
+//         comps.map(x => {println(x.getClass()); println(x)})
+//         
+//         val translator = new Translator(controller)
+//         val q = translator.translate(comps.head)
+//         val res = comps.map(x => {
+//           val q =  translator.translate(x)
+//           q
+//         })
+//         res.map(x => {println(x.getClass()); println(x)})
+//         
+         
          if (! err.isEmpty) {
                     println("errors occured: ")
                     println(err.toString())
+                    return
          }
+
+         
+
+//         translator.translate(controller.get(controller.library.getAllPaths.head))
+         
+         //TODO give .hs data structures
+         // via info.kwarc.mmt.lf.compile ?
+         // translate to data structures?
+//         val exp  = Haskell.exp(ID("name"))
+//         println(exp)
          
        
-       
-       }
+       } // <------------ end of main
       catch {
         case e : java.lang.ArrayIndexOutOfBoundsException => println("Error: array index out of bounds")
         													e.printStackTrace()
@@ -59,11 +114,12 @@ object Main {
 
         case e : FileNotFoundException => println("no such file: " + args(0) + ", check spelling")
         case e : IOException => println("IO exception")
-        case e => println("unknown error:")
-        				e.printStackTrace()
+//        case e => println("unknown error:")
+//        				throw e
+//        				e.printStackTrace()
       }
        
-     }
+     
      
    }
 }
