0381bdf0ebd57c40de9270d7eb0e60b87a0b0a9c
Florian Rabe <florian.rabe@gmail.com>
2012-08-13 16:53:44 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT/src@1050 09069c07-60fa-4895-8316-727242e252e4

---
+++ mmt-lf/src/info/kwarc/mmt/lf/Typed.scala
@@ -0,0 +1,42 @@
+package info.kwarc.mmt.lf
+import info.kwarc.mmt.api._
+import objects._
+
+object Typed {
+   val path = DPath(utils.URI("http", "cds.omdoc.org") / "foundations") ? "Typed"
+   /** the MMT URI of type */
+   val ktype = path ? "type"
+   /** the MMT URI of kind */
+   val kind = path ? "kind"
+}
+
+class TypedRules extends RuleSet {
+   val rules = List(UniverseType,UniverseKind,UnivTerm)
+}
+
+/** provides apply/unapply methods for a universes
+   in particular, Univ(1), Univ(2) are type and kind, respectively
+ */
+object Univ {
+   def apply(level : Int) : Term = if (level == 1) OMS(Typed.ktype) else OMS(Typed.kind)
+   def unapply(t : Term) : Option[Int] =
+      if (t == OMS(Typed.kind)) Some(2) else if (t == OMS(Typed.ktype)) Some(1) else None
+}
+
+/** the rule that makes type a valid universe */
+object UniverseType extends UniverseRule(Typed.ktype) {
+   def apply(solver: Solver)(tm: Term)(implicit stack: Stack) : Boolean = tm == OMS(Typed.ktype)
+}
+
+/** the rule that makes kind a valid universe */
+object UniverseKind extends UniverseRule(Typed.kind) {
+   def apply(solver: Solver)(tm: Term)(implicit stack: Stack) : Boolean = tm == OMS(Typed.kind)
+}
+
+/** the type inference rule type:kind */
+object UnivTerm extends InferenceRule(Typed.ktype) {
+   def apply(solver: Solver)(tm: Term)(implicit stack: Stack) : Option[Term] = tm match {
+      case OMS(Typed.ktype) => Some(OMS(Typed.kind))
+      case _ => None
+   }
+}
