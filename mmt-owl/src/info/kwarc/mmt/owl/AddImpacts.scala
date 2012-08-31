d5825f55f161eb14596ccae20d2c48519d444e2d
fihorozal <fihorozal@9069c07-60fa-4895-8316-727242e252e4>
2012-08-31 16:51:08 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT/src@1096 09069c07-60fa-4895-8316-727242e252e4

--- mmt-owl/src/info/kwarc/mmt/owl/AddImpacts.scala
+++ mmt-owl/src/info/kwarc/mmt/owl/AddImpacts.scala
@@ -12,109 +12,146 @@ import scala.collection._
 import info.kwarc.mmt.api.ontology._
 
 class AddImpacts(memory: ROMemory) extends ImpactPropagator(memory){
-	//changed constant
+	
+    /**
+    * identifies impacts of a changed constant
+    * @param a path of a changed constant
+    * @return paths of constants that are impacted by the changed constant
+    */
 	def dependsOn(path : Path) : Set[Path] = {
 	    var impacts = new mutable.HashSet[Path]()
 	    var whichTerm : Term = null
 	    path match { 
-	       	 case  CPath(parent : GlobalName, comp) =>   
-	       	 	   val whichConstant = memory.content.getConstant(parent)
-	        	   whichTerm = whichConstant.tp match {
-	       	 	      			    					   case Some(t) => t
-	       	 	      			    					   case None => throw Exception("Not a Term!")
-	       	 	   }
-	         case _ => None
+       	case  CPath(parent : GlobalName, comp) =>   
+       	      val whichConstant = memory.content.getConstant(parent)
+        	  whichTerm = whichConstant.tp match {
+    					  case Some(t) => t
+    					  case None => throw Exception("Not a Term!")
+       	 	  }
+        case _ => None
 	    }
 	    whichTerm match { 
-	        case OMS(s) =>  memory.ontology.query(path,ToSubject(RefersTo)) (p => impacts += p) //entity  
-	        case _ =>   memory.ontology.query(path,ToObject(RefersTo)) (p => impacts += p) //axiom 
-	  }	         			
- 	   impacts 
+	    case OMS(s) =>  memory.ontology.query(path,ToSubject(RefersTo)) (p => impacts += p) //entity  
+	    case _ =>   memory.ontology.query(path,ToObject(RefersTo)) (p => impacts += p) //axiom 
+	    }	         			
+ 	    impacts 
 	}
 
-	//impacted constant, set of changes that have impact on the constant
-	def propFunc(path : Path, changes : Set[ContentChange]) : Option[StrictChange] = {
+    /**
+    * adds an impact of each change to an impacted constant; adds metadata to an impacted constant
+    * @param path of an impacted constant, changes that have impacts on the constant
+    * @return None
+    */
+	def propFunc(path : Path, changes : Set[ContentChange]) = { //: Option[StrictChange] 
 		path match {
-		case CPath(parent : GlobalName, comp) => 
-			val impactedConstant = memory.content.getConstant(parent) 
-			val mdatum = changes.map(c => setMetaDatum(c, impactedConstant))
-	 		impactedConstant.metadata.add(mdatum.toList:_*)
-	 		None
+	    case CPath(parent : GlobalName, comp) => 
+		 	 val impactedConstant = memory.content.getConstant(parent) 
+			 val mdatum = changes.map(c => setMetaDatum(impactedConstant, c))
+			 impactedConstant.metadata.add(mdatum.toList:_*)
+			 None
 		case _ => None
-	   }
+		}
 	}
 	
-	def setMetaDatum(change: Change, impCons: Constant) : MetaDatum = {
+	/**
+    * sets metadatum for an impacted constant
+    * @param an impacted constant, a change that have an impact on the impacted constant
+    * @return metadatum about the change and its impact
+    */
+	def setMetaDatum(impCons: Constant, change: Change) : MetaDatum = {
 	    val (changeType : String, changePath : GlobalName) = change match {
-      		case DeleteDeclaration(d) => ("delete", d.path)
-	      	case AddDeclaration(d)    => ("add", d.path)
-            //case rename ? => ("rename", d.path)
-	      	case UpdateComponent(path, name, old, nw) => ("update", path match { 
-	      	  												             case path : GlobalName => path
-	      	  												             case _ => None
-	      		      									            })
-	    	}
+      														 case DeleteDeclaration(d) => ("delete", d.path)
+															 case AddDeclaration(d)    => ("add", d.path)
+														     //case rename ? => ("rename", d.path)
+															 case UpdateComponent(path, name, old, nw) => ("update", path match { 
+															      	  												 case path : GlobalName => path
+															      	  												 case _ => None
+															      		      									     })
+															 }
     	val changedConstant : Constant =  memory.content.getConstant(changePath)
 	    //set key for metadatum
-		val name : String = changeType match {
-	    	case "delete" => "delete"
-	    	case "add"    => "add"
-	    	//case "rename" => "rename"
-	    	case "update" => "update"
-	    }
-    	
-    	val base = impCons.tp match {
+		val name : String = impCons.tp match {
+    						case Some(t) => t match { 
+	      		     				   		case OMS(s) => changeType match { 
+	      		     				   					   case "delete" => "possibleImpactByDelete"
+	      		     				   		  		       case "add"    => "possibleImpactByAdd"
+	      		     				   					   case "update" => "possibleImpactByUpdate"
+	      		     				   					   }
+	      		     				   		case _      => changeType match{ 
+	      		     				   					   case "delete" => "impactByDelete"
+	      		     				   					   //case "add"    => "impactByAdd"
+	      		     				   					   case "rename" => "impactByRename"
+	      		     				   					   }
+	    							        }
+    						case None => throw Exception("Not a Term!")
+    						}
+	    val base = impCons.tp match {
 	      		   case Some(t) => t match { 
-	      		     				 case OMS(s) => "http://omdoc.org/possibleImpact"
-	      		     				 case _      => "http://omdoc.org/impact"
+	      		     			   case OMS(s) => "http://omdoc.org/impact"
+	      		     			   case _      => "http://omdoc.org/impact"
 	      		   				   }
 			       case None => throw Exception("Not a Term!")
-	       	 	}
-	    val dpath = DPath(utils.URI(base))
+	       	 	   }
+    	val dpath = DPath(utils.URI(base))
 	    val key : GlobalName = dpath ? "_" ? name 
 	    //set value for metadatum
-	    val value : Term = ApplySpine(OWL2OMS("OWL2SUB","literal"), OMSTR(setLiteral(changeType, changedConstant, impCons)), OWLOMS("OWL1Datatype","string"))
+	    val value : Term = ApplySpine(OWL2OMS("OWL2SUB","literal"), OMSTR(setLiteral(impCons, changeType, changedConstant)), OWLOMS("OWL1Datatype","string"))
 	    val md = new MetaDatum(key, value)
 		md
 	} 
-
-	def setLiteral(changeType : String, changedConstant : Constant, impCons : Constant) : String = {
+	
+	/**
+    * sets a literal of type string for a value of metadatum
+    * @param an impacted constant, a change type, a changed constant that have an impact on the impacted constant
+    * @return literal of type string explaining the change and its impact
+    */
+	def setLiteral(impCons : Constant, changeType : String, changedConstant : Constant) : String = {
 	    val changedConstantName : LocalName = changedConstant.name
 	    val term : Term = (changedConstant.tp match { 
-	                                  case Some(t) => t
-	                                  case None => throw Exception("Not a Term!")
+	                       case Some(t) => t
+	                       case None => throw Exception("Not a Term!")
 	    				   })
 	    var entityType : String = " "				   
 	        entityType = term match { 
-	                          case OMS(p) => p.name.toString
-	                          case _ => entityType
+	                     case OMS(p) => p.name.toString
+	                     case _ => entityType
 	    }	    				   
 	            	    				   
 	    impCons.tp match {
-	      		   case Some(t) => t match { 
-	      		     				 case OMS(s) => setLiteralForImpactedConstantSymbol(changeType, changedConstantName)
-	      		     				 case _      => setLiteralForImpactedConstantApplication(changeType, changedConstantName, entityType)
-	      		   				   }
-			       case None => throw Exception("Not a Term!")
-	       	 	}
+	    case Some(t) => t match { 
+	    		     	case OMS(s) => setLiteralForImpactedConstantSymbol(changeType, changedConstantName)
+	      		     	case _      => setLiteralForImpactedConstantApplication(changeType, changedConstantName, entityType)
+	      		   		}
+	    case None => throw Exception("Not a Term!")
+	    }
 	}
 	
+	/**
+    * sets a literal of type string for a value of metadatum for the impacted constant that is type of symbol
+    * @param a change type, the changed constant's name 
+    * @return a literal of type string explaining the change and its impact
+    */
 	def setLiteralForImpactedConstantSymbol(chgType : String, chgdConsName : LocalName) : String ={
 	  val str = chgType match { 
 	      case "delete" => "An axiom with ID " + chgdConsName.toString + " that includes this entity has been deleted." 
 	      case "add"=> "An axiom with ID " + chgdConsName.toString + " that includes this entity has been added."
-	      case "update" => "An axiom with ID " + chgdConsName.toString + " that includes this entity has been updated." // think about it
+	      case "update" => "An axiom with ID " + chgdConsName.toString + " that includes this entity has been updated." 
 	      }
-	    str
+	  str
 	 }
 	
+	/**
+    * sets a literal of type string for a value of metadatum for the impacted constant that is type of application
+    * @param a change type, the changed constant's name, the type of the changed constant
+    * @return a literal of type string explaining the change and its impact
+    */
 	def setLiteralForImpactedConstantApplication(chgType : String, chgdConsName : LocalName, entityType : String) : String = {
 	  val str = chgType match { 
-	      case "delete" => "This axiom has been impacted by deleting " +  entityType + chgdConsName.toString + "." 
-	      case "add"=> "This axiom has been impacted by adding " + entityType + chgdConsName.toString + "."
-	      //case "rename" => This axiom has been impacted by renaming " + entityType + chgdConsName.toString + "." // update it
-	     }
-	    str
+	            case "delete" => "This axiom has been impacted by deleting " +  entityType + chgdConsName.toString + "." 
+	            // case "add"=> "This axiom has been impacted by adding " + entityType + chgdConsName.toString + "."
+	            //case "rename" => This axiom has been impacted by renaming " + entityType + chgdConsName.toString + "." 
+	            }
+	  str
 	}
 	
 }	
