62db658404256c1387869136b96b145b7397bd1a
ajakubauskas <ajakubauskas@09069c07-60fa-4895-8316-727242e252e4>
2012-09-03 15:11:46 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@1098 09069c07-60fa-4895-8316-727242e252e4

---
+++ src/mmt-lf/src/info/kwarc/mmt/lf/compile/PL.scala
@@ -0,0 +1,31 @@
+package info.kwarc.mmt.lf.compile
+
+import info.kwarc.mmt.lf.compile._
+
+
+/*
+ * for testing purposes
+ * PL logical syntax object that we'd get from MMT/theories/source/plWPatterns.mmt
+ * 
+ */
+
+object PL {
+
+  val cats = List(
+      Connective("or",List(CatRef("bool"), CatRef("bool"))), 
+      Connective("and",List(CatRef("bool"), CatRef("bool"))), 
+      Connective("not",List(CatRef("bool")))
+      )
+  
+  val consymb = ConstantSymbol("boolvar","bool",List())     
+  
+  val formCat = CatRef("bool")
+  
+  val decls = List(
+		  Declaration("boolvar",List()), 
+		  Declaration("axiom",List(CatRef("bool")))
+  )
+  
+
+  val Syn = LogicSyntax(List(Category("bool", cats ++ List(consymb) )),formCat,decls)
+}
