cb86c9144a0e14539d6b87c7824319ac76c225c8
fihorozal <fihorozal@9069c07-60fa-4895-8316-727242e252e4>
2012-09-04 22:38:48 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT/src@1107 09069c07-60fa-4895-8316-727242e252e4

--- mmt-owl/src/info/kwarc/mmt/owl/MoCinOWL.scala
+++ mmt-owl/src/info/kwarc/mmt/owl/MoCinOWL.scala
@@ -1,6 +1,62 @@
 package info.kwarc.mmt.owl
+import java.io.File
+import java.net.URI
+import info.kwarc.mmt.api._
+import utils.FileConversion._
+import info.kwarc.mmt.api.frontend._
+import info.kwarc.mmt.api.libraries._
+import info.kwarc.mmt.api.moc._
+import scala.xml._
+import org.semanticweb.owlapi.model._
+import org.semanticweb.owlapi.apibinding.OWLManager
 
-class MoCinOWL {
-   
+
+  //take two owl files as inputs
+  //add identifiers to them
+  //translate them to mmt files
+  //apply mmt diff to the mmt files
+  //dependsOn
+  //propFunc
+  object MoCinOWL {
+	def main(args: Array[String]) {
+		val controllerOlder = new Controller
+	    val controllerCurrent = new Controller
+
+	    controllerOlder.handle(ExecFile(new java.io.File("startup.mmt")))
+	    controllerCurrent.handle(ExecFile(new java.io.File("startup.mmt")))
+	   
+	    var olderVersion : File = null
+	    var currentVersion : File = null
+	   
+	    if(args.length < 1) 
+	    { println("USAGE: OLDFILE")  
+	      exit
+	    }
+	
+	    olderVersion = new File(args(0)) //has to be in the source folder
+	    currentVersion = new File(args(1)) //this should not be in the source folder; elsewhere
+	    //we need the older version in the source folder to be able to get dependency relations.
+	    //two versions have the same ontology IRI, so they should not be in the source folder together, because we cannot compare them
+	   
+	   //transates older OWL document to MMT in compiled folder
+	   val controllerBuild = new Controller
+	   controllerBuild.handleLine("File build-test.mmt")
+	   	     
+	   //translates current OWL document to MMT
+	   val manager : OWLOntologyManager = OWLManager.createOWLOntologyManager()
+	   val importer = new Import (manager, controllerCurrent)
+       val ontology : OWLOntology  = manager.loadOntologyFromOntologyDocument(currentVersion)
+	   val dpath : DPath = importer.ontologyToLF(ontology)
+	  
+	   val olderDoc : DPath  = controllerOlder.read(olderVersion)
+       val currentDoc : DPath = controllerCurrent.read(currentVersion)
+        
+       var diff = Differ.diff(controllerOlder, controllerCurrent, olderDoc, currentDoc)
+       
+	   //propagate(diff)
+	  		
+	   
+	   
+	}
   
 }
