18a4ac589a04c413cb5b0a6eaad38205c69be98f
ajakubauskas <ajakubauskas@09069c07-60fa-4895-8316-727242e252e4>
2012-09-19 12:59:59 +0000

    some renaming, comments added
    git-svn-id: https://svn.kwarc.info/repos/MMT@1119 09069c07-60fa-4895-8316-727242e252e4

--- src/mmt-api/branches/next-version/src/main/info/kwarc/mmt/api/patterns/InstanceCompiler.scala
+++ src/mmt-api/branches/next-version/src/main/info/kwarc/mmt/api/patterns/InstanceCompiler.scala
@@ -30,12 +30,15 @@ import scala.io.Source
  * output the instance files into separate folder (not "/compiled/")
  */
 
-case class SomeError(msg : String) extends java.lang.Throwable(msg)  
+//case class SomeError(msg : String) extends java.lang.Throwable(msg)  
 
-class SubsCompiler extends Compiler {
+class InstanceCompiler extends Compiler {
   
   def isApplicable(src : String) : Boolean = src.substring(src.lastIndexOf(".")) == "omdoc"    
   
+
+  case class CompilationError(msg : String) extends java.lang.Throwable(msg)     
+    
   override def compile(in : File, out : File) : List[SourceError] = {
     var errors: List[SourceError] = Nil
     val fileName = in.toJava.getName
@@ -52,23 +55,23 @@ class SubsCompiler extends Compiler {
     if (!dir.exists)
       dir.mkdirs
     
-      
-   val patcon = new PatternController()
+    // pattern controller contains hard-coded declaration patterns  
+    val patcon = new PatternController()
 	val pc = new PatternChecker(patcon.controller)
     
-    // retrieve constant list
 	val tptpbase = DPath(URI("http://latin.omdoc.org/logics/tptp"))
 	val pbbase = DPath(URI("http://oaff.omdoc.org/tptp/problems"))// problem base  
 	val thName = fileName.substring(0,fileName.lastIndexOf("."))
+	// retrieve constant declarations
    	val constTheory = patcon.controller.globalLookup.getTheory(pbbase  ? thName) match {
       case c : DeclaredTheory => c
-      case _ => throw SomeError("no constants in " + pbbase + "?" + thName)
+      case _ => //throw SomeError("no constants in " + pbbase + "?" + thName)
+        throw CompilationError("no constants in " + pbbase + "?" + thName)
     }
     val constList : List[Constant] = constTheory.valueListNG mapPartial {
       case p : Constant => Some(p)
       case _ => None
     }
-    
     // retrieve patterns    
     val pattTheory = patcon.controller.globalLookup.getTheory(tptpbase ? "THF0") match {
       case t : DeclaredTheory => t
@@ -78,10 +81,7 @@ class SubsCompiler extends Compiler {
       case p : Pattern => Some(p)
       case _ => None
     }    
-    
-    val inst = null // pc.getInstance(constList, pattList) FR: I've commented this out because it didn't compile
-    
-//    val q = constTheory.
+    val inst =  pc.getInstance(constList, pattList)// FR: I've commented this out because it didn't compile
     
     // write to output file            
     write(out.setExtension("omdoc"), fileDir, fileName, inst)
@@ -102,7 +102,7 @@ class SubsCompiler extends Compiler {
 		
 		val nd : scala.xml.Node = 
 		<omdoc xmlns="http://omdoc.org/ns" xmlns:om="http://www.openmath.org/OpenMath" base={tptpbase.toString()}>
-		   <theory name={"test"} base={tptpbase.toString()} meta={""}>     
+		   <theory name={"test"} base={tptpbase.toString()} meta={"http://cds.omdoc.org/foundations/lf/lf.omdoc?lf"}>     
 			{ins.map(x => x.toNode)}
 			 </theory>
 		</omdoc>
@@ -115,16 +115,14 @@ class SubsCompiler extends Compiler {
     
 }
 
-
-
-
+// controller with patterns in it
 class PatternController {
   
         val controller = new Controller
         val tptpbase = DPath(URI("http://latin.omdoc.org/logics/tptp"))
         val pbbase = DPath(URI("http://oaff.omdoc.org/tptp/problems"))// problem base
         
-        controller.handleLine("file test.mmt")
+        controller.handleLine("file instCompTest.mmt")
         
         val baseType = new Pattern(OMID(tptpbase ? "THF0"), LocalName("baseType"),Context(), OMV("t") % OMS(tptpbase ? "Types" ? "$tType"))
         val typedCon = new Pattern(OMID(tptpbase ? "THF0"), LocalName("typedCon"), OMV("A") % OMS(tptpbase ? "Types" ? "$tType") , OMV("c") % OMA(OMS(tptpbase ? "Types" ? "$tm"), List(OMV("A"))) )
@@ -136,10 +134,9 @@ class PatternController {
         controller.add(axiom)
         controller.add(typedConDef)
         controller.add(theorem) 
-        
-  
 }
 
+// compilation test
 object CompTest {
 	
 	val tptpbase = DPath(URI("http://latin.omdoc.org/logics/tptp"))
@@ -149,54 +146,17 @@ object CompTest {
 
 	def main(args : Array[String]) {
 	  
-//	  controller.handleLine("file pattern-test.mmt")
-//	  controller.add(baseType)
-//	  controller.add(typedCon)
-//	  controller.add(axiom)
-//	  controller.add(typedConDef)
-//	  controller.add(theorem)
 	  
 	 val thName : String = "AGT027^1"
 	 
-//	 val constTheory = patcon.controller.globalLookup.getTheory(pbbase  ? thName) match {
-//      case c : DeclaredTheory => c
-//      case _ => throw Error("no constants in " + pbbase + "?" + thName)
-//    }
-//    val constList : List[Constant] = constTheory.valueListNG mapPartial {
-//      case p : Constant => Some(p)
-//      case _ => None
-//    }
-	  
-    //     get list of patterns from controller
-//    val pp = try {
-//      patcon.controller.globalLookup.getPattern(tptpbase ? "THF0" ? "axiom")
-//    } catch {
-//      case GetError(m) => throw GetError("tptpbase ? \"THF0\" ? \"axiom\" \n" + m)
-//    }
-//    val pattTheory = patcon.controller.globalLookup.getTheory(tptpbase ? "THF0") match {
-//      case t : DeclaredTheory => t
-//      case _ => throw GetError("no patterns in " + tptpbase + "?" + "THF0")      
-//    }        
-//    val pattList : List[Pattern] = pattTheory.valueListNG mapPartial {
-//      case p : Pattern => Some(p)
-//      case _ => None
-//    }
 		 
-	  val compiler = new SubsCompiler
+	  val compiler = new InstanceCompiler
 	  
 	  val in = new File(new java.io.File("/home/aivaras/TPTP/tptp/compiled/Problems/AGT/AGT027^1.omdoc"))
-	  val out = new File(new java.io.File("/home/aivaras/TPTP/tptp/instance/Problems/AGT/AGT027^1p.omdoc"))
+	  val out = new File(new java.io.File("/home/aivaras/TPTP/tptp/pragmatic/Problems/AGT/AGT027^1p.omdoc"))
 	  
 	  compiler.compile(in, out)
-    
-//	  val matches = constList.map {
-//        a => 
-//          pattList.map {
-//          p =>  
-//            pc.patternCheck(List(a),p)
-//        }
-//      }.flatten.flatten      
-	  
+    	  
 	  println("\nend")
 	  
 	}
