043de08f8156df294e1eff333a1209b9f34e1943
sdumbrava <sdumbrava@09069c07-60fa-4895-8316-727242e252e4>
2012-09-20 18:33:14 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@1131 09069c07-60fa-4895-8316-727242e252e4

--- src/mmt-reflection/src/info/kwarc/mmt/reflection/Test.scala
+++ src/mmt-reflection/src/info/kwarc/mmt/reflection/Test.scala
@@ -51,7 +51,11 @@ object Test {
       val succ_refl = new Constant(OMID(Nat2.path), LocalName("s"), Some(Arrow(OMID(nat_refl.path),OMID(nat_refl.path))), Some(TermRefl(OMMOD(Nat2.path), OMID(succ.path))), None, None))
       controller.add(succ_refl)
 
-      //val addnat = ExplicitMorph(Record(List((LocalName("nat"),Arrow(OMID(tp),OMID(tp.toMpath))))),OMMOD(Nat2.path))
+      val sigma = Record(List((LocalName("nat"),Arrow(OMID(nat_refl.path),OMID(nat_refl.path)))))
+      val sigma1 = Record(List((LocalName("zero"),Lambda(LocalName("lambda"),OMV(LocalName("x")),OMV(LocalName("x"))))))
+      //val sigma2 = Record(List((LocalName("succ"),Lambda(LocalName("lambda"),OMV(LocalName("x")),OMV(LocalName("x"))))))
+
+      val addnat = ExplicitMorph(sigma,OMMOD(Nat2.path))
       //(LocalName("zero"), Lambda(LocalName("lambda"),OMV(LocalName("x")),OMV(LocalName("x")))),(LocalName("succ")...)  not sure about these
       //controller.add(addnat)
 
