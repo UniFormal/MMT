1e4c6840ba3bb18bcdaeab6ce943d5547c7a85b3
Florian Rabe <florian.rabe@gmail.com>
2012-11-10 15:24:35 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT/src@1198 09069c07-60fa-4895-8316-727242e252e4

--- mmt-api/trunk/src/main/info/kwarc/mmt/api/documents/Document.scala
+++ mmt-api/trunk/src/main/info/kwarc/mmt/api/documents/Document.scala
@@ -1,8 +1,9 @@
 package info.kwarc.mmt.api.documents
 import info.kwarc.mmt.api._
-import info.kwarc.mmt.api.presentation._
-import info.kwarc.mmt.api.libraries._
-import info.kwarc.mmt.api.modules._
+import presentation._
+import libraries._
+import modules._
+import utils.MyList._
 
 /**
  * A Document represents an MMT document.
@@ -14,23 +15,28 @@ import info.kwarc.mmt.api.modules._
  */
 class Document(val path : DPath) extends NarrativeElement {
    /** constructs a non-empty document */
-   def this(path : DPath, inititems : List[XRef]) = {
+   def this(path : DPath, inititems : List[DocumentItem]) = {
 	   this(path)
 	   inititems foreach add
    }
-   private var items : List[XRef] = Nil
-   /** returns the list of children of the document */
-   def getItems = items
+   private var items : List[DocumentItem] = Nil
+   /** returns the list of children of the document (including narration) */
+   def getNarratedItems = items
+   /** returns the list of children of the document (excluding narration) */
+   def getItems : List[XRef] = items mapPartial {
+     case r: XRef => Some(r)
+     case _ => None
+   }
    /** returns the list of local children (as opposed to remotely referenced ones) in document-order */
-   def getLocalItems : List[XRef] = items filter (_.isGenerated)
+   def getLocalItems : List[XRef] = getItems filter (_.isGenerated)
    /** returns the list of modules declared in the document (not user-written references) */
-   def getModulesResolved(lib: Lookup) : List[Module] = items flatMap {
+   def getModulesResolved(lib: Lookup) : List[Module] = getItems flatMap {
        case r: MRef if r.isGenerated => List(lib.getModule(r.target))
        case _ => Nil
    }
    /** adds a child at the end of the documents */
-   def add(r : XRef) {
-      items = items ::: List(r)
+   def add(i : DocumentItem) {
+      items = items ::: List(i)
    }
    val parent = path ^!
    val role = Role_Document
@@ -47,42 +53,6 @@ class Document(val path : DPath) extends NarrativeElement {
      </omdoc>
 }
 
-/**
- * An XRef represents a reference from a document to an external document part or module.
- * An XRef is semantically equivalent to copy-pasting the referenced module.
- * All documents are represented as lists of XRefs no matter whether a module is given locally or remotely.
- * @param parent the containing document
- * @param target the referenced module
- * @param generated true iff the module was given directly in the document rather than referenced remotely
- */
-abstract class XRef(val parent : DPath, val target : Path) extends NarrativeElement {
-   val path = parent
-   def components = List(StringLiteral(target.toString), StringLiteral(target.last))
-   def toNode : scala.xml.Node
-   override def toString = "ref " +  target.toPath
-}
-
-/** reference to a document section */
-class DRef(p : DPath, override val target : DPath) extends XRef(p, target) {
-   val role = Role_DRef
-   def toNode = <omdoc href={target.toPath}/>
-}
-object DRef {
-   def apply(p : DPath, target : DPath, generated: Boolean = false): DRef = {
-      val r = new DRef(p, target)
-      if (generated) r.setOrigin(DocumentSkeleton)
-      r
-   }
-}
-/** reference to a module */
-class MRef(p : DPath, override val target : MPath) extends XRef(p, target) {
-   val role = Role_MRef
-   def toNode = <mref target={target.toPath}/>
-}
-object MRef {
-   def apply(p : DPath, target : MPath, generated: Boolean = false): MRef = {
-      val r = new MRef(p, target) 
-      if (generated) r.setOrigin(DocumentSkeleton)
-      r
-   }
+trait DocumentItem extends Content {
+  
 }
