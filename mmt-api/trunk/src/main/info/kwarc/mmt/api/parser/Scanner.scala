f7cbcf13450fe565483c8964d9c5be61e88b724f
Florian Rabe <florian.rabe@gmail.com>
2012-11-17 22:20:49 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT/src@1203 09069c07-60fa-4895-8316-727242e252e4

--- mmt-api/trunk/src/main/info/kwarc/mmt/api/parser/Scanner.scala
+++ mmt-api/trunk/src/main/info/kwarc/mmt/api/parser/Scanner.scala
@@ -11,35 +11,69 @@ class TokenList(tokens: List[Token]) {
    def top: Token = tokens.head
 }
 
-case class TokenSlice(tokens: TokenList, start: Int, end: Int) { 
+case class TokenSlice(tokens: TokenList, start: Int, next: Int) { 
 }
 
-class Scanner(tl: TokenList, notations: Unit => List[ActiveNotation]) {
+class Scanner(tl: TokenList, notations: List[Notation]) {
    private var seen = TokenSlice(tl, 0, 0)
-   def top: Token = tl(seen.end)
+   def top: Token = tl(seen.next)
+   def current = TokenSlice(tl, seen.next, seen.next + 1)
    def pick: TokenSlice = {
       val s = seen
-      seen = TokenSlice(tl, seen.end, seen.end)
+      seen = TokenSlice(tl, seen.next, seen.next)
       s
    }
-   private var active: List[List[ActiveNotation]] = List(notations())
-   def pushActive(n: ActiveNotation) {
-      active ::= n :: notations()
+   // the currently open notations, inner-most first
+   private var active: List[ActiveNotation] = Nil
+   /*
+    * precondition: closable.reverse ::: ans == active
+    * return condition:
+    *   None no active notation can be applied
+    *   Some(l) the prefix l.reverse of active can be closed, and then the next notation can be applied   
+    */
+   private def checkActive(ans: List[ActiveNotation], closable: List[ActiveNotation]) : Option[List[ActiveNotation]] = ans match {
+      case Nil => None
+      case an::rest => 
+         if (an.applicable) {
+            Some(closable)
+         } else {
+            if (an.closable) {
+               checkActive(rest, an :: closable)
+            } else {
+               None
+            }
+         }
    }
-   def popActive {
-      active = active.tail
+   private def applyFirst {
+      val toClose = active.head.apply
+      if (toClose)
+         active = active.tail
    }
-   def next {
-      val t = tl(seen.end)
-      val applicable = active.head filter {a => a.applicable(this)}
-      applicable match {
-         case hd :: Nil =>
-            applicable.head.apply(this)
-            next
-         case Nil =>
-            seen = seen.copy(end = seen.end + 1)
-            next
-         case _ => throw Ambiguous() //some kind of ambiguity-handling here (maybe look for next delimiter)
+   private def next {
+      val activeApplicable = checkActive(active, Nil)
+      activeApplicable match {
+         case Some(closable) =>
+            closable.reverseMap {an => 
+                an.close
+                active = active.tail
+            }
+            applyFirst
+         case None =>
+            val applicable = notations filter {a => a.applicable(this)}
+            applicable match {
+               case hd :: Nil =>
+                  val an = hd.activate(this)
+                  active ::= an
+                  applyFirst
+               case Nil =>
+                  seen = seen.copy(next = seen.next + 1)
+               case _ => throw Ambiguous() //some kind of ambiguity-handling here (maybe look for next delimiter)
+            }
+      }
+      if (seen.next < tl.length)
+        next
+      else {
+         //active notations must eat seen tokens and close
       }
    }
 }
@@ -52,29 +86,52 @@ case class SqArg(n: Int, sep: Delim) extends Marker
 
 abstract class Found
 case class FoundDelim(delim: TokenSlice) extends Found
-case class FoundArg(arg: TokenSlice) extends Found
+case class FoundArg(arg: TokenSlice, n: Int) extends Found
 case class FoundSeqArg(seqarg: TokenSlice, delims: List[FoundDelim], args: List[FoundArg]) extends Found
 
-class ActiveNotation(markers: List[Marker]) {
-   private var found : List[Found] = Nil
-   private var left : List[Marker] = markers
-   private var first = true
-
-   def applicable(scanner: Scanner): Boolean = {
-      scanner.top == left.head 
-   }
-   def apply(scanner: Scanner) {
-      if (first) {
-         scanner.pushActive(this)
-         first = false
-      }
-      //found = FoundDelim(scanner) 
-      left = left.tail
-      found ::= FoundArg(scanner.pick)
-      if (markers.isEmpty) {
-         //apply notation here
-         scanner.popActive
-      }
+class Notation {
+   val markers: List[Marker]
+   def applicable(scanner: Scanner): Boolean
+   def activate(scanner: Scanner): ActiveNotation = {
+      val an = new ActiveNotation(scanner, markers)
+      an
    }
 }
 
+class ActiveNotation(scanner: Scanner, markers: List[Marker]) {
+   private var found : List[Found] = Nil
+   private var left : List[Marker] = markers
+   private var remember : Int = 0
+
+   def applicable: Boolean = {
+      left match {
+         case Arg(n) :: Delim(t) :: _ =>
+            remember = n
+            scanner.top == t
+         case _ => false
+      } 
+   }
+   def apply: Boolean = {
+      found ::= FoundArg(scanner.pick, remember)
+      left = left.tail
+      found ::= FoundDelim(scanner.current)
+      left = left.tail
+      if (markers.isEmpty) {
+         true
+      } else {
+         false
+      }
+   }
+   def closable : Boolean = {
+      left match {
+         case Arg(n) :: Nil =>
+            remember = n
+            true
+         case _ => false
+      }
+   }
+   def close {
+      found ::= FoundArg(scanner.pick, remember)
+      left = left.tail
+   }
+}
