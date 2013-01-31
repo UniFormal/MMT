cfe03743f1a742cc50e39e982baf8ff26648637f
Florian Rabe <florian.rabe@gmail.com>
2013-01-31 14:52:23 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT/src@1325 09069c07-60fa-4895-8316-727242e252e4

--- lfcatalog/trunk/src/tiscaf-0.3/src/zgs/sync/Sync.scala
+++
@@ -1,6 +0,0 @@
-package zgs.sync
-
-object Sync {
-  def spawn(code: => Unit) : Unit =
-    new Thread(new Runnable { def run : Unit = { code } }) start
-}
--- lfcatalog/trunk/src/tiscaf-0.3/stopHomeServer.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-scala -classpath classes zgs.httpd.HomeServerStop
