5c28f5af2ee7f751b6c3f8445e9728bd61c26fe2
miancu <miancu@09069c07-60fa-4895-8316-727242e252e4>
2013-02-14 16:49:33 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@1369 09069c07-60fa-4895-8316-727242e252e4

--- src/mmt-api/trunk/resources/mmt-web/script/jobad/service.js
+++ src/mmt-api/trunk/resources/mmt-web/script/jobad/service.js
@@ -3,10 +3,6 @@
  * @author Florian Rabe, based on previous code by Catalin David
  */
 
-/** @global-field focus: holds a reference to the object that was clicked by the user */
-var focus = null;
-/** @global-field focus: true if focus is within a math object */
-var focusIsMath = false;
 
 // if console is not defined, e.g., Firebug console is not enabled or Non-Firefox browser
 if (typeof console == 'undefined') {
@@ -19,23 +15,6 @@ if (typeof console == 'undefined') {
     };
 }
 
-/** 
- * nsResolver - function that resolves which prefix corresponds to which namespace
- *
- * @param prefix : a string value of the prefix to be resolved
- * @returns the corresponding namespace or null if prefix undefined
- */
-// used (only) by folding
-function nsResolver(prefix){
-    var ns = {
-        'xhtml': 'http://www.w3.org/1999/xhtml',
-        'mathml': 'http://www.w3.org/1998/Math/MathML',
-        'm': 'http://www.w3.org/1998/Math/MathML',
-        'openmath': 'http://www.openmath.org/OpenMath',
-        'jobad': 'http://omdoc.org/presentation'
-    };
-    return ns[prefix] || null;
-}
 
 /**
  * Clones an object into another object (useful for prototypal inheritance)
@@ -79,112 +58,18 @@ var Service = {
 /** the array of services; every services registers itself by adding itself to this array */ 
 var loadedModules = new Array();
 
-var cmenu;
 //creating the context menu
 $(document).ready(function(){
 	$('body').append('<ul id="JOBADcontextMenu" class="jqcontextmenu"></ul>');
-   cmenu = $(document).addcontextmenu('JOBADcontextMenu');
+   $(document).addcontextmenu('JOBADcontextMenu');
    cmClear();
-   if (location.hash != "") {
-        //handle highlighting
-        var t = document.getElementById(location.hash.split('#')[1]);
-        if (t) {
-            handled_light(t);
-        }
-    }
 });
 
-function handled_light(t){
-    try {
-        $(t).effect('highlight', {}, 5000);
-    } 
-    catch (err) {
-        highlight(t);
-        setTimeout(function(){
-            lowlight(t);
-        }, 5000);
-    }
-}
-
-// highlight by changing background color
-function highlight(target){
-    $(target).attr('mathbackground', 'silver'); // Firefox 2 and MathML spec
-    $(target).attr('style', 'background:silver'); // Firefox 3
-}
-
-// undo highlight
-function lowlight(target){
-    $(target).removeAttr('mathbackground');
-    $(target).removeAttr('style'); // assuming there are no other styles
-}
-
-/** helper function for the methods below: gets the classes of an element as an array */
-function getClassArray(elem) {
-   var classes = (elem.hasAttribute('class')) ? elem.getAttribute('class') : "";
-   return classes.split(/\s+/);
-}
-
-// the following functions $.fn.f add functionality to jQuery and can be used as $(...).f
-// contrary to the built-in jQuery analogues, these work for 'pref:name' attributes and math elements
-/** add a class cl to all matched elements */
-$.fn.addMClass = function(cl){
-   this.each(function(){
-      if (this.hasAttribute('class'))
-         $(this).attr('class', $(this).attr('class') + ' ' + cl);   
-      else
-         this.setAttribute('class', cl);
-   });
-   return this;
-}
-/** remove a class cl from all matched elements */
-$.fn.removeMClass = function(cl){
-   this.each(function(){
-      var classes = getClassArray(this);
-      var newclasses = classes.filter(function(elem){return (elem !== cl) && (elem !== "")});
-      var newclassesAttr = newclasses.join(' ');
-      if (newclassesAttr == "")
-         $(this).removeAttr('class');
-      else
-         this.setAttribute('class', newclassesAttr);
-   });
-   return this;
-}
-/** toggle class cl in all matched elements */
-$.fn.toggleMClass = function(cl){
-   this.each(function(){
-      var classes = getClassArray(this);
-      if (classes.indexOf(cl) == -1)
-         $(this).addMClass(cl);
-      else
-         $(this).removeMClass(cl);
-   });
-   return this;
-}
-/** keep elements that have class cl */
-$.fn.filterMClass = function(cl){
-   return this.filter(function(){
-      var classes = getClassArray(this);
-      return (classes.indexOf(cl) !== -1)
-   });
-}
-/** keep elements that have attribute attr */
-$.fn.hasMAttr = function(attr) {
-    return this.filter(function() {
-        return this.getAttribute(attr) !== undefined;
-    });
-};
-/** keep elements that have attribute attr=value */
-$.fn.filterMAttr = function(attr, value) {
-    return this.filter(function() {
-        return this.getAttribute(attr) == value;
-    });
-};
 
 //bind all document events to the respective functions
 function keyPress(key){
     for (x in loadedModules) {
-	console.log(loadedModules[x])
-        //loadedModules[x][2].keyPressed(key);
+        loadedModules[x][2].keyPressed(key);
     }
 }
 //document.onkeypress = keyPress;
@@ -232,10 +117,6 @@ function expandCMsubItems(a, subitem){
 
 function click(e){
     if (e.which == 3) {//we have a right click
-        focus = e.target;
-        focusIsMath = ($(focus).closest('math').length !== 0);
-        xpos = e.clientX;
-        ypos = e.clientY;
         //clear the contextMenu
         cmClear();
         //retrieve new entries and add them to the context menu - add class='JOBADcmenu' to <a> elements + onclick=a[x][1]   
@@ -283,53 +164,3 @@ function hover(e){
 }
 
 document.onmouseover = hover;
-
-// TODO: remove these functions
-function proxyGet(URL, data, fun){
-    if (typeof proxyURL == 'undefined') {
-        //normal jQuery GET request
-        $.get(URL, data, fun);
-    }
-    else {
-        //send request to proxy
-		$.get(proxyURL, {'url':URL, 'data':data, 'method':'get'}, fun);
-    }
-}
-
-function proxyPost(URL, data, fun){
-    if (typeof proxyURL == 'undefined') {
-        //normal jQuery POST request
-        $.post(URL, data, fun);
-    }
-    else {
-        //send request to proxy
-		$.get(proxyURL, {'url':URL, 'data':data, 'method':'post'}, fun);
-    }
-}
-
-function proxyAjax(Atype, Aurl, Adata, Asuccess, Aasync, Aaccept){
-    if (typeof proxyURL == 'undefined') {
-        $.ajax({
-            'type': Atype,
-            'url': Aurl,
-            'data': Adata,
-            'dataType': 'xml',
-            'success': Asuccess,
-            'async': Aasync,
-            'beforeSend' : function(xhr){
-        		if(Aaccept)
-        			xhr.setRequestHeader("Accept", Aaccept);
-        	}
-        });
-    }
-    else {
-        //send request to proxy
-		$.ajax({
-			'type': 'POST',
-			'url': proxyURL, 
-			'data': {'url':Aurl, 'data':Adata, 'method':Atype, 'header':Aaccept}, 
-			'success': Asuccess,
-			'async': Aasync
-		});
-    }
-}
