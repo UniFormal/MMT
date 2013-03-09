a1ed4e7989852ff160b75807ffe2291d61dd85af
frabe <frabe@09069c07-60fa-4895-8316-727242e252e4>
2013-03-09 20:10:56 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@1446 09069c07-60fa-4895-8316-727242e252e4

---
+++ deploy/run-file.bat
@@ -0,0 +1,1 @@
+java -Xmx1024m -cp %~dp0/lib/*;%~dp0/mmt/*;%~dp0/lfcatalog/* info.kwarc.mmt.api.frontend.Run file %1
---
+++ deploy/run-plain.bat
@@ -0,0 +1,1 @@
+java -Xmx1024m -cp lib/*;mmt/*;/lfcatalog/* info.kwarc.mmt.api.frontend.Run
