786483edd8b5921b475243e2fe1697240fa6cdad
Florian Rabe <florian.rabe@gmail.com>
2013-03-20 13:59:44 +0000

    the shell commands "plugin" and "foundation" are now special cases of "extension" Just replace the keywords accordingly to migrate msl files
    git-svn-id: https://svn.kwarc.info/repos/MMT/src@1489 09069c07-60fa-4895-8316-727242e252e4

--- mmt-api/trunk/src/main/info/kwarc/mmt/api/frontend/Plugin.scala
+++ mmt-api/trunk/src/main/info/kwarc/mmt/api/frontend/Plugin.scala
@@ -4,24 +4,10 @@ package info.kwarc.mmt.api.frontend
  * plugins are identified by a string giving the Java-style URI of the respective instance of this class
  * e.g., info.kwarc.mmt.lf.Plugin
  */
-abstract class Plugin {
+abstract class Plugin extends Extension {
    /** lists the id's of depended-on plugins
     * they will be loaded an initialized before this one if they have not been yet
     * plugins are added to a Controller by executing the AddPlugin action
     */
    val dependencies : List[String]
-   /**
-    * initializes the plugin
-    * plugins may do with the Controller whatever they want
-    * but if they have to cleanup after themselves in the cleanup method
-    * @param c the controller that received the command to add the plugin
-    * @param args any arguments that were passed in that command 
-    */
-   def init(c: Controller, args: List[String])
-   /** called when the plugin is unloaded
-    *  this method must release all resources that are not anyway released by garbage collection
-    *  in particular, additional threads created by the Plugin must be terminated
-    *  does nothing by default
-    */
-   def cleanup(c: Controller) {}
 }
