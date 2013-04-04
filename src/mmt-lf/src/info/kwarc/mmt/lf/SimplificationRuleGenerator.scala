90e4a0c7d1173e96603fd2279caf20f7b64a5ff1
frabe <frabe@09069c07-60fa-4895-8316-727242e252e4>
2013-04-04 14:11:54 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@1516 09069c07-60fa-4895-8316-727242e252e4

--- src/mmt-lf/src/info/kwarc/mmt/lf/SimplificationRuleGenerator.scala
+++ src/mmt-lf/src/info/kwarc/mmt/lf/SimplificationRuleGenerator.scala
@@ -5,22 +5,11 @@ import objects._
 import uom._
 import utils._
 
-
-//object LF {
-//   val path = DPath(utils.URI("http","cds.omdoc.org") / "urtheories") ? "LF"
-//   val apply = path ? "apply"
-//   val pi = path ? "Pi"
-//   val lambda = path ? "lambda"
-//   val arrow = path ? "arrow"
-//}
-
 class SimplificationRuleGenerator extends RoleHandler {
-  case class DoesNotMatch(var msg : String = "") extends java.lang.Throwable(msg)
+  case class DoesNotMatch(msg : String = "") extends java.lang.Throwable(msg)
   def isApplicable(role: String) : Boolean = role == "Simplify"
-  def apply(c: symbols.Constant) = {
-    // register rule via controller.uom.register
-    //TODO
-    // error msgs 
+  def apply(c: symbols.Constant) {
+    //TODO error msgs 
     if (! c.tp.isEmpty) {
     	val tm = c.tp.get
     	tm match {
@@ -29,7 +18,7 @@ class SimplificationRuleGenerator extends RoleHandler {
     	      // match to OMA(OMID(=), (lhs, rhs))
     	      case ApplySpine(OMID(eq), List(t1, t2)) =>
     	        if (controller.localLookup.get(eq).role.toString == "Constant:Eq") {
-    	    // match lhs to OMA(op, (var,...,var,OMA/OMID,var,...,var))
+                // match lhs to OMA(op, (var,...,var,OMA/OMID,var,...,var))
     	          t1 match {
     	            case ApplySpine(OMS(outer), args) =>
     	              var bfr : List[LocalName] = Nil
@@ -54,52 +43,42 @@ class SimplificationRuleGenerator extends RoleHandler {
     	            	     } else
     	            	        throw DoesNotMatch()    	  
     	              }
-    	              if (isBefore) throw DoesNotMatch() // only OMVs were detected
-    	              //TODO check all var names must be different
+    	              if (isBefore) throw DoesNotMatch() // check that inner OMA was detected
+    	              // check that all var names are different
     	              val varls = bfr ++ ins ++ aft
     	              val unique = varls.distinct
     	              if (varls.length != unique.length) throw DoesNotMatch()
     	              
-    	            	  val simplify = new DepthRule(outer, inr){
-    	            	   	private val bfrNames = bfr.reverse
-    	            	   	private val aftNames = aft.reverse
-    	            	   	private val insNames = ins
-    	            	   	def apply = { 
-    	            	   	  ((before : List[Term],inside : List[Term],after : List[Term]) 
-    	            	   	  => {
-    	            	   	    val bfrch = (bfrNames zip before).map {
-    	            	   	      case (x,t) => OMV(x) / t
-    	            	   	    }
-    	            	   	    val aftch = (aftNames zip after).map {
-    	            	   	      case (x,t) => OMV(x) / t
-    	            	   	    }
-    	            	   	    val insch = (insNames zip inside).map {
-    	            	   	      case (x,t) => OMV(x) / t
-    	            	   	    }
-    	            	   	    val subs : Substitution = Substitution((bfrch ++ insch ++ aftch) : _*)
-    	            	   	    val qq = t2^subs
-    	            	   	    GlobalChange(qq)
-    	            	   	  }
-    	            	   	) : Rewrite } 	
-    	            	  }
-    	            	  controller.uom.register(simplify)
+ 	            	  val simplify = new DepthRule(outer, inr){
+ 	            	   	private val bfrNames = bfr.reverse
+ 	            	   	private val aftNames = aft.reverse
+ 	            	   	private val insNames = ins
+ 	            	   	def apply : Rewrite = { 
+ 	            	   	  (before : List[Term],inside : List[Term],after : List[Term]) => { 
+ 	            	   	    val bfrch = (bfrNames zip before).map {
+ 	            	   	      case (x,t) => OMV(x) / t
+ 	            	   	    }
+ 	            	   	    val aftch = (aftNames zip after).map {
+ 	            	   	      case (x,t) => OMV(x) / t
+ 	            	   	    }
+ 	            	   	    val insch = (insNames zip inside).map {
+ 	            	   	      case (x,t) => OMV(x) / t
+ 	            	   	    }
+ 	            	   	    val subs : Substitution = Substitution(bfrch ::: insch ::: aftch : _*)
+ 	            	   	    val qq = t2^subs
+ 	            	   	    GlobalChange(qq)
+ 	            	   	  }
+ 	            	   	} 	
+ 	            	  }
+ 	            	  controller.extman.ruleStore.add(simplify)
     	              
-    	            case _ =>
+    	            case _ => // no match
     	          }
     	        }
-    	      case _ =>  
+    	      case _ => // no match 
     	    }
-    	  case _ =>  
+    	  case _ => // impossible case
     	}
     }
-  }
-}
-
-
-object OMVcheck {
-  def apply(t : Term) : Boolean = t match {
-    case t : OMV => true
-//    case OMA(_,args) => args.foldLeft(true)( (a,b) => a && this.apply(b))
-    case _ => false
   }
 }
