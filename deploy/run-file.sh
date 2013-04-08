0825558ac94bfcbd3a628afb215dae6db15051f8
fmance <fmance@09069c07-60fa-4895-8316-727242e252e4>
2013-04-08 19:52:06 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@1528 09069c07-60fa-4895-8316-727242e252e4

--- deploy/run-file.sh
+++ deploy/run-file.sh
@@ -1,2 +1,2 @@
 dir=`dirname $0`
-java -Xmx2048m -cp $dir/lib/*:$dir/lfcatalog/*:$dir/mmt/* info.kwarc.mmt.api.frontend.Run file $1
+/usr/lib/jvm/java-7-openjdk-amd64/bin/java -Xmx2048m -cp $dir/lib/*:$dir/lfcatalog/*:$dir/mmt/* info.kwarc.mmt.api.frontend.Run file $1
