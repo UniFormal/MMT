004c762183982b67a1ca00a6f518698046d8d7d0
frabe <frabe@09069c07-60fa-4895-8316-727242e252e4>
2013-04-10 09:23:32 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@1531 09069c07-60fa-4895-8316-727242e252e4

--- deploy/run-file.sh
+++ deploy/run-file.sh
@@ -1,7 +1,2 @@
 dir=`dirname $0`
-if [ -e "/usr/lib/jvm/java-7-openjdk-amd64/bin/java" ]; then
-  /usr/lib/jvm/java-7-openjdk-amd64/bin/java -Xmx2048m -cp $dir/lib/*:$dir/lfcatalog/*:$dir/mmt/* info.kwarc.mmt.api.frontend.Run file $1
-else
-    echo 'calling default java'
-    java -Xmx2048m -cp $dir/lib/*:$dir/lfcatalog/*:$dir/mmt/* info.kwarc.mmt.api.frontend.Run file $1
-fi
+java -Xmx2048m -cp $dir/lib/*:$dir/lfcatalog/*:$dir/mmt/* info.kwarc.mmt.api.frontend.Run file $1
--- deploy/run-plain.sh
+++ deploy/run-plain.sh
@@ -1,8 +1,3 @@
 dir=`dirname $0`
-if [ -e "/usr/lib/jvm/java-7-openjdk-amd64/bin/java" ]; then
-  /usr/lib/jvm/java-7-openjdk-amd64/bin/java -Xmx2048m -cp $dir/lib/*:$dir/lfcatalog/*:$dir/mmt/* info.kwarc.mmt.api.frontend.Run
-else
-    echo 'calling default java'
-    java -Xmx2048m -cp $dir/lib/*:$dir/lfcatalog/*:$dir/mmt/* info.kwarc.mmt.api.frontend.Run
-fi
+java -Xmx2048m -cp $dir/lib/*:$dir/lfcatalog/*:$dir/mmt/* info.kwarc.mmt.api.frontend.Run
 
