8e7d5a331edd8559d471678be9507aee2bedbcee
m-iancu <mihnea.iancu88@gmail.com>
2013-05-18 12:07:23 +0000

    maven setup
    git-svn-id: https://svn.kwarc.info/repos/MMT/src@1558 09069c07-60fa-4895-8316-727242e252e4

---
+++ mmt-api/trunk/pom_bootstrap.sh
@@ -0,0 +1,3 @@
+mvn install:install-file -DgroupId=org.gnieh -DartifactId=tiscaf -Dpackaging=jar -Dversion=0.8-SNAPSHOT -Dfile=../../../deploy/lib/tiscaf.jar  
+mvn install:install-file -DgroupId=info.kwarc.mmt -DartifactId=tntbase-plugin-api -Dpackaging=jar -Dversion=1.0.0-alpha -Dfile=../../../deploy/lib/tntbase-plugin-api.jar  
+mvn install:install-file -DgroupId=sun.java -DartifactId=jfxrt -Dpackaging=jar -Dversion=2.0 -Dfile=lib/jfxrt.jar  
