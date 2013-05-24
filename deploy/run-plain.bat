295cde5b4c27ba1438931a946b06c4504668a04f
frabe <frabe@09069c07-60fa-4895-8316-727242e252e4>
2013-05-24 13:59:11 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@1563 09069c07-60fa-4895-8316-727242e252e4

--- deploy/run-plain.bat
+++ deploy/run-plain.bat
@@ -2,4 +2,4 @@ rem This is the main extry point for running MMT on Windows.
 rem Executing this script opens an interactive MMT shell.
 rem MMT is "empty" by default, and the first thing to do is usually to issue a command like "file startup.msl" to load some configuration.
 
-java -Xmx1024m -cp lib/*;mmt/*;/lfcatalog/* info.kwarc.mmt.api.frontend.Run
+java -Xmx1536m -cp lib/*;mmt/*;/lfcatalog/* info.kwarc.mmt.api.frontend.Run
