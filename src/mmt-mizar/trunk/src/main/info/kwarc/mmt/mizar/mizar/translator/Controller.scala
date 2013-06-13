6eac47b2a331e3eb99addac254846390a1684076
miancu <miancu@09069c07-60fa-4895-8316-727242e252e4>
2013-06-13 10:51:19 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@1601 09069c07-60fa-4895-8316-727242e252e4

--- src/mmt-mizar/trunk/src/main/info/kwarc/mmt/mizar/mizar/translator/Controller.scala
+++ src/mmt-mizar/trunk/src/main/info/kwarc/mmt/mizar/mizar/translator/Controller.scala
@@ -191,7 +191,7 @@ object TranslationController {
 	}
 	
 	def makeConstant(n: LocalName, t: Term) =
-      new Constant(OMMOD(currentTheory), n, None, Some(t), None, None, None)
+      new Constant(OMMOD(currentTheory), n, None, TermContainer(t), TermContainer(None), None, None)
    def makeConstant(n: LocalName, t: Term, d: Term) =
-      new Constant(OMMOD(currentTheory), n, None, Some(t), Some(d), None, None)
+      new Constant(OMMOD(currentTheory), n, None, TermContainer(t), TermContainer(d), None, None)
 }
