9b03a172c45c24b371253f38a55f071e47dc0402
miancu <miancu@09069c07-60fa-4895-8316-727242e252e4>
2013-06-29 23:56:32 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@1630 09069c07-60fa-4895-8316-727242e252e4

--- src/stex-mmt/src/STeXImporter.scala
+++ src/stex-mmt/src/STeXImporter.scala
@@ -9,7 +9,8 @@ import info.kwarc.mmt.api.modules._
 import info.kwarc.mmt.api.symbols._
 import info.kwarc.mmt.api.objects._
 import info.kwarc.mmt.api.frontend._
-
+import info.kwarc.mmt.api.web._
+import info.kwarc.mmt.api.parser._
 
 
 import scala.xml.Node
@@ -42,6 +43,20 @@ class STeXImporter extends Compiler with Logger {
     out.close()
     errors
   }
+  
+  def compileOne(inText : String, dpath : DPath) : String = {
+    val node = scala.xml.Utility.trim(scala.xml.XML.loadString(inText))    
+    val errors = translateArticle(node)(dpath)
+    errors match {
+      case Nil => //returning result
+        val docXML = controller.getDocument(dpath).toNodeResolved(controller.memory.content)
+        docXML.toString
+      case _ => //reporting errors
+        "Errors Ocurred : " + errors.toString
+    }
+
+  } 
+  
 
   val xmlNS = "http://www.w3.org/XML/1998/namespace"
   val omdocNS = "http://omdoc.org/ns"
@@ -77,7 +92,7 @@ class STeXImporter extends Compiler with Logger {
           controller.add(thy)
           errors ++= n.child.map(translateDeclaration).flatten
         case "metadata" => //ignore for now
-          
+        case _ => //ignore for now (e.g. div)
       }
     } catch {
       case e : Error => errors ::= e
@@ -120,13 +135,94 @@ class STeXImporter extends Compiler with Logger {
               val dfn = new Definition(doc.path, targets.toList, cmp)
               doc.add(dfn)
           }
-        case "notation" => //TODO
+        case "notation" => 
+          val prototype = n.child(0)
+          val rendering = n.child(1)
+          val notation = makeNotation(prototype.child.head, rendering.child.head)(doc.path, mpath)
+          
+          val cd = (n \ "@cd").text
+          val name = (n \ "@name").text
+          val refPath = Path.parseM("?" + cd, doc.path)
+          val refName = refPath ? LocalName(name)
+          val c = controller.memory.content.getConstant(refName, p => "Notation for nonexistent constant " + p)
+          val const = new Constant(c.home, c.name, c.alias, c.tpC, c.dfC, c.rl, Some(notation))
+          controller.memory.content.update(const) 
+          val res = controller.memory.content.getConstant(refName, p => "Notation for nonexistent constant " + p)
+          
+          
+          
         case _ => println("Ignoring elem" + n)//TODO
       }
     } catch {
       case e : Error => errors ::= e
     }
     errors
+  }
+  
+  def makeNotation(proto : scala.xml.Node, rendering : scala.xml.Node)(implicit dpath : DPath, mpath : MPath) : parser.TextNotation = {
+	val argMap : collection.mutable.Map[String, Int] = new collection.mutable.HashMap() 
+
+    val symName = proto.label match {
+      case "OMA" => 
+        val n = proto.child.head
+        n.label match {
+          case "OMS" => 
+            val cd = (n \ "@cd").text
+            val name = (n \ "@name").text
+            val refPath = Path.parseM("?" + cd, dpath)
+            refPath ? LocalName(name)
+          case _ => throw ParseError("invalid prototype")
+        }
+        
+      case _ => throw ParseError("invalid prototype")
+    }
+    //computing map of arg names to positions
+	proto.child.tail.zipWithIndex foreach {p => 
+	  val name = (p._1 \ "@name").text
+	  argMap(name) = p._2 + 1 //args numbers start from 1
+	}
+   
+   
+   val markers =  parseRenderingMarkers(rendering, argMap.toMap)
+   val precS = try {
+     (rendering \ "@precedence").text.toInt
+   } catch {
+     case _ : Exception => 1
+   }
+   
+   new TextNotation(symName, Mixfix(markers), presentation.Precedence.integer(precS))
+  }
+  
+  
+  def parseRenderingMarkers(n : scala.xml.Node,argMap : Map[String, Int])(implicit dpath : DPath, mpath : MPath) : List[Marker] = n.label match {
+    case "mrow" => n.child.flatMap(parseRenderingMarkers(_, argMap)).toList
+    case "msub" => n.child.flatMap(parseRenderingMarkers(_, argMap)).toList //treated mrow because there is no subscript support in MMT TextNotation
+    case "msup" => n.child.flatMap(parseRenderingMarkers(_, argMap)).toList //treated mrow because there is no superscript support in MMT TextNotation
+    case "mpadded" => n.child.flatMap(parseRenderingMarkers(_, argMap)).toList
+    case "mo" => makeDelim(n.child.mkString) :: Nil
+    case "mi" => makeDelim(n.child.mkString) :: Nil //for now treated exactly like mo        
+    case "mtext" => makeDelim(n.child.mkString) :: Nil
+    case "render" => 
+      val argName = (n \ "@name").text
+      val argNr = argMap(argName)
+      Arg(argNr) :: Nil
+    case "iterate" =>
+      val argName = (n \ "@name").text
+      val argNr = argMap(argName)
+      n.child.find(_.label == "separator") match {
+        case None => SeqArg(argNr, makeDelim(",")) :: Nil
+        case Some(sep) => 
+          val delim = sep.child.head.child.mkString
+          SeqArg(argNr, makeDelim(delim)) :: Nil
+      }
+    case "none" => Nil
+  }
+  
+  def makeDelim(s : String) : Delim = {
+    s match {
+      case "…" => Delim("...")
+      case _ => Delim(s)
+    }
   }
   
   def translateCMP(n : scala.xml.Node)(implicit dpath : DPath, mpath : MPath) : NarrativeObject = n.label match {
@@ -153,6 +249,7 @@ class STeXImporter extends Compiler with Logger {
         val docName = cd + ".omdoc"
         val doc = dpath.^! / docName //smglo library uses same name for doc and theories
         <om:OMS base={doc.toPath} module={cd} name={name}/>
+      case "#PCDATA" => new scala.xml.Text(node.toString)
       case _ => new scala.xml.Elem(node.prefix, node.label, node.attributes, node.scope, false, node.child.map(rewriteNode) :_*)
     }
     
