0854241889705da32674f86247a636b8c9a15ea0
frabe <frabe@09069c07-60fa-4895-8316-727242e252e4>
2013-10-01 13:55:39 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@1737 09069c07-60fa-4895-8316-727242e252e4

---
+++ deploy/jedit-plugin/install.sh
@@ -0,0 +1,16 @@
+dir=`dirname $0`
+jedit=$1
+
+# copy the jars
+cp $dir/plugin/jars/*.jar $jedit/jars
+
+# copy the modes
+cp $dir/modes/*.xml $jedit/modes
+
+# remove the final </MODES> tag in the mode catalog, append the entries for the modes, and then append the final </MODES> tag again
+sed -i 's/<\/MODES>//' $jedit/modes/catalog
+cat $dir/modes/catalog >> $jedit/modes/catalog
+echo \</MODES\> >> $jedit/modes/catalog
+
+# append abbreviations for the mmt mode
+cat $dir/abbrevs >> $jedit/abbrevs
