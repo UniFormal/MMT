b04d2ec9e2e2da08985cf9d56e1c967032f8a730
miancu <miancu@09069c07-60fa-4895-8316-727242e252e4>
2013-10-22 16:46:44 +0000

    merged from trunk, minor updates to mws harvest exporter
    git-svn-id: https://svn.kwarc.info/repos/MMT@1784 09069c07-60fa-4895-8316-727242e252e4

--- src/mmt-api/branches/mmt-informal/src/main/info/kwarc/mmt/api/archives/MWSHarvestExporter.scala
+++ src/mmt-api/branches/mmt-informal/src/main/info/kwarc/mmt/api/archives/MWSHarvestExporter.scala
@@ -1,7 +1,6 @@
 package info.kwarc.mmt.api.archives
 
 import info.kwarc.mmt.api._
-import documents._
 import modules._
 import symbols._
 import presentation._
@@ -10,47 +9,11 @@ import backend._
 import objects._
 import utils._
 
-
-class MWSHarvestNarrationExporter extends NarrationExporter {
-  val outDim = "mws-narration"
-  val key = "mws-narration"
-  override val outExt = "mws"
-  
-  val custom : ArchiveCustomization = new DefaultCustomization    
-   
-    
-  def doDocument(doc: Document) {
-    rh("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
-    rh("<mws:harvest xmlns:mws=\"http://search.mathweb.org/ns\" xmlns:m=\"http://www.w3.org/1998/Math/MathML\">\n")
-    try {
-      def narrToCML(n : NarrativeObject) : List[scala.xml.Node] = n match {
-        case nt : NarrativeTerm => List(custom.prepareQuery(nt.term))
-        case nn : NarrativeNode => nn.child.flatMap(narrToCML)
-        case _ => Nil
-      }
-      doc.components collect {
-        case n : Narration =>  
-          val exprs = narrToCML(n.content)
-          val url = custom.mwsurl(doc.path)
-          exprs foreach {cml =>
-            val out = <mws:expr url={url}>{cml}</mws:expr>
-            rh(out.toString + "\n")
-          }        
-      }
-    } catch {
-      case e : GetError => //doc not found, can safely ignore 
-    }   
-    rh("</mws:harvest>\n")
-    
-  } 
-}
-
-
 class MWSHarvestContentExporter extends ContentExporter {
   val outDim = "mws-content"
   val key = "mws-content"
   override val outExt = "mws"
-  
+
   val custom : ArchiveCustomization = new DefaultCustomization    
     
   def doTheory(t: DeclaredTheory) { 
@@ -78,6 +41,7 @@ class MWSHarvestContentExporter extends ContentExporter {
   def doView(v: DeclaredView) { 
     //excluding expressions from views for now
   }
+  
   
   def doNamespace(dpath: DPath, namespaces: List[(BuiltDir,DPath)], modules: List[(BuiltFile,MPath)]) {
     //Nothing to do, no MathML in directly in namespaces
