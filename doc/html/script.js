27d666686c0f4d0e32558d41e65e43d5750b4bdd
frabe <frabe@09069c07-60fa-4895-8316-727242e252e4>
2013-11-14 15:56:08 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@1834 09069c07-60fa-4895-8316-727242e252e4

--- doc/html/script.js
+++ doc/html/script.js
@@ -6,7 +6,9 @@ function addStyle() {
   var cssTag = document.createElement("link");
   cssTag.setAttribute("rel", "stylesheet");
   cssTag.setAttribute("type", "text/css");
-  cssTag.setAttribute("href", 'style.css');
+  var depth = $('#content').attr('src').split("/").length - 1;
+  var up = new Array(depth + 1).join("../");
+  cssTag.setAttribute("href", up + 'style.css');
   frames[1].document.head.appendChild(cssTag)
 }
 
