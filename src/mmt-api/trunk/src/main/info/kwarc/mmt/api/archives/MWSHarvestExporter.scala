92c3b38615c5e094098eeff7c79a5b1d64feea4e
frabe <frabe@09069c07-60fa-4895-8316-727242e252e4>
2014-01-28 15:58:08 +0000

    better harvests, search pane
    git-svn-id: https://svn.kwarc.info/repos/MMT@1924 09069c07-60fa-4895-8316-727242e252e4

--- src/mmt-api/trunk/src/main/info/kwarc/mmt/api/archives/MWSHarvestExporter.scala
+++ src/mmt-api/trunk/src/main/info/kwarc/mmt/api/archives/MWSHarvestExporter.scala
@@ -14,27 +14,19 @@ class MWSHarvestExporter extends Exporter {
   val outDim = Dim("export", "mws")
   val key = "mws-harvest"
   override val outExt = "harvest"
-
-  val custom : ArchiveCustomization = new DefaultCustomization    
     
   def exportTheory(t: DeclaredTheory, bf: BuildFile) { 
     rh("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
     rh("<mws:harvest xmlns:mws=\"http://search.mathweb.org/ns\" xmlns:m=\"http://www.w3.org/1998/Math/MathML\">\n")
-    t.getDeclarations foreach {
-      case c: Constant =>
-        List(c.tp,c.df).map(tO => tO map { 
-          t =>
-            val url = custom.mwsurl(c.path)
-            val cml = custom.prepareQuery(t)
-            val node = <mws:expr url={url}><content>{cml}</content></mws:expr>
-            rh(node.toString + "\n")
-        })
-      /*case i : Instance => {
-          val url = mwsbase + "/" + thy.name + ".html" + "#" + i.name.flat
-          val node = <mws:expr url={url}>{i.matches.toCML}</mws:expr>
-          outStream.write(node.toString + "\n")
-        }*/
-      case _ => 
+    t.getDeclarations foreach {d =>
+      d.getComponents.foreach {
+         case (comp, tc: AbstractTermContainer) =>
+            tc.get.foreach {t =>
+               val node = <mws:expr url={CPath(d.path,comp).toPath}><content>{t.toCML}</content></mws:expr>
+               rh(node.toString + "\n")
+            }
+         case _ => 
+      }
     }
     rh("</mws:harvest>\n")
   }
@@ -51,5 +43,55 @@ class MWSHarvestExporter extends Exporter {
   def exportDocument(doc : Document, bt: BuildTask) {
        //Nothing to do - no MathML at document level
   }
-
 }
+
+
+import scala.xml.Node
+import parser._
+/**
+ * a wrapper around a remote MathWebSearch server
+ * @param url the URL of the server
+ */
+class MathWebSearch(url: java.net.URL) {
+   val qvarBinder = utils.mmt.mmtcd ? "qvar"
+   private val qvarMarkers = List(Delim("$"), Var(1, false, Some(Delim(","))), Delim(":"), Arg(2))
+   val qvarNot = new TextNotation(qvarBinder, Mixfix(qvarMarkers), presentation.Precedence.infinite, utils.mmt.mmtcd)
+
+   private def queryToXML(query: MathWebSearchQuery) = {
+      val queryCML = query.term match {
+         case OMBIND(OMS(this.qvarBinder), qvars, qBody) => qBody.toCMLQVars(qvars)
+         case t => t.toCML
+      }
+      <mws:query xmlns:mws="http://www.mathweb.org/mws/ns" xmlns:m="http://www.w3.org/1998/Math/MathML"
+             limitmin={query.limitmin.toString} answsize={query.answsize.toString} totalreq="yes" output="xml">
+            <mws:expr>{queryCML}</mws:expr>
+      </mws:query>
+   }
+   
+   /** sends a query
+    *  @param query the query, using this.qvarBinder at the toplevel to bind the query variables
+    *  @return MathWebSearch's reply
+    */
+   def apply(query: MathWebSearchQuery): List[MathWebSearchAnswer] = {
+      println(query.term)
+      val responseXML = utils.xml.post(url, queryToXML(query))
+      val response = responseXML match {
+         case <mws:answset>{answs @_*}</mws:answset> =>
+            answs.toList.map {
+               case n @ <mws:answ>{_*}</mws:answ> =>
+                  val p = Path.parseC(utils.xml.attr(n, "uri"), utils.mmt.mmtbase)
+                  val xpS = utils.xml.attr(n, "xpath")
+                  // xpath has format "/*[Int].../*[Int]
+                  val xp = xpS.substring(3, xpS.length-4).split("]/*[").toList.map(_.toInt)
+                  MathWebSearchAnswer(p, Position(xp))
+            }
+      }
+      println(response)
+      response
+   }
+}
+
+/** an answer in an MathWebSearch answer set */
+case class MathWebSearchAnswer(cpath: CPath, pos: Position)
+
+case class MathWebSearchQuery(term: Term, answsize: Int = 1000, limitmin: Int = 0)
--- src/mmt-api/trunk/src/main/info/kwarc/mmt/api/gui/SearchPane.scala
+++ src/mmt-api/trunk/src/main/info/kwarc/mmt/api/gui/SearchPane.scala
@@ -2,28 +2,52 @@ package info.kwarc.mmt.api.gui
 
 import info.kwarc.mmt.api._
 import frontend._
+import parser._
+import objects._
 
 import javax.swing._
+import tree._
+import java.awt._
 
 class SearchPane(controller: Controller) extends JPanel {
+   setLayout(new BorderLayout())
    private val query = new JPanel
+   private val theory = new JTextField
    private val queryText = new JTextField
    private val searchButton = Swing.Button("search")(search)
+   theory.setText("http://cds.omdoc.org/examples?PL")
+   query.add(theory)
+   queryText.setText("$x,y: x ⇒ y")
    query.add(queryText)
    query.add(searchButton)
-   add(query)
+   add(query, BorderLayout.NORTH)
    
-   private val results = new JPanel
-   add(results)
-
+   private val resultRoot = new DefaultMutableTreeNode("MathWebSearchResults")
+   private val resultTree = new JTree(resultRoot)
+   //resultTree.setRootVisible(false)
+   //private val scrollResults = new JScrollPane(results)
+   add(resultTree, BorderLayout.CENTER)
+   
+   private val mws = new archives.MathWebSearch(utils.URI("http://opal.eecs.jacobs-university.de:8082").toURL)
+   
    private def search {
+      val t = Path.parseM(theory.getText, utils.mmt.mmtcd)
       val q = queryText.getText
-      val res : List[Path] = List(utils.mmt.mmtcd)
-      results.removeAll
-      res.foreach {p =>
-         val r = new JLabel(p.toString) 
-         results.add(r)
+      val pu = ParsingUnit(SourceRef.anonymous(q), OMMOD(t), Context(), q, Some(mws.qvarNot))
+      val mwsQuery = controller.termParser(pu, () => _)
+      val mwsResults = mws(archives.MathWebSearchQuery(mwsQuery))
+      val root = new DefaultMutableTreeNode(q)
+      val orderedResults = mwsResults.map(_.cpath).groupBy(_.parent.module).toList.sortBy(_._1.toMPath.toString)
+      orderedResults.foreach {
+         case (k,ps) =>
+            val node = new DefaultMutableTreeNode(k.toMPath.toString)
+            ps.foreach {p =>
+               val n = new DefaultMutableTreeNode(p.parent.toString + "?" + p.component.toString)
+               node.add(n)
+            }
+            root.add(node)
       }
-      revalidate
+      resultRoot.add(root)
+      resultTree.updateUI
    }
 }
