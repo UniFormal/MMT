8ed5283f08fcbc3a7029c32ede1b0e46c53c56d3
eluzhnica <eluzhnica@09069c07-60fa-4895-8316-727242e252e4>
2014-02-14 15:57:39 +0000

    Changing max memory for running immt
    Summary: setting 1024mb from 2048
    
    Test Plan: check
    
    Reviewers: m.iancu
    
    Reviewed By: m.iancu
    
    Differential Revision: http://phab.code4fun.de/D38
    
    git-svn-id: https://svn.kwarc.info/repos/MMT@1950 09069c07-60fa-4895-8316-727242e252e4

--- deploy/run-immt.sh
+++ deploy/run-immt.sh
@@ -1,2 +1,2 @@
 dir=`dirname $0`
-java -Xmx2048m -cp $dir/lib/*:$dir/mmt/branches/informal/*:$dir/lfcatalog/*:$dir/mmt/* info.kwarc.mmt.api.frontend.Run file $1
+java -Xmx1024m -cp $dir/lib/*:$dir/mmt/branches/informal/*:$dir/lfcatalog/*:$dir/mmt/* info.kwarc.mmt.api.frontend.Run file $1
