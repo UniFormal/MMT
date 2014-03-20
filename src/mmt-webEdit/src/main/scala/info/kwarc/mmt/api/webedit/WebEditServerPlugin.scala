ddf22057a6f6d10cb5c1127eacef779fa70678f6
eluzhnica <eluzhnica@09069c07-60fa-4895-8316-727242e252e4>
2014-03-20 14:15:18 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@2008 09069c07-60fa-4895-8316-727242e252e4

--- src/mmt-webEdit/src/main/scala/info/kwarc/mmt/api/webedit/WebEditServerPlugin.scala
+++ src/mmt-webEdit/src/main/scala/info/kwarc/mmt/api/webedit/WebEditServerPlugin.scala
@@ -1,6 +1,7 @@
 package scala.info.kwarc.mmt.api.webedit
-
+import java.lang.String
 import info.kwarc.mmt.api._
+import info.kwarc.mmt.api.notations._
 import info.kwarc.mmt.api.utils._
 import info.kwarc.mmt.api.web._
 import info.kwarc.mmt.api.frontend._
@@ -13,6 +14,7 @@ import scala.util.parsing.json._
 import scala.concurrent._
 import tiscaf._
 import scala.collection.mutable._
+
 
 class WebEditServerPlugin extends ServerExtension("editing") with Logger {
   
@@ -29,6 +31,7 @@ class WebEditServerPlugin extends ServerExtension("editing") with Logger {
         case "autocomplete" :: _ => getAutocompleteResponse
         case "resolve" :: _ => getResolveResponse
         case "minIncludes" :: _ => getMinIncludes
+        case "symbolCompletion" :: _ => getSymbolCompletion
         case _ => error("Invalid request: " + uriComps.mkString("/"))
       }
     } catch {
@@ -123,11 +126,49 @@ class WebEditServerPlugin extends ServerExtension("editing") with Logger {
       val mpath = Path.parseM(mpathS, mmt.mmtbase)
       
       val myPaths = Names.resolve(OMMOD(mpath), Nil, prefix)(controller.globalLookup)
-      //mPaths.map(controller.get) collect {
-      //  case c : Constant => c.not.toString
-      // return in form of ((string,meaning),arguments,return type))
-      //}
       val response = new JSONArray(myPaths.map(_.path.toPath))
+      Server.JsonResponse(response).aact(tk)
+    }
+  }
+  
+  
+  private def getSymbolCompletion : HLet = new HLet {
+    def aact(tk : HTalk)(implicit ec : ExecutionContext) : Future[Unit] = try {
+      val reqBody = new Body(tk)
+      val params = reqBody.asJSON.obj
+      
+      val prefix = params.get("prefix").getOrElse(throw ServerError("No prefix found")).toString
+      val mpathS = params.get("mpath").getOrElse(throw ServerError("No mpath found")).toString
+      val mpath = Path.parseM(mpathS, mmt.mmtbase)
+      
+      val myPaths = Names.resolve(OMMOD(mpath), Nil, prefix)(controller.globalLookup)
+      val paths=(myPaths.map(_.path)) 
+      
+      val constants = paths.map(controller.get) collect {
+       case c : Constant => c.not match {
+         case None => c.name -> Nil 
+         case Some(a) => c.name -> a.fixity.markers
+       }
+      }
+      
+      def modifyStringRepresent (name:LocalName,markers:List[Marker],accumulator:String,hasSymbol:Boolean) : (String) = {
+		val separator = " " 
+         markers match {
+		  case Nil => if(hasSymbol) accumulator 
+				  	  else name.toString + accumulator
+		  case hd::b  => if(hd.toString=="%n"){ 
+			  				modifyStringRepresent(name,b,accumulator,false)
+			  				}
+						else if(hd.toString.length()>1 && hd.toString.substring(0,2)=="%I") 
+							modifyStringRepresent(name,b,accumulator,hasSymbol) 
+		  				else 
+		  				    modifyStringRepresent(name,b,accumulator+ separator +hd.toString,hasSymbol)
+		}
+		
+      }
+
+      val stringResponse = constants.map({case (x,y)=>modifyStringRepresent(x,y,"",true)})
+      val response = new JSONArray(stringResponse)
       Server.JsonResponse(response).aact(tk)
     }
   }
