9112a185e6a1685e2c8f9b30edf9edb4a4e1c8f2
m-iancu <mihnea.iancu88@gmail.com>
2014-05-03 18:58:20 +0000

    added uncommited files
    git-svn-id: https://svn.kwarc.info/repos/MMT/src@2047 09069c07-60fa-4895-8316-727242e252e4

---
+++ planetary-mmt/src/GlossaryGenerator.scala
@@ -0,0 +1,139 @@
+package info.kwarc.mmt.planetary
+
+import info.kwarc.mmt.api._
+import modules._
+import symbols._
+import documents._
+import presentation._
+import frontend._
+import libraries._
+import objects._
+import utils._
+import flexiformal._
+import archives._
+import notations._
+import scala.xml.{Node }
+import ontology.{ isDefinedBy }
+import info.kwarc.mmt.stex._
+
+object GlossaryGenerator {
+  private var presenter: PlanetaryPresenter = null
+  private var controller: Controller = null
+  private var rh: StringBuilder = null
+  private var langList = "en" :: "de" :: Nil
+  def generate(controller: Controller): String = {
+    this.controller = controller
+    this.presenter = controller.extman.getPresenter("planetary") match {
+      case Some(p: PlanetaryPresenter) => p
+      case _ => throw new Exception("Expected planetary presenter to be loaded")
+    }
+    this.rh = new StringBuilder
+    presenter.setRh(rh)
+    val modules = controller.library.getModules
+    val verbs = modules collect {
+      case thy: DeclaredTheory =>
+        thy.getDeclarations collect {
+          case c: Constant if c.notC.verbalizationDim.isDefined =>
+            c.notC.verbalizationDim.notations.values.flatten
+        }
+    }
+    present(verbs.flatten.flatten)
+    rh.get
+  }
+
+  // easy-to-use HTML markup
+  protected val htmlRh = utils.HTML(s => rh(s))
+  import htmlRh._
+
+  private def present(verbs: Iterable[TextNotation]): Unit = {
+    val langVerbs = verbs.groupBy[String](n => sTeX.getLanguage(n.name).getOrElse(""))
+    def getCls(lang: String) = if (lang == "en") "active" else ""
+    div(attributes = List("id" -> "glossary")) {
+      ul("nav nav-tabs") {
+        langVerbs.foreach { p =>
+          li(getCls(p._1)) {
+            rh(<a data-target={ "#gtab_" + p._1 } style="cursor: pointer;" onclick={ "$(this).tab('show');" }> { p._1 } </a>)
+          }
+        }
+      }
+      div("tab-content") {
+        langVerbs.foreach { p =>
+          div(cls = ("tab-pane " + getCls(p._1)), attributes = List("id" -> ("gtab_" + p._1))) {
+            ul("glossary") {
+              val glossary = p._2.toList.sortWith((x,y) => x.markers.mkString(" ").toLowerCase() < y.markers.mkString(" ").toLowerCase())
+              glossary.foreach(v => present(v))
+            }
+          }
+        }
+      }
+    }
+  }
+
+  private def present(not: TextNotation): Unit = {
+    val spath = not.name
+
+    val doc = spath.doc
+    val mod = spath.module.toMPath.name
+    val name = spath.name
+    val lang = sTeX.getLanguage(spath)
+    val masterPath = sTeX.getMasterPath(spath)
+    val alternatives = langList.filter(l => Some(l) != lang) map { l =>
+      (l -> sTeX.getLangPath(masterPath, l))
+    }
+    val notations = try {
+      controller.library.getConstant(masterPath).notC.getAllNotations
+    } catch {
+      case e: Throwable => Nil
+    }
+    val defs = controller.depstore.getObjects(not.name, isDefinedBy) collect {
+          case fp: FragPath if fp.isPath =>
+            controller.get(fp.path) match {
+              case fd: Definition =>
+                val thyName = fd.home.toMPath.name.toPath
+                val parts = thyName.split('.')
+                if (parts.length == 2) {
+                  Some(fd)
+                } else {
+                  None
+                }
+              case _ => None
+            }
+        }
+    if (defs.isEmpty)
+      return;
+    
+    li(cls = "entry") {
+      div {
+        span(cls="name keyword", attributes=List("id" -> presenter.encPath(spath))) { 
+          val smks = not.markers map {
+            case d : Delimiter => d.text
+            case x => x.toString //TODO
+          }
+          text(smks.mkString(" ")) 
+        }
+        //addings defs
+        
+        presenter.doNotations(notations, spath)
+
+        for (alt <- alternatives) {
+          val lang = alt._1
+          val path = alt._2
+          try {
+            controller.get(path)
+            rh(<a style="cursor: pointer;" onclick={ "$('#glossary a[data-target=\\\'#gtab_" + lang + "\\\']').tab('show'); window.location.href = \'#" + presenter.encPath(path) + "\';" }> { lang } </a>)
+          } catch {
+            case e: Error => //invalid path, nothing to do
+            //           rh(<a href="#" style="cursor: pointer;" onclick={"$('#glossary a[\\\'data-toggle=#gtab_" + lang + "\\\']').tab('show');"}> {lang} </a>)
+          }
+        }
+        defs.flatten foreach { fd =>
+          div(attributes = ("id" -> ("def_" + spath.toPath + "_" + fd.path.toPath)) :: Nil) {
+            presenter(fd)(rh)
+          }
+        }
+      }
+      rh("<hr/>")
+    }
+  }
+
+}
---
+++ planetary-mmt/src/PlanetaryPresenter.scala
@@ -0,0 +1,312 @@
+package info.kwarc.mmt.planetary
+
+import info.kwarc.mmt.api._
+import modules._
+import symbols._
+import documents._
+import presentation._
+import frontend._
+import objects._
+import utils._
+import flexiformal._
+import archives._
+import notations._
+
+
+class NotationPresenter(controller : Controller, var notations : List[TextNotation] = Nil) 
+  extends presentation.MathMLPresenter(controller) {
+  override def getNotation(p: GlobalName): Option[TextNotation] = {
+    notations.find(n => n.name == p) match {
+      case None => super.getNotation(p)
+      case not => not
+    }
+  }
+  //this overrides variable presentation to placeholders for notation rendering (currently changing color to red)
+  override def doVariable(n: LocalName)(implicit pc : PresentationContext) {
+      val vdAtt = pc.context.find(_.decl.name == n) match {
+         case None => Nil
+         case Some(vd) => List("jobad:varref" -> vd.declpos.toString)
+      }
+      val mi = xml.element("mi", ("style" -> "color:red;") :: vdAtt ::: jobadattribs, n.toString)
+      pc.out(mi)
+   }
+}
+
+
+abstract class PlanetaryAbstractPresenter(name : String) extends Presenter {
+  override val outExt = "html"
+  val key = name
+  val outDim = Dim("export", name)
+  protected lazy val mmlPres = new presentation.MathMLPresenter(controller) // must be lazy because controller is provided in init only
+  def isApplicable(format : String) = format == name
+  def apply(o : Obj, owner : Option[CPath])(implicit rh : RenderingHandler) = mmlPres(o, owner)(rh)
+  protected lazy val notPres = new NotationPresenter(controller)
+  
+  // easy-to-use HTML markup
+  protected val htmlRh = utils.HTML(s => rh(s))
+   
+  import htmlRh._
+  def wrapScope(body : => Unit) {
+    div(attributes=List("xmlns" -> utils.xml.namespace("html"),
+                        "xmlns:jobad" -> utils.xml.namespace("jobad"))) {
+      body
+    }
+  }
+}
+
+class PlanetaryHeadersPresenter extends PlanetaryAbstractPresenter("planetary-headers") {
+  def apply(s : StructuralElement, standalone: Boolean = false)(implicit rh : RenderingHandler) = {
+     this._rh = rh
+     s match { 
+       case doc : Document => 
+         wrapScope(doDocument(doc))
+       case thy : DeclaredTheory => 
+         wrapScope(doTheory(thy))
+       case view : DeclaredView =>
+         wrapScope(doView(view))
+       case d : Declaration => rh("Headers presenter not applicable to declarations " + s.getClass().toString())
+       case _ => rh("TODO: Not implemented yet, presentation function for " + s.getClass().toString())
+     }
+     //TODO? reset this._rh 
+   }
+  
+  import htmlRh._
+  def doDocument(doc : Document) = {
+    div("document") {
+      ul("doc-body") {
+        doc.getItems foreach {
+          case d: DRef => li("dref") {
+            rh(<span jobad:href={d.target.toPath} data-relative="true"> {d.target.last} </span>)
+          }
+          case m: MRef => li("mref") {a(m.target.last) {text{m.target.last}}}
+          case s: SRef => li("sref") {a(s.target.last) {text{s.target.last}}}
+        }
+      }
+    }
+  }
+  
+  def doTheory(t : DeclaredTheory) = {
+    //TODO
+  } 
+  
+  def doView(t : DeclaredView) = { 
+    //TODO
+  }
+  
+}
+
+
+class PlanetaryPresenter extends PlanetaryAbstractPresenter("planetary") {
+   
+   def setRh(rh : RenderingHandler) = this._rh = rh 
+  
+   def apply(s : StructuralElement, standalone: Boolean = false)(implicit rh : RenderingHandler) = {
+     this._rh = rh
+     s match { 
+       case doc : Document => 
+         wrapScope(doDocument(doc))
+       case thy : DeclaredTheory => 
+         wrapScope(doTheory(thy))
+       case view : DeclaredView =>
+         wrapScope(doView(view))
+       case fd : FlexiformalDeclaration => 
+         wrapScope(doFlexiformalDeclaration(fd))
+       case _ => rh("TODO: Not implemented yet, presentation function for " + s.getClass().toString())
+     }
+     //TODO? reset this._rh 
+   }   
+   
+   import htmlRh._
+   
+   private def doName(p: Path) {
+      val s = p.last
+      var attrs = p match {
+        case g : GlobalName => List("id" -> (g.module.toMPath.name.toPath + "?" + g.name.toPath))
+        case m : MPath => List("id" -> m.name.toPath)
+        case _ => Nil
+      }
+      attrs ::= ("jobad:href" -> p.toPath)
+      span(cls="name", attributes=attrs) {text(s)}
+   }
+   
+    def doName(path : Path, loadable : Boolean) : Unit = loadable match {
+     case true =>
+       span(cls = "name loadable", attributes = List("jobad:load" -> path.toPath)) {
+         text(path.last)
+       }
+     case false => doName(path)
+   }
+   
+   private def doMath(t: Obj) {
+        rh(mmlPres.asString(t))
+   }
+   private def doComponent(comp: DeclarationComponent, t: Obj) {
+      td {span {text(comp.toString)}}
+      td {doMath(t)}
+   }
+   private def doNotComponent(comp: NotationComponent, tn: TextNotation) {
+      td {span {text(comp.toString)}}
+      td {span {text(tn.toText)}}
+   }
+   private val scriptbase = "https://svn.kwarc.info/repos/MMT/src/mmt-api/trunk/resources/mmt-web/script/"
+   private val cssbase    = "https://svn.kwarc.info/repos/MMT/src/mmt-api/trunk/resources/mmt-web/css/"
+   
+   def doTheory(t: DeclaredTheory) {
+     div("theory") {
+       div("theory-header") {doName(t.path)}
+       t.getPrimitiveDeclarations.foreach {
+         case PlainInclude(from,to) => 
+           span("include") {
+             table {
+               tr {
+                 td { span("keyword") {text("includes")} }
+                 td { rh(<span jobad:href={from.toPath}> {from.name.toPath} </span>) }
+               }
+             }
+           } 
+         case c : Constant =>
+           doConstant(c)
+         case f : FlexiformalDeclaration =>  
+           doFlexiformalDeclaration(f)
+       }
+     }
+   }
+   
+   def doConstant(c : Constant) {
+     div(cls = "constant", attributes = List("jobad:presents" -> c.path.toPath)) {
+       span("keyword"){text("constant")}
+       doName(c.path)
+       doNotations(c.notC.getAllNotations, c.path)
+     }
+   }
+   
+   def encPath(path : GlobalName) : String = {
+     path.module.toMPath.name.toString + "_" + path.name.toString
+   }
+   
+   def doNotations(notations : List[TextNotation], path : GlobalName) {
+     if (!notations.isEmpty) {
+       val onclick = "onclick=\"if ($(this).html() == \'Show Notations\') {$(this).html(\'Hide Notations\')} else {$(this).html(\'Show Notations\')};" +
+                     "$(document.getElementById(\'not_" + encPath(path) + "\')).toggle( \'fold\' );\""
+       rh(" <small><a style=\"cursor:pointer;\" " + onclick + ">" + "Show Notations" + "</a></small>")
+       table(cls = "table table-striped table-condensed", attributes = ("id" -> ("not_" + encPath(path))) :: ("style" -> "display:none;") :: Nil) {
+         thead {
+           tr {
+             th { text{"Languages"} } 
+             th { text{"Arguments"} }
+             th { text{"Rendering"} }
+           }
+         }
+         tbody {
+           notations.foreach(doNotation)
+         }
+       }
+     }
+   }
+   
+   def doNotation(not : TextNotation)  = {
+     def getVarName(i : Int) : String = {
+       val name = ((i % 26) + 'a'.toInt).toChar
+       val indices =  (i / 26) match {
+         case 0 => ""
+         case n => n.toString
+       }
+       name.toString + indices
+     }
+     
+     val arity = not.arity.length
+     val tm = OMA(OMID(not.name), (0 until arity).map(n => OMV(getVarName(n))).toList)
+     tr {
+       td { text{not.scope.languages.mkString("/")} }
+       td { text{not.arity.length.toString} }
+       td {
+         notPres.notations = List(not)
+         notPres(tm, None)(rh)
+         notPres.notations = Nil
+       }
+     }
+   }
+   
+   def doFlexiformalDeclaration(fd : FlexiformalDeclaration) : Unit = fd match {
+     case n : PlainNarration => 
+       div("flexiformal plain") {
+         doNarrativeObject(fd.content)
+       }
+     case d : Definition => 
+       div(cls = "flexiformal definition", 
+           attributes = List(("jobad:defines" -> d.targets.head.toPath))) {
+         //span("keyword"){text("definition" )}
+         doNarrativeObject(fd.content)
+       }
+     case x => 
+       throw ImplementationError("Presentation for " + x.getClass() + " not implemented yet")
+   }
+   
+   def doNarrativeObject(no : NarrativeObject) : Unit = no match {
+     case n : NarrativeXML => rh(n.node)
+     case r : NarrativeRef => r.self match {
+      case false => 
+        rh("<span jobad:href=\"" + r.target.toPath + "\">")
+        r.objects.foreach(doNarrativeObject)
+        rh("</span>")
+      case true => 
+        rh("<span class=\"definiendum\" jobad:href=\"" + r.target.toPath + "\">")
+        r.objects.foreach(doNarrativeObject)
+        rh("</span>")
+     }
+     case tm : NarrativeTerm => mmlPres(tm.term, None)(rh)
+     case n : NarrativeNode => 
+       rh.writeStartTag(n.node.prefix, n.node.label, n.node.attributes, n.node.scope)
+       n.child.map(doNarrativeObject)
+       rh.writeEndTag(n.node.prefix, n.node.label)
+   }
+   
+   def doView(v: DeclaredView) {}
+   override def exportNamespace(dpath: DPath, bd: BuildDir, namespaces: List[(BuildDir,DPath)], modules: List[(BuildFile,MPath)]) {
+     div("namespace") {
+        namespaces.foreach {case (bd, dp) =>
+            div("subnamespace") {
+               val name = bd.dirName + "/" + bd.outFile.segments.last
+               a(name) {
+                  text(dp.toPath)
+               }
+            }
+         }
+         modules.foreach {case (bf, mp) =>
+            div("submodule") {
+               a(bf.outFile.segments.last) {
+                  text(mp.toPath)
+               }
+            }
+         }
+      }
+   }
+   
+   def doDocument(doc: Document) {
+     div("document") {
+       ul("doc-body") { doc.getItems foreach {
+         case d: DRef => 
+           //val doc = controller.getDocument(d.path)
+           li("dref") {
+             doName(d.target, true) 
+           }
+         case m : MRef =>
+           controller.get(m.target) match {
+             case t : DeclaredTheory => 
+               if (t.name != OMV.anonymous) {
+                 li("mref") {
+                   doTheory(t)
+                 }
+               }
+             case v : DeclaredView => 
+               li("mref") {
+                 doView(v)
+               }
+             case s => throw ImplementationError("Presenting for " + s.getClass() + " not implemented yet ")
+           }
+         case s => throw ImplementationError("Presenting for " + s.getClass() + " not implemented yet ")
+       }}
+     }
+   }
+}
+
