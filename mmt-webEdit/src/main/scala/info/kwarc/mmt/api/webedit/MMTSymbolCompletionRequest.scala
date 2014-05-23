d9a621615b4e2638267b18717fca250962d159fa
m-iancu <mihnea.iancu88@gmail.com>
2014-05-23 11:41:33 +0000

    Minor API change for webedit
    git-svn-id: https://svn.kwarc.info/repos/MMT/src@2061 09069c07-60fa-4895-8316-727242e252e4

--- mmt-webEdit/src/main/scala/info/kwarc/mmt/api/webedit/MMTSymbolCompletionRequest.scala
+++ mmt-webEdit/src/main/scala/info/kwarc/mmt/api/webedit/MMTSymbolCompletionRequest.scala
@@ -1,7 +1,7 @@
 package main.scala.info.kwarc.mmt.api.webedit
 
-class MMTSymbolCompletionRequest(prefix:String, mpath : String) {
+class MMTSymbolCompletionRequest(symName:String, mpath : String) {
 
-  def getPrefix() = prefix
+  def getSymName() = symName
   def getMPath () = mpath
 }
