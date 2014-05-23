12efb58e3803fa428f56e61be6955827a1e64b1c
Florian Rabe <florian.rabe@gmail.com>
2014-05-23 10:06:58 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT/src@2059 09069c07-60fa-4895-8316-727242e252e4

---
+++ mmt-lf/src/info/kwarc/mmt/reflection/Constants.scala
@@ -0,0 +1,45 @@
+package info.kwarc.mmt.reflection
+
+import info.kwarc.mmt.api._
+import uom._
+import objects._
+import objects.Conversions._
+
+object Terms extends TheoryScala {
+  val _base = DPath(utils.URI("http", "cds.omdoc.org") / "urtheories" / "reflection")
+  val _name = LocalName("Terms")
+
+  object formation extends ConstantScala {
+    val parent = _path
+    val name = "formation"
+    def apply(p: MPath, t: Term) = OMBINDC(OMID(this.path), Context(IncludeVarDecl(p)), List(t))
+    def unapply(t: Term): Option[(MPath, Term)] = t match {
+      case OMBINDC(OMID(this.path), Context(IncludeVarDecl(p)), List(t)) => Some((p, t))
+      case _ => None
+    }
+  }
+
+  object refl extends ConstantScala {
+    val parent = _path
+    val name = "refl"
+    def apply(p: MPath, t: Term) = OMBINDC(OMID(this.path), Context(IncludeVarDecl(p)), List(t))
+    def unapply(t: Term): Option[(MPath, Term)] = t match {
+      case OMBINDC(OMID(this.path), Context(IncludeVarDecl(p)), List(t)) => Some((p, t))
+      case _ => None
+    }
+  }
+
+  object elim extends ConstantScala {
+    val parent = _path
+    val name = "elim"
+    def apply(t: Term, mor: Term) = OMA(OMID(this.path), List(t, mor))
+    def unapply(t: Term): Option[(Term, Term)] = t match {
+      case OMA(OMID(this.path), List(t, mor)) => Some((t, mor))
+      case _ => None
+    }
+  }
+  
+  object eval {
+     def apply(t: Term) = elim(t, OMCOMP())
+  }
+}
---
+++ mmt-lf/src/info/kwarc/mmt/reflection/Inductive.scala
@@ -0,0 +1,82 @@
+package info.kwarc.mmt.reflection
+
+import info.kwarc.mmt.api._
+import objects._
+import objects.Conversions._
+
+import info.kwarc.mmt.lf._
+
+/**
+ * p |- t: U  --->  |- formation(p,t) : U
+ */
+object FormationInfer extends InferenceRule(Terms.formation.path, OfType.path) {
+   def apply(solver: Solver)(tm: Term)(implicit stack: Stack, history: History): Option[Term] =
+     tm match {
+       case Terms.formation(p, t) =>
+          //TODO check that inferred type does not depend on p
+          solver.inferType(t)(stack++IncludeVarDecl(p), history)
+     }
+}
+
+/**
+ * p |- t: a  --->  |- refl(p,t) : formation(p,a)
+ */
+object ReflInfer extends InferenceRule(Terms.refl.path, OfType.path) {
+  def apply(solver: Solver)(tm: Term)(implicit stack: Stack, history: History): Option[Term] =
+    tm match {
+       case Terms.refl(p,t) =>
+          solver.inferType(t)(stack ++ IncludeVarDecl(p), history) map {tI => Terms.formation(p,tI)}
+     }
+}
+
+/**
+ * |- t: formation(p,a)  --->  |- elim(t,m) : m(a)
+ */
+object ElimInfer extends InferenceRule(Terms.elim.path, OfType.path) {
+  def apply(solver: Solver)(tm: Term)(implicit stack : Stack, history: History) : Option[Term] = {
+     tm match {
+       case Terms.elim(t,mor) =>
+          solver.inferType(t) flatMap {
+             case Terms.formation(p,a) =>
+                solver.check(IsRealization(stack, mor, OMMOD(p))) // TODO must also check identity condition
+                Some(OMM(a, mor))
+             case _ => None
+          }
+     }
+   }
+}
+
+
+/**
+ * p |- eval(t) : a  --->  |- q: formation(p,a)
+ */
+object TypingRule extends TypingRule(Terms.formation.path) {
+  def apply(solver: Solver)(tm: Term, tp: Term)(implicit stack : Stack, history: History) : Boolean = {
+    tp match {
+      case Terms.formation(p,a) =>
+         solver.check(Typing(stack++IncludeVarDecl(p), Terms.eval(tm), a))
+    }
+  }
+}
+
+/**
+ * p |- eval(t1) = eval(t2): a  --->  |- t1 = t2 : formation(p,a)
+ */
+object Extensionality extends TypeBasedEqualityRule(Terms.formation.path) {
+  def apply(solver: Solver)(tm1: Term, tm2: Term, tp: Term)(implicit stack: Stack, history: History) : Boolean = tp match {
+     case Terms.formation(p, a) =>
+        solver.check(Equality(stack++IncludeVarDecl(p), Terms.eval(tm1), Terms.eval(tm2), Some(a)))
+  }
+}
+
+/** |- m: p -> .  ---> |- elim(refl(p, t), m) = m(t) */
+object Computation extends ComputationRule(Terms.elim.path) {
+   def apply(solver: Solver)(tm: Term)(implicit stack : Stack, history: History) : Option[Term] = {
+      tm match {
+         case Terms.elim(Terms.refl(p,t),mor) =>
+            solver.check(IsRealization(stack,mor,OMMOD(p)))
+            Some(OMM(t, mor))
+         case _ => None
+      }
+   }
+}
