34b9e2a193436fe2de7441c8ac9df177eb1332ea
frabe <frabe@09069c07-60fa-4895-8316-727242e252e4>
2014-05-23 10:06:58 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@2059 09069c07-60fa-4895-8316-727242e252e4

---
+++ src/mmt-lf/src/info/kwarc/mmt/morphisms/Plugin.scala
@@ -0,0 +1,13 @@
+package info.kwarc.mmt.morphisms
+
+import info.kwarc.mmt._
+import info.kwarc.mmt.api._
+
+class MorphismPlugin extends frontend.Plugin {
+   val dependencies = Nil
+   override def start(args: List[String]) {
+      val em = controller.extman
+      em.ruleStore.add(TheoryTypeUniverse,MorphTypeUniverse,ComplexTheoryInfer,MorphCheck)
+      em.addExtension(new lf.InstanceElaborator)
+   }
+}
