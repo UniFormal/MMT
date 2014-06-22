2ff803e35517881fac5bbe1b389913ff5ce0279c
eluzhnica <eluzhnica@09069c07-60fa-4895-8316-727242e252e4>
2014-06-22 10:44:08 +0000

    Updated webEdit
    Summary: updated to work with the last MMT major updates
    
    Test Plan: check
    
    Reviewers: m.iancu
    
    Reviewed By: m.iancu
    
    Differential Revision: http://phab.code4fun.de/D80
    
    git-svn-id: https://svn.kwarc.info/repos/MMT@2115 09069c07-60fa-4895-8316-727242e252e4

--- src/mmt-webEdit/src/info/kwarc/mmt/api/webedit/WebEditServerPlugin.scala
+++ src/mmt-webEdit/src/info/kwarc/mmt/api/webedit/WebEditServerPlugin.scala
@@ -1,4 +1,4 @@
-package scala.info.kwarc.mmt.api.webedit
+package info.kwarc.mmt.api.webedit
 import java.lang.String
 import info.kwarc.mmt.api._
 import info.kwarc.mmt.api.notations._
@@ -9,18 +9,19 @@ import info.kwarc.mmt.api.symbols._
 import info.kwarc.mmt.api.libraries._
 import info.kwarc.mmt.api.objects._
 import info.kwarc.mmt.api.parser._
+import info.kwarc.mmt.api.checking._
 import info.kwarc.mmt.api.modules.DeclaredTheory
 import objects._
-import checking._
 import libraries._
 import scala.util.parsing.json._
 import scala.concurrent._
 import tiscaf._
 import scala.collection.mutable.HashMap._
-import main.scala.info.kwarc.mmt.api.webedit._
-import info.kwarc.mmt.api.presentation.StringLiteral
+import info.kwarc.mmt.api.web._
+import info.kwarc.mmt.api.presentation.{Hole => PHole,_}
 
 class WebEditServerPlugin extends ServerExtension("editing") with Logger {
+  private lazy val editingService = new EditingServicePlugin(controller)
   
   def error(msg : String) : HLet = {
     log("ERROR: " + msg)
@@ -31,7 +32,7 @@ class WebEditServerPlugin extends ServerExtension("editing") with Logger {
     try {
       uriComps match {
         case "autocomplete" :: _ => getAutocompleteResponse
-        case "resolve" :: _ => getResolveResponse
+        case "resolve" :: _ => getResolveIncludesResponse
         case "minIncludes" :: _ => getMinIncludes
         case "symbolCompletion" :: _ => getSymbolCompletion
         case "termInference" :: _ => getTermInference
@@ -46,28 +47,19 @@ class WebEditServerPlugin extends ServerExtension("editing") with Logger {
     }
   }
   
-  
-  private def getResolveResponse : HLet = new HLet {
+   private def getResolveIncludesResponse : HLet = new HLet {
     def aact(tk : HTalk)(implicit ec : ExecutionContext) : Future[Unit] = try {
       val reqBody = new Body(tk)
       val params = reqBody.asJSON.obj
       
       val symbol = params.get("symbol").getOrElse(throw ServerError("No symbol found")).toString
       val mpathS = params.get("mpath").getOrElse(throw ServerError("No mpath found")).toString
-      val mpath = Path.parseM(mpathS, mmt.mmtbase)
       
-      val respMap = new collection.mutable.HashMap[String, Any]()
-      Names.resolveIncludes(OMMOD(mpath),symbol)(controller.library) match {
-        case None => respMap("found") = true
-        case Some(myPaths) => 
-          respMap("found") = false
-          respMap("options") = new JSONArray(myPaths.map(_.from.toPath))
-      }
-      Server.JsonResponse(new JSONObject(respMap.toMap)).aact(tk)
+      val response = editingService.getResolveIncludesResponse(new MMTResolveIncludesRequest(symbol,mpathS))
+      Server.JsonResponse(new JSONArray(response.getResponse())).aact(tk)
     }
   }
   
-   
   private def getCoverCardinality(includes: List[MPath], theory : MPath)=  {
       val thy = controller.get(theory) match {
       case theor: DeclaredTheory => theor
@@ -95,37 +87,15 @@ class WebEditServerPlugin extends ServerExtension("editing") with Logger {
     if(removed == Nil) k:::CoverTheories else minInclLevel(removed,availTheories,k:::CoverTheories)  
   }
   
-  
-  private def minIncl(to: MPath) : List[MPath] = {
-    val thy = controller.get(to) match {
-      case theor: DeclaredTheory => theor
-      case _ => throw ServerError("No theory found")
-    }
-    val incl = thy.getIncludes
-    //thy.components.length 
-    def remover(checked: List[MPath], rest : List[MPath]) : List[MPath] = rest match{
-      case Nil => checked
-      case (hd::tl) => 
-        if(tl.exists(x=>controller.globalLookup.visible(OMMOD(x)) .contains(OMMOD(hd))) ||
-            checked.exists(x=>controller.globalLookup.visible(OMMOD(x)) .contains(OMMOD(hd))))
-        	remover(checked,tl)
-    	else remover(hd::checked,tl)	
-    }
-    
-   remover(Nil,incl)
-  }
-  
- 
   private def getMinIncludes : HLet = new HLet {
     def aact(tk : HTalk)(implicit ec : ExecutionContext) : Future[Unit] = try {
       val reqBody = new Body(tk)
       val params = reqBody.asJSON.obj
       
-      val mpathS = params.get("mpath").getOrElse(throw ServerError("No mpath found")).toString
-      val mpath = Path.parseM(mpathS, mmt.mmtbase)		
+      val mpathS = params.get("mpath").getOrElse(throw ServerError("No mpath found")).toString		
     
-      val newIncludes = minIncl(mpath)
-      val response = new JSONArray(newIncludes.map(_.toPath))
+      val newIncludes = editingService.getMinIncludes(new MMTMinIncludesRequest(mpathS))
+      val response = new JSONArray(newIncludes.getResponse())
       Server.JsonResponse(response).aact(tk)
     }
   }
@@ -137,57 +107,11 @@ class WebEditServerPlugin extends ServerExtension("editing") with Logger {
       
       val prefix = params.get("prefix").getOrElse(throw ServerError("No prefix found")).toString
       val mpathS = params.get("mpath").getOrElse(throw ServerError("No mpath found")).toString
-      val mpath = Path.parseM(mpathS, mmt.mmtbase)
       
-      val myPaths = Names.resolve(OMMOD(mpath), Nil, prefix)(controller.globalLookup)
-      val response = new JSONArray(myPaths.map(_.path.toPath))
+      val myPaths = editingService.getAutocompleteResponse(new MMTAutoCompleteRequest(prefix,mpathS))
+      val response = new JSONArray(myPaths.getResponse())
       Server.JsonResponse(response).aact(tk)
     }
-  }
-  
-  private def getTermInference : HLet = new HLet {
-    def aact(tk : HTalk)(implicit ec : ExecutionContext) : Future[Unit] = try {
-      val reqBody = new Body(tk)
-      val params = reqBody.asJSON.obj
-      
-      val termS = params.get("term").getOrElse(throw ServerError("No type found")).toString
-      val mpathS = params.get("mpath").getOrElse(throw ServerError("No mpath found")).toString
-          
-      val mpath = Path.parseM(mpathS, mmt.mmtbase)
-      val sref = new SourceRef(mpath.doc.uri, SourceRegion(SourcePosition(-1,0,0),SourcePosition(-1,0,1)))
-      
-      val term = controller.textParser(ParsingUnit(sref, Context(), termS))(new ErrorLogger(report))
-      
-      def getHoles(term: Term , context : Context) : List[(Term,Context)] = {
-        term match {
-          case Hole(t) => (t,context)::Nil
-          case OMBINDC(nterm,ncontext,nbodyList)=> (nterm::nbodyList).flatMap(getHoles(_,context++ncontext))
-          case OMBIND(nterm,ncontext,nbody) => getHoles(nbody, context++ncontext)
-          case OMA(f, args) => (f :: args).flatMap(getHoles(_, context))
-          case _ => Nil
-        }
-      }
-      val returnNoHoles = "Term Complete"
-      val holeContextList =  getHoles(term,Context())
-      
-      //response Term Complete if there are no Holes, No Rules if no rule is applicable otherwise return the rules
-      holeContextList match { 
-        case Nil => val returnNoHoles = "Term Complete" :: Nil
-        			Server.JsonResponse(new JSONArray(returnNoHoles)).aact(tk)
-        case _ =>  
-          val hole = holeContextList.head._1
-          val context = holeContextList.head._2
-          val prover = new Prover(controller) 
-          val rules = prover.applicable(hole) (Stack(OMMOD(mpath),context))
-          rules match{ 
-        			case Nil => val returnNoRules = "No Rules" :: Nil
-        						Server.JsonResponse(new JSONArray(returnNoRules)).aact(tk)
-        			case _ => val response = new JSONArray(rules.map(_.label.toString))
-        						Server.JsonResponse(response).aact(tk)
-          }
-      }
-    }
-    
   }
   
   private def getSymbolCompletion : HLet = new HLet {
@@ -197,35 +121,9 @@ class WebEditServerPlugin extends ServerExtension("editing") with Logger {
       
       val prefix = params.get("prefix").getOrElse(throw ServerError("No prefix found")).toString
       val mpathS = params.get("mpath").getOrElse(throw ServerError("No mpath found")).toString
-      val mpath = Path.parseM(mpathS, mmt.mmtbase)
       
-      val myPaths = Names.resolve(OMMOD(mpath), Nil, prefix)(controller.globalLookup)
-      val paths=(myPaths.map(_.path)) 
+      val stringResponse = editingService.getSymbolCompletion(prefix)
       
-      val constants = paths.map(controller.get) collect {
-       case c : Constant => c.not match {
-         case None => c.name -> Nil 
-         case Some(a) => c.name -> a.fixity.markers
-       }
-      }
-      
-      def modifyStringRepresent (name:LocalName,markers:List[Marker],accumulator:String,hasSymbol:Boolean) : (String) = {
-		val separator = " " 
-         markers match {
-		  case Nil => if(hasSymbol) accumulator 
-				  	  else name.toString + accumulator
-		  case hd::b  => if(hd.toString=="%n"){ 
-			  				modifyStringRepresent(name,b,accumulator,false)
-			  				}
-						else if(hd.toString.length()>1 && hd.toString.substring(0,2)=="%I") 
-							modifyStringRepresent(name,b,accumulator,hasSymbol) 
-		  				else 
-		  				    modifyStringRepresent(name,b,accumulator+ separator +hd.toString,hasSymbol)
-		}
-		
-      }
-
-      val stringResponse = constants.map({case (x,y)=>modifyStringRepresent(x,y,"",true)})
       val response = new JSONArray(stringResponse)
       Server.JsonResponse(response).aact(tk)
     }
@@ -262,9 +160,22 @@ class WebEditServerPlugin extends ServerExtension("editing") with Logger {
 	 	case List(_,_,StringLiteral(b),_) => LocalName(b)
 	 }
 	 val includesDeclarations = decl.toSet
-	
 	 includesDeclarations.intersect(usedDeclarationsNames).size
 	
  }
-  
+ 
+ private def getTermInference : HLet = new HLet {
+    def aact(tk : HTalk)(implicit ec : ExecutionContext) : Future[Unit] = try {
+      val reqBody = new Body(tk)
+      val params = reqBody.asJSON.obj
+      
+      val termS = params.get("term").getOrElse(throw ServerError("No type found")).toString
+      val mpathS = params.get("mpath").getOrElse(throw ServerError("No mpath found")).toString     
+      
+      val resp = editingService.getTermInference(new MMTTermInferenceRequest(termS,mpathS))
+      Server.JsonResponse(new JSONArray(resp.getResponse())).aact(tk)
+    }   
+ }
+ 
+ 
 }
