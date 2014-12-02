dc763be2a5c88f9329400229399d3e94af9d15e7
frabe <frabe@09069c07-60fa-4895-8316-727242e252e4>
2014-12-02 20:19:03 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@2277 09069c07-60fa-4895-8316-727242e252e4

--- deploy/mmt.bat
+++ deploy/mmt.bat
@@ -1,3 +1,4 @@
+@echo off
 rem This is the main extry point for running MMT on Windows.
 rem Executing this script opens an interactive MMT shell.
 rem MMT is "empty" by default, and the first thing to do is usually to issue a command like "file startup.msl" to load some configuration.
