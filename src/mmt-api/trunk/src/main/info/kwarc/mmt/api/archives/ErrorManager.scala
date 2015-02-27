24a7e9f6f7da6b463928cd72b45a1fc1b8632613
cmaeder <cmaeder@09069c07-60fa-4895-8316-727242e252e4>
2015-02-27 10:52:31 +0000

    reformatted code automatically
    (just indented by two spaces)
    
    git-svn-id: https://svn.kwarc.info/repos/MMT@2371 09069c07-60fa-4895-8316-727242e252e4

--- src/mmt-api/trunk/src/main/info/kwarc/mmt/api/archives/ErrorManager.scala
+++ src/mmt-api/trunk/src/main/info/kwarc/mmt/api/archives/ErrorManager.scala
@@ -32,99 +32,100 @@ class ErrorManager extends Extension with Logger {
   /** the mutable data: one ErrorMap per open Archive */
   private var errorMaps: List[ErrorMap] = Nil
 
-   /**
-    * @param id the archive
-    * @return the [[ErrorMap]] of the archive
-    */
-   def apply(id: String): ErrorMap =
-      errorMaps.find(_.archive.id == id).getOrElse(throw GeneralError("archive does not exist: " + id))
+  /**
+   * @param id the archive
+   * @return the [[ErrorMap]] of the archive
+   */
+  def apply(id: String): ErrorMap =
+    errorMaps.find(_.archive.id == id).getOrElse(throw GeneralError("archive does not exist: " + id))
 
-   /**
-    * @param a the archive
-    * load all errors of this archive
-    */
-   def loadAllErrors(a: Archive) {
-     a.traverse(errors, Nil, _ => true, parallel = false) { case Current(_, target :: path) =>
-         loadErrors(a, target, path)
+  /**
+   * @param a the archive
+   *          load all errors of this archive
+   */
+  def loadAllErrors(a: Archive) {
+    a.traverse(errors, Nil, _ => true, parallel = false) { case Current(_, target :: path) =>
+      loadErrors(a, target, path)
+    }
+  }
+
+  /**
+   * @param a the archive
+   * @param target the build target
+   * @param path the file
+   *             load all errors of a build target applied to a file
+   */
+  def loadErrors(a: Archive, target: String, path: List[String]) {
+    val f = a / errors / target / path
+    val node = xml.readFile(f)
+    var bes: List[BuildError] = Nil
+    node.child.foreach { x =>
+      val (stacks, others) = x.child partition (_.label == "stacktrace")
+      def getAttrs(attrs: List[String], x: Node): List[String] = {
+        val as = x.attributes
+        attrs map (a => as.get(a).getOrElse("").toString)
       }
-   }
+      val List(tgt, srcRef, errType, shortMsg, level) = getAttrs(List("target", "sref", "type", "shortMsg", "level"), x)
+      def infoMessage(msg: String) =
+        log(msg + "\nFile: " + f + "\nNode: " + shortMsg)
+      var lvl: Level.Level = Level.Error
+      if (level.isEmpty) infoMessage("empty error level")
+      else
+        try {
+          lvl = Level.parse(level)
+        } catch {
+          case e: ParseError => infoMessage(e.getMessage)
+        }
+      val trace: List[List[String]] = stacks.toList map (e => e.child.toList map (_.text))
+      val longMsg: String = (others map (_.text)).mkString
+      val elems = others filter (_.isInstanceOf[Elem])
+      if (elems.nonEmpty)
+        infoMessage("ignored sub-elements")
+      val be = BuildError(a.id, target, path, errType, lvl, SourceRef.fromURI(URI(srcRef)), shortMsg, longMsg, trace)
+      bes ::= be
+    }
+    val em = apply(a.id)
+    em((target, path)) = bes.reverse
+  }
 
-   /**
-    * @param a the archive
-    * @param target the build target
-    * @param path the file
-    * load all errors of a build target applied to a file
-    */
-   def loadErrors(a: Archive, target: String, path: List[String]) {
-     val f = a / errors / target / path
-     val node = xml.readFile(f)
-     var bes: List[BuildError] = Nil
-     node.child.foreach { x =>
-       val (stacks, others) = x.child partition (_.label == "stacktrace")
-       def getAttrs(attrs: List[String], x: Node): List[String] = {
-         val as = x.attributes
-         attrs map (a => as.get(a).getOrElse("").toString)
-       }
-       val List(tgt, srcRef, errType, shortMsg, level) = getAttrs(List("target", "sref", "type", "shortMsg", "level"), x)
-       def infoMessage(msg: String) =
-         log(msg + "\nFile: " + f + "\nNode: " + shortMsg)
-       var lvl: Level.Level = Level.Error
-       if (level.isEmpty) infoMessage("empty error level")
-       else
-         try {
-           lvl = Level.parse(level)
-         } catch {
-           case e: ParseError => infoMessage(e.getMessage)
-         }
-       val trace: List[List[String]] = stacks.toList map (e => e.child.toList map (_.text))
-       val longMsg: String = (others map (_.text)).mkString
-       val elems = others filter (_.isInstanceOf[Elem])
-       if (elems.nonEmpty)
-         infoMessage("ignored sub-elements")
-       val be = BuildError(a.id, target, path, errType, lvl, SourceRef.fromURI(URI(srcRef)), shortMsg, longMsg, trace)
-       bes ::= be
-     }
-     val em = apply(a.id)
-     em((target, path)) = bes.reverse
-   }
+  /** iterator over all errors given as (archive, target, path, error) */
+  def iterator: Iterator[BuildError] = {
+    errorMaps.iterator.flatMap { em =>
+      em.values.iterator.flatten
+    }
+  }
 
-   /** iterator over all errors given as (archive, target, path, error) */
-   def iterator: Iterator[BuildError] = {
-      errorMaps.iterator.flatMap {em =>
-         em.values.iterator.flatten
+  /** registers a [[ChangeListener]] and a [[ServerExtension]] */
+  override def start(args: List[String]) {
+    controller.extman.addExtension(cl)
+    controller.extman.addExtension(serve)
+  }
+
+  /** adds/deletes [[ErrorMap]]s for each opened/closed [[Archive]] */
+  private val cl = new ChangeListener {
+    /** creates an [[ErrorMap]] for the archive and asynchronously loads its errors */
+    override def onArchiveOpen(a: Archive) {
+      errorMaps ::= new ErrorMap(a)
+      Future {
+        loadAllErrors(a)
       }
-   }
+    }
 
-   /** registers a [[ChangeListener]] and a [[ServerExtension]] */
-   override def start(args: List[String]) {
-      controller.extman.addExtension(cl)
-      controller.extman.addExtension(serve)
-   }
+    /** deletes the [[ErrorMap]] */
+    override def onArchiveClose(a: Archive) {
+      errorMaps = errorMaps.filter(_.archive != a)
+    }
+  }
 
-   /** adds/deletes [[ErrorMap]]s for each opened/closed [[Archive]] */
-   private val cl = new ChangeListener {
-      /** creates an [[ErrorMap]] for the archive and asynchronously loads its errors */
-      override def onArchiveOpen(a: Archive) {
-         errorMaps ::= new ErrorMap(a)
-         Future {
-            loadAllErrors(a)
-         }
+  /** serves lists of [[Error]]s */
+  private val serve = new ServerExtension("errors") {
+    def apply(path: List[String], query: String, body: Body) = {
+      val wq = WebQuery.parse(query)
+      val result = iterator.filter { be =>
+        true // TODO select BuildErrors according to query
       }
-      /** deletes the [[ErrorMap]] */
-      override def onArchiveClose(a: Archive) {
-         errorMaps = errorMaps.filter(_.archive != a)
-      }
-   }
-
-   /** serves lists of [[Error]]s */
-   private val serve = new ServerExtension("errors") {
-      def apply(path: List[String], query: String, body: Body) = {
-         val wq = WebQuery.parse(query)
-         val result = iterator.filter {be =>
-            true // TODO select BuildErrors according to query
-         }
-         val json = JSONArray(result.toList.map(_.toJSON) :_*)
-         Server.JsonResponse(json)
-      }
-   }
+      val json = JSONArray(result.toList.map(_.toJSON): _*)
+      Server.JsonResponse(json)
+    }
+  }
 }
