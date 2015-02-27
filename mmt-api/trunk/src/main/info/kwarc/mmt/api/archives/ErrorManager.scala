e4c4d8c0404c722a497fac4bd0f3575262457361
cmaeder <c.maeder@jacobs-university.de>
2015-02-27 18:08:48 +0000

    added grouping and corrected searching
    a "+" in group strings makes problems
    
    git-svn-id: https://svn.kwarc.info/repos/MMT/src@2383 09069c07-60fa-4895-8316-727242e252e4

--- mmt-api/trunk/src/main/info/kwarc/mmt/api/archives/ErrorManager.scala
+++ mmt-api/trunk/src/main/info/kwarc/mmt/api/archives/ErrorManager.scala
@@ -165,15 +165,23 @@ class ErrorManager extends Extension with Logger {
       val bes = iterator.map(_.toStrList)
       val result = bes.filter { be =>
         assert(args.length == be.length)
-        args.zip(be).forall { case (a, b) => b.contains(a)}
+        args.zip(be).forall { case (a, b) => b.indexOf(a) > -1}
       }
       var json: JSON = JSONNull
       log("PATH:" + path)
       path match {
         case List("count2") => json = JSONArray(JSONObject("count" -> JSONInt(result.length)))
+        case List("group", field) =>
+          val idx = Table.columns.indexOf(field)
+          assert (idx >= 0) 
+          val l: List[String] = result.map(be => be(idx)).toList.sorted
+          val g = l.groupBy(identity).mapValues(_.length).toList.sortBy(_._2).reverse
+          json = JSONArray(g.take(limit).map { case (s, i) =>
+              JSONObject("count" -> JSONInt(i), "content" -> JSONString(s))
+          }: _*)
         case _ =>
           json = JSONArray(result.toList.take(limit).map(l =>
-            JSONObject(Table.columns.zip(l.map(s => JSONString(s))): _*)): _*)
+            JSONObject(Table.columns.zip(l.map(JSONString(_))): _*)): _*)
       }
       Server.JsonResponse(json)
     }
