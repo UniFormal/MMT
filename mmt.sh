a20150bed87406bcb3deed5e2e574c862c80470d
cmaeder <c.maeder@jacobs-university.de>
2015-03-09 15:17:46 +0000

    allow to execute scala-mmt
    git-svn-id: https://svn.kwarc.info/repos/MMT/src@2415 09069c07-60fa-4895-8316-727242e252e4

--- mmt.sh
+++ mmt.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 dir=`dirname $0`
 mvn -f $dir/pom.xml install
-mvn -f $dir/pom.xml -pl mmt-tptp exec:java -Dexec.mainClass=info.kwarc.mmt.api.frontend.Run -Dexec.args="$*"
+mvn -f $dir/pom.xml -pl mmt-tptp exec:exec \
+  -Dexec.workingdir=$PWD \
+  -Dexec.executable="java" \
+  -Dexec.args="-cp %classpath info.kwarc.mmt.api.frontend.Run $*"
