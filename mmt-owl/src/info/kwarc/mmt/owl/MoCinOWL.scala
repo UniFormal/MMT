eab08690dcd2f9638d2da0c41a8dc869ae06babe
cmaeder <c.maeder@jacobs-university.de>
2015-03-26 18:50:18 +0000

    reformatted and optimized imports
    git-svn-id: https://svn.kwarc.info/repos/MMT/src@2495 09069c07-60fa-4895-8316-727242e252e4

--- mmt-owl/src/info/kwarc/mmt/owl/MoCinOWL.scala
+++ mmt-owl/src/info/kwarc/mmt/owl/MoCinOWL.scala
@@ -1,61 +1,57 @@
 package info.kwarc.mmt.owl
-import java.io.File
+
 import info.kwarc.mmt.api._
-import utils.FileConversion._
 import info.kwarc.mmt.api.frontend._
-import info.kwarc.mmt.api.libraries._
 import info.kwarc.mmt.api.moc._
-import scala.xml._
-import org.semanticweb.owlapi.model._
-import org.semanticweb.owlapi.apibinding.OWLManager
+import info.kwarc.mmt.api.utils.FileConversion._
 import info.kwarc.mmt.api.utils.URI
 
+import scala.xml._
 
-  //take two owl files as inputs
-  //add identifiers to them
-  //translate them to mmt files
-  //apply mmt diff to the mmt files
-  //dependsOn
-  //propFunc
-  object MoCinOWL {
-	def main(args: Array[String]) {
-		val controllerOld = new Controller
-	    val controllerCurr = new Controller
 
-	    controllerOld.handle(ExecFile(new java.io.File("startup.mmt"), None))
-	    controllerCurr.handle(ExecFile(new java.io.File("startupCurrent.mmt"), None))
-	   	    
-	   //http://docs.omdoc.org/older.omdoc
-	    if(args.length < 1) 
-	    { println("USAGE: DocumentPath")  
-	      sys.exit
-	    }
-	    
-		val dpath : DPath = new DPath(URI(args(0)))
-		
-		//we need the older version in the source folder to be able to get dependency relations.
-	    //two versions have the same ontology IRI, so they should not be in the source folder together, because we cannot compare them
-	    //olderVersion has to be in the source folder
-	    //currentVersion should not be in the source folder; elsewhere
-	    
-	    //transates older and the current OWL documents to MMT 
-	    controllerOld.handleLine("file build-test.mmt")
-	    controllerCurr.handleLine("file build-testCurrent.mmt")
-          
-	      
-	    //read mmt documents
-	    //val olderDoc : DPath  = controllerOld.read()
-        //val currentDoc : DPath = controllerCurr.read()
-	    
-        //val mpath = new MPath(dpath,new LocalPath("_"))
-        var diff = Differ.diff(controllerOld, controllerCurr, dpath, dpath)
-        val pretty = new PrettyPrinter (150, 3) 
-        println(pretty.format(diff.toNode))
-        //propagate(diff)
-     
-        
-    
-        
-	}
-  
+//take two owl files as inputs
+//add identifiers to them
+//translate them to mmt files
+//apply mmt diff to the mmt files
+//dependsOn
+//propFunc
+object MoCinOWL {
+  def main(args: Array[String]) {
+    val controllerOld = new Controller
+    val controllerCurr = new Controller
+
+    controllerOld.handle(ExecFile(new java.io.File("startup.mmt"), None))
+    controllerCurr.handle(ExecFile(new java.io.File("startupCurrent.mmt"), None))
+
+    //http://docs.omdoc.org/older.omdoc
+    if (args.length < 1) {
+      println("USAGE: DocumentPath")
+      sys.exit
+    }
+
+    val dpath: DPath = new DPath(URI(args(0)))
+
+    //we need the older version in the source folder to be able to get dependency relations.
+    //two versions have the same ontology IRI, so they should not be in the source folder together, because we cannot compare them
+    //olderVersion has to be in the source folder
+    //currentVersion should not be in the source folder; elsewhere
+
+    //transates older and the current OWL documents to MMT
+    controllerOld.handleLine("file build-test.mmt")
+    controllerCurr.handleLine("file build-testCurrent.mmt")
+
+
+    //read mmt documents
+    //val olderDoc : DPath  = controllerOld.read()
+    //val currentDoc : DPath = controllerCurr.read()
+
+    //val mpath = new MPath(dpath,new LocalPath("_"))
+    var diff = Differ.diff(controllerOld, controllerCurr, dpath, dpath)
+    val pretty = new PrettyPrinter(150, 3)
+    println(pretty.format(diff.toNode))
+    //propagate(diff)
+
+
+  }
+
 }
--- mmt-owl/src/info/kwarc/mmt/owl/Utils.scala
+++ mmt-owl/src/info/kwarc/mmt/owl/Utils.scala
@@ -1,36 +1,45 @@
 package info.kwarc.mmt.owl
-import org.semanticweb.owlapi.model._
+
 import info.kwarc.mmt.api._
-import objects._
+import info.kwarc.mmt.api.objects._
+import org.semanticweb.owlapi.model._
 
 object Utils {
-	def IRILast(i: IRI) : String = {
-	   val uri = utils.URI.fromJava(i.toURI)
-	   uri.fragment match {
-	     case Some(s) => s
-         case None => uri.path.last
-	   }
-	}
+  def IRILast(i: IRI): String = {
+    val uri = utils.URI.fromJava(i.toURI)
+    uri.fragment match {
+      case Some(s) => s
+      case None => uri.path.last
+    }
+  }
 }
 
 object OWLOMS {
-  val path = new DPath(utils.URI("http", "latin.omdoc.org") / "logics" / "description" / "owl" / "owl.omdoc") 
-  def apply(m: String, n: String) = {  OMID(path ? m ? n) }
-  def unapply(t: Term) : Option[(String,String)] = { 
-	  t match {
-	  	case OMID(OMMOD((this.path ? !(SimpleStep(m)))) % !(SimpleStep(n))) => Some((m,n))
-	    case _ => None
-  	  }
+  val path = new DPath(utils.URI("http", "latin.omdoc.org") / "logics" / "description" / "owl" / "owl.omdoc")
+
+  def apply(m: String, n: String) = {
+    OMID(path ? m ? n)
+  }
+
+  def unapply(t: Term): Option[(String, String)] = {
+    t match {
+      case OMID(OMMOD((this.path ? !(SimpleStep(m)))) % !(SimpleStep(n))) => Some((m, n))
+      case _ => None
+    }
   }
 }
 
 object OWL2OMS {
-   val path = new DPath(utils.URI("http", "latin.omdoc.org") / "logics" / "description" / "owl" / "owl2.omdoc") 
-   def apply(m: String, n: String) = {  OMID(path ? m ? n) }
-   def unapply(t: Term) : Option[(String,String)] = { 
-	   t match {
-	     case OMID(OMMOD((this.path ? !(SimpleStep(m)))) % !(SimpleStep(n))) => Some((m,n))
-	     case _ => None
-	   }
-   }
+  val path = new DPath(utils.URI("http", "latin.omdoc.org") / "logics" / "description" / "owl" / "owl2.omdoc")
+
+  def apply(m: String, n: String) = {
+    OMID(path ? m ? n)
+  }
+
+  def unapply(t: Term): Option[(String, String)] = {
+    t match {
+      case OMID(OMMOD((this.path ? !(SimpleStep(m)))) % !(SimpleStep(n))) => Some((m, n))
+      case _ => None
+    }
+  }
 }
