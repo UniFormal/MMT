f41a50754594d8e53b333afec769d2e072cf5f85
cmaeder <cmaeder@09069c07-60fa-4895-8316-727242e252e4>
2015-03-31 14:05:46 +0000

    delete elf files that caused errors and avoid existing files being extended by namespace prefixes
    git-svn-id: https://svn.kwarc.info/repos/MMT@2533 09069c07-60fa-4895-8316-727242e252e4

--- src/mmt-tptp/src/info/kwarc/mmt/tptp/TPTPImporter.scala
+++ src/mmt-tptp/src/info/kwarc/mmt/tptp/TPTPImporter.scala
@@ -4,69 +4,72 @@ import info.kwarc.mmt.api.archives._
 import info.kwarc.mmt.api.utils._
 
 class TPTPImporter extends TraversingBuildTarget {
-   val key: String = "tptp-twelf"
-   val inDim = source
-   val outDim = Dim("twelf")
-   def includeFile(name: String) =
-      name.endsWith(".ax") || name.endsWith(".p") || name.endsWith(".tptp")
+  val key: String = "tptp-twelf"
+  val inDim = source
+  val outDim = Dim("twelf")
 
-   /** command to run TPTP */
-   private var tptpCommand: List[String] = null
+  def includeFile(name: String) =
+    name.endsWith(".ax") || name.endsWith(".p") || name.endsWith(".tptp")
 
-   /**
-    * expects command to run TPTP as arguments, e.g.,
-    *
-    * "C:\Program Files\swipl\bin\swipl.exe" -f C:/other/oaff/TPTP/Distribution/TPTP2X/tptp2x.main -g tptp2X('INFILE',none,lf,'OUTDIR'),halt.
-    */
-   override def start(args: List[String]) {
-      if (args.isEmpty)
-         throw LocalError("expected TPTP command")
-      tptpCommand = args
-   }
+  /** command to run TPTP */
+  private var tptpCommand: List[String] = null
 
-   private def escape(f: File) = f.toString.replace("\\","/")
+  /**
+   * expects command to run TPTP as arguments, e.g.,
+   *
+   * "C:\Program Files\swipl\bin\swipl.exe" -f C:/other/oaff/TPTP/Distribution/TPTP2X/tptp2x.main -g tptp2X('INFILE',none,lf,'OUTDIR'),halt.
+   */
+  override def start(args: List[String]) {
+    if (args.isEmpty)
+      throw LocalError("expected TPTP command")
+    tptpCommand = args
+  }
 
-   def buildFile(a: Archive, bt: BuildTask) {
-      /*if (bt.inFile.toJava.length > 1000000) {
-         bt.errorCont(LocalError("skipped big file: " + bt.inFile))
-         return
-      }*/
-      try {
-         val outFile = bt.outFile.setExtension("elf")
-         val command = tptpCommand.map { s =>
-            s.replace("INFILE", escape(bt.inFile)).replace("OUTDIR", escape(outFile.up))
-         }
-         log(command.mkString(" "))
-         val result = ShellCommand.run(command: _*)
-         result foreach { s =>
-            bt.errorCont(LocalError(s))
-            return
-         }
-         // outFile wraps module header/footer around non-modular tempFile
-         val tempFile = outFile.setExtension("temp")
-         outFile.renameTo(tempFile)
-         val outWriter = File.Writer(outFile)
-         val problemName = bt.inFile.removeExtension.name.replace(".", "_") 
-         val prefix = s"""%namespace "${a.narrationBase}".
+  private def escape(f: File) = f.toString.replace("\\", "/")
+
+  def buildFile(a: Archive, bt: BuildTask) {
+    /*if (bt.inFile.toJava.length > 1000000) {
+       bt.errorCont(LocalError("skipped big file: " + bt.inFile))
+       return
+    }*/
+    val outFile = bt.outFile.setExtension("elf")
+    outFile.delete()
+    val command = tptpCommand.map { s =>
+      s.replace("INFILE", escape(bt.inFile)).replace("OUTDIR", escape(outFile.up))
+    }
+    log(command.mkString(" "))
+    try {
+      val result = ShellCommand.run(command: _*)
+      result foreach { s =>
+        bt.errorCont(LocalError(s))
+        return
+      }
+      // outFile wraps module header/footer around non-modular tempFile
+      val tempFile = outFile.setExtension("temp")
+      outFile.renameTo(tempFile)
+      val outWriter = File.Writer(outFile)
+      val problemName = bt.inFile.removeExtension.name.replace(".", "_")
+      val prefix = s"""%namespace "${a.narrationBase}".
 %namespace tptp = "http://latin.omdoc.org/logics/tptp".
 
 %sig $problemName = {
    %meta tptp.THF.
 """
-         outWriter.write(prefix)
-         File.ReadLineWise(tempFile) { l =>
-            outWriter.println(l)
-         }
-         outWriter.println("}.")
-         outWriter.close
-         tempFile.delete
-      } catch {
-         case e: Throwable =>
-            val msg = e.getMessage match {
-               case null => "(no message)"
-               case m => m
-            }
-            bt.errorCont(LocalError("exception for file: " + bt.inFile + "\n" + msg).setCausedBy(e))
-         }   
-   }
+      outWriter.write(prefix)
+      File.ReadLineWise(tempFile) { l =>
+        outWriter.println(l)
+      }
+      outWriter.println("}.")
+      outWriter.close
+      tempFile.delete
+    } catch {
+      case e: Throwable =>
+        val msg = e.getMessage match {
+          case null => "(no message)"
+          case m => m
+        }
+        outFile.delete()
+        bt.errorCont(LocalError("exception for file: " + bt.inFile + "\n" + msg).setCausedBy(e))
+    }
+  }
 }
