79e88335b87930090847e870c019756082480fa1
cmaeder <cmaeder@09069c07-60fa-4895-8316-727242e252e4>
2015-04-14 17:29:43 +0000

    a first build action
    git-svn-id: https://svn.kwarc.info/repos/MMT@2586 09069c07-60fa-4895-8316-727242e252e4

---
+++ src/stex-mmt/src/info/kwarc/mmt/stex/STeX.scala
@@ -0,0 +1,41 @@
+package info.kwarc.mmt.stex
+
+import info.kwarc.mmt.api._
+import info.kwarc.mmt.api.archives._
+import info.kwarc.mmt.api.utils.{File, ShellCommand}
+
+/** importer wrapper for stex
+  */
+class STeX extends Importer with frontend.ChangeListener {
+  override val key = "stex-omdoc"
+
+  private var lmlPath: File = File("run-latexml.sh")
+
+  def includeFile(n: String): Boolean = n.endsWith(".tex") && n != "localpaths.tex"
+
+  /**
+   * Compile a .tex file to OMDoc
+   */
+  def importDocument(bt: BuildTask, seCont: documents.Document => Unit) {
+    val command: List[String] = List(lmlPath, bt.inFile) map (_.toString)
+    log(command.mkString(" "))
+    bt.outFile.delete()
+    try {
+      val result = ShellCommand.run(command: _*)
+      result foreach { s =>
+        bt.errorCont(LocalError(s))
+        return
+      }
+      val logFile = bt.outFile.setExtension("ltxlog")
+    }
+    catch {
+      case e: Throwable =>
+        val msg = e.getMessage match {
+          case null => "(no message)"
+          case m => m
+        }
+        bt.outFile.delete()
+        bt.errorCont(LocalError("exception for file: " + bt.inFile + "\n" + msg).setCausedBy(e))
+    }
+  }
+}
