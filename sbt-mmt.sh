db6e78ece4d8bb32c7233682d85c401d87769e92
cmaeder <c.maeder@jacobs-university.de>
2015-05-08 17:26:57 +0000

    execute mmt via sbt
    git-svn-id: https://svn.kwarc.info/repos/MMT/src@2682 09069c07-60fa-4895-8316-727242e252e4

---
+++ sbt-mmt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+script="$(readlink -f $0)"
+dir="$(dirname $script)"
+
+# relative filename arguments will not work
+# and also spaces in filenames are not supported.
+cd $dir
+exec sbt "mmt/run $*"
