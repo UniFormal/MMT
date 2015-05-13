ff68238a23f870fd6d007ee3d4b218012aac6685
cmaeder <c.maeder@jacobs-university.de>
2015-05-13 07:52:27 +0000

    re-use a non-private ivy repository
    git-svn-id: https://svn.kwarc.info/repos/MMT/src@2699 09069c07-60fa-4895-8316-727242e252e4

--- sbt-mmt.sh
+++ sbt-mmt.sh
@@ -5,4 +5,4 @@ dir="$(dirname $script)"
 # relative filename arguments will not work
 # and also spaces in filenames are not supported.
 cd $dir
-exec sbt "mmt/run $*"
+exec sbt -ivy /tmp/.ivy2 "mmt/run $*"
