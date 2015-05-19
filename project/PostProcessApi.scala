a097597f3b2af17196490e463c8e303483599c86
cmaeder <c.maeder@jacobs-university.de>
2015-05-19 16:28:51 +0000

    avoid test error in mmt-api added cleandoc and apidoc tasks
    git-svn-id: https://svn.kwarc.info/repos/MMT/src@2717 09069c07-60fa-4895-8316-727242e252e4

--- project/PostProcessApi.scala
+++ project/PostProcessApi.scala
@@ -3,11 +3,19 @@ import java.io._
 import sbt.Logger
 
 object PostProcessApi {
+  def delRecursive(path: File) {
+    path.listFiles foreach { f =>
+      if (f.isDirectory) delRecursive(f)
+      else f.delete()
+    }
+    path.delete()
+  }
+
   def postProcess(log: Logger) = {
     val mmtFolder = new File(System.getProperty("user.dir")).getParent
     val oldPrefix = "file:/" + mmtFolder
     def doFolder(path: File, newPrefix: String) {
-      log.info("processing: " + path)
+      log.debug("processing: " + path)
       path.list foreach { e =>
         if (!e.startsWith(".")) {
           val f = new File(path + "/" + e)
@@ -17,10 +25,10 @@ object PostProcessApi {
             val s = io.Source.fromFile(f).getLines().mkString("", "\n", "\n")
             if (s.indexOf(oldPrefix) >= 0) {
               val w = new BufferedWriter(new FileWriter(f))
-              log.info("rewrote: " + f)
+              log.debug("rewrote: " + f)
               w.write(s.replace(oldPrefix, newPrefix))
             } else
-              log.info("skipping: " + f)
+              log.debug("skipping: " + f)
           }
         }
       }
