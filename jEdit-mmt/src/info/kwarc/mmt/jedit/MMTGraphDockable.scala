35a3d4548f594bc73e385507e13e482a838d883f
Jazzpirate <d.mueller@jacobs-university.de>
2015-06-02 15:31:12 +0000

    Okay, NOW it should compile!
    git-svn-id: https://svn.kwarc.info/repos/MMT/src@2775 09069c07-60fa-4895-8316-727242e252e4

--- jEdit-mmt/src/info/kwarc/mmt/jedit/MMTGraphDockable.scala
+++ jEdit-mmt/src/info/kwarc/mmt/jedit/MMTGraphDockable.scala
@@ -4,7 +4,7 @@ import java.awt.BorderLayout
 import java.awt.event.{ActionEvent, ActionListener}
 import javax.swing._
 
-import info.kwarc.mmt.api.gui.{GraphPanel, FXPanel}
+import info.kwarc.mmt.api.gui.{GraphPanel}
 import info.kwarc.mmt.api.modules.{DeclaredView, DeclaredTheory}
 import info.kwarc.mmt.api.ontology._
 import info.kwarc.mmt.api.utils.{ShellCommand, File}
