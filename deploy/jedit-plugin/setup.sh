dfc0595f0282e0226bdf00cbb0bfeafa3fa1e328
frabe <frabe@09069c07-60fa-4895-8316-727242e252e4>
2015-06-04 12:38:11 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT@2793 09069c07-60fa-4895-8316-727242e252e4

--- deploy/jedit-plugin/setup.sh
+++ deploy/jedit-plugin/setup.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 dir=`dirname $0`/plugin/jars
 java -cp $dir/scala-library.jar:$dir/mmt-api.jar:$dir/MMTPlugin.jar info.kwarc.mmt.jeditsetup.Setup "$@"
-
-plug="$2/plugins/info.kwarc.mmt.jedit.MMTPlugin" 
-mar="${plug}/mars/urtheories.mar"
-
-if [ "$1" == "install" ]
-then
-  echo "extracting ${mar}"
-  unzip -d "${plug}/mars" "${mar}"
-else
-  echo "deleting ${plug}/"
-  rm -rf "${plug}"
-fi
