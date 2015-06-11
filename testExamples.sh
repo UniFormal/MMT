5a3f9dc9685a598b0f7c33df7c732ddb58869a1b
cmaeder <cmaeder@09069c07-60fa-4895-8316-727242e252e4>
2015-06-11 14:07:39 +0000

    clear export and use mmt.jar without move
    git-svn-id: https://svn.kwarc.info/repos/MMT@2816 09069c07-60fa-4895-8316-727242e252e4

--- testExamples.sh
+++ testExamples.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 git clone http://gl.mathhub.info/MMT/urtheories.git
 git clone http://gl.mathhub.info/MMT/examples.git
-for i in urtheories examples; do rm -rf $i/{content,narration,relational}; done
+for i in urtheories examples; do rm -rf $i/{export,content,narration,relational}; done
 for i in `seq 1 3`
 do
-   deploy/mmt file urtheories/build.msl build
-   deploy/mmt file examples/build.msl build
+   deploy/mmt.jar file urtheories/build.msl build
+   deploy/mmt.jar file examples/build.msl build
 done
-mv deploy/mmt deploy/mmt.jar
 cd examples
 ../deploy/mmt.jar file build.msl export
 sbt compile
