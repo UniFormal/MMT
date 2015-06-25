fb97d2a442a0966bf34294bf59260962d45ab67f
cmaeder <cmaeder@09069c07-60fa-4895-8316-727242e252e4>
2015-06-25 12:35:40 +0000

    switched to new traversing method and cleaned up
    (scala code is no longer traversed in parallel)
    
    git-svn-id: https://svn.kwarc.info/repos/MMT@2874 09069c07-60fa-4895-8316-727242e252e4

--- src/mmt-api/trunk/src/main/info/kwarc/mmt/api/archives/ScalaCode.scala
+++ src/mmt-api/trunk/src/main/info/kwarc/mmt/api/archives/ScalaCode.scala
@@ -1,41 +1,44 @@
 package info.kwarc.mmt.api.archives
+
 import info.kwarc.mmt.api._
-import frontend._
-import backend._
-import utils._
+import info.kwarc.mmt.api.frontend._
+import info.kwarc.mmt.api.utils._
 
 trait ScalaArchive extends WritableArchive {
-  
-    private var loader: java.net.URLClassLoader = null
-    def loadJava(controller: Controller, cls: String) {
-       if (loader == null) {
-          val jar = File(root / (id+".jar"))
-          try {
-            loader = new java.net.URLClassLoader(Array(jar.toURI.toURL))
-          } catch {
-            case _:Exception => 
-             log("could not create class loader for " + jar.toString)
-             return
-          }
-       }
-       // scala appends $ to classes generated for objects
-       // and uses "MODULE$" as the name of the companion object
-       val clsJ = loader.loadClass(cls + ".NAMESPACE$")
-       //val clsJ = java.lang.Class.forName(cls + ".NAMESPACE$")
-       val ns = clsJ.getField("MODULE$").get(clsJ).asInstanceOf[uom.DocumentScala]
-       // run tests
-       ns.test(controller, (s: String) => log(s))
+
+  private var loader: java.net.URLClassLoader = null
+
+  def loadJava(controller: Controller, cls: String) {
+    if (loader == null) {
+      val jar = File(root / (id + ".jar"))
+      try {
+        loader = new java.net.URLClassLoader(Array(jar.toURI.toURL))
+      } catch {
+        case _: Exception =>
+          log("could not create class loader for " + jar.toString)
+          return
+      }
     }
-  
-    /** Integrate scala into a dimension */
-    def integrateScala(controller: Controller, in : List[String] = Nil) {
-       traverse(content, in, Archive.extensionIs("omdoc"), true) {case Current(inFile, inPath) =>
-              val mpath = Archive.ContentPathToMMTPath(inPath)
-              val mod = controller.globalLookup.getModule(mpath)
-              val scalaFile = (root / "scala" / inPath).setExtension("scala")
-              uom.Integrator.doModule(controller, mod, scalaFile)
-              val omdocNode = <omdoc xmlns="http://omdoc.org/ns" xmlns:om="http://www.openmath.org/OpenMath">{mod.toNode}</omdoc>
-              xml.writeFile(omdocNode, inFile)
-        }
+    // scala appends $ to classes generated for objects
+    // and uses "MODULE$" as the name of the companion object
+    val clsJ = loader.loadClass(cls + ".NAMESPACE$")
+    //val clsJ = java.lang.Class.forName(cls + ".NAMESPACE$")
+    val ns = clsJ.getField("MODULE$").get(clsJ).asInstanceOf[uom.DocumentScala]
+    // run tests
+    ns.test(controller, (s: String) => log(s))
+  }
+
+  /** Integrate scala into a dimension */
+  def integrateScala(controller: Controller, in: List[String] = Nil) {
+    traversing(content, in, Archive.traverseIf("omdoc")) { case Current(inFile, inPath) =>
+      val mpath = Archive.ContentPathToMMTPath(inPath)
+      val mod = controller.globalLookup.getModule(mpath)
+      val scalaFile = (root / "scala" / inPath).setExtension("scala")
+      uom.Integrator.doModule(controller, mod, scalaFile)
+      val omdocNode = <omdoc xmlns="http://omdoc.org/ns" xmlns:om="http://www.openmath.org/OpenMath">
+        {mod.toNode}
+      </omdoc>
+      xml.writeFile(omdocNode, inFile)
     }
+  }
 }
