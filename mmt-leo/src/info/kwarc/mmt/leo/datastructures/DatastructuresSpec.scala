f6c5b344abfeccbcf00bb6e82c9ec5c6a4a28e69
mhamilton723 <mark.hamilton@yale.edu>
2015-06-25 16:15:31 +0000

    added node functionality
    git-svn-id: https://svn.kwarc.info/repos/MMT/src@2878 09069c07-60fa-4895-8316-727242e252e4

--- mmt-leo/src/info/kwarc/mmt/leo/datastructures/DatastructuresSpec.scala
+++ mmt-leo/src/info/kwarc/mmt/leo/datastructures/DatastructuresSpec.scala
@@ -8,18 +8,55 @@ import org.scalatest._
 
 class DatastructuresSpec extends FlatSpec with Matchers {
 
+  val node0 = new Node[Int](0)
+  val node1 = new Node[Int](1)
+  val node2 = new Node[Int](2)
+  val node3 = new Node[Int](3)
+  val node4 = new Node[Int](4)
+  val node5 = new Node[Int](5)
+  node0.addChild(node1)
+  node1.addChild(node2)
+  node1.addChild(node3)
+  node2.addChild(node4)
+  node4.addChild(node5)
+  //node1.setRoot(node0)
+
   "A Node" should "have a root and children" in {
-    val node = new Node[Int](1)
-    val node2 = new Node[Int](2)
-    val node3 = new Node[Int](3)
-    val node4 = new Node[Int](4)
-    node.addChild(node2)
-    node.addChild(node3)
-    node.setRoot(node4)
-    node.data should be (1)
-    node.root should be (Some(node4))
-    node.children should be (List(node2,node3))
+    node1.data should be (1)
+    node1.root should be (Some(node0))
+    node0.root should be (None)
+    node1.children should be (List(node2,node3))
+    node2.children should be (List(node4))
+    node0.depth should be (0)
+
   }
+
+  it should "have a mapping function that preserves the structure of the tree" in {
+    val tnode0 = new Node(0.5)
+    val tnode1 = new Node(1.5)
+    val tnode2 = new Node(2.5)
+    val tnode3 = new Node(3.5)
+    val tnode4 = new Node(4.5)
+    val tnode5 = new Node(5.5)
+    tnode0.addChild(tnode1)
+    tnode1.addChild(tnode2)
+    tnode1.addChild(tnode3)
+    tnode2.addChild(tnode4)
+    tnode4.addChild(tnode5)
+
+    var fnode0 = node0.map(i => i.toDouble + .5)
+    fnode0.data should be (0.5)
+    fnode0.children.head.isEquivTo(tnode1) should be (true)
+  }
+
+/*  it should "have breadth and depth first search capabilities" in {
+    var listB = List(-1)
+    var listD = List(-1)
+    node1.preorderDepth({n => listB=listB:::List(n.data) })
+    listB should be (List(-1,1,2,3,4))
+    node1.postorderDepth({n => listD=listD:::List(n.data) })
+    listD should be (List(-1,1,2,4,3))
+  }*/
 
   /*it should "throw NoSuchElementException if an empty stack is popped" in {
     val emptyStack = new Stack[Int]
--- mmt-leo/src/info/kwarc/mmt/leo/datastructures/Node.scala
+++ mmt-leo/src/info/kwarc/mmt/leo/datastructures/Node.scala
@@ -1,5 +1,7 @@
 package datastructures
 
+import scala.collection.immutable.Queue
+
 /**
  * The node class represents a n-tree with arbitrary data at the node.
  *
@@ -15,6 +17,25 @@ class Node[A](var dataVar: A) {
 
   /** returns the path of nodes to the root of the tree*/
   def path: List[Node[A]] = this :: root.map(_.path).getOrElse(Nil)
+
+  /** returns the number of nodes above the current node*/
+  def depth: Int = {
+    this.path.length-1
+  }
+
+  /** componentwise equivalence*/
+  def isEquivTo(n: Node[A]):Boolean ={
+    (this.children,n.children) match {
+      case (Nil,Nil) => this.data == n.data
+      case (l1,l2) =>
+        if (l1.length != l2.length) {return false}
+        for(x <- 0 to l1.length-1){
+          if (l1(x).isEquivTo(l2(x))== false) {return false}
+        }
+        return true
+      case _ => false
+    }
+  }
 
   /** checks if a node is equal to or below the current node*/
   def isBelow(that: Node[A]): Boolean = this == that || root.exists(_ isBelow that)
@@ -42,8 +63,8 @@ class Node[A](var dataVar: A) {
 
   /** removes a specific child from the node*/
   def removeChild(child :Node[A]):Unit={
-    child.root = None
     this.children = this.children.filter( p => p.root.get != child)
+    child.root = None
   }
 
   /** removes all children from the node*/
@@ -67,17 +88,29 @@ class Node[A](var dataVar: A) {
 
   /** returns a nice looking printable string **/
   override def toString: String = {
-    val total_depth = this.height
     var output = ""
-    this.preorder { subnode :Node[A] =>
-      var depth = total_depth-subnode.height
-      output = output+"\t"*depth + subnode.data.toString + "\n"
+    this.preorderDepth { subnode :Node[A] =>
+      output = output++ "\n"+"\t"*subnode.depth + subnode.data.toString
     }
-    output
+    output+ "\n"
   }
 
-  /** traverses the tree breadth first and performs an action */
-  def preorder(visit: Node[A] => Unit): Unit = {
+  /** maps a tree of one datatype to a tree of another*/
+  def map[B](f: A => B): Node[B] = {
+    var fthis = new Node[B](f(this.data))
+    def recur(n : Node[A],fn: Node[B]): Unit = {
+      for (r <- n.children) {
+        val addition = new Node[B](f(r.data))
+        fn.addChild(addition)
+        recur(r,addition)
+      }
+    }
+    recur(this,fthis)
+    fthis
+  }
+
+  /** traverses the tree depth first performs an action as it comes to the node*/
+  def preorderDepth(visit: Node[A] => Unit): Unit = {
     def recur(n: Node[A]): Unit = {
       visit(n)
       for (r <- n.children) {
@@ -87,8 +120,8 @@ class Node[A](var dataVar: A) {
     recur(this)
   }
 
-  /** traverses the tree depth first and performs an action */
-  def postorder(visit: Node[A] => Unit): Unit = {
+  /** traverses the tree depth first and performs an action after it reaches the leaves */
+  def postorderDepth(visit: Node[A] => Unit): Unit = {
     def recur(n: Node[A]): Unit = {
       for (r <- n.children) {
         recur(r)
@@ -97,6 +130,14 @@ class Node[A](var dataVar: A) {
     }
     recur(this)
   }
+
+  /** traverses the tree depth first performs an action as it comes to the node*/
+/*  def breadthTraverse(visit: Node[A] => Unit): Unit = {
+    var q = new Queue[Node[A]]()
+
+    this.children foreach {v => visit(v) }
+    recur(this)
+  }*/
 
   /** returns the height of the tree*/
   def height: Int = {
@@ -107,9 +148,7 @@ class Node[A](var dataVar: A) {
   def size: Int = {
     this.children.foldLeft(0) { (s, c) => s + c.size } + 1
   }
-
 }
-
 
 //class AndOrNode[A] extends Node(dataVar)
 
