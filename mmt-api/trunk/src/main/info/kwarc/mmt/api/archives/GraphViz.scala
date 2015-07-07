d92fd350481f0b6886774f57d27d65c6f6c8a41d
cmaeder <c.maeder@jacobs-university.de>
2015-07-07 17:53:43 +0000

    cleaned up
    git-svn-id: https://svn.kwarc.info/repos/MMT/src@2915 09069c07-60fa-4895-8316-727242e252e4

--- mmt-api/trunk/src/main/info/kwarc/mmt/api/archives/GraphViz.scala
+++ mmt-api/trunk/src/main/info/kwarc/mmt/api/archives/GraphViz.scala
@@ -1,72 +1,66 @@
 package info.kwarc.mmt.api.archives
+
 import info.kwarc.mmt.api._
-import documents._
-import modules._
-import ontology._
-import utils._
+import info.kwarc.mmt.api.documents._
+import info.kwarc.mmt.api.modules._
+import info.kwarc.mmt.api.ontology._
+import info.kwarc.mmt.api.utils._
 
 /**
  * uses [[ontology.GraphExporter]] to produce a dot file and then calls dot to produce an svg file
  */
 class GraphViz extends Exporter {
-   val key = "svg"
+  val key = "svg"
 
-   private var tg : ontology.TheoryGraph = null
-   /** path to graphviz (dot) binary */
-   private var graphviz: String = null
+  private var tg: ontology.TheoryGraph = null
+  /** path to graphviz (dot) binary */
+  private var graphviz: String = "dot"
 
-   /** expects one argument: the path to graphviz; alternatively set variable GraphViz */
-   override def start(args: List[String]) {
-      tg = new ontology.TheoryGraph(controller.depstore)
-      graphviz = getFromFirstArgOrEnvvar(args, "GraphViz", "dot")
-   }
+  /** expects one argument: the path to graphviz; alternatively set variable GraphViz */
+  override def start(args: List[String]): Unit = {
+    tg = new ontology.TheoryGraph(controller.depstore)
+    graphviz = getFromFirstArgOrEnvvar(args, "GraphViz", graphviz)
+  }
 
-   /** contains at least all elements of the document */
-   def exportDocument(doc : Document, bf: BuildTask) {
-      val theories = controller.depstore.querySet(doc.path, Transitive(+Declares) * HasType(IsTheory))
-      val views = controller.depstore.querySet(doc.path, Transitive(+Declares) * HasType(IsView))
-      produceGraph(theories, views, bf)
-   }
-   /** contains at least the theory */
-   def exportTheory(thy : DeclaredTheory, bf: BuildTask) {
-      produceGraph(List(thy.path), Nil, bf)
-   }
-   /** contains at least domain, codomain, and view */
-   def exportView(view : DeclaredView, bf: BuildTask) {
-      val theories = List(view.from,view.to).flatMap(objects.TheoryExp.getSupport)
-      produceGraph(theories, List(view.path), bf)
-   }
-   /** nothing for now */
-   def exportNamespace(dpath: DPath, bd: BuildTask, namespaces: List[BuildTask], modules: List[BuildTask]) {}
+  /** contains at least all elements of the document */
+  def exportDocument(doc: Document, bf: BuildTask): Unit = {
+    val theories = controller.depstore.querySet(doc.path, Transitive(+Declares) * HasType(IsTheory))
+    val views = controller.depstore.querySet(doc.path, Transitive(+Declares) * HasType(IsView))
+    produceGraph(theories, views, bf)
+  }
 
-   private def produceGraph(theories: Iterable[Path], views: Iterable[Path], bt: BuildTask) {
-      val outStringPostProcessed = getSVG(theories,views,Some(bt.outFile),Some(tg),m => bt.errorCont(LocalError(m)))
-      File.write(bt.outFile, outStringPostProcessed)
-   }
+  /** contains at least the theory */
+  def exportTheory(thy: DeclaredTheory, bf: BuildTask): Unit = {
+    produceGraph(List(thy.path), Nil, bf)
+  }
 
-   def getSVG(theories: Iterable[Path], views:Iterable[Path], file: Option[File],graph:Option[TheoryGraph],
-                    errorcont: String => Unit): String = {
-      val outFile = file.getOrElse(File(System.getProperty("java.io.tmpdir"))/"MMTGraph.svg")
-      val gv = new ontology.GraphExporter(theories, views, graph match {
-         case Some(g) => g
-         case None => tg
-      })
-      val dotFile = outFile.setExtension("dot")
-      gv.exportDot(dotFile)
-      val result = ShellCommand.run(graphviz, "-Tsvg", "-o" + outFile, dotFile.toString)
-      result foreach {m => errorcont(m)}
-      dotFile.delete
-      //TODO remove width/height attributes of svg element to allow for automatic resizing in the browser
-      File.read(outFile).replace("xlink:title", "class").replace("xlink:href", presentation.HTMLAttributes.symref)
-   }
+  /** contains at least domain, codomain, and view */
+  def exportView(view: DeclaredView, bf: BuildTask): Unit = {
+    val theories = List(view.from, view.to).flatMap(objects.TheoryExp.getSupport)
+    produceGraph(theories, List(view.path), bf)
+  }
 
-/*   def buildFile(a: Archive, bf: BuildFile) = {
-   }
+  /** nothing for now */
+  def exportNamespace(dpath: DPath, bd: BuildTask, namespaces: List[BuildTask], modules: List[BuildTask]): Unit = {}
 
-   /** same as buildOne but for the document given by the directory */
-   override def buildDir(a: Archive, bd: BuildDir, buildChildren: List[BuildTask]) = {
-      val bf = new BuildFile(bd.inFile, bd.inPath, DPath(a.narrationBase / bd.inPath), bd.outFile)
-      buildFile(a,bf)
-      bd.errors = bf.errors
-   }*/
+  private def produceGraph(theories: Iterable[Path], views: Iterable[Path], bt: BuildTask): Unit = {
+    val outStringPostProcessed = getSVG(theories, views, Some(bt.outFile), Some(tg), m => bt.errorCont(LocalError(m)))
+    File.write(bt.outFile, outStringPostProcessed)
+  }
+
+  def getSVG(theories: Iterable[Path], views: Iterable[Path], file: Option[File], graph: Option[TheoryGraph],
+             errorcont: String => Unit): String = {
+    val outFile = file.getOrElse(File(System.getProperty("java.io.tmpdir")) / "MMTGraph.svg")
+    val gv = new ontology.GraphExporter(theories, views, graph match {
+      case Some(g) => g
+      case None => tg
+    })
+    val dotFile = outFile.setExtension("dot")
+    gv.exportDot(dotFile)
+    val result = ShellCommand.run(graphviz, "-Tsvg", "-o" + outFile, dotFile.toString)
+    result foreach { m => errorcont(m) }
+    dotFile.delete
+    //TODO remove width/height attributes of svg element to allow for automatic resizing in the browser
+    File.read(outFile).replace("xlink:title", "class").replace("xlink:href", presentation.HTMLAttributes.symref)
+  }
 }
