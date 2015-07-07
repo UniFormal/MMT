8d7390ab5df459009460fb79a8192a0dd929691b
cmaeder <c.maeder@jacobs-university.de>
2015-07-07 17:46:20 +0000

    use twelf-server as default argument
    git-svn-id: https://svn.kwarc.info/repos/MMT/src@2914 09069c07-60fa-4895-8316-727242e252e4

--- mmt-lf/src/info/kwarc/mmt/lf/Twelf.scala
+++ mmt-lf/src/info/kwarc/mmt/lf/Twelf.scala
@@ -45,7 +45,7 @@ class Twelf extends Importer with frontend.ChangeListener {
    * first argument is the location of the twelf-server script; alternatively set variable GraphViz
    */
   override def start(args: List[String]): Unit = {
-    val p = getFromFirstArgOrEnvvar(args, "Twelf")
+    val p = getFromFirstArgOrEnvvar(args, "Twelf", "twelf-server")
     path = File(p)
     catalog = new Catalog(port = port, searchPort = true, log = report("lfcatalog", _))
     catalog.init //  throws PortUnavailable
