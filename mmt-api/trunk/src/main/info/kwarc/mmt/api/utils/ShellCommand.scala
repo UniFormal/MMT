29d87ff3c7332c599041a275877529da5c40ead5
cmaeder <c.maeder@jacobs-university.de>
2015-07-13 14:19:06 +0000

    cleaned up
    git-svn-id: https://svn.kwarc.info/repos/MMT/src@2953 09069c07-60fa-4895-8316-727242e252e4

--- mmt-api/trunk/src/main/info/kwarc/mmt/api/utils/ShellCommand.scala
+++ mmt-api/trunk/src/main/info/kwarc/mmt/api/utils/ShellCommand.scala
@@ -3,38 +3,42 @@ package info.kwarc.mmt.api.utils
 import java.io._
 
 object ShellCommand {
-   private def runInOpt(dir: Option[File], command: String*) = {
-      val pb = new java.lang.ProcessBuilder(command: _*)// use .inheritIO() for debugging
-      pb.redirectErrorStream(true)
-      dir.foreach {d => pb.directory(d.toJava)}
-      val proc = pb.start()
-      
-      // read all output immediately to make sure proc does not deadlock if buffer size is limited
-      val output = new StringBuilder
-      val outputReader = new BufferedReader(new InputStreamReader(proc.getInputStream))
-      var line: String = null
-      while ({line = outputReader.readLine; line != null}) {
-         output.append(line)
-      }
+  private def runInOpt(dir: Option[File], command: String*) = {
+    val pb = new java.lang.ProcessBuilder(command: _*) // use .inheritIO() for debugging
+    pb.redirectErrorStream(true)
+    dir.foreach { d => pb.directory(d.toJava) }
+    val proc = pb.start()
 
-      proc.waitFor
-      outputReader.close
-      
-      val ev = proc.exitValue
-      if (ev != 0) {
-         Some(output.result)
-      } else
-         None
-   }
-   
-   /**
-    * @param command the command to run
-    * @return output+error stream if not successful
-    */
-   def run(command: String*): Option[String] = runInOpt(None, command:_*)
-   /**
-    * like run
-    * @param dir the directory in which to run the command
-    */
-   def runIn(dir: File, command: String*): Option[String] = runInOpt(Some(dir), command:_*)
+    // read all output immediately to make sure proc does not deadlock if buffer size is limited
+    val output = new StringBuilder
+    val outputReader = new BufferedReader(new InputStreamReader(proc.getInputStream))
+    var line: Option[String] = None
+    while ( {
+      line = Option(outputReader.readLine)
+      line.isDefined
+    }) {
+      output.append(line.get)
+    }
+
+    proc.waitFor
+    outputReader.close()
+
+    val ev = proc.exitValue
+    if (ev != 0) {
+      Some(output.result())
+    } else
+      None
+  }
+
+  /**
+   * @param command the command to run
+   * @return output+error stream if not successful
+   */
+  def run(command: String*): Option[String] = runInOpt(None, command: _*)
+
+  /**
+   * like run
+   * @param dir the directory in which to run the command
+   */
+  def runIn(dir: File, command: String*): Option[String] = runInOpt(Some(dir), command: _*)
 }
