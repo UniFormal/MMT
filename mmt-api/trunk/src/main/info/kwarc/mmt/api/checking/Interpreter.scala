3264585e9f6437e4daf63f95aa5ebc6024d8444f
cmaeder <c.maeder@jacobs-university.de>
2015-09-16 12:00:28 +0000

    reformatted
    git-svn-id: https://svn.kwarc.info/repos/MMT/src@3260 09069c07-60fa-4895-8316-727242e252e4

--- mmt-api/trunk/src/main/info/kwarc/mmt/api/checking/Interpreter.scala
+++ mmt-api/trunk/src/main/info/kwarc/mmt/api/checking/Interpreter.scala
@@ -1,53 +1,53 @@
 package info.kwarc.mmt.api.checking
 
 import info.kwarc.mmt.api._
-import parser._
-import archives._
-import documents._
-import utils._
+import info.kwarc.mmt.api.archives._
+import info.kwarc.mmt.api.documents._
+import info.kwarc.mmt.api.parser._
+import info.kwarc.mmt.api.utils._
 
-/**
- * parses and returns a checked result
- */
+/** parses and returns a checked result */
 abstract class Interpreter extends Importer {
-   /** the input format (e.g., the file extension) to which this interpreter is applicable */
-   def format: String
-   def key = format + "-omdoc"
-   override def isApplicable(s: String) = s == format || super.isApplicable(s)
-   def inExts = List(format)
+  /** the input format (e.g., the file extension) to which this interpreter is applicable */
+  def format: String
 
-   /** parses a [[ParsingStream]] and returns a checked result */
-   def apply(ps: ParsingStream)(implicit errorCont: ErrorHandler): Document
+  def key = format + "-omdoc"
 
-   /** creates a [[ParsingStream]] for the input file and interprets it */
-   def importDocument(bf: BuildTask, index: Document => Unit) {
-      val inPathOMDoc = bf.inPath.toFile.setExtension("omdoc").filepath
-      val dpath = DPath(bf.base / inPathOMDoc.segments) // bf.narrationDPath except for extension
-      val ps = new ParsingStream(bf.base / bf.inPath.segments, dpath, bf.archive.namespaceMap, format, File.Reader(bf.inFile))
-      val doc = apply(ps)(bf.errorCont)
-      index(doc)
-   }
+  override def isApplicable(s: String) = s == format || super.isApplicable(s)
+
+  def inExts = List(format)
+
+  /** parses a [[ParsingStream]] and returns a checked result */
+  def apply(ps: ParsingStream)(implicit errorCont: ErrorHandler): Document
+
+  /** creates a [[ParsingStream]] for the input file and interprets it */
+  def importDocument(bf: BuildTask, index: Document => Unit) {
+    val inPathOMDoc = bf.inPath.toFile.setExtension("omdoc").filepath
+    val dPath = DPath(bf.base / inPathOMDoc.segments) // bf.narrationDPath except for extension
+    val ps = new ParsingStream(bf.base / bf.inPath.segments, dPath, bf.archive.namespaceMap, format, File.Reader(bf.inFile))
+    val doc = apply(ps)(bf.errorCont)
+    index(doc)
+  }
 }
-/**
- * a combination of a Parser and a Checker
+
+/** a combination of a Parser and a Checker
  *
  * @param parser the parser
  * @param checker the checker
- *
  */
 class TwoStepInterpreter(val parser: Parser, val checker: Checker) extends Interpreter {
-   def format = parser.format
+  def format = parser.format
 
-   /** parses a [[ParsingStream]] and checks the result */
-   def apply(ps: ParsingStream)(implicit errorCont: ErrorHandler): Document = {
-      try {
-         val doc = parser(ps)(errorCont)
-         checker(doc)(new CheckingEnvironment(errorCont, RelationHandler.ignore))
-         doc
-      } finally {
-         ps.stream.close
-      }
-   }
+  /** parses a [[ParsingStream]] and checks the result */
+  def apply(ps: ParsingStream)(implicit errorCont: ErrorHandler): Document = {
+    try {
+      val doc = parser(ps)(errorCont)
+      checker(doc)(new CheckingEnvironment(errorCont, RelationHandler.ignore))
+      doc
+    } finally {
+      ps.stream.close()
+    }
+  }
 }
 
 /** an interpreter created from a trusted parser */
