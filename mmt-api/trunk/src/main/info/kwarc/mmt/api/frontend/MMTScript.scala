341455d69dbdb320e8b562aedafdd151dc2aac6f
cmaeder <c.maeder@jacobs-university.de>
2015-10-14 09:36:21 +0000

    cleaned up
    git-svn-id: https://svn.kwarc.info/repos/MMT/src@3365 09069c07-60fa-4895-8316-727242e252e4

--- mmt-api/trunk/src/main/info/kwarc/mmt/api/frontend/MMTScript.scala
+++ mmt-api/trunk/src/main/info/kwarc/mmt/api/frontend/MMTScript.scala
@@ -2,136 +2,138 @@ package info.kwarc.mmt.api.frontend
 
 import info.kwarc.mmt.api._
 import info.kwarc.mmt.api.archives._
-import utils._
-import scala.collection.immutable.{ListMap}
+import info.kwarc.mmt.api.utils._
 
-
-/**
- * .mbt files schould contain a single object that extends MMTScript
- */
+/** .mbt files should contain a single object that extends MMTScript */
 abstract class MMTScript extends Extension {
   //MMT Actions
-  def logToFile(f : String, ts : Boolean = false) = controller.handle(AddReportHandler(new TextFileHandler(File(f), ts)))
-  def logToConsole() = controller.handle(AddReportHandler(ConsoleHandler))
-  def logToHTML(f : String) = controller.handle(AddReportHandler(new HtmlFileHandler(File(f))))
-  def logModule(s : String) = controller.handle(LoggingOn(s))
-  def addArchive(location : String) = controller.handle(AddArchive(File(location)))
-  def loadExtension(uri : String, args : List[String] = Nil) = controller.handle(AddExtension(uri, args))
+  def logToFile(f: String, ts: Boolean = false): Unit = controller.handle(AddReportHandler(new TextFileHandler(File(f), ts)))
 
-  def config = controller.config
+  def logToConsole(): Unit = controller.handle(AddReportHandler(ConsoleHandler))
+
+  def logToHTML(f: String): Unit = controller.handle(AddReportHandler(new HtmlFileHandler(File(f))))
+
+  def logModule(s: String): Unit = controller.handle(LoggingOn(s))
+
+  def addArchive(location: String): Unit = controller.handle(AddArchive(File(location)))
+
+  def loadExtension(uri: String, args: List[String] = Nil): Unit = controller.handle(AddExtension(uri, args))
+
+  def config: MMTConfig = controller.config
+
   //Utility
 
-    def runImporters(aid : String, btm : BuildTargetModifier = Build) = {
-      config.getArchive(aid).formats.flatMap(config.getImporters).distinct foreach {imp =>
-        build(List(aid), imp, btm)
-      }
+  def runImporters(aid: String, btm: BuildTargetModifier = Build): Unit = {
+    config.getArchive(aid).formats.flatMap(config.getImporters).distinct foreach { imp =>
+      build(List(aid), imp, btm)
     }
+  }
 
-    def runExporters(aid : String, btm : BuildTargetModifier = Build) = {
-      config.getArchive(aid).formats.flatMap(config.getImporters).distinct foreach {imp =>
-        build(List(aid), imp, btm)
-      }
+  def runExporters(aid: String, btm: BuildTargetModifier = Build): Unit = {
+    config.getArchive(aid).formats.flatMap(config.getImporters).distinct foreach { imp =>
+      build(List(aid), imp, btm)
     }
+  }
 
-    def cleanBuild() {
-      config.archives.foreach(a => runImporters(a.id, Clean))
-      config.archives.foreach(a => runImporters(a.id, Build))
-      config.archives.foreach(a => runExporters(a.id, Clean))
-      config.archives.foreach(a => runExporters(a.id, Build))
-    }
+  def cleanBuild() {
+    config.archives.foreach(a => runImporters(a.id, Clean))
+    config.archives.foreach(a => runImporters(a.id, Build))
+    config.archives.foreach(a => runExporters(a.id, Clean))
+    config.archives.foreach(a => runExporters(a.id, Build))
+  }
 
-    def updateBuild(ifHadErrors : Boolean) {
-      config.archives.foreach(a => runImporters(a.id, Update(Level.Error)))
-      config.archives.foreach(a => runExporters(a.id,  Update(Level.Error)))
-    }
+  def updateBuild(ifHadErrors: Boolean) {
+    config.archives.foreach(a => runImporters(a.id, Update(Level.Error)))
+    config.archives.foreach(a => runExporters(a.id, Update(Level.Error)))
+  }
 
-    def plainBuild(btm : BuildTargetModifier) = {
-      config.archives.foreach(a => runImporters(a.id, btm))
-      config.archives.foreach(a => runExporters(a.id, btm))
-    }
+  def plainBuild(btm: BuildTargetModifier): Unit = {
+    config.archives.foreach(a => runImporters(a.id, btm))
+    config.archives.foreach(a => runExporters(a.id, btm))
+  }
 
-    def compUpdateBuild(changedCompsIds : List[String], btm : BuildTargetModifier) = {
-       config.archives foreach {a =>
-         config.getArchive(a.id).formats foreach {f =>
-           val imps = config.getImporters(f)
-           val exps = config.getExporters(f)
-           var foundChanged = false
-           imps foreach { imp =>
-             if (changedCompsIds.contains(imp)) foundChanged = true
-             if (foundChanged) build(List(a.id), imp, Build)
-           }
+  def compUpdateBuild(changedCompsIds: List[String], btm: BuildTargetModifier): Unit = {
+    config.archives foreach { a =>
+      config.getArchive(a.id).formats foreach { f =>
+        val imps = config.getImporters(f)
+        val exps = config.getExporters(f)
+        var foundChanged = false
+        imps foreach { imp =>
+          if (changedCompsIds.contains(imp)) foundChanged = true
+          if (foundChanged) build(List(a.id), imp, Build)
+        }
 
-           exps foreach { exp =>
-             if (changedCompsIds.contains(exp) || foundChanged) build(List(a.id), exp, Build)
-           }
-         }
-       }
-    }
-
-    def parse(f : String, autoload : Boolean = true) = {
-      val file = File(f)
-      val s = File.read(file)
-      val lines = s.split("\n")
-      var section = ""
-      for (line <- lines) {
-        if (line.startsWith("//")) {
-          //ignore
-        } else if (line.startsWith("#")) {
-          section = line.substring(1)
-        } else section match {
-          case "importers" => line.split(" ").toList match {
-            case uri :: key :: args =>
-              config.addImporter(ImporterConf(uri, key, args))
-            case _ => println("Invalid importer line: `" + line + "`")
-          }
-          case "exporters" => line.split(" ").toList match {
-            case uri :: key :: args =>
-              config.addExporter(ExporterConf(uri, key, args))
-            case _ => println("Invalid exporter line: `" + line + "`")
-          }
-          case "archives" => line.split(" ").toList match {
-            case id :: fmtsS :: Nil =>
-              val fmts = fmtsS.split(",").toList
-              config.addArchive(ArchiveConf(id, fmts))
-            case _ => println("Invalid archives line: `" + line + "`")
-          }
-          case "formats" => line.split(" ").toList match {
-            case id :: impsS :: expsS :: Nil =>
-              val imps = impsS.split(",").toList
-              val exps = expsS.split(",").toList
-              config.addFormat(FormatConf(id, imps, exps))
-            case _ => println("Invalid formats line: `" + line + "`")
-          }
-          case "base" => config.setBase(line)
-          case s => println("ignoring invalid section: `" + s + "`")
+        exps foreach { exp =>
+          if (changedCompsIds.contains(exp) || foundChanged) build(List(a.id), exp, Build)
         }
       }
     }
+  }
+
+  def parse(f: String, autoload: Boolean = true): Unit = {
+    val file = File(f)
+    val s = File.read(file)
+    val lines = s.split("\n")
+    var section = ""
+    for (line <- lines) {
+      if (line.startsWith("//")) {
+        //ignore
+      } else if (line.startsWith("#")) {
+        section = line.substring(1)
+      } else section match {
+        case "importers" => line.split(" ").toList match {
+          case uri :: key :: args =>
+            config.addImporter(ImporterConf(uri, key, args))
+          case _ => println("Invalid importer line: `" + line + "`")
+        }
+        case "exporters" => line.split(" ").toList match {
+          case uri :: key :: args =>
+            config.addExporter(ExporterConf(uri, key, args))
+          case _ => println("Invalid exporter line: `" + line + "`")
+        }
+        case "archives" => line.split(" ").toList match {
+          case id :: fmtsS :: Nil =>
+            val fmts = fmtsS.split(",").toList
+            config.addArchive(ArchiveConf(id, fmts))
+          case _ => println("Invalid archives line: `" + line + "`")
+        }
+        case "formats" => line.split(" ").toList match {
+          case id :: impsS :: expsS :: Nil =>
+            val imps = impsS.split(",").toList
+            val exps = expsS.split(",").toList
+            config.addFormat(FormatConf(id, imps, exps))
+          case _ => println("Invalid formats line: `" + line + "`")
+        }
+        case "base" => config.setBase(line)
+        case _ => println("ignoring invalid section: `" + section + "`")
+      }
+    }
+  }
 
 
-  def build(ids : List[String], target : String, modifier: archives.BuildTargetModifier, in : FilePath = EmptyPath) : Unit = {
+  def build(ids: List[String], target: String, modifier: archives.BuildTargetModifier, in: FilePath = EmptyPath): Unit = {
     controller.handle(ArchiveBuild(ids, target, modifier, in))
   }
 
-   def main()
+  def main()
 }
 
 class MMTScriptEngine(controller: Controller) {
-   def apply(f: File) {
-     val code = File.read(f)
-     val imports = "import info.kwarc.mmt.api._\nimport info.kwarc.mmt.api.archives.{Build}\n"
-     val textscript = imports + s"object UserScript extends info.kwarc.mmt.api.frontend.MMTScript {\ndef main() {\n$code\n}\n}\nUserScript"
-     import scala.reflect.runtime._
-     val cm = universe.runtimeMirror(getClass.getClassLoader)
-     import scala.tools.reflect.ToolBox
-     val tb = cm.mkToolBox()
-     //println(textscript)
-     val scalascript = tb.eval(tb.parse(textscript))
-     scalascript match {
-        case s: MMTScript =>
-           s.init(controller)
-           s.main()
-        case _ =>
-     }
-   }
+  def apply(f: File) {
+    val code = File.read(f)
+    val imports = "import info.kwarc.mmt.api._\nimport info.kwarc.mmt.api.archives.{Build}\n"
+    val textscript = imports + s"object UserScript extends info.kwarc.mmt.api.frontend.MMTScript {\ndef main() {\n$code\n}\n}\nUserScript"
+    import scala.reflect.runtime._
+    val cm = universe.runtimeMirror(getClass.getClassLoader)
+    import scala.tools.reflect.ToolBox
+    val tb = cm.mkToolBox()
+    //println(textscript)
+    val scalascript = tb.eval(tb.parse(textscript))
+    scalascript match {
+      case s: MMTScript =>
+        s.init(controller)
+        s.main()
+      case _ =>
+    }
+  }
 }
