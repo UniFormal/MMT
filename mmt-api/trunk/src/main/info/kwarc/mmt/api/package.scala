9c30821a84eef0b0cc93282483f112fa04cf0e2b
cmaeder <c.maeder@jacobs-university.de>
2015-12-04 13:36:36 +0000

    corrected link to parser
    git-svn-id: https://svn.kwarc.info/repos/MMT/src@3519 09069c07-60fa-4895-8316-727242e252e4

--- mmt-api/trunk/src/main/info/kwarc/mmt/api/package.scala
+++ mmt-api/trunk/src/main/info/kwarc/mmt/api/package.scala
@@ -2,9 +2,9 @@ package info.kwarc.mmt
 
 import api._
 
-/**
-  * This is the main package of the MMT API.
-  * It holds subpackages for all data structures, data containers, and the central algorithms and 
+/** This is the main package of the MMT API.
+  *
+  * It holds subpackages for all data structures, data containers, and the central algorithms and
   * services.
   *
   * Some classes that are used everywhere are defined in this package, in particular:
@@ -20,40 +20,40 @@ import api._
   *   - [[objects]]
   *    The former 3 statefully maintain their child content and use an MMT URI to refer to their parent element.
   *    Objects are stateless (except for metadata and [[objects.ClientProperties]]).
-  *    
+  *
   *  - The package [[frontend]] maintains several classes that are essential for understanding the whole code.
   *
   *   Data structures and containers for associated content are defined in the packages
-  *   - [[opaque]]: external (i.e., informal, computation) content 
-  *   - [[ontology]]: relational data, query language, and query engine 
+  *   - [[opaque]]: external (i.e., informal, computation) content
+  *   - [[ontology]]: relational data, query language, and query engine
   *   - [[metadata]]: metadata annotations (stateful children of content)
   *
-  *  - The central data container is in the package
+  *   - The central data container is in the package
   *   - [[libraries]]: maintains the diagram of theories
-  *  
-  *  - Central algrorithms are defined in the packages
-  *   - [[parsing]]: reading of native MMT text syntax
+  *
+  *   - Central algrorithms are defined in the packages
+  *   - [[parser]]: reading of native MMT text syntax
   *   - [[presentation]]: HTML+presentation MathML generation
   *   - [[notations]]: the common code or parsing and presentation
   *   - [[checking]]: structural checking, type reconstruction
   *   - [[proving]]: theorem prover
   *   - [[uom]]: simplification of objects
   *   - [[moc]]: change management
-  *   
+  *
   *   All algorithms are defined in two separate levels for structure and objects.
   *   See [[frontend.LeveledExtension]].
-  *   
+  *
   *   - User and machine interfaces are defined in the packages
   *    - [[gui]]: graphical
   *    - [[web]]: HTTP
   *    - [[frontend]]: shell and scala-toplevel prompt
-  *   
+  *
   *   - Interfaces to physical storage are defined in the packages
   *    - [[backend]]: abstraction from physical storage
   *    - [[archives]]: MMT archives (= projects), build system, also a kind of physical storage
-  *   
+  *
   *   The package defines general purpose APIs for files, URIs, HTML building, etc.
-  * 
+  *
   */
 package object api {
 }
