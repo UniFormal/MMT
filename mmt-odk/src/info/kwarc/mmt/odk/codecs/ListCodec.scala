42c6633e1531c5259b6794fca60090b5ee08a570
Florian Rabe <florian.rabe@gmail.com>
2016-01-27 16:24:11 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT/src@3651 09069c07-60fa-4895-8316-727242e252e4

--- mmt-odk/src/info/kwarc/mmt/odk/codecs/ListCodec.scala
+++ mmt-odk/src/info/kwarc/mmt/odk/codecs/ListCodec.scala
@@ -11,8 +11,7 @@ import info.kwarc.mmt.lf.{ApplySpine, Apply}
 abstract class ListCodec[Code](id: GlobalName, list: GlobalName, nil: GlobalName, cons: GlobalName)
   extends CodecOperator[Code](id, list) {self =>
 
-  val numberOfOtherParameters = 1
-  val numberOfTypeParameters = 1
+  val typeParameterPositions = List(1)
 
   def aggregate(cs: List[Code]): Code
   def separate(c: Code): List[Code]
@@ -32,8 +31,6 @@ abstract class ListCodec[Code](id: GlobalName, list: GlobalName, nil: GlobalName
     * @return a codec for OMA(OMS(tp), cs.head.tp)
     */
   def apply(cs: Codec[Code]*) = {
-    if (cs.length != numberOfTypeParameters)
-      throw CodecNotApplicable
     val codec = cs.head
     new Codec[Code](id(codec.exp), tp(codec.tp)) {
       def encode(t: Term) = self.aggregate(self.destruct(t) map codec.encode)
