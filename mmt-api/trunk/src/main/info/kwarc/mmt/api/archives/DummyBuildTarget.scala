0ebe438d03067b24c2ddc0a15b73025fac4535a3
cmaeder <c.maeder@jacobs-university.de>
2016-01-28 18:53:19 +0000

    need, use and provide dependencies
    git-svn-id: https://svn.kwarc.info/repos/MMT/src@3658 09069c07-60fa-4895-8316-727242e252e4

--- mmt-api/trunk/src/main/info/kwarc/mmt/api/archives/DummyBuildTarget.scala
+++ mmt-api/trunk/src/main/info/kwarc/mmt/api/archives/DummyBuildTarget.scala
@@ -1,6 +1,7 @@
 package info.kwarc.mmt.api.archives
 
-import info.kwarc.mmt.api.utils.File
+import info.kwarc.mmt.api.utils._
+import File._
 
 /** a simple build target for testing */
 class DummyBuildTarget extends TraversingBuildTarget {
@@ -14,37 +15,51 @@ class DummyBuildTarget extends TraversingBuildTarget {
 
   override val outExt = "bar"
 
-  def buildFile(bf: BuildTask): BuildResult = {
-    var missingDeps: List[ResourceDependency] = Nil
+  override def getDeps(bt: BuildTask): Set[Dependency] = {
+    val (needed, _) = readSource(bt: BuildTask)
+    val fooFiles = needed.map(l => (l, File(bt.inFile.up / l))).collect {
+      case (l, f) if f.exists => l
+    }
+    fooFiles.map(l => BuildDependency(key, bt.archive, File(l).toFilePath)).toSet
+  }
+
+  def readSource(bt: BuildTask): (List[String], List[String]) = {
+    var missingDeps: List[String] = Nil
     var providedEntities: List[String] = Nil
     var readDeps = true
-    val source = scala.io.Source.fromFile(bf.inFile)
+    val source = scala.io.Source.fromFile(bt.inFile)
     source.getLines().foreach { line =>
       val l = line.trim
       if (l == "#provides") {
         readDeps = false
       } else if (readDeps) {
-        val d = File((bf.outFile.up / l).setExtension(outExt))
-        if (!d.exists) {
-          missingDeps ::= PhysicalDependency(d)
-        }
-      } else {
+        missingDeps ::= l
+      }
+      else {
         providedEntities ::= l
       }
     }
+    (missingDeps, providedEntities)
+  }
+
+  def buildFile(bf: BuildTask): BuildResult = {
+    val (needed, provided) = readSource(bf)
+    val providedFiles = provided.map(File(_))
+    val (used, missingDeps) = needed.map(l => File((bf.outFile.up / l).setExtension(outExt))).partition(_.exists)
+    val usedPDs = used.map(PhysicalDependency)
     if (missingDeps.isEmpty) {
-      if (providedEntities.isEmpty) {
+      if (provided.isEmpty) {
         logResult("failure for " + bf.inFile)
-        BuildFailure(Nil, Nil)
+        BuildFailure(usedPDs, Nil)
       } else {
-        File.write(bf.outFile, providedEntities.mkString("", "\n", "\n"))
+        File.write(bf.outFile, provided.mkString("", "\n", "\n"))
         logResult("success " + bf.outPath)
-        BuildSuccess(Nil, Nil)
+        BuildSuccess(usedPDs, (bf.outFile :: providedFiles).map(PhysicalDependency))
       }
     }
     else {
       logResult("missing dependencies for " + bf.inPath + ": " + missingDeps)
-      MissingDependency(missingDeps, Nil)
+      MissingDependency(missingDeps.map(PhysicalDependency), providedFiles.map(PhysicalDependency))
     }
   }
 }
