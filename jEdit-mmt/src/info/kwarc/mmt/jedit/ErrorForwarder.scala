f01c229ab2708b6234bb2459afdecb0aa2d74620
Florian Rabe <florian.rabe@gmail.com>
2016-02-18 21:41:49 +0000

    git-svn-id: https://svn.kwarc.info/repos/MMT/src@3722 09069c07-60fa-4895-8316-727242e252e4

--- jEdit-mmt/src/info/kwarc/mmt/jedit/ErrorForwarder.scala
+++ jEdit-mmt/src/info/kwarc/mmt/jedit/ErrorForwarder.scala
@@ -9,56 +9,57 @@ import parser._
 import utils.MyList._
 import utils._
 
+/** customizes the default errors of the ErrorList plugin
+ *  @param mainFile the file whose checking led to the error (may differ from the file that contains the error)
+ */
 class MMTError(val mainFile: File, es: ErrorSource, el: Int, sf: String, sl: Int, sc: Int, ec: Int, msg: String)
    extends DefaultErrorSource.DefaultError(es, el, sf, sl, sc, ec, msg)
 
+/** customizes the default error source of the ErrorList plugin */
 class MMTErrorSource extends DefaultErrorSource("MMT") {
 
-   private def removeErrorMsg(e: ErrorSource.Error) {
-     	gui.Swing.invokeLater {
-				 val msg = new ErrorSourceUpdate(this, ErrorSourceUpdate.ERROR_REMOVED, e)
-				  org.gjt.sp.jedit.EditBus.send(msg)
-			}
-	 }
-   private def getErrorSet(file: File) = {
-      val key = file.toString
-      val es = errors.get(key)
-      if (es != null) {
-        es
-      } else { 
-        val empty = new DefaultErrorSource.ErrorListForPath()
-        errors.put(key, empty)
-        empty      }
+   /** like superclass but allows only [[MMTError]]s */
+   override def addError(e: DefaultErrorSource.DefaultError) {
+      e match {
+        case e: MMTError => super.addError(e)
+        case _ => throw ImplementationError("illegal error")
+      }
    }
-
+  
+   /** remove errors produced when checking this file */
    def removeFileErrors(file: File) {
-     val errorSet = getErrorSet(file)
-     val iter = errorSet.iterator
-     while (iter.hasNext) {
-       iter.next() match {
-         case e: MMTError =>
-           if (e.mainFile == file)
-             errorSet.remove(e)
-             removeErrorMsg(e)
-         case e =>
-           // should be impossible
-           if (e.getFilePath == file.toString)
-             errorSet.remove(e)
-             removeErrorMsg(e)
+     val sets = errors.values.iterator
+     while (sets.hasNext) {
+       val set = sets.next
+       val es = set.iterator
+       while (es.hasNext) {
+          es.next match {
+            case e: MMTError =>
+               if (e.mainFile == file) {
+                 set.remove(e)
+                 gui.Swing.invokeLater {
+           	        val msg = new ErrorSourceUpdate(this, ErrorSourceUpdate.ERROR_REMOVED, e)
+            			  org.gjt.sp.jedit.EditBus.send(msg)
+            		 }
+               }
+            case e =>
+               // should be impossible
+          }
        }
      }
    }
 }
+
 /**
  * sends MMT errors directly to jEdit ErrorList
  * @param file the source file, in which the errors are found
  */
-class ErrorListForwarder(errorSource: MMTErrorSource, controller: Controller, src: File) extends ErrorHandler {
+class ErrorListForwarder(errorSource: MMTErrorSource, controller: Controller, mainFile: File) extends ErrorHandler {
    /**
     * remove all errors whose mainFile is src
     */
    def reset {
-      errorSource.removeFileErrors(src)
+      errorSource.removeFileErrors(mainFile)
    }
    protected def addError(e: Error) = e match {
       case s: SourceError =>
@@ -74,7 +75,7 @@ class ErrorListForwarder(errorSource: MMTErrorSource, controller: Controller, sr
                case u => u.toString
             }
          }
-         val error = new MMTError(src, errorSource, tp, file, pos.line, pos.column, pos.column + reg.length, s.mainMessage)
+         val error = new MMTError(mainFile, errorSource, tp, file, pos.line, pos.column, pos.column + reg.length, s.mainMessage)
          s.extraMessages foreach {m => error.addExtraMessage(m)}
          errorSource.addError(error)
       case e: Invalid =>
@@ -109,17 +110,17 @@ class ErrorListForwarder(errorSource: MMTErrorSource, controller: Controller, sr
          }
          val ref = causeOpt.flatMap {cause => SourceRef.get(cause)}.getOrElse {
             mainMessage = "error with unknown location: " + mainMessage
-            SourceRef(utils.FileURI(src), SourceRegion(SourcePosition(0,0,0), SourcePosition(0,0,0)))
+            SourceRef(utils.FileURI(mainFile), SourceRegion(SourcePosition(0,0,0), SourcePosition(0,0,0)))
          }
          val reg = ref.region
-         val error = new MMTError(src, errorSource, ErrorSource.ERROR, src.toString,
+         val error = new MMTError(mainFile, errorSource, ErrorSource.ERROR, mainFile.toString,
                reg.start.line, reg.start.column, reg.start.column + reg.length, mainMessage)
          extraMessages foreach {m => error.addExtraMessage(m)}
          errorSource.addError(error)
       case e: Error =>
          // other errors, should not happen
-         val error = new MMTError(src,
-             errorSource, ErrorSource.ERROR, src.toString, 0, 0, 1, "error with unknown location: " + e.getMessage
+         val error = new MMTError(mainFile,
+             errorSource, ErrorSource.ERROR, mainFile.toString, 0, 0, 1, "error with unknown location: " + e.getMessage
          )
          e.toStringLong.split("\\n").foreach {m => error.addExtraMessage(m)}
          errorSource.addError(error)
