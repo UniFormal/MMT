142a28a5a74718362563e6d4b9ace32da28eb6f2
m-iancu <mihnea.iancu88@gmail.com>
2016-02-25 17:34:31 +0000

    some structuralFeature experiments, unused yet
    git-svn-id: https://svn.kwarc.info/repos/MMT/src@3740 09069c07-60fa-4895-8316-727242e252e4

--- mmt-api/trunk/src/main/info/kwarc/mmt/api/ContainerElement.scala
+++ mmt-api/trunk/src/main/info/kwarc/mmt/api/ContainerElement.scala
@@ -21,6 +21,8 @@ trait ElementContainer[+S <: NamedElement] {
    }
 }
 
+trait NamedElementContainer[+S <: NamedElement] extends ElementContainer[S] with NamedElement
+
 trait DefaultLookup[S <: NamedElement] {self: ElementContainer[S] =>
   def domain = getDeclarations.map(_.name)
   def getO(name: LocalName) = getDeclarations.reverse.find(_.name == name)
@@ -45,10 +47,55 @@ class ListContainer[S <: NamedElement](items: List[S]) extends ElementContainer[
   def getDeclarations = items
 }
 
-trait ElaboratableElement[I <: NamedElement, O <: StructuralElement] extends ElementContainer[I] {
+/**
+ * This is now a Container[O] rather than I
+ * internals are done via encapsulation
+ * this allows the inherited method .getDeclarations to work as expected during lookup
+ * so that Containers and ElaboratableElements behave the same 
+ * (i.e. look by default in the elaboration)
+ */
+trait ElaboratableElement[I <: NamedElement, O <: StructuralElement] extends NamedElementContainer[O] {
+   def components : List[I] 
+   //just awful 
+   private val outer = this
+   def internalView = new ListContainer[I](components) with NamedElementContainer[I] {
+     val name = outer.name
+   }
    def elaborate: ElementContainer[O]
 }
 
+/**
+ * Rework of StructuralFeature
+ * check and elaborate taking just a Context is to weak I think
+ * generally they need access to the library to look up dependant theories (e.g. for realms) 
+ */
+abstract class ElaboratorExtension[I <: NamedElement, O <: StructuralElement](val key : String) extends frontend.FormatBasedExtension {
+   def isApplicable(s: String) = s == key
+   
+   def check(context: objects.Context, internal: NamedElementContainer[I])(implicit env: checking.CheckingEnvironment): Unit
+
+   def elaborate(context: objects.Context, internal: NamedElementContainer[I]): ElementContainer[O]
+//   def modules(d: DerivedDeclaration): List[Module]
+}
+
+/**
+ * Rework of DerivedDeclaration
+ * elaborator is now taken as a constructor argument
+ * this has the positive side-effect that elaboration can be local and library doesn't need a controller anymore
+ * the (big) negative is that parsing/construction fails if the right extension isn't loaded
+ * Considering lookup is such a central feature of MMT a missing elaborator would break things anyway though
+ * Extends ElaboratableElement (as it should) and trivially implements the elaborate method based on its elaborator
+ */
+abstract class ElaboratingDeclaration[I <: NamedElement, O <: StructuralElement](elaborator : ElaboratorExtension[I,O]) extends ElaboratableElement[I,O] {
+  def elaborate : ElementContainer[O] = elaborator.elaborate(objects.Context.empty, internalView)
+  def getDeclarations: List[O] = elaborate.getDeclarations
+  
+}
+
+/**
+ * Something like DeclarationGroups would extend this
+ * Theories and Views also if they were elaboratable elements to begin with
+ */
 trait RecursivelyElaboratableElement[S <: StructuralElement] extends StructuralElement with ElaboratableElement[S,S] {
   def elaborate = {
     val ds = getDeclarations.flatMap {
@@ -57,5 +104,26 @@ trait RecursivelyElaboratableElement[S <: StructuralElement] extends StructuralE
       case e => List(e)
     }
     new ListContainer(ds)
+  }
+}
+
+/**
+ * Attempt to rework some part of Library code without disturbing existing code
+ * get, getDeclarationInTerm stay the same (mostly)
+  
+ */
+abstract class GenericLookup {
+  //generic lookup method where containers lazyly resolve names inside 
+  //e.g. getInTheory method from library would become .get or .getElab method of DeclaredTheory Class
+  def getDeclarationInElement(elem : NamedElement, name : LocalName) : NamedElement = name match {
+    case LocalName.empty => elem
+    case _ => elem match {
+      case cont : ElementContainer[_] =>
+        cont.getMostSpecific(name) match {
+          case Some((newElem, newName)) => getDeclarationInElement(newElem, newName)
+          case None => throw new GetError("container cannot find name" + name + " inside")
+        }
+      case _ => throw new GetError("loooking for name inside non-container element")      
+    }    
   }
 }
