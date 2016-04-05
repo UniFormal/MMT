a24432feba3b3ab164b41e5c8ff4aed54921a57e
cmaeder <c.maeder@jacobs-university.de>
2016-04-05 12:00:15 +0000

    a few implementation additions
    git-svn-id: https://svn.kwarc.info/repos/MMT/src@3856 09069c07-60fa-4895-8316-727242e252e4

--- mmt-api/trunk/src/main/info/kwarc/mmt/api/symbols/DerivedDeclaration.scala
+++ mmt-api/trunk/src/main/info/kwarc/mmt/api/symbols/DerivedDeclaration.scala
@@ -7,7 +7,7 @@ import checking._
 import objects._
 
 /** A [[DerivedDeclaration]] is a syntactically like a nested theory.
- *  Its semantics is defined by the corresponding [[StructuralFeature]] 
+ *  Its semantics is defined by the corresponding [[StructuralFeature]]
  */
 class DerivedDeclaration(h: Term, name: LocalName, val feature: String, components: List[DeclarationComponent]) extends {
    private val t = new DeclaredTheory(h.toMPath.parent, h.toMPath.name/name, None)
@@ -23,13 +23,13 @@ class StructuralFeatureRule(val feature: String) extends Rule {
 
 /**
  * A StructureFeature defines the semantics of a [[DerivedDeclaration]]
- * 
+ *
  * The semantics consists of a set of declarations that are injected into the parent theory after the [[DerivedDeclaration]]
- * These are called the 'outer declarations' 
+ * These are called the 'outer declarations'
  */
 abstract class StructuralFeature(val feature: String) extends FormatBasedExtension {
    def isApplicable(s: String) = s == feature
-   
+
    /** called after checking components and inner declarations */
    def check(d: DerivedDeclaration)(implicit env: CheckingEnvironment): Unit
 
@@ -39,7 +39,7 @@ abstract class StructuralFeature(val feature: String) extends FormatBasedExtensi
     * @param dd the derived declaration (pre: dd.feature == feature)
     */
    def elaborate(parent: DeclaredModule, dd: DerivedDeclaration): Elaboration
-      
+
    def modules(d: DerivedDeclaration): List[Module]
 
 }
@@ -55,7 +55,7 @@ abstract class Elaboration extends ElementContainer[Declaration] {
      */
     def getDeclarations = {
       domain.map {n => getO(n).get}
-    }  
+    }
 
     def getMostSpecific(name: LocalName): Option[(Declaration,LocalName)] = {
       domain.reverse.foreach {n =>
@@ -72,7 +72,7 @@ abstract class Elaboration extends ElementContainer[Declaration] {
  * called structures in original MMT
  */
 class GenerativePushout extends StructuralFeature("generative") {
-   
+
   def elaborate(parent: DeclaredModule, dd: DerivedDeclaration) = {
       val dom = dd.getComponent(DomComponent) getOrElse {
         throw GetError("")
@@ -85,13 +85,13 @@ class GenerativePushout extends StructuralFeature("generative") {
       }
       val context = parent.getInnerContext
       val body = controller.simplifier.getBody(context, dom)
-      
+
       new Elaboration {
         /** the morphism dd.dom -> parent of the pushout diagram: it maps every n to dd.name/n */
         private val morphism = new DeclaredView(parent.parent, parent.name/dd.name, dom, parent.toTerm, false)
-        
+
         private def translate(t: Term): Term = ??? //TODO
-        
+
         /** precompute domain and build the morphism */
         val domain = body.getDeclarations.map {d =>
           val ddN = dd.name / d.name
@@ -99,19 +99,19 @@ class GenerativePushout extends StructuralFeature("generative") {
           morphism.add(assig)
           ddN
         }
-        
+
         def getO(name: LocalName): Option[Declaration] = body.getO(name) map {
           case VarDecl(n,tp,df,nt) =>
              VarDecl(dd.name/n, tp map translate, df map translate, nt).toDeclaration(parent.toTerm)
           case c: Constant =>
-             Constant(parent.toTerm, dd.name/c.name, Nil, c.tp map translate, c.df map translate, None, c.notC)
+             Constant(parent.toTerm, dd.name/c.name, c.alias map (dd.name / _), c.tp map translate, c.df map translate, c.rl, c.notC)
           case nm: NestedModule => ???
           case d => throw LocalError("unexpected declaration in body of domain: " + d)
         }
-        
+
       }
    }
-   
+
    def modules(d: DerivedDeclaration): List[Module] = Nil
    def check(d: DerivedDeclaration)(implicit env: CheckingEnvironment) {}
 }
@@ -129,7 +129,7 @@ class InductiveDataTypes extends StructuralFeature("inductive") {
        }
      }
   }
-  
+
    def modules(d: DerivedDeclaration): List[Module] = Nil
    def check(d: DerivedDeclaration)(implicit env: CheckingEnvironment) {}
 }
