UniFormal/MMT

commit dbffd7aff08164265b20a27ca9853571484faf0a

cleanup of extension manager: no more special treatment for mathwebsearch, notation extensions, and lexer extensions
urtheories now contains the default notations and lexer extensions as rules

git-svn-id: https://svn.kwarc.info/repos/MMT/src@3900 09069c07-60fa-4895-8316-727242e252e4

mmt-api/trunk/src/main/info/kwarc/mmt/api/notations/Pragmatics.scala
@@ -5,31 +5,53 @@ import objects._
 import modules._
 import frontend._
 import libraries._
+import symbols._
 
 import objects.Conversions._
 
-class Pragmatics(controller: Controller) {
+class Pragmatics extends ChangeListener {
    private lazy val lup = controller.globalLookup // must be lazy due to order of class initialization
-   private lazy val notExts = controller.extman.notationExtensions
 
-   private def applicableByLevel(level: Option[MPath]): NotationExtension = {
-      val applicable = notExts.filter {ne => (ne.applicableLevel, level) match {
-         case (None, None) => true
-         case (Some(aL), Some(l)) => controller.globalLookup.hasImplicit(OMMOD(aL), OMMOD(l))
-         case _ => false
-      }}
-      if (applicable.isEmpty) {
-         level match {
-            case None => MixfixNotation
-            case Some(l) =>
-               lup.getTheory(l) match {
-                  case t: DeclaredTheory => applicableByLevel(t.meta)
-                  case t: DefinedTheory => MixfixNotation 
-               }
-         }
-      } else {
-         applicable.maxBy(_.priority) 
+   /** caches (via change-listening) all known NotationExtensions */
+   private var notExts: List[(MPath, NotationExtension)] = Nil
+   
+   override def onAdd(se: StructuralElement) {
+     se match {
+       case rc: RuleConstant => rc.df match {
+         case ne: NotationExtension =>
+           notExts ::= (rc.home.toMPath, ne)
+         case _ =>
+       }
+       case _ =>
+     }
+   }
+   override def onDelete(se: StructuralElement) {
+     se match {
+       case rc: RuleConstant => rc.df match {
+         case ne: NotationExtension =>
+           notExts = notExts.filterNot {case (_,neC) => ne == neC}
+         case _ =>
+       }
+       case _ =>
+     }
+   }
+   override def onClear {
+     notExts = Nil
+   }
+   
+   /** a NotationExtension is applicable at level mp if it is visible to mp */
+   private def applicableByLevel(levelOpt: Option[MPath]): NotationExtension = {
+      val level = levelOpt getOrElse {return MixfixNotation}
+      val applicable = notExts.flatMap {case (thy, ne) => 
+        if (controller.globalLookup.hasImplicit(OMMOD(thy), OMMOD(level)))
+          List(ne)
+        else
+          Nil
       }
+      if (applicable.isEmpty)
+        MixfixNotation
+      else
+         applicable.maxBy(_.priority) 
    }
    def makeStrict(level: Option[MPath], op: GlobalName, subs: Substitution, con: Context, args: List[Term], attrib: Boolean, not: TextNotation
          )(implicit newUnkwown: () => Term) : Term = {
@@ -45,7 +67,7 @@ class Pragmatics(controller: Controller) {
    }
    
    def makePragmatic(t: Term)(implicit getNotations: GlobalName => List[TextNotation]) : Option[PragmaticTerm] = {
-      val applicable = notExts.filter(_.isApplicable(t)).sortBy(_.priority).reverse
+      val applicable = notExts.view.map(_._2).filter(_.isApplicable(t)).sortBy(_.priority).reverse
       applicable.foreach {ne =>
          ne.destructTerm(t).foreach {tP => return Some(tP)}
       }
@@ -58,7 +80,7 @@ class Pragmatics(controller: Controller) {
    }
  
   private def hoass = notExts.flatMap {
-     case h: HOASNotation => List(h.hoas)
+     case (_, h: HOASNotation) => List(h.hoas)
      case _ => Nil
   } 
    

mmt-api/trunk/src/main/info/kwarc/mmt/api/checking/MMTStructureChecker.scala
@@ -335,9 +335,9 @@ class MMTStructureChecker(objectChecker: ObjectChecker) extends Checker(objectCh
         val l = controller.globalLookup.getLink(p)
         env.pCont(p)
         (m, l.from, l.to)
-      case OMDL(to, name) =>
-        checkTheory(context, OMMOD(to))
-        content.get(to ? name) match {
+      case OMS(p) =>
+        checkTheory(context, OMMOD(p.module))
+        content.get(p) match {
           case l: Structure =>
             env.pCont(l.path)
             (m, l.from, l.to)

mmt-api/trunk/src/main/info/kwarc/mmt/api/frontend/ActionHandling.scala
@@ -64,7 +64,7 @@ trait ActionHandling {self: Controller =>
           case AddExtension(c, args) =>
             extman.addExtension(c, args)
           case AddMWS(uri) =>
-            extman.mws = Some(new MathWebSearch(uri.toURL))
+            extman.addExtension(new MathWebSearch(uri.toURL))
           case OAFInit(path) =>
             getOAFOrError.init(path)
           case OAFClone(path) =>

mmt-api/trunk/src/main/info/kwarc/mmt/api/frontend/Controller.scala
@@ -95,7 +95,7 @@ class Controller extends ROController with ActionHandling with Logger {
   def buildManager = extman.get(classOf[BuildManager]).head
 
   /** converts between strict and pragmatic syntax using [[notations.NotationExtension]]s */
-  val pragmatic = new Pragmatics(this)
+  def pragmatic = extman.get(classOf[Pragmatics]).head
   /** the http server */
   var server: Option[Server] = None
   /** the catalog maintaining all registered physical storage units */

mmt-api/trunk/src/main/info/kwarc/mmt/api/frontend/ExtensionManager.scala
@@ -169,11 +169,6 @@ class ExtensionManager(controller: Controller) extends Logger {
     getOrAddExtension(cls, format)
    }
 
-  var lexerExtensions: List[LexerExtension] = Nil
-  var notationExtensions: List[notations.NotationExtension] = Nil
-
-  var mws: Option[ontology.MathWebSearch] = None
-
   val report = controller.report
   val logPrefix = "extman"
 
@@ -288,6 +283,8 @@ class ExtensionManager(controller: Controller) extends Logger {
     List(new XMLStreamer, nbp, kwp, rbc, msc, mmtint, nbpr, rbs, mss, msp, mmtextr, prover, rbe).foreach {e => addExtension(e)}
     // build manager
     addExtension(new TrivialBuildManager)
+    // pragmatic-strict converter
+    addExtension(new notations.Pragmatics)
     //targets, opaque formats, and presenters
     List(new archives.HTMLExporter, new archives.PythonExporter, new uom.GenericScalaExporter, new uom.OpenMathScalaExporter,
       new TextInterpreter, new HTMLInterpreter,
@@ -306,13 +303,6 @@ class ExtensionManager(controller: Controller) extends Logger {
       new ontology.Present, new ontology.PresentDecl).foreach(addExtension(_))
     // shell extensions
     List(new ShellSendCommand, new execution.ShellCommand).foreach(addExtension(_))
-
-    lexerExtensions ::= GenericEscapeLexer
-    lexerExtensions ::= UnicodeReplacer
-    //lexerExtensions ::= new PrefixedTokenLexer('\\')
-    lexerExtensions ::= new PrefixedTokenLexer('`', false, false)
-
-    notationExtensions ::= notations.MixfixNotation
   }
 
   def cleanup {

mmt-api/trunk/src/main/info/kwarc/mmt/api/gui/SearchPane.scala
@@ -35,7 +35,7 @@ class SearchPane(controller: Controller) extends JPanel {
    add(new JScrollPane(resultTree), BorderLayout.CENTER)
    
    private def search {
-      val mws = controller.extman.mws.getOrElse(throw ParseError("no mws defined"))
+      val mws = controller.extman.get(classOf[MathWebSearch]).headOption.getOrElse(throw ParseError("no mws defined"))
       val q = queryText.getText
       val format = formatText.getText
       val mwsQuery = TermPattern.parse(controller, theory.getText, q, format)

mmt-api/trunk/src/main/info/kwarc/mmt/api/libraries/ImplicitGraph.scala
@@ -128,6 +128,7 @@ class ThinGeneratedCategory {
    }
    
    def applyAtomic(from: MPath, to: MPath) = if (from == to) Some(OMCOMP()) else impl(OMMOD(from), OMMOD(to))
+   
    private def checkUnique(mors: List[Term]) =
       if (mors.isEmpty)
          None
@@ -135,11 +136,7 @@ class ThinGeneratedCategory {
          Some(mors.head)
       else
          None
-   private def checkAgree(morOpts: List[Option[Term]]): Option[Term] = { 
-      val mors = morOpts.map(_.getOrElse(return None))
-      //TODO check that all morphisms agree on joint domains
-      Some(MUnion(mors))
-   }
+
    /** retrieves the implicit morphism between two theories (if any)
     * @param from domain
     * @param to codomain
@@ -162,8 +159,9 @@ class ThinGeneratedCategory {
             apply(OMMOD(p), to)
          case (TUnion(ts), _) =>
             if (ts.isEmpty) return Some(OMCOMP())
-            val tsMors = ts.map {t => apply(t, from)}
-            checkAgree(tsMors)
+            val tsMors = ts.map {t => (t, apply(t, from).getOrElse(return None))}
+            //TODO check agreement and return amalgamation of morphisms
+            None
          // TODO remove unions or handle their interaction with ComplexTheory
          case (ComplexTheory(fromC), _) =>
             val fromCMors = fromC.map {
@@ -174,7 +172,8 @@ class ThinGeneratedCategory {
                   case _ => None
                }
             }
-            checkAgree(fromCMors)
+            //TODO check agreement and return amalgamation of morphisms
+            None
          case _ => None // catches semiformal theories, which may be generated by the parser
       }
    }

mmt-api/trunk/src/main/info/kwarc/mmt/api/libraries/Library.scala
@@ -292,11 +292,6 @@ class Library(extman: ExtensionManager, val report: Report) extends Lookup with 
       case l: Structure => DefLinkAssignment(home, name, l.from, l.toTerm)
     }
     case Morph.empty => error("empty morphism has no assignments")
-    case MUnion(ms) => ms mapFind {
-      m => getO(m, name)
-    } getOrElse {
-      error("union of morphisms has no assignments except includes")
-    }
     case _ => error("unknown module: " + home)
   }
 

commit 97de9ebd05963cb23386ec5b01aa7f34a90f5900

fixed notation bug introduced in r3900 plus minor stex/glossary fixes
git-svn-id: https://svn.kwarc.info/repos/MMT/src@3908 09069c07-60fa-4895-8316-727242e252e4

mmt-api/trunk/src/main/info/kwarc/mmt/api/notations/Pragmatics.scala
@@ -67,8 +67,9 @@ class Pragmatics extends ChangeListener {
    }
    
    def makePragmatic(t: Term)(implicit getNotations: GlobalName => List[TextNotation]) : Option[PragmaticTerm] = {
-      val applicable = notExts.view.map(_._2).filter(_.isApplicable(t)).sortBy(_.priority).reverse
-      applicable.foreach {ne =>
+      var applicable = notExts.view.map(_._2).filter(_.isApplicable(t)).sortBy(_.priority).reverse
+      applicable :+= MixfixNotation
+      applicable.foreach { ne =>
          ne.destructTerm(t).foreach {tP => return Some(tP)}
       }
       return None

mmt-stex/src/info/kwarc/mmt/stex/STeXImporter.scala
@@ -654,7 +654,6 @@ class STeXImporter extends Importer {
         <om:OMV name={"mixref" + refNr}> </om:OMV>
       } catch {
         case e : Exception => 
-          println(e)
           node
       }
     case "#PCDATA" => new scala.xml.Text(node.toString())

planetary-mmt/src/info/kwarc/mmt/planetary/GlossaryGenerator.scala
@@ -113,8 +113,8 @@ object GlossaryGenerator {
 
     val defs = controller.depstore.queryList(spath, ToObject(IRels.isDefinedBy)) flatMap {
           case p: Path =>
-            controller.get(p) match {
-              case fd: Constant =>
+            controller.getO(p) match {
+              case Some(fd: Constant) =>
                 val mpath = fd.home.toMPath
                 if (sTeX.getLanguage(mpath) == Some(lang)) {
                   Some(fd)