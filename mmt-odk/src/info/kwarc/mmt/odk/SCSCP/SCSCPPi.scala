f43d5b0c94c5baf8a1e462f0444c9e7dffc5993f
Tom Wiesing <tkw01536@gmail.com>
2016-05-31 16:34:45 +0000

    Improvements in SCSCP implementation
    * fix issues in SCSCPPi
    * implement an SCSCPSocket to read / write the basic protocol level
    * implement version negotiation in SCSCPClient
    
    
    git-svn-id: https://svn.kwarc.info/repos/MMT/src@3976 09069c07-60fa-4895-8316-727242e252e4

---
+++ mmt-odk/src/info/kwarc/mmt/odk/SCSCP/SCSCPPi.scala
@@ -0,0 +1,62 @@
+package info.kwarc.mmt.odk.SCSCP
+
+import scala.xml.{Attribute, Text, Null}
+
+/**
+  * Represents a single XML processing instruction
+ */
+case class SCSCPPi(key : Option[String], attributes : Map[String, String]) {
+  override def toString : String = {
+      // create a normal node
+      val node = attributes.toList.map(np => Attribute(None, np._1, Text(np._2), Null)).foldLeft(<scscp />)(_ % _).toString
+
+      // and replace with a regex
+      "^<scscp(\\s*)(.*)(\\s*)/>$".r.replaceFirstIn(node, key match {
+        case Some(k) => "<?scscp "+k+" $2 ?>"
+        case None => "<?scscp $2 ?>"
+      })
+  }
+
+  /**
+    * Returns the value of the given attribute
+    * @param key
+    * @return
+    */
+  def apply(key : String) = attributes(key)
+
+  /**
+    * Returns the key of this SCSCPI
+    * @return
+    */
+  def apply() = key.get
+}
+
+object SCSCPPi {
+  private val regex_parse = "^<\\?scscp\\s+(([a-zA-Z0-9]+)\\s)?(.*)\\?>$".r
+
+  /**
+    * Parses a string into an SCSCP Processing instruction
+    * @param data
+    * @return
+    */
+  def apply(data : String): SCSCPPi = {
+
+    // match the regular expression to the data
+    val data_match = regex_parse.findFirstMatchIn(data).get
+
+    // extract the key
+    val key = data_match.group(1) match {
+      case null => None
+      case k => Some(k.dropRight(1))
+    }
+
+    //parse the attributes
+    val node = scala.xml.XML.loadString("<scscp "+data_match.group(3)+" />")
+    val atts = node.attributes.map((att: scala.xml.MetaData) => (att.key, att.value.head.text)).toMap
+
+    // and return a proper XMLPi
+    SCSCPPi(key, atts)
+  }
+
+  def apply(attributes : Map[String, String]) : SCSCPPi = SCSCPPi(None, attributes)
+}
---
+++ mmt-odk/src/info/kwarc/mmt/odk/SCSCP/SCSCPSocket.scala
@@ -0,0 +1,157 @@
+package info.kwarc.mmt.odk.SCSCP
+
+import java.net.Socket
+import OpenMath._
+import java.io.{BufferedReader, InputStreamReader}
+
+
+/**
+  * Represents a socket that can be used for SCSP communication
+  * @param socket Underlying socket object to use
+  */
+class SCSCPSocket (val socket: Socket) {
+
+  /**
+    * A parser to read / write OpenMath XML
+    */
+  private val om_parser = new OMXMLEncoding()
+
+
+  /**
+    * Processing instruction used to indicate the start of an OpenMath Element
+    */
+  private val start_pi = SCSCPPi(Some("start"), Map())
+
+  /**
+    * Processing instruction used to indicate the end of an OpenMath Element
+    */
+  private val end_pi = SCSCPPi(Some("end"), Map())
+
+  /**
+    * Processing instruction used to indicate the cancellation of an OpenMath Element
+    */
+  private val cancel_pi = SCSCPPi(Some("cancel"), Map())
+
+  /**
+    * Processing instruction to indicate the quitting of the server / client
+    */
+  private val quit_pi = SCSCPPi(Some("quit"), Map())
+
+  /**
+    * Input stream
+    */
+  private val input = new BufferedReader(new InputStreamReader(socket.getInputStream, "UTF-8"))
+
+  /**
+    * Output Stream
+    */
+  private val output = socket.getOutputStream
+
+  /**
+    * Waits and reads a single line from the input
+    * @return
+    */
+  private def read_socket_line : String = {
+    input.readLine
+  }
+
+
+  /**
+    * Tries to read either a processing instruction or an OMObject
+    * from the socket.
+    * @return
+    */
+  def try_read_socket : Option[Either[SCSCPPi, OMObject]] = {
+    try {
+      // read the processing instruction
+      val inst = SCSCPPi(read_socket_line)
+
+      // for the start instruction, continue
+      if(inst != start_pi){
+        return Some(Left[SCSCPPi, OMObject](inst))
+      }
+    } catch {
+      case e:Exception =>
+        return None
+    }
+
+    // create a buffer for the openMath object
+    val buffer = new StringBuilder
+
+
+    while(true){
+
+      // read a line from the socket
+      val line = read_socket_line
+
+      try {
+        // read the processing instruction
+        val inst = SCSCPPi(line)
+
+        // if we have an end node
+        if (inst == end_pi){
+          val node = om_parser.decodeObject(scala.xml.XML.loadString(buffer.toString))
+
+          return Some(Right[SCSCPPi, OMObject](node))
+        } else if (inst == cancel_pi){
+
+          // we cancelled the object
+          return None
+        }
+      } catch {
+        case e:Exception =>
+          // add the line to the buffer
+          buffer ++= line + "\n"
+      }
+    }
+
+    null
+  }
+
+  /**
+    * Reads a processing instruction from the socket or throws an exception.
+    * @return
+    */
+  def read_pi : SCSCPPi = try_read_socket match {case Some(Left(a)) => a}
+
+  /**
+    * Reads an OpenMath element from the socket or throws an exception.
+    * @return
+    */
+  def read_om = try_read_socket match {case Some(Right(a)) => a}
+
+  /**
+    * Writes a string to the socket
+    * @param s
+    * @param flush
+    */
+  private def write_string (s : String, flush : Boolean = true): Unit = {
+    output.write(s.getBytes)
+
+    if(flush){
+      output.flush()
+    }
+  }
+
+  /**
+    * Writes a processing instruction to the client
+    * @param pi
+    */
+  def write_pi(pi : SCSCPPi) : Unit = {
+    write_string(pi.toString)
+  }
+
+  /**
+    * Writes an OmDoc element to the socket.
+    * @param om
+    */
+  def write_om(om : OMObject) : Unit = {
+    write_pi(start_pi)
+    write_string(om_parser.encode(om).toString)
+    write_pi(end_pi)
+  }
+
+  def apply(pi : SCSCPPi) : Unit = write_pi(pi)
+  def apply(om : OMObject) : Unit = write_om(om)
+  def apply() : Option[Either[SCSCPPi, OMObject]] = try_read_socket
+}
