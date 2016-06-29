f28700b0c050f51db8f5d048e360b10f91fe9e87
Dennis Müller <d.mueller@jacobs-university.de>
2016-06-29 10:41:44 +0200

    ODK Stuff

--- src/mmt-api/src/main/info/kwarc/mmt/api/utils/OS.scala
+++ src/mmt-api/src/main/info/kwarc/mmt/api/utils/OS.scala
@@ -25,7 +25,7 @@ object OS {
      detect match {
         case Windows => File(System.getenv("APPDATA"))
         case MacOS => uh / "Library"
-        case ProbablyUnix => uh
+        case os : Unix => uh
      }
   }
   
