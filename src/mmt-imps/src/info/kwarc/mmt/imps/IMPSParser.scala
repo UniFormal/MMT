2394e1d9276de606a4fa219ad77a8cb8a1eab629
Jonas Betzendahl <jbetzend@techfak.uni-bielefeld.de>
2017-01-12 00:24:26 +0100

    Documentation, cleanup, cosmetics

--- src/mmt-imps/src/info/kwarc/mmt/imps/IMPSParser.scala
+++ src/mmt-imps/src/info/kwarc/mmt/imps/IMPSParser.scala
@@ -1,19 +1,23 @@
 package info.kwarc.mmt.imps
 
+/* Java Imports */
 import java.io._
 
+/* Scala Imports */
 import scala.io.Source
 import Character.isWhitespace
 
+/* Scala KWARC Imports */
 import info.kwarc.mmt.api.parser.SourceRef
 import info.kwarc.mmt.api.parser.SourceRegion
 import info.kwarc.mmt.api.parser.SourcePosition
 
 import info.kwarc.mmt.api.utils.Unparsed
-
 import info.kwarc.mmt.api._
 import frontend._
 import utils._
+
+/* Parser abstract class and case classes. */
 
 abstract class LispExp {
     override def toString() : String = "<~ tokenized but unparsed expression ~>"
@@ -30,7 +34,8 @@ case class Str(str : String) extends LispExp {
     override def toString() : String = { return "Str(" + str + ")"}
 }
 
-// small IMPS Special Forms
+/* IMPS SPECIAL FORM ARGUMENTS */
+
 case class Theory(thy : String, src : SourceRef) extends LispExp {
     override def toString() : String = { return "(theory " + thy + ")"}
 }
@@ -64,18 +69,23 @@ case class LoadSection(section : String, src : SourceRef) extends LispExp {
     override def toString() : String = { return "(load-section " + section + ")"}
 }
 
-// larger IMPS Special Forms
+/* IMPS SPECIAL FORMS */
 
-case class AtomicSort(sortName        : String,
-                      quasiSortString : String,
-                      theory          : Option[Theory],
-                      usages          : Option[Usages],
-                      witness         : Option[Witness],
-                      src : SourceRef) extends LispExp {
-    override def toString() : String = {
+/* def-atomic-sort 
+ * Documentation: IMPS manual pgs. 158, 159 */
+case class AtomicSort(sortName        : String,	         /* Positional Argument, Required */
+                      quasiSortString : String,          /* Positional Argument, Required */
+                      theory          : Theory,          /* Keyword Argument, Required */
+                      usages          : Option[Usages],  /* Keyword Argument, Optional */
+                      witness         : Option[Witness], /* Keyword Argument, Optional */
+                      src             : SourceRef)       /* SourceRef for MMT */
+                      extends LispExp
+{
+    override def toString() : String =
+    {
         var str : String = "(def-atomic-sort " + sortName
         str = str + "\n  " + quasiSortString
-        if (!(theory.isEmpty)) { str = str + "\n  " + theory.get.toString}
+        str = str + "\n  " + theory.toString
         if (!(usages.isEmpty)) { str = str + "\n  " + usages.get.toString}
         if (!(witness.isEmpty)) { str = str + "\n  " + witness.get.toString}
         str = str + ")"
@@ -83,12 +93,17 @@ case class AtomicSort(sortName        : String,
     }
 }
 
+/* ######### PARSER ######### */
+
 class LispParser
 {
     def parse(s: String): LispExp = parse(new Unparsed(s, msg => throw GeneralError(msg)))
 
-    private def parse(u: Unparsed): LispExp =
+    /* Take an Unparsed object (info.kwarc.mmt.api.utils.Unparsed) 
+     * and parse the heck out of it */
+    private def parse(u : Unparsed) : LispExp =
     {
+		/* Expression starts at the very beginning */
         val sr_start : SourcePosition = u.getSourcePosition
         var exprs : List[Exp] = List.empty
 
@@ -106,6 +121,9 @@ class LispParser
         }
         catch
         {
+			// TODO: Re-work this into something else other than
+			//       an ugly try-catch block
+			
             case ex : StringIndexOutOfBoundsException =>
             {
                 // Some printouts for manual inspection, to be removed later
@@ -113,44 +131,49 @@ class LispParser
             }
         }
 
+        /* Expression ends after parsing has finished. */
         val sr_end    : SourcePosition = u.getSourcePosition
         val sr_region : SourceRegion   = SourceRegion(sr_start, sr_end)
         val sr        : SourceRef      = SourceRef(null, sr_region)
 
-		// Actually parse Exps and filter for successes
+		/* Actually parse Exps and filter for successes */
         val parsedExprs : List[LispExp] = exprs.map(parseExpression).filter(y => !(y.isEmpty)).map(z => z.get)
 
-		// Truncate output.t to 0 length
+		/* Truncate output.t to 0 length */
 		val pw = new PrintWriter("output.t");
 		pw.close
 		
-		// Print parsed expressions for diff
+		/* Print parsed expressions for diff */
 		val fw = new FileWriter("output.t", true)
         for (p <- parsedExprs)
         {
             println("\n" + p.toString)
-            fw.write(p.toString + "\n")
+            fw.write(p.toString + "\n\n")
         }
         fw.close()
 
-        // Return one expression with all the smaller expressions as children
+        /* Return one expression with all the smaller expressions as children */
         return Exp(parsedExprs, sr)
     }
 
+    /* Create an EXP expression from Unparsed object, until brackets 
+     * are balanced again. This way, it is extremely easy to parse the
+     * correct amount of source. Yay Lisp. */
     private def parseExpAndSourceRef (u : Unparsed) : Exp =
     {
         val sourceRef_start : SourcePosition = u.getSourcePosition
         var children : List[Exp] = List.empty
 
-        var open   : Int = 1
+        var open   : Int = 1 /* we started with an open bracket */
         var closed : Int = 0
 
-        // While expression is still ongoing
+        /* While expression is still ongoing, keep parsing */
         while (open - closed != 0)
         {
+			// TODO: Casematch here more beautiful?
             if (u.head == '"')
             {
-                // String literal parsing (defStrings etc.)
+                /* String literal parsing (defStrings etc.) */
                 val sr_start : SourcePosition = u.getSourcePosition
 
                 u.next
@@ -165,8 +188,8 @@ class LispParser
             }
             else if (u.head == '(')
             {
-                // Some children are complete expressions in themselves
-                // necessitating a recursive call
+                /* Some children are complete expressions in themselves 
+                 * necessitating a recursive call here */
                 u.next
                 val chld : Exp = parseExpAndSourceRef(u)
                 children = children ::: List(chld)
@@ -174,18 +197,18 @@ class LispParser
             }
             else if (u.head == ')')
             {
-                // Don't forget to take care of closed brackets
+                /* Don't forget to take care of closed brackets */
                 closed += 1
             }
             else if (isWhitespace(u.head))
             {
-                // Skip through whitespace
+                /* Skip through whitespace */
                 u.next
             }
             else
             {
-                // Some children, as for example but not limited to
-                // names, are just blank strings
+                /* Some children, as for example but not limited to
+                 * names, are just blank strings */
                 val sr_start : SourcePosition = u.getSourcePosition
 
                 val str : String = u.takeWhile(c => !(isWhitespace(c)) && c != ')')
@@ -194,6 +217,7 @@ class LispParser
                 val sr_region : SourceRegion   = SourceRegion(sr_start, sr_end)
                 val sr        : SourceRef      = SourceRef(null, sr_region)
 
+				// TODO: Is this nesting overkill / overcommplicated?
                 children = children ::: List(Exp(List(new Str(str)), sr))
             }
             // TODO: This doesn't handle lists (e.g. of names, lambdas) correctly, I think
@@ -202,33 +226,31 @@ class LispParser
         val sourceRef_end    : SourcePosition = u.getSourcePosition
         val sourceRef_region : SourceRegion   = SourceRegion(sourceRef_start, sourceRef_end)
         val sourceRef        : SourceRef      = SourceRef(null, sourceRef_region)
-        // TODO: Implement correct URI
+        // TODO: Implement correct MMT-URI
 
         return Exp(children, sourceRef)
     }
 
+    /* Parse a single EXP expression into a special form or similar (if possible) */
     private def parseExpression (e : Exp) : Option[LispExp] =
     {
+		/* Patter matching down/through to appropriate level */
         e.children.head match
         {
-            case Exp(cs,s) => cs.head match {
+            case Exp(cs,s) => cs.head match
+            {
+				/* toplevel stuff */
 				case Str("herald") => var eprime : Option[LispExp] = parseHeralding(e)
-                                      if (!(eprime.isEmpty)) {
-                                          println("DBG: heralding parsed")
-                                          return eprime
-                                      } else { println("DBG: heralding not parsed") }
+                                      if (!(eprime.isEmpty)) { return eprime }
                                       
                 case Str("load-section") => var eprime : Option[LispExp] = parseLoadSection(e)
-                                            if (!(eprime.isEmpty)) {
-                                                println("DBG: load-section parsed")
-                                                return eprime
-                                            } else { println("DBG: load-section not parsed") }
-                                            
+                                            if (!(eprime.isEmpty)) { return eprime }
+                
+                /* Actual IMPS special forms */
 			    case Str("def-atomic-sort") => var as : Option[LispExp] = parseAtomicSort(e)
-                                               if (!(as.isEmpty)) {
-                                                   println("DBG: atomic sort parsed")
-                                                   return as
-                                               } else { println("DBG: atomic sort not parsed") }
+                                               if (!(as.isEmpty)) { return as }
+                                               
+                /* Catchall case */
                 case _                      => println("DBG: unrecognised structure, not parsed!")
             }
              
@@ -240,50 +262,58 @@ class LispParser
         return None
     }
 
-    // ######################### Smaller Parsers
+    /* ######### Smaller parsers, mosty IMPS special forms ######### */
 
+    /* Parser for IMPS special form def-atomic sort 
+     * Documentation: IMPS manual pgs. 158, 159 */
     private def parseAtomicSort (e : Exp) : Option[LispExp] =
     {
+		// Required arguments
         var name : Option[String] = None
         var qss  : Option[String] = None
-
-        var thy     : Option[Theory]  = None
+        var thy  : Option[Theory] = None
+        
+        // Optional arguments
         var usages  : Option[Usages]  = None
         var witness : Option[Witness] = None
 
         val cs : Int = e.children.length
-        if (cs >= 2)
+        
+        /* Three arguments minimum because three req. arguments */
+        if (cs >= 3)
         {
             /* Parse positional arguments */
             e.children(1) match { case Exp(List(Str(x)), _) => name = Some(x) }
             e.children(2) match { case Exp(List(Str(y)), _) => qss  = Some(y) }
 
-            /* Parse modifier and keyword arguments */
+            /* Parse keyword arguments, these can come in any order */
             var i : Int = 3
             while (cs - i > 0)
-            {
-				println(e.children(i).toString)
-				
-				e.children(i) match{
-					case Exp(ds,src) => ds.head match {
-						case Exp(List(Str("theory")),_)  => println("parsing theory") ; thy     = parseTheory(Exp(ds,src))
-						case Exp(List(Str("usages")),_)  => println("parsing usages") ; usages  = parseUsages(Exp(ds,src))
-						case Exp(List(Str("witness")),_) => println("parsing witness") ; witness = parseWitness(Exp(ds,src))
-						case _         => ()
+            {				
+				e.children(i) match {
+					case Exp(ds,src) => ds.head match
+					{
+						case Exp(List(Str("theory")),_)  => thy     = parseTheory(Exp(ds,src))
+						case Exp(List(Str("usages")),_)  => usages  = parseUsages(Exp(ds,src))
+						case Exp(List(Str("witness")),_) => witness = parseWitness(Exp(ds,src))
+						case _                           => ()
 					}
-					case _                                  => ()
+					case _ => ()
 				}
                 i += 1
             }
 
-            if (name.isEmpty || qss.isEmpty) { return None }
-            else { return Some(AtomicSort(name.get, qss.get, thy, usages, witness, e.src)) }
+			/* check for required arguments */
+            if (name.isEmpty || qss.isEmpty || thy.isEmpty) { return None }
+            else { return Some(AtomicSort(name.get, qss.get, thy.get, usages, witness, e.src)) }
 
         } else { return None }
     }
 
-    // ######### Tiny parsers
+    /* ######### Tiny parsers, mostly arguments to def-forms ######### */
     
+    /* Parser for IMPS load-section objects
+     * used in: toplevel imports */
     private def parseLoadSection (e : Exp) : Option[LoadSection] = 
     {
 		if (e.children.length == 2)
@@ -295,6 +325,8 @@ class LispParser
         } else { return None }
 	}
 
+    /* Parser for IMPS herald objects
+     * used in: toplevel module declaration */
     private def parseHeralding (e : Exp) : Option[Heralding] =
     {
         if (e.children.length == 2)
@@ -306,6 +338,8 @@ class LispParser
         } else { return None }
     }
 
+    /* Parser for IMPS theory objects
+     * used in: def-atomic-sort... */
     private def parseTheory (e : Exp) : Option[Theory] =
     {
         if (e.children.length == 2) {
@@ -316,6 +350,8 @@ class LispParser
         } else { return None }
     }
 
+    /* Parser for IMPS witness objects
+     * used in: def-atomic-sort, ... */
     private def parseWitness (e : Exp) : Option[Witness] =
     {
         if (e.children.length == 2) {
@@ -326,14 +362,16 @@ class LispParser
         } else { return None }
     }
 
+    /* Parser for IMPS usages objects
+     * used in: def-atomic-sort */
     private def parseUsages (e : Exp) : Option[Usages] =
     {
+		// Can contain one or multiple usages
         var usgs : List[String] = List.empty
 
         if (e.children.length >= 2)
         {
             var i : Int = 2
-
             while (i <= e.children.length)
             {
                 e.children(i) match
@@ -356,6 +394,7 @@ class IMPSImporter extends ShellExtension("imps")
 
     def run(shell: Shell, args: List[String]) =
     {
+		/* Read first argument as filepath, load and parse file */
         println("Scanning file " + args(0))
         val fileContents = Source.fromFile(args(0)).getLines.mkString
 
