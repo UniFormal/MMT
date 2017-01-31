588aee586c1dc6b481f7e4ce593a74b09e7fd737
Jonas Betzendahl <jbetzend@techfak.uni-bielefeld.de>
2017-01-31 13:58:08 +0100

    Forgot to add Theory to git

---
+++ src/mmt-imps/src/info/kwarc/mmt/imps/IMPSTheory.scala
@@ -0,0 +1,38 @@
+package info.kwarc.mmt.imps
+
+import info.kwarc.mmt.api._
+import info.kwarc.mmt.api.objects._
+import info.kwarc.mmt.api.symbols.FinalConstant
+import info.kwarc.mmt.api.uom._
+import info.kwarc.mmt.lf.{Lambda, Apply, ApplySpine}
+import utils._
+import objects.Conversions._
+
+object IMPSTheory
+{
+  val rootdpath = DPath(URI.http colon "latin.omdoc.org/foundations/lutins")
+  val thname = "Lutins"
+
+  val thpath = rootdpath ? thname
+
+  class Sym(s: String)
+  {
+    val path = thpath ? s
+    val term = OMS(path)
+  }
+
+  object tp extends Sym("tp")
+
+  object exp extends Sym("exp")
+  {
+    def apply(t:Term) = Apply(this.term,t)
+  }
+
+  object impsLambda extends Sym("lambda")
+  {
+    def apply(typeA : Term, typeB : Term, sortA : Term, sortB : Term, funAB : Term) =
+    {
+       ApplySpine(this.term, typeA, typeB, sortA, sortB, funAB)
+    }
+  }
+}
