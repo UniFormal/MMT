b3f53cb2b689cb8c8002dba1fe5e4b90118563e5
Tom Wiesing <tkw01536@gmail.com>
2017-02-21 17:52:43 +0100

    Add active computation plugin
    Implement a basic version of the Active Computation Plugin to be used
    for OpenDreamKit deliverable 4.9

---
+++ src/mmt-odk/src/info/kwarc/mmt/odk/activecomp/Plugin.scala
@@ -0,0 +1,56 @@
+package info.kwarc.mmt.odk.activecomp
+
+import info.kwarc.mmt.api.utils.{JSON, JSONObject, JSONString, JSONArray}
+import info.kwarc.mmt.api.web.{Body, Server, ServerExtension, Session}
+import jdk.nashorn.internal.runtime.JSONListAdapter
+import tiscaf.HLet
+
+import scala.collection.immutable.List
+
+class Plugin extends ServerExtension("activecomp") {
+  /**
+    * handles the HTTP request
+    *
+    * @param path  the PATH from above (excluding CONTEXT)
+    * @param query the QUERY from above
+    * @param body  the body of the request
+    * @return the HTTP response
+    *
+    *         Implementing classes can and should use Server.XmlResponse etc to construct responses conveniently.
+    *
+    *         Errors thrown by this method are caught and sent back to the browser.
+    */
+  def apply(path: List[String], query: String, body: Body, session: Session): HLet = {
+    path match {
+      case List("actions") => handleActions(body)
+      case Nil => handleComputation(body)
+    }
+  }
+
+  /* handles listing all active computations */
+  def handleActions(body : Body): HLet = {
+    Server.JsonResponse(JSONObject.fromList(activeComputation.all.map(ac => (JSONString(ac.key), JSONString(ac.desc)))))
+  }
+
+  /** handles an active computation */
+  def handleComputation(body: Body): HLet = {
+    // read the objective and the context
+    val objective = body.asJSON.asInstanceOf[JSONObject]
+
+    // get the math element we are talking about
+    val context = objective("context").get.asInstanceOf[JSONObject]
+    val math = scala.xml.XML.loadString(context("math").get.asInstanceOf[JSONString].value)
+
+    // read the values for the computation into an Active Computation Context
+    val values = ACContext.fromJSON(objective("values").get)
+
+    // parse the action we want to make
+    val action = activeComputation.parse(objective("action").get.asInstanceOf[JSONString].value).get
+
+    // and compute
+    val response = action(math, values)
+
+    // and return some json
+    Server.JsonResponse(response.toJSON)
+  }
+}
