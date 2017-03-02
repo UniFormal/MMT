e29f58655078d3c62e3ad7bf6c26c689feaa4aab
Tom Wiesing <tkw01536@gmail.com>
2017-03-02 16:29:38 +0100

    Try to load the ExtendedREPL by default
    This commit changes the behaviour when trying to load a REPL. If
    available, MMT will now load the ExtendedREPL (which depends on jLine3).
    When not available, it will as previously use the StandardREPL.

--- src/mmt-api/src/main/info/kwarc/mmt/api/frontend/Shell.scala
+++ src/mmt-api/src/main/info/kwarc/mmt/api/frontend/Shell.scala
@@ -191,6 +191,9 @@ class Shell extends StandardIOHelper {
 
     // if we want a shell, prompt and handle input
     if (args.prompt) {
+        // try to load the extended repl by default
+        controller.extman.addExtensionO("info.kwarc.mmt.repl.ExtendedREPL", List())
+
         repl.enter(args)
 
         // run the repl and cleanup
