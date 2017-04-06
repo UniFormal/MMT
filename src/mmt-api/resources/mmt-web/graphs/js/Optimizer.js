5df49a39525e7c00f8c06189a7a883a968fbea29
Shadow992 <m.rupprecht@gmx.net>
2017-04-07 01:51:47 +0200

    Minor Fixes

--- src/mmt-api/resources/mmt-web/graphs/js/Optimizer.js
+++ src/mmt-api/resources/mmt-web/graphs/js/Optimizer.js
@@ -280,7 +280,7 @@ function Optimizer(nodes, edges)
 		var success = 0;
 
 		var area = myWidth * myHeight;
-		var kVal =   (myAllNodes.length+edgesCount)/2/countNodesInGraph.length * 0.4;
+		var kVal =   (myAllNodes.length+edgesCount)/2 * 0.5;
 		var kSquared = kVal * kVal;
 
 		for( var i = 0; i < iterations; i++ )
