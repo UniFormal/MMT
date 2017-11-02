45c2bf89ee547774ee93411fcf0cd63a1aba5633
Dennis Müller <d.mueller@jacobs-university.de>
2017-11-02 16:16:46 +0100

    Somewhat improved testing stuff

--- src/mmt-api/test/scala/info/kwarc/mmt/api/test/Test.scala
+++ src/mmt-api/test/scala/info/kwarc/mmt/api/test/Test.scala
@@ -15,7 +15,7 @@ abstract class MMTTest(archives : String*)(extensions : String*) extends FlatSpe
       "info.kwarc.mmt.api.web.JSONBasedGraphServer"
   )
   val setupFolder = File("test/resources").canonical
-  val content = setupFolder / "content" 
+  val content = setupFolder / "content"
   lazy val mathhub = new MathHub(MathHub.defaultURL,content,report = controller.report,https=true)
   def hl(s : String) = controller.handleLine(s)
   def shouldhl(s : String) = it should s in hl(s)
@@ -25,6 +25,10 @@ abstract class MMTTest(archives : String*)(extensions : String*) extends FlatSpe
   val setup = new Setup
   controller.extman.addExtension(setup)
   setup.setup(setupFolder/"system", content, None)
+  (archives.toList) foreach (a =>
+      if (!(content / a).toJava.exists()) it should "git clone " + a in mathhub.clone(a)
+      else it should "git pull " + a in mathhub.pull(a)
+  )
 
   (standardextensions ::: extensions.toList) foreach (e =>
     it should "add Extension " + e in hl("extension " + e)
@@ -38,7 +42,7 @@ abstract class MMTTest(archives : String*)(extensions : String*) extends FlatSpe
   // override protected def afterAll(): Unit = content.deleteDir
 }
 
-class APITest extends MMTTest("MMT/examples")() {
+class APITest extends MMTTest()() {
   behavior of "APITest"
   lazy val brackets = (DPath(URI.http colon "cds.omdoc.org") / "mmt") ? "mmt" ? "brackets"
   it should "get a Constant: " in controller.getConstant(brackets)
--- src/mmt-lf/test/scala/info/kwarc/mmt/lf/test/LFTest.scala
+++ src/mmt-lf/test/scala/info/kwarc/mmt/lf/test/LFTest.scala
@@ -2,10 +2,18 @@ package scala.info.kwarc.mmt.lf.test
 
 import scala.info.kwarc.mmt.api.test.MMTTest
 
-class LFTest extends MMTTest("MMT/LFX","MitM/Foundation","MMT/examples")("info.kwarc.mmt.lf.Plugin") {
+class LFTest extends MMTTest("MMT/LFX","MitM/Foundation","Test/General")("info.kwarc.mmt.lf.Plugin") {
   behavior of "LF"
-  //shouldhl("build MMT/urtheories scala-bin")
+  // shouldhl("build MMT/urtheories scala-bin")
   shouldhl("build MMT/urtheories mmt-omdoc")
   //shouldhl("build MMT/examples scala-bin")
-  shouldhl("build MMT/examples mmt-omdoc")
+  //shouldhl("build MMT/examples mmt-omdoc")
+  it should "fully build MMT/LFX: " in {
+    // hl("build MMT/LFX scala-bin")
+    hl("build MMT/LFX mmt-omdoc")
+  }
+
+  shouldhl("build Test/General mmt-omdoc hol.mmt")
+  shouldhl("build Test/General mmt-omdoc views.mmt")
+  shouldhl("build Test/General mmt-omdoc structures.mmt")
 }
