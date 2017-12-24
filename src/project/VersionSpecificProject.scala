5f2fd2e776f93fc1bdb840fbd2bd025eee2f64e4
Tom Wiesing <tkw01536@gmail.com>
2017-12-24 21:42:17 +0100

    Make project exclusions ProjectReferences
    This commit makes project exclusions a ProjectReference instance instead
    of a Project instance, allowing for non-local projects to be excluded.

--- src/project/VersionSpecificProject.scala
+++ src/project/VersionSpecificProject.scala
@@ -1,17 +1,23 @@
 import sbt._
 
-
+/**
+  * Represents a project which excludes a given list of projects from aggregation and dependency
+  * @param project Project begin wrapped
+  * @param excludes Object representing exclusions
+  */
 case class VersionSpecificProject(project: Project, excludes: Exclusions) {
+  /** sets an [[Exclusions]] object to exclude specific projects */
   def exclusions(newExclusions : Exclusions) : VersionSpecificProject = VersionSpecificProject(project, newExclusions)
 
-  def aggregate(projects: Project*) : Project = {
-    def toProjectReference(p : Project) : ProjectReference = p
+  /** aggregates a list of projects in this project, excluding the configured [[Exclusions]]s */
+  def aggregate(projects: ProjectReference*) : Project = {
     project
-      .aggregate(excludes(projects.toList).map(toProjectReference(_)) :_*)
+      .aggregate(excludes(projects.toList) :_*)
   }
 
-  def dependsOn(projects: Project*) : Project = {
-    def toClassPathDep(p: Project) : ClasspathDep[ProjectReference] = p
+  /** marks this project as depending on a given set of projects */
+  def dependsOn(projects: ProjectReference*) : Project = {
+    def toClassPathDep(p: ProjectReference) : ClasspathDep[ProjectReference] = p
     project.dependsOn(excludes(projects.toList).map(toClassPathDep(_)) :_*)
   }
 }
@@ -21,23 +27,36 @@ object VersionSpecificProject {
   implicit def toProject(vProject: VersionSpecificProject): Project = vProject.project
 }
 
-case class Exclusions(lst: Project*) {
-  private def javaVersion(versions: List[String], exclusions: List[Project]) : Exclusions = {
+case class Exclusions(lst: ProjectReference*) {
+  private def javaVersion(versions: List[String], exclusions: List[ProjectReference]) : Exclusions = {
     val theVersion = System.getProperty("java.version")
     if(versions.exists(v => theVersion.startsWith(s"$v."))){:::(exclusions)} else this
   }
-  def java7(exclusions: Project*): Exclusions = javaVersion(List("1.7", "7"), exclusions.toList)
-  def java8(exclusions: Project*): Exclusions = javaVersion(List("1.8", "8"), exclusions.toList)
-  def java9(exclusions: Project*): Exclusions = javaVersion(List("1.9", "9"), exclusions.toList)
+  def java7(exclusions: ProjectReference*): Exclusions = javaVersion(List("1.7", "7"), exclusions.toList)
+  def java8(exclusions: ProjectReference*): Exclusions = javaVersion(List("1.8", "8"), exclusions.toList)
+  def java9(exclusions: ProjectReference*): Exclusions = javaVersion(List("1.9", "9"), exclusions.toList)
 
-  def :::(lst2: List[Project]) = Exclusions(lst.toList ::: lst2 : _*)
+  def :::(lst2: List[ProjectReference]) = Exclusions(lst.toList ::: lst2 : _*)
   def :::(other: Exclusions) = Exclusions(lst.toList ::: other.lst.toList :_*)
 
   def toFilter : ScopeFilter.ProjectFilter = {
-    def toProjectReference(p : Project) : ProjectReference = p
-    inAnyProject -- inProjects(lst.map(toProjectReference) :_*)
+    inAnyProject -- inProjects(lst :_*)
   }
 
-  def excludes(project: Project) : Boolean = lst.contains(project)
-  def apply(projects: List[Project]) : List[Project] = projects.filterNot(this.excludes)
+  /** check if two references are equal */
+  private def equals(left: ProjectReference, right: ProjectReference) : Boolean = {
+    // either they are equal directly
+    (left == right) || {
+      val leftUri = Reference.uri(left)
+      val rightUri = Reference.uri(right)
+      // or their valid URIs are equal
+      leftUri.isDefined && rightUri.isDefined && leftUri.get == rightUri.get
+    }
+  }
+
+  def excludes(project: ProjectReference) : Boolean = lst.exists(equals(_, project))
+  def apply(projects: List[ProjectReference]) : List[ProjectReference] = projects.filterNot(this.excludes)
+
+  def map[B](f: ProjectReference => B) : Seq[B] = lst.map(f)
+  def foreach[U](f: ProjectReference => U) : Exclusions = {lst.foreach[U](f); this }
 }
