3f2b3c59dd075328662da5da31b793eead6b226c
Tom Wiesing <tkw01536@gmail.com>
2018-02-06 17:29:02 +0100

    Add prasedQuery to debug page

--- src/mmt-api/src/main/info/kwarc/mmt/api/web/Server.scala
+++ src/mmt-api/src/main/info/kwarc/mmt/api/web/Server.scala
@@ -124,6 +124,7 @@ class Server(val port: Int, val host: String, controller: Controller) extends Ti
       s"HTTP Request Method: ${RequestMethod.toString(request.method)}",
       s"HTTP Request Path:   ${request.pathString}",
       s"HTTP Query String:   ${request.query}",
+      s"HTTP Parsed Query:   ${request.parsedQuery}",
       "",
       "HTTP Header fields:",
       request.headers.map(kv => s"${kv._1}: ${kv._2}").mkString("\n")
