e8df9e7a868d5708ad821c0943342800dbb45f89
Jonas Betzendahl <jonas.betzendahl@fau.de>
2018-04-17 14:16:01 +0200

    modified test file

--- src/test/IMPSTest.scala
+++ src/test/IMPSTest.scala
@@ -1,4 +1,6 @@
-object IMPSTest extends JonasTest() {
+// Helpful: debug, object-checker
+
+object IMPSTest extends JonasTest("debug") {
   override def doFirst: Unit = {
     hl("extension info.kwarc.mmt.imps.IMPSImporter")
     hl("build imps imps-omdoc")
