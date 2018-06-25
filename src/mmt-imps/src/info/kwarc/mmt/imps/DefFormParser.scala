184e3b5e57e1db3f2383bd0eb5f9e703ba35f4bd
Jonas Betzendahl <jonas.betzendahl@gmail.com>
2018-06-26 00:16:50 +0200

    more combinator love

--- src/mmt-imps/src/info/kwarc/mmt/imps/DefFormParser.scala
+++ src/mmt-imps/src/info/kwarc/mmt/imps/DefFormParser.scala
@@ -34,9 +34,9 @@ object DefFormParser extends Parsers with UnparsedParsers
     }
   }
 
-  lazy val parseImpsSource : PackratParser[List[DefForm]] = { rep1(parseDefForms) }
+  lazy val parseImpsSource : PackratParser[List[DefForm]] = { rep1(parseDefForm) }
 
-  lazy val parseDefForms : PackratParser[DefForm] = { parseLineComment | parseHeralding }
+  lazy val parseDefForm : PackratParser[DefForm] = { parseLineComment | parseHeralding }
 
   lazy val parseName = { regex("""[^()\t\r\n ]+""".r) }
   lazy val parseText = { regex("""[^\r\n]+""".r) }
@@ -45,15 +45,30 @@ object DefFormParser extends Parsers with UnparsedParsers
     (";" ~> parseText) ^^ { case txt => LineComment(txt.dropWhile(_ == ';').trim, None, None) }
   }
 
-  lazy val pHeralding: PackratParser[Heralding] = {
-    ("(herald " ~> parseName <~ ")") ^^ { case name => Heralding(name, None, None) }
-  }
-
   def parseLineComment : DefFormParser[LineComment] = { new DefFormParser[LineComment](pLineComment) }
 
+  /* I <3 Parser Combinators */
   def withComment[T <: DefForm](dfp : DefFormParser[T]) : Parser[T] = {
     (dfp ~ (parseLineComment?)) ^^ { case r ~ c => r.addComment(c) ; r }
   }
 
-  def parseHeralding : Parser[Heralding] = { withComment(new DefFormParser[Heralding](pHeralding)) }
+  def fullParser[T <: DefForm](p : Parser[T]) : Parser[T] = { withComment(new DefFormParser[T](p)) }
+
+  /* Positional Arguments must all appear in exactly the order given */
+  def positional(ps : List[Parser[DefForm]]) : Parser[List[DefForm]] = {
+    (fullParser(ps.head) ~ positional(ps.tail)) ^^ { case p ~ ps => List(p) ::: ps }
+  }
+
+  /* Keyword Arguments are all optional and can appear in any order */
+  def keyworded(ks : List[Parser[DefForm]]) : Parser[List[DefForm]] = {
+    /* Too tired now, but idea: transform all parsers in list from Parsers for X to Parsers for (X,i) where i is
+     * the index in the original list, then return arrangement sorted via index. Should be doable? */
+    ???
+  }
+
+  lazy val pHeralding: PackratParser[Heralding] = {
+    ("(herald " ~> parseName <~ ")") ^^ { case name => Heralding(name, None, None) }
+  }
+
+  def parseHeralding : Parser[Heralding] = fullParser(pHeralding)
 }
