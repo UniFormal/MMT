9b6f66abb5d2811226bcb13fb535ae1acde241ec
Colin Rothgang <colin.rothgang@gmx.de>
2018-07-05 09:51:43 +0200

    Cleanups.

--- src/mmt-lf/src/info/kwarc/mmt/lf/TypedRelationalExtractor.scala
+++ src/mmt-lf/src/info/kwarc/mmt/lf/TypedRelationalExtractor.scala
@@ -19,35 +19,43 @@ class TypedRelationalExtractor extends RelationalExtractor {
    val allUnary = List(IsDatatypeConstructor, IsDataConstructor, IsJudgementConstructor, IsRule, IsHighUniverse)
    val allBinary = Nil
            
-   /** apply a continuation function to every relational element of a StructuralElement */
+   /** Discriminate the constant declarations contained in every structural element into the types data constructor, datatype constructor, rule, judgement constructor and high universe. 
+    * Convert them into relational elements and apply a continuation function f to each such relational element
+    * @note postcondition: Each constant in e, is annotated as exactly one of the types: 
+    * data constructor, datatype constructor, rule, judgement constructor or high universe
+    * 
+    * @param e the structural element to look for constants in
+    * @param f the continuation function
+    */
    def apply(e: StructuralElement)(implicit f: RelationalElement => Unit) {
       val path = e.path
       e match {
          case t: DeclaredModule =>
             t.getDeclarations foreach {
 					  case c: Constant =>
-               	c.tp match {
+               	val declType : Unary = c.tp match {
                	  case Some(x) => x match {
                  	  case FunType(_, tp) => tp match {
                    	  case Univ(1) => 
                    	    if (controller.globalLookup.getConstant(c.path).rl.contains("Judgment")) {
-                   	      f(IsJudgementConstructor(c.path))
+                   	      IsJudgementConstructor
                    	    }
                    	    else {
-                   	      f(IsDatatypeConstructor(c.path))
+                   	      IsDatatypeConstructor
                         }
-                   	  case Univ(n) if n > 1 => println("found high universe at "+c.path.toString()); (IsHighUniverse(c.path))
+                   	  case Univ(n) if n > 1 => println("found high universe at "+c.path.toString()); IsHighUniverse
                    	  case _ => 
                    	    if (controller.globalLookup.getConstant(c.path).rl.contains("Judgment")) {
-                   	      f(IsRule(c.path))
+                   	      IsRule
                    	    }
                    	    else {
-                   	      f(IsDataConstructor(c.path))
+                   	      IsDataConstructor
                    	    }
                    	  }
                  	  }
-               	  case None => f(IsUntypedConstant(c.path))
+               	  case None => IsUntypedConstant
                	}
+               	f(declType(c.path))
             }
          case _ =>
       }
