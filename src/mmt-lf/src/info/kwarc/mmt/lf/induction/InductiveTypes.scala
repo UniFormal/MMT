77f1a27bb32cd8c919b3f6a187326c76f8eb95b0
Colin Rothgang <colin.rothgang@gmx.de>
2018-07-17 18:11:26 +0200

    More cleanups.

--- src/mmt-lf/src/info/kwarc/mmt/lf/induction/InductiveTypes.scala
+++ src/mmt-lf/src/info/kwarc/mmt/lf/induction/InductiveTypes.scala
@@ -29,43 +29,38 @@ class InductiveTypes extends StructuralFeature("inductive") with ParametricTheor
    * @param dd the derived declaration to be elaborated
    */
   def elaborate(parent: DeclaredModule, dd: DerivedDeclaration) = {
-    val context = Type.getParameters(dd)
-    //log("Path of eq: "+Eq.path+"\nPath of neq: "+Neq.path+"\nPath of contradiction: "+Contra.path)
-    implicit val parentTerm = parent.toTerm
+    val context = Type.getParameters(dd) 
+//Type.getParameters(dd)//getInnerContext(dd)//Context.empty//Type.getParameters(dd)//this.getInnerContext(dd)//Type.getParameters(dd)//this.getInnerContext(dd)
+    val name = LocalName(dd.path.last)
+    implicit val parentTerm = OMID(parent.path / name)
     // to hold the result
     var elabDecls : List[Constant] = Nil
     implicit var tmdecls : List[TermLevel]= Nil
     implicit var statdecls : List[StatementLevel]= Nil
     implicit var tpdecls : List[TypeLevel]= Nil
-    try {
-      val decls = dd.getDeclarations map {
+    val decls = dd.getDeclarations map {
       case c: Constant =>
-        val intDecl = InternalDeclaration.fromConstant(c, controller)
+        val intDecl = InternalDeclaration.fromConstant(c, controller, (context++dd.getDeclarations filter {case _ : Constant => true
+        case _ => false}) map({case c:Constant=>VarDecl.apply(c.name, None, c.tp, c.tp, None)}))
         intDecl match {
           case d @ TermLevel(_, _, _, _, _) => tmdecls :+= d; intDecl
           case d @ TypeLevel(_, _, _, _) => tpdecls :+= d; intDecl
           case d @ StatementLevel(_, _, _, _) => statdecls :+= d; intDecl 
-        }
+         }
       case _ => throw LocalError("unsupported declaration")
-      }
-     
-      // copy all the declarations
-      decls foreach {d => elabDecls ::= d.toConstant}
-      
-      // the no confusion axioms for the data constructors
-      elabDecls = elabDecls.reverse ::: TermLevel.noConfs(tmdecls, context)
-      
-      // the no junk axioms
-      elabDecls ++= InternalDeclaration.noJunk(decls, tpdecls, tmdecls, statdecls, context)
-      
-      elabDecls foreach {d =>
-        println(controller.presenter.asString(d).replaceAll("%2F", "/").replaceAll("\n", ""))
-      }
     }
-    catch {
-      case e : Error => 
-       log("error in elaborate: ")
-       log(e.getMessage)
+
+    // copy all the declarations
+    decls foreach {d => elabDecls ::= d.toConstant}
+    
+    // the no confusion axioms for the data constructors
+    elabDecls = elabDecls.reverse ::: TermLevel.noConfs(tmdecls, context)
+    
+    // the no junk axioms
+    elabDecls ++= InternalDeclaration.noJunk(decls, tpdecls, tmdecls, statdecls, context)
+    
+    elabDecls foreach {d =>
+      log(controller.presenter.asString(d).replaceAll("%2F", "/").replaceAll("\n", ""))
     }
     new Elaboration {
       val elabs : List[Declaration] = Nil 
