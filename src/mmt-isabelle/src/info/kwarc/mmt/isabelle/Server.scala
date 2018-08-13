c9a14db2ba7b5944aad724c9e39ea38a0ed2027a
Makarius <makarius@sketis.net>
2018-08-13 18:45:06 +0200

    basic setup for mmt_server

---
+++ src/mmt-isabelle/src/info/kwarc/mmt/isabelle/Server.scala
@@ -0,0 +1,47 @@
+package info.kwarc.mmt.isabelle
+
+import info.kwarc.mmt.api.frontend.Controller
+import info.kwarc.mmt.api.utils.File
+
+object Server
+{
+  val default_output_dir = isabelle.Path.explode(Importer.Arguments.default_output_dir)
+  val default_port: Int = 8080
+
+  def main(args: Array[String])
+  {
+    isabelle.Command_Line.tool0 {
+      var output_dir = default_output_dir
+      var port = default_port
+
+      val getopts = isabelle.Getopts("""
+Usage: isabelle mmt_server [OPTIONS]
+
+  Options are:
+    -O DIR       output directory for MMT (default: """ + default_output_dir + """)
+    -p PORT      server port (default: """ + default_port + """)
+
+  Start MMT HTTP server on output directory.
+""",
+        "O:" -> (arg => output_dir = isabelle.Path.explode(arg)),
+        "p:" -> (arg => port = isabelle.Value.Int.parse(arg)))
+
+      val more_args = getopts(args)
+      if (more_args.nonEmpty) getopts.usage()
+
+      val controller = new Controller
+      controller.setHome(File(output_dir.file))
+
+      controller.handleLine("mathpath archive .")
+      controller.handleLine("server on " + port)
+
+      println("Server http://127.0.0.1:" + port + " for archive " + output_dir.absolute)
+      println("Waiting for INTERRUPT signal ...")
+
+      try { isabelle.POSIX_Interrupt.exception { while(true) Thread.sleep(Integer.MAX_VALUE) } }
+      catch { case isabelle.Exn.Interrupt() => }
+
+      controller.handleLine("server off")
+    }
+  }
+}
