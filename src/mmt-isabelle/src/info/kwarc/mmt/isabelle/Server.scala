0382c5ac9ebe902cde9f852679c20f4093449671
Makarius <makarius@sketis.net>
2018-08-13 20:37:27 +0200

    more robust

--- src/mmt-isabelle/src/info/kwarc/mmt/isabelle/Server.scala
+++ src/mmt-isabelle/src/info/kwarc/mmt/isabelle/Server.scala
@@ -2,6 +2,7 @@ package info.kwarc.mmt.isabelle
 
 import info.kwarc.mmt.api.frontend.Controller
 import info.kwarc.mmt.api.utils.File
+import info.kwarc.mmt.api.web.Util
 
 object Server
 {
@@ -21,13 +22,15 @@ Usage: isabelle mmt_server [OPTIONS]
     -O DIR       output directory for MMT (default: """ + default_output_dir + """)
     -p PORT      server port (default: """ + default_port + """)
 
-  Start MMT HTTP server on output directory.
+  Start MMT HTTP server on localhost, using output directory as archive.
 """,
         "O:" -> (arg => output_dir = isabelle.Path.explode(arg)),
         "p:" -> (arg => port = isabelle.Value.Int.parse(arg)))
 
       val more_args = getopts(args)
       if (more_args.nonEmpty) getopts.usage()
+
+      if (Util.isTaken(port)) isabelle.error("Port " + port + " already taken")
 
       val controller = new Controller
       controller.setHome(File(output_dir.file))
