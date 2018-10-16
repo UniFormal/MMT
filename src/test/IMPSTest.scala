bf2de441abefc8528e1fe2a956f262b3b5f3b48b
Jonas Betzendahl <jonas.betzendahl@gmail.com>
2018-10-16 17:06:46 +0200

    update IMPSTest

--- src/test/IMPSTest.scala
+++ src/test/IMPSTest.scala
@@ -1,6 +1,6 @@
 // Helpful: debug, object-checker
 
-object IMPSTest extends MagicTest("debug") { //}, "object-checker") {
+object IMPSTest extends Mag:icTest("debug") { //}, "object-checker") {
   override def doFirst: Unit = {
     hl("extension info.kwarc.mmt.imps.IMPSImporter")
     hl("build imps imps-omdoc")
