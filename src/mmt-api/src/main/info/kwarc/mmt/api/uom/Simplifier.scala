50082ec06c9eea575e06bca8d3db4d8537a358fd
Dennis Müller <d.mueller@jacobs-university.de>
2018-10-23 22:24:04 +0200

    RuleBasedSimplifier.inferType and .safeSimplifyUntil

--- src/mmt-api/src/main/info/kwarc/mmt/api/uom/Simplifier.scala
+++ src/mmt-api/src/main/info/kwarc/mmt/api/uom/Simplifier.scala
@@ -26,7 +26,7 @@ trait ObjectSimplifier extends Extension {self =>
  *  @param expandDefinitions if true, expand all definitions of constant (we speak of *deep* simplification) 
  *  @param fullRecursion if false, only recurse into subexpressions that contribute to head-normalizations 
  */
-case class SimplificationUnit(context: Context, expandDefinitions: Boolean, fullRecursion: Boolean) extends MMTTask {
+case class SimplificationUnit(context: Context, expandDefinitions: Boolean, fullRecursion: Boolean,solverO :Option[Solver] = None) extends MMTTask {
   def ++(c: Context) = copy(context = context ++ c)
 }
 
