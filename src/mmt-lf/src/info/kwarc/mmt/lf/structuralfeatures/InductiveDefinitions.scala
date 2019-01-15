0fae6692ef7287ab470dd08e763359c75dffa6c9
Colin Rothgang <colin.rothgang@gmx.de>
2019-01-15 15:37:43 +0100

    Modified header parsing of inductive definitions.

--- src/mmt-lf/src/info/kwarc/mmt/lf/structuralfeatures/InductiveDefinitions.scala
+++ src/mmt-lf/src/info/kwarc/mmt/lf/structuralfeatures/InductiveDefinitions.scala
@@ -13,7 +13,7 @@ import InternalDeclaration._
 import InternalDeclarationUtil._
 
 /** theories as a set of types of expressions */ 
-class InductiveDefinitions extends StructuralFeature("inductive_definition") with ParametricTheoryLike {
+class InductiveDefinitions extends StructuralFeature("inductive_definition") {
   object noLookupPresenter extends presentation.NotationBasedPresenter {
     override def getNotations(p: GlobalName) = if (! (p.doc.uri.path contains "urtheories")) Nil else super.getNotations(p)
     override def getAlias(p: GlobalName) = if (true) Nil else super.getAlias(p)
@@ -31,6 +31,42 @@ class InductiveDefinitions extends StructuralFeature("inductive_definition") wit
    */
   override def check(dd: DerivedDeclaration)(implicit env: ExtendedCheckingEnvironment) {}
   
+  object DefType {
+    val mpath = SemanticObject.javaToMMT(getClass.getCanonicalName)
+
+    def apply(p: GlobalName, params: Context, args: List[Term]) = OMBINDC(OMMOD(mpath), params, OMS(p)::args)
+    def unapply(t: Term) = t match {
+      case OMBINDC(OMMOD(this.mpath), params, OMS(p)::args) => Some(p, params, args)
+      case _ => None
+    }
+
+    /** retrieves the parameters */
+    def getParameters(dd: DerivedDeclaration) = {
+      dd.tpC.get.getOrElse(throw LocalError("missing type of derived declaration")) match {
+        case DefType(p, pars, args) => (p, pars, args)
+      }
+    }
+  }
+
+  override def getHeaderNotation = List(LabelArg(2, LabelInfo.none), Delim("("), Var(1, true, Some(Delim(","))), SimpSeqArg(3, Delim(";"), CommonMarkerProperties.noProps), Delim(")"))
+  override def processHeader(header: Term) = header match {
+     case OMBINDC(OMMOD(`mpath`), cont, OML(name,None,None,_,_)::args) => 
+       (name, DefType(getInd(args.last)._1, cont, args))
+     case hdr => throw InvalidObject(header, "ill-formed header: "+hdr.toNode)
+  }
+  override def makeHeader(dd: DerivedDeclaration) = getParams(dd) match {
+    case (p, cont, args) => OMBINDC(OMMOD(dd.modulePath), cont, OML(dd.name, None,None)::args)
+    case _ => throw LocalError("ffkkfkffdsafdaslkhf")
+  }
+  def getInd(t: Term) : (GlobalName, List[Term]) = t match {
+    case OMA(OMS(p), args) => (p, args) 
+  }
+  def getParams(dd: DerivedDeclaration): (GlobalName, Context, List[Term]) = {
+    dd.tpC.get match {
+      case Some(OMBINDC(_, params, args)) => val (p, ags) = getInd(args.last); (p, params, ags)
+    }
+  }
+  
     
   /**
    * Elaborates an declaration of one or multiple mutual inductive types into their declaration, 
@@ -41,15 +77,25 @@ class InductiveDefinitions extends StructuralFeature("inductive_definition") wit
    */
   def elaborate(parent: Module, dd: DerivedDeclaration) = {
     implicit val parentTerm = dd.path
-    val context = Type.getParameters(dd)
-    val indTplPath : GlobalName = try {
+    val (indTplPath, context, indParams) = getParams(dd)
+    println("tpl path: "+indTplPath)
+    println("context: "+context)
+    println("indParams: "+indParams)
+      /* match {
+      case OMA(_, pars) => pars.last match {
+        case ApplyGeneral(OMS(p), indParams) =>println(p); (p, indParams)
+      }
+      case OMS(p) => (p, Nil)}
+      case t => throw LocalError("Expected path to TypeLevel but found "+noLookupPresenter.asString(t))
+    }*/
+    /*val indTplPath : GlobalName = try {
       dd.getDeclarations.head match {case c: Constant if (c.name.toString() == "tpl") => c.df.get match {case OMS(p) => p}
       case _ => throw LocalError("the first declaration should be a constant. ")}
     } catch {
       case e: NoSuchElementException => throw LocalError("The first declaration should be defined as the TypeLevel for which type the inductively-defined function is to be defined. ")
       case e: Exception => throw LocalError("the definien of the first declaration should be the declaration of the TypeLevel for which type the inductively-defined function is to be defined. ")
-    }
-    val indTpl = controller.library.get(indTplPath) match {case c: Constant => c}
+    }*/
+    val indTpl = controller.library.getO(indTplPath).getOrElse(throw LocalError("bla")) match {case c: Constant => c}
     
     val indTplName = LocalName(indTplPath.last)
     val indDerDeclPath = indTplPath.copy(name = LocalName(indTplPath.name.head))
@@ -92,7 +138,7 @@ class InductiveDefinitions extends StructuralFeature("inductive_definition") wit
     // check whether all declarations match their corresponding constructors
     decls foreach { d => checkDecl(d, fromConstant(correspondingDecl(d.name),controller, None)) }
     
-    val indParams = Type.getParameters(inductiveType)
+    //val indParams = Type.getParameters(inductiveType)
     val induct_path = indDerDeclPath / LocalName("induct_"+indTplName)
     
     val modelDf = decls map (_.df.get)
@@ -101,7 +147,7 @@ class InductiveDefinitions extends StructuralFeature("inductive_definition") wit
     val indTplDef = decls.find(_.name == indTplName).getOrElse(
         throw LocalError("No declaration found for the typelevel of the type of the inductively-defined function.")).df.get
     val Tp = PiOrEmpty(context, Arrow(indTpl.toTerm, indTplDef))
-    val Df = PiOrEmpty(context, PiOrEmpty(indTplArgs, ApplyGeneral(OMS(induct_path), indParams.map(_.toTerm)++modelDf++indTplArgs.map(_.toTerm))))
+    val Df = PiOrEmpty(context, PiOrEmpty(indTplArgs, ApplyGeneral(OMS(induct_path), indParams++modelDf++indTplArgs.map(_.toTerm))))
     
     val inductDef = makeConst(dd.name, ()=> {Tp}, () => {Some(Df)})(indDerDeclPath)
     log(defaultPresenter(inductDef)(controller))
@@ -118,7 +164,9 @@ class InductiveDefinitions extends StructuralFeature("inductive_definition") wit
    * @param decl the corresponding declaration which is to be defined by d
    * @note this will return an error if d.tp doesn't match decl.externalTp
    */
-  def checkDecl(d: InternalDeclaration, decl: InternalDeclaration) {}//TODO: This should be implemented to provide more accurate error messages
+  def checkDecl(d: InternalDeclaration, decl: InternalDeclaration) {//TODO: This should be implemented to provide more accurate error messages
+    
+  }
 }
 
 object InductiveDefinitionRule extends StructuralFeatureRule(classOf[InductiveDefinitions], "inductive_definition")
