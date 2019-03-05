07e0fc405d3dd5a4bbbe9036581e4fc3f1bdbfe3
Makarius <makarius@sketis.net>
2019-03-05 21:26:09 +0100

    tuned whitespace and comments

--- src/mmt-isabelle/src/info/kwarc/mmt/isabelle/Ontology.scala
+++ src/mmt-isabelle/src/info/kwarc/mmt/isabelle/Ontology.scala
@@ -3,7 +3,8 @@ package info.kwarc.mmt.isabelle
 import java.net.URI
 
 
-object Ontology {
+object Ontology
+{
   def unary(subject: String, predicate: String): isabelle.RDF.Triple =
     isabelle.RDF.Triple(subject, predicate, Nil)
 
@@ -27,33 +28,33 @@ object Ontology {
     val statement = ulo("statement")
 
     val theory = ulo("theory") // theory, locale
-    val instance = ulo("instance") // locale interpretation, class instantiation, unbundle (?)
+    val instance = ulo("instance") // TODO locale interpretation, class instantiation (?), unbundle (?)
 
     val section = ulo("section") // document headings: chapter, section, ..., subparagraph
-    val file = ulo("file") // auxiliary file (from thy_load command) (?)
-    val folder = ulo("folder") // Isabelle session (?)
-    val library = ulo("library") // e.g. Isabelle Distribution, AFP, IsaFoR
+    val file = ulo("file") // TODO auxiliary file (from thy_load command) (?)
+    val folder = ulo("folder") // TODO Isabelle session (?)
+    val library = ulo("library") // TODO e.g. Isabelle Distribution, AFP, IsaFoR
     val group = ulo("group")
 
-    val primitive = ulo("primitive") // e.g. axioms
-    val derived = ulo("derived") // e.g. definitions, theorems
+    val primitive = ulo("primitive") // TODO e.g. axioms
+    val derived = ulo("derived") // TODO e.g. definitions, theorems
 
     val theorem = ulo("theorem") // command "theorem" or "proposition"
     val lemma = ulo("lemma") // command "lemma" or "lemmas
     val corollary = ulo("corollary") // command "corollary"
     val conjecture = ulo("conjecture") // proof contains "sorry"
 
-    val simplification_rule = ulo("simplification-rule") // fact within simpset (??)
+    val simplification_rule = ulo("simplification-rule") // TODO fact within simpset (??)
 
-    val automatically_proved = ulo("automatically_proved") // unused!? could be result of "Judgement Day" Sledgehammer exploration
-    val experimental = ulo("experimental") // unused!? could be "theorem A oops"
-    val deprecated = ulo("deprecated") // unused!? could be command with "legacy_feature" message
+    val automatically_proved = ulo("automatically_proved") // TODO e.g. result of "Judgement Day" Sledgehammer exploration
+    val experimental = ulo("experimental") // unused
+    val deprecated = ulo("deprecated") // unused
 
 
     /* binaries */
 
     val declares = ulo("declares") // theory/locale declares item
-    val uses = ulo("uses")
+    val uses = ulo("uses") // TODO
     val type_depends_on_definition_of = ulo("type-depends-on-definition-of")
     val type_depends_on_type_of = ulo("type-depends-on-type-of")
     val definition_depends_on_type_of = ulo("definition-depends-on-type-of")
@@ -61,23 +62,22 @@ object Ontology {
 
     val generated_by = ulo("generated-by")
     val inductive_on = ulo("inductive-on")
-    val mutual_with = ulo("mutual-with") // e.g. Spec_Rules peer group
+    val mutual_with = ulo("mutual-with") // TODO e.g. Spec_Rules peer group
 
 
-    // manual markers!?
+    // TODO manual markers!?
     val same_as = ulo("same-as")
     val similar_to = ulo("similar-to")
     val alternative_for = ulo("alternative-for")
     val see_also = ulo("see-also")
     val antonym_of = ulo("antonym-of")
-
     val formalizes = ulo("formalizes")
     val aligned_with = ulo("aligned_with")
     val inspired_by = ulo("inspired_by")
 
-    val source_ref = ulo("source-ref")
-    val check_time = ulo("check-time")  // elapsed time (ms)
-    val external_size = ulo("external-size")  // source size (UTF-8 bytes)
-    val internal_size = ulo("internal-size")
+    val source_ref = ulo("source-ref") // TODO
+    val check_time = ulo("check-time") // elapsed time (ms)
+    val external_size = ulo("external-size") // source size (UTF-8 bytes)
+    val internal_size = ulo("internal-size") // unused
   }
 }
