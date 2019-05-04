37d51187941e0539ac737f3b0fad3f00f1946018
rappatoni <maxgrapp@googlemail.com>
2019-05-04 15:09:43 +0200

    Notation

--- src/mmt-contextgraphs/src/info/kwarc/mmt/contextgraphs/BinaryDefinedRelation.scala
+++ src/mmt-contextgraphs/src/info/kwarc/mmt/contextgraphs/BinaryDefinedRelation.scala
@@ -18,7 +18,13 @@ import scala.xml.Elem
 import Theory._
 import info.kwarc.mmt.api.utils.MMT_TODO
 
-class BinaryDefinedRelation(feature: String, p : DPath, n : LocalName, meta: Option[MPath], tpC: TermContainer, dfC : TermContainer, notC: NotationContainer, val fromC : TermContainer, val toC : TermContainer, val isImplicit : Boolean)
+/*class BinaryDefinedRelation(feature: String, p : DPath, n : LocalName, meta: Option[MPath], tpC: TermContainer, dfC : TermContainer, notC: NotationContainer, val fromC : TermContainer, val toC : TermContainer, val isImplicit : Boolean)
   extends DerivedModule(feature, p, n, meta, tpC, dfC, notC) with Link {
 
   def namePrefix = LocalName(path)
+} */
+
+class BinaryDefinedRelation extends ModuleLevelFeature("attack"){
+  def getHeaderNotation = List(SimpArg(1),Delim("->"), SimpArg(2), Delim("with"), SimpArg(3))
+  def check(dd: DerivedModule)(implicit env: ExtendedCheckingEnvironment) {}
+}
