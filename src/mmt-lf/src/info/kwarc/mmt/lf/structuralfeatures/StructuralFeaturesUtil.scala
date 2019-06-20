3e20236ed9908fe9d713b22b1ccb7069acbeb1b4
Colin Rothgang <colin.rothgang@gmx.de>
2019-06-20 18:20:06 +0200

    Added some utility definitions.

--- src/mmt-lf/src/info/kwarc/mmt/lf/structuralfeatures/StructuralFeaturesUtil.scala
+++ src/mmt-lf/src/info/kwarc/mmt/lf/structuralfeatures/StructuralFeaturesUtil.scala
@@ -17,14 +17,23 @@ object StructuralFeatureUtils {
   object Ded {
     val path = LF._base ? "Ded" ? "DED"
     def apply(t: Term) = ApplySpine(OMS(path), t)
+    def unapply(t: Term) : Option[Term] = t match {
+      case Apply(OMS(path),tm) => Some(tm)
+    }
   }
   object Eq {
     val path = theory ? "EQUAL"
-    def apply(a: Term, s: Term, t: Term) = Ded(ApplySpine(OMS(path), a, s, t))
+    def apply(tp: Term, tm1: Term, tm2: Term) = Ded(ApplySpine(OMS(path), tp, tm1, tm2))
+    def unapply(t: Term) : Option[(Term, Term, Term)] = t match {
+      case Ded(ApplyGeneral(OMS(path), List(tp, tm1, tm2))) => Some((tp, tm1, tm2))
+    }
   }
   object Neq {
     val path = theory ? "NOTEQUAL"
-    def apply(a: Term, s: Term, t: Term) = Ded(ApplySpine(OMS(path), a, s, t))
+    def apply(tp: Term, tm1: Term, tm2: Term) = Ded(ApplySpine(OMS(path), tp, tm2, tm2))
+    def unapply(t: Term) : Option[(Term, Term, Term)] = t match {
+      case Ded(ApplyGeneral(OMS(path), List(tp, tm1, tm2))) => Some((tp, tm1, tm2))
+    }
   }
   
   // Needed for the testers
@@ -54,7 +63,28 @@ object StructuralFeatureUtils {
   
   val Contra = OMS(theory ? "CONTRA")
   /** negate the statement in the type */
-  def neg(tp: Term) : Term = Arrow(tp, Contra) 
+  def neg(tp: Term) : Term = Arrow(tp, Contra)
+  
+  /** To allow for more user friendly inductive-proof declarations */
+  object CONG {
+    val path = theory ? "CONG"
+    def apply(A: Term, B: Term, x: Term, y: Term, p_xeqy: Term, f: Term) = ApplyGeneral(OMS(path), List(A,B,x,y, p_xeqy, f))
+  }
+  /** More convenient version, which manually infers the first four arguments */
+  def Cong(pr: VarDecl, func: VarDecl) = {
+    val (pred, ptp) = (pr.toTerm, pr.tp.getOrElse(throw ImplementationError("Type missing for the proof passed to Cong.")))
+    val (tp, x, y) = ptp match {
+      case Eq(tp, tm1, tm2) => (tp, tm1, tm2)
+      case _ => throw ImplementationError("Unexpected type found for the proof. Can't apply Cong. ")  
+    }
+    val (f, ftp) = (func.toTerm, func.tp.getOrElse(throw ImplementationError("Type missing for the function. Can't apply Cong.")))
+    val (a, b) = ftp match {
+      case Arrow(dom, codom) => (dom, codom)
+      case _ => throw ImplementationError("Unexpected type found for the function. Can't apply Cong.")
+    }
+    CONG.apply(a, b, x, y, pred, f)
+  }
+  
   
   def parseInternalDeclarationsSubstitutingDefiniens(decls: List[Constant], con: Controller, ctx: Option[Context])(implicit parent : GlobalName): List[InternalDeclaration] = {
     val context = ctx.getOrElse(Context.empty)
