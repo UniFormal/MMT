29e8e8c13571d9aa6b5b5297baa8d384a3d21349
Michael Banken <michael.banken@fau.de>
2019-07-04 20:34:49 +0200

    Redone intersect recursively

--- src/mmt-api/src/main/info/kwarc/mmt/api/refactoring/Intersecter.scala
+++ src/mmt-api/src/main/info/kwarc/mmt/api/refactoring/Intersecter.scala
@@ -1,47 +1,247 @@
 package info.kwarc.mmt.api.refactoring
 
-import info.kwarc.mmt.api.{ComplexStep, LocalName, MPath}
+import info.kwarc.mmt.api.{ComplexStep, GlobalName, LocalName, MPath}
 import info.kwarc.mmt.api.frontend.{Controller, Extension}
 import info.kwarc.mmt.api.modules.{Theory, View}
-import info.kwarc.mmt.api.objects.Term
-import info.kwarc.mmt.api.symbols.{Declaration, FinalConstant, IdentityTranslator, Include, Renamer, Structure}
+import info.kwarc.mmt.api.objects.{Context, Term}
+import info.kwarc.mmt.api.symbols.{Declaration, FinalConstant, IdentityTranslator, Include, PlainInclude, Renamer, Structure, TraversingTranslator}
 
+import scala.collection.mutable
 import scala.util.{Success, Try}
 
 class Intersecter extends Extension {
-
-  def apply(view : View) : (Theory, Theory, Theory, Theory) = {
-    apply(controller.getTheory(view.from.toMPath), controller.getTheory(view.to.toMPath), view)
+  /**
+    *
+    * @param view
+    * @return
+    */
+  def apply(view : View) = {
+    intersect(view)
   }
 
-  def apply(th1 : Theory, th2 : Theory, view : View) : (Theory, Theory, Theory, Theory) = {
-    val th1p = Theory.empty(th1.parent, LocalName(th1.name.toString+"Mod"), th1.meta) // T1'
-    val th2p = Theory.empty(th2.parent, LocalName(th2.name.toString+"Mod"), th2.meta) // T2'
-    val sec1 = Theory.empty(th1.parent, LocalName(th1.name.toString+"CUT"+th2.name.toString), th1.meta) // intersection between T1 and T2
-    val sec2 = Theory.empty(th1.parent, LocalName(th2.name.toString+"CUT"+th1.name.toString), th2.meta) // intersection between T2 and T1
+  /**
+    *
+    * @param view
+    * @return
+    */
+  def intersect(view : View) : List[(Theory, Theory)] = {
+    val intersections = collection.mutable.HashMap[(MPath, MPath), (Theory, Theory)]()
+    val view_map = ViewSplitter(view)(controller).toMap
+    val view_map_inverse = ViewSplitter(view)(controller).map(_.swap).toMap
+    val th1 = controller.getTheory(view.from.toMPath)
+    val th2 = controller.getTheory(view.to.toMPath)
+    val renaming = mutable.HashMap[GlobalName, GlobalName]()
+    intersect(th1, th2, view_map, view_map_inverse, intersections, renaming)
 
-    val renaming = ViewSplitter(view)(controller)
-
-    val constSec1 = renaming.map(_._1)
-    val constSec2 = renaming.map(_._2)
-
-    val constRem1 = th1.getConstants.filter(!renaming.map(_._1).contains(_)).map(_.asInstanceOf[FinalConstant])
-    val structRem1 = th1.getDeclarations.flatMap(_ match{case s : Structure => Some(s) case _ => None})
-    val constRem2 = th2.getConstants.filter(!renaming.map(_._2).contains(_)).map(_.asInstanceOf[FinalConstant])
-    val structRem2 = th2.getDeclarations.flatMap(_ match{case s : Structure => Some(s) case _ => None})
-
-    Moduleadder(sec1, constSec1)
-    Moduleadder(sec2, constSec2)
-    structRem1.foreach{moveStructure(_, th1p)}
-    Moduleadder(th1p, constRem1)
-    th1p.add(Include(th1p.toTerm, sec1.path, List()))
-    structRem2.foreach{moveStructure(_, th2p)}
-    Moduleadder(th2p, constRem2)
-    th2p.add(Include(th2p.toTerm, sec2.path, List()))
-
-    (sec1, sec2, th1p, th2p)
-
+    intersections.values.toList
   }
+
+  /** Creates intersection
+    *
+    * creates intersection of th1 and th2 over view
+    *
+    * @param th1 left side theory to intersect
+    * @param th2 right side theory to intersect
+    * @param view_map mapping of view to intersect over
+    * @param view_map_inverse reverse mapping of view to intersect over
+    * @param renamings map of renamings of GlobalNames
+    * @return
+    */
+  protected def fillConstantsIntersection(th1: Theory, th2: Theory, int1 : Theory, int2 : Theory, view_map: collection.immutable.Map[FinalConstant, FinalConstant], view_map_inverse: collection.immutable.Map[FinalConstant, FinalConstant], renamings: mutable.HashMap[GlobalName, GlobalName]): (Theory, Theory) = {
+    //Add constants from th1
+    addDeclaration(th1.getDeclarations.flatMap{
+      _ match {
+        case c : FinalConstant => Some(c)
+        //TODO all cases???
+        case default => None
+      }
+    }.filter{
+      c => view_map.contains(c) && view_map.get(c).get.parent==th2.path
+    }, int1, renamings)
+
+    //Add constants from th2
+    addDeclaration(th2.getDeclarations.flatMap{
+      _ match {
+        case c : FinalConstant => Some(c)
+        case default => None
+      }
+    }.filter{
+      c => view_map_inverse.contains(c) && view_map_inverse.get(c).get.parent==th1.path
+    }, int2, renamings)
+    (int1, int2)
+  }
+
+  /** Recursive method to generate intersections
+    *
+    * Recursively generates intersections for dependencies of th1 and th2
+    *
+    * @param th1 left side theory to intersect
+    * @param th2 right side theory to intersect
+    * @param view_map mapping of view to intersect over
+    * @param view_map_inverse reverse mapping of view to intersect over
+    * @param intersections map of generated intersections, is filled recursively in this method
+    * @param renamings map of renamings of GlobalNames
+    * @return
+    */
+  protected def intersect(th1: Theory, th2: Theory, view_map: collection.immutable.Map[FinalConstant, FinalConstant], view_map_inverse: collection.immutable.Map[FinalConstant, FinalConstant], intersections: mutable.HashMap[(MPath, MPath), (Theory, Theory)], renamings: mutable.HashMap[GlobalName, GlobalName]): Unit = {
+    if (intersections.contains((th1.path, th2.path))) {
+      return
+    }
+    val includes1 = collect_structures(th1)
+    val includes2 = collect_structures(th2)
+
+    //Recurse through dependent Theories of th1 and th2
+    //th1:
+    includes1.foreach(_ match {
+      case Include(_, from, args) =>
+        intersect(controller.getTheory(from), th2, view_map, view_map_inverse, intersections, renamings)
+      //TODO named structures
+      case default => ???
+    })
+    //th2:
+    includes2.foreach(_ match {
+      case Include(_, from, args) =>
+        intersect(th1, controller.getTheory(from), view_map, view_map_inverse, intersections, renamings)
+      //TODO named structures
+      case default => ???
+    })
+
+    //Generate intersection of th1 and th2 over view and add them to intersections map
+    val int1 = Theory.empty(th1.parent, LocalName(th1.name.toString+"∩"+th2.name.toString), th1.meta)
+    val int2 = Theory.empty(th2.parent, LocalName(th2.name.toString+"∩"+th1.name.toString), th2.meta)
+    intersections.put((th1.path, th2.path), (int1, int2))
+
+    //Generate inclusions of dependent intersections
+    includes1.foreach{_ match {
+      case Include(_, from, args) =>
+        intersections.get((from, th1.path)).foreach (
+          _ match { case (pre_int1, pre_int2) =>
+            int1.add(PlainInclude(int1.path, pre_int1.path))
+            int2.add(PlainInclude(int2.path, pre_int2.path))
+          }
+        )
+      case default => ???
+    }}
+    includes2.foreach{_ match {case Include(_, from, args) =>
+      case Include(_, from, args) =>
+        intersections.get((th2.path, from)).foreach (
+          _ match { case (pre_int1, pre_int2) =>
+            int1.add(PlainInclude(int1.path, pre_int1.path))
+            int2.add(PlainInclude(int2.path, pre_int2.path))
+          }
+        )
+      case default => ???
+    }}
+
+    fillConstantsIntersection(th1, th2, int1, int2, view_map, view_map_inverse, renamings)
+  }
+
+  /**
+    *
+    * @param rem
+    * @param targetTheory
+    * @param intersectedTheory
+    * @param intersected
+    * @param intersection
+    * @param renamings
+    * @return
+    */
+  protected def fillConstantsRemainder(rem : Theory, targetTheory : Theory, intersectedTheory : Theory, intersected: Set[FinalConstant], intersection : Theory, renamings : mutable.HashMap[GlobalName, GlobalName]) : Theory  = {
+    rem.add(PlainInclude(rem.path, intersection.path))
+    targetTheory.getDeclarations.filter(_ match {
+      case c : FinalConstant => !intersected.contains(c)
+        //TODO all cases???
+      case default => ???
+    }).foreach(addDeclaration(_, rem, renamings))
+
+    rem
+  }
+
+  /**
+    *
+    * @param th1
+    * @param th2
+    * @param view_map
+    * @param view_map_inverse
+    * @param intersections
+    * @param renamings
+    */
+  protected def remainder1(th1: Theory, th2: Theory, view_map: collection.immutable.Map[FinalConstant, FinalConstant], view_map_inverse: collection.immutable.Map[FinalConstant, FinalConstant], intersections: mutable.HashMap[(MPath, MPath), (Theory, Theory)], renamings: mutable.HashMap[GlobalName, GlobalName]): Unit = {
+    val includes1 = collect_structures(th1)
+    val rem = Theory.empty(th1.parent, LocalName(th1.name.toString+"'"), th1.meta) // T'
+
+    //Recurse through dependent Theories of th1
+    includes1.foreach(_ match {
+      case Include(_, from, args) =>
+        remainder1(controller.getTheory(from), th2, view_map, view_map_inverse, intersections, renamings)
+      //TODO named structures
+      case default => ???
+    })
+    //Fill remainder with remaining constants
+    fillConstantsRemainder(rem, th1, th2, view_map.keySet, intersections.get((th1.path, th2.path)).get._1, renamings)
+  }
+
+  /**
+    *
+    * @param th1
+    * @param th2
+    * @param view_map
+    * @param view_map_inverse
+    * @param intersections
+    * @param renamings
+    */
+  protected def remainder2(th1: Theory, th2: Theory, view_map: collection.immutable.Map[FinalConstant, FinalConstant], view_map_inverse: collection.immutable.Map[FinalConstant, FinalConstant], intersections: mutable.HashMap[(MPath, MPath), (Theory, Theory)], renamings : mutable.HashMap[GlobalName, GlobalName]): Unit = {
+    val includes2 = collect_structures(th2)
+    val rem = Theory.empty(th2.parent, LocalName(th2.name.toString+"'"), th2.meta) // T'
+
+    //Recurse through dependent Theories of th2
+    includes2.foreach(_ match {
+      case Include(_, from, args) =>
+        remainder2(th1, controller.getTheory(from), view_map, view_map_inverse, intersections, renamings)
+      //TODO named structures
+      case default => ???
+    })
+    //Fill remainder with remaining constants
+    fillConstantsRemainder(rem, th2, th1, view_map_inverse.keySet, intersections.get((th1.path, th2.path)).get._2, renamings)
+  }
+
+  /**
+    *
+    * @param dec
+    * @param th
+    * @param renamings
+    * @return
+    */
+  protected def addDeclaration(dec : Declaration, th : Theory, renamings : mutable.HashMap[GlobalName, GlobalName]) = {
+    val renamer = Renamer(name => renamings.get(name))
+    val translator = TraversingTranslator(renamer)
+
+    val renamed = dec.translate(th.toTerm, LocalName.empty,translator, Context())
+    th.add(renamed)
+    renamings.put(dec.path, renamed.path)
+    renamed
+  }
+
+  /**
+    *
+    * @param decs
+    * @param th
+    * @param renamings
+    * @return
+    */
+  protected def addDeclaration(decs : Iterable[Declaration], th : Theory, renamings : mutable.HashMap[GlobalName, GlobalName]) : List[Declaration] = {
+    decs.map(addDeclaration(_, th, renamings)).toList
+  }
+
+  /**
+    *
+    * @param theory
+    * @return
+    */
+  protected def collect_structures(theory : Theory) = theory.getDeclarations.filter(_ match {
+    case Include(term, mpath, list) => true
+    case default => false
+  })
 }
 
 object ViewSplitter {
@@ -116,18 +316,3 @@ object ViewSplitter {
   }
 }
 
-object moveStructure {
-  def apply(struct : Structure, dest : Theory) : Structure = {
-    val structMod = repatriateStructure(struct, dest)
-    dest.add(structMod)
-    structMod
-  }
-
-  def repatriateStructure(struct : Structure, dest : Theory) : Structure = {
-    val structMod = Structure(dest.toTerm, struct.name, struct.from, struct.isImplicit)
-    for (decl <- struct.getDeclarations) {
-      structMod.add(decl.translate(dest.toTerm, LocalName.empty, IdentityTranslator, dest.parameters))
-    }
-    structMod
-  }
-}
--- src/test/RunMichael.scala
+++ src/test/RunMichael.scala
@@ -1,16 +1,21 @@
+import info.kwarc.mmt.MitM.MitM
+import info.kwarc.mmt.MitM.MitM.{eq, implicitProof, logic}
 import info.kwarc.mmt.api.frontend.Controller
 import info.kwarc.mmt.api.modules.{Theory, View}
+import info.kwarc.mmt.api.objects.{Context, OMS, StatelessTraverser, Term, Traverser}
 import info.kwarc.mmt.api.presentation.MMTSyntaxPresenter
-import info.kwarc.mmt.api.refactoring.{Intersecter, Moduleadder, ViewFinder, Viewset}
+import info.kwarc.mmt.api.refactoring.{Intersecter, Moduleadder, Preprocessor, SimpleParameterPreprocessor, ViewFinder, Viewset}
 import info.kwarc.mmt.api.symbols.FinalConstant
 import info.kwarc.mmt.api.{LocalName, NamespaceMap, Path}
 import info.kwarc.mmt.got.GraphOptimizationTool
 import info.kwarc.mmt.jedit.MMTOptimizationAnnotationReader
+import info.kwarc.mmt.lf.{ApplySpine, LFClassicHOLPreprocessor}
 
 object RunMichael extends MagicTest {
 
   def run : Unit = {
     intersect
+    //viewfinder
   }
 
   def got : Unit = {
@@ -38,19 +43,45 @@ object RunMichael extends MagicTest {
       val mitmmonoid = Path.parseM("http://mathhub.info/Tutorials/Mathematicians?Monoid",NamespaceMap.empty)
 
       // val from = Path.parseM("http://mathhub.info/MitM/Foundation?RealLiterals",NamespaceMap.empty)
-      val from = Path.parseM("http://cds.omdoc.org/testcases?BeautifulSets",NamespaceMap.empty)
+      //val from = Path.parseM("http://cds.omdoc.org/testcases?BeautifulSets",NamespaceMap.empty)
       // val from = Path.parseM("http://cds.omdoc.org/testcases?CommTest",NamespaceMap.empty)
       // val from = Path.parseM("http://cds.omdoc.org/testcases?PVSTest",NamespaceMap.empty)
+      val from = Path.parseM("http://mydomain.org/testarchive/mmt-example?addition",NamespaceMap.empty)
 
       // val to = "PVS/NASA"
       // val to = "HOLLight/basic"
-      val to = "MitM/smglom"
+      // val to = "MitM/smglom"
       // val to = "PVS/Prelude"
+      val to = "testarchive"
 
+      val eq = logic ? "eq"
+      object EliminateImplicits extends Preprocessor {
+        val trav = new StatelessTraverser {
+          override def traverse(t: Term)(implicit con: Context, state: State): Term = t match {
+            case ApplySpine(OMS(`implicitProof`),_) => OMS(implicitProof)
+            case _ => Traverser(this,t)
+          }
+        }
+        override protected def doTerm(tm: Term): Term = super.doTerm(tm)
+      }
+      val preproc = (SimpleParameterPreprocessor + info.kwarc.mmt.api.refactoring.DefinitionExpander + EliminateImplicits +
+        new LFClassicHOLPreprocessor(
+        ded = MitM.ded,
+        and = MitM.and,
+        not = MitM.not,
+        or = Some(MitM.or),
+        implies = Some(MitM.implies),
+        equiv = Some(MitM.equiv),
+        forall = Some(MitM.forall),
+        exists = Some(MitM.exists),
+        equal = Some(eq)
+      )).withKey("testarchive").withKey(logic)
+      controller.extman.addExtension(preproc)
       val vf = new ViewFinder
       controller.extman.addExtension(vf,List(
-        "MitM/smglom"
-        //,"HOLLight/basic"
+        "testarchive"
+        , "MitM/smglom"
+        // ,"HOLLight/basic"
         // ,"PVS/Prelude"
         // ,"PVS/NASA"
       ))
@@ -67,8 +98,13 @@ object RunMichael extends MagicTest {
     val int = new Intersecter
     controller.extman.addExtension(int, Nil)
 
-    val view = controller.get(Path.parseM("http://mydomain.org/testarchive/mmt-example?homo",NamespaceMap.empty)).asInstanceOf[View]
+    val view = controller.get(Path.parseM("http://mydomain.org/testarchive/mmt-example?mono",NamespaceMap.empty)).asInstanceOf[View]
+    //val (s1, s2, t1, t2) =
     println(int(view))
+    //println(s1)
+    //println(s2)
+    //println(t1)
+    //println(t2)
   }
 
   def exportMMT: Unit = {
