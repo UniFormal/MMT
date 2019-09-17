f7fdeaf11e6a454a0b288bf0ce79bedb7dc803ec
Colin Rothgang <colin.rothgang@gmx.de>
2019-09-17 20:26:27 +0200

    Several fixes for Reflections.

--- src/mmt-lf/src/info/kwarc/mmt/lf/structuralfeatures/Reflections.scala
+++ src/mmt-lf/src/info/kwarc/mmt/lf/structuralfeatures/Reflections.scala
@@ -24,7 +24,7 @@ class Reflections extends StructuralFeature("reflect") with TypedParametricTheor
    * @param dd the derived declaration from which the inductive type(s) are to be constructed
    */
   override def check(dd: DerivedDeclaration)(implicit env: ExtendedCheckingEnvironment) {}
-    
+
   /**
    * Elaborates an declaration of one or multiple mutual inductive types into their declaration, 
    * as well as the corresponding no confusion and no junk axioms
@@ -33,16 +33,28 @@ class Reflections extends StructuralFeature("reflect") with TypedParametricTheor
    * @param dd the derived declaration to be elaborated
    */
   def elaborate(parent: ModuleOrLink, dd: DerivedDeclaration)(implicit env: Option[uom.ExtendedSimplificationEnvironment] = None) = {
-    val (indDefPath, context, indParams) = ParamType.getParams(dd)
-    val consts = controller.getO(indDefPath) match {
-      case Some(m : ModuleOrLink) => getConstants(m.getDeclarations, controller)
-      case Some(t) => throw GeneralError("reflection over unsupported target (expected module or link)"+t.path)
+
+    val (indDefPathGN, context, indParams) = ParamType.getParams(dd)
+    val indDefPath = indDefPathGN.toMPath
+    // TODO: figure out how to work with theory parameters
+    val (indCtx, consts) = controller.getO(indDefPath) match {
+      case Some(str) => str match {
+        case t: Theory => (Context.empty, getConstants(t.getDeclarations, controller))
+        case t: StructuralElement if (t.feature =="theory") => 
+          val decls = t.getDeclarations map {case d: Declaration => d case _ => throw LocalError("unsupported structural element at "+t.path)}
+          (Context.empty, getConstants(decls, controller))
+        case m : ModuleOrLink => (Context.empty, getConstants(m.getDeclarations, controller))
+        case t => throw GeneralError("reflection over unsupported target (expected module or link)"+t.path+" of feature "+t.feature + ": "+t)
+      }
       case None => throw GeneralError("target of reflection not found at "+indDefPath)
     }
-    val decls = readInternalDeclarations(consts, controller, Some(context))(indDefPath)
-        
     
-    structuralfeatures.InductiveTypes.elaborateDeclarations(context, decls)(dd.path)
+    val declsPre = readInternalDeclarations(consts, controller, Some(context))(indDefPathGN)
+    val subst = indCtx zip indParams map {case (vd, param) => Sub(vd.name, param)}
+    val tr = OMSReplacer({p:GlobalName => if (p.module == indDefPath.module/indDefPath.name) Some(OMS(dd.modulePath ? p.name)) else None})
+    val decls = declsPre map (_.translate(ApplySubs(subst)).translate(TraversingTranslator(tr)))
+    
+    structuralfeatures.InductiveTypes.elaborateDeclarations(context, decls, controller, Some({c => log(defaultPresenter(c)(controller))}))(dd.path)
   }
 }
 
