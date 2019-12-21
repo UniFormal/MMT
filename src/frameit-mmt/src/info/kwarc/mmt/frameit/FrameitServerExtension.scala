776ba303830f78e07bbdef19781416da0a9a1eee
ComFreek <comfreek@outlook.com>
2019-12-21 12:45:04 +0100

    FrameIT: improve a lot the server backend

--- src/frameit-mmt/src/info/kwarc/mmt/frameit/FrameitServerExtension.scala
+++ src/frameit-mmt/src/info/kwarc/mmt/frameit/FrameitServerExtension.scala
@@ -6,12 +6,15 @@ import info.kwarc.mmt.api.archives.Archive
 import info.kwarc.mmt.api.documents.Document
 import info.kwarc.mmt.api.frontend.{Extension, Logger}
 import info.kwarc.mmt.api.modules.{Module, Theory}
+import info.kwarc.mmt.api.objects.{Context, Obj}
+import info.kwarc.mmt.api.presentation.MMTSyntaxPresenter
+import info.kwarc.mmt.api.symbols.{Constant, Declaration, FinalConstant, TermContainer}
+import info.kwarc.mmt.api.uom.SimplificationUnit
 import info.kwarc.mmt.api.utils.URI
-import info.kwarc.mmt.api.{DPath, LocalName, MPath}
+import info.kwarc.mmt.api.{DPath, LocalName, MPath, RuleSet}
 import io.circe.generic.auto._
 import io.finch._
 import io.finch.circe._
-import io.circe.syntax._
 
 import scala.collection.immutable._
 
@@ -21,15 +24,16 @@ class FrameitServerExtension extends Extension with Logger with Endpoint.Module[
 
   var frameitArchive: Archive = _
   var factTheory: Theory = _
+  var presenter: MMTSyntaxPresenter = _
 
-  import FactEnconderWrapper.factEncoder
-  import MPathEncoderWrapper.mpathEncoder
+  implicit val factEncoder = FactEnconderWrapper.getFactEncoder(() => presenter)
+  import MPathEncoderWrapper.mpathEncoder  // possibly shown as unused by IntelliJ. Do not remove, it IS used!
 
   override def start(args: List[String]): Unit = {
     super.start(args)
 
     val endpoints =
-      init :+: addPointFact :+: addDistanceFact :+: getAllFacts :+: getAllScrolls
+      init :+: build :+: rebuild :+: addPointFact :+: addDistanceFact :+: getAllFacts :+: getAllScrolls
 
     Http.server.serve(
       ":8081",
@@ -37,47 +41,85 @@ class FrameitServerExtension extends Extension with Logger with Endpoint.Module[
     )
   }
 
-  // val home : Term, val name : LocalName, val alias: List[LocalName],
-  //               val tpC : TermContainer, val dfC : TermContainer, val rl : Option[String], val notC: NotationContainer, val vs: Visibility)
-
   def init: Endpoint[IO, Unit] = post(path("init")) {
-    controller.handleLine(s"build $FRAMEIT_ARCHIVE_ID mmt-omdoc")
-    frameitArchive = controller.backend.getArchive(FRAMEIT_ARCHIVE_ID).getOrElse(
-      throw info.kwarc.mmt.api.GetError(s"Archive $FRAMEIT_ARCHIVE_ID not found")
-    )
-    factTheory = Theory.empty(
-      DPath(URI("https://examples.com/frameit")),
-      LocalName("factTheory"),
-      None
-    )
+    presenter = controller.extman.getOrAddExtension(classOf[MMTSyntaxPresenter], classOf[MMTSyntaxPresenter].getCanonicalName, Nil).get
+
+    frameitArchive = {
+      // Try quering the archive, possibly building once, then giving up
+      controller.backend.getArchive(FRAMEIT_ARCHIVE_ID).getOrElse {
+        controller.handleLine(s"build $FRAMEIT_ARCHIVE_ID mmt-omdoc")
+        controller.backend.getArchive(FRAMEIT_ARCHIVE_ID).getOrElse(
+          throw info.kwarc.mmt.api.GetError(s"Archive $FRAMEIT_ARCHIVE_ID not found")
+        )
+      }
+    }
+    factTheory = controller.getTheory((DPath(frameitArchive.narrationBase) / "annotation") ? "FactTheory")
 
     Ok(())
   } handle {
     case e: info.kwarc.mmt.api.Error => ServiceUnavailable(e)
   }
 
-  def addPointFact: Endpoint[IO, Unit] = post(path("fact") :: path("point") :: path("add") :: jsonBody[UnityPoint]) { (point: UnityPoint) =>
-    factTheory.add(MMTConstantUnityFactBridge(point, factTheory.toTerm))
+  def build: Endpoint[IO, Unit] = post(path("build")) {
+    controller.handleLine(s"build $FRAMEIT_ARCHIVE_ID mmt-omdoc")
 
     Ok(())
   }
 
-  def addDistanceFact: Endpoint[IO, Unit] = post(path("fact") :: path("distance") :: path("add") :: jsonBody[UnityDistance]) { distance: UnityDistance =>
-    factTheory.add(MMTConstantUnityFactBridge(distance, factTheory.toTerm))
+  def rebuild: Endpoint[IO, Unit] = post(path("rebuild")) {
+    controller.handleLine(s"build $FRAMEIT_ARCHIVE_ID -mmt-omdoc")
+    controller.handleLine(s"build $FRAMEIT_ARCHIVE_ID mmt-omdoc")
 
     Ok(())
   }
 
-  def getAllFacts: Endpoint[IO, List[UnityFact]] = get(path("fact") :: path("list") :: paramOption[Boolean]("simplified").withDefault(false)) { simplified: Boolean => {
-    // implicit lazy val ce: CheckingEnvironment = new CheckingEnvironment(controller.simplifier, ErrorThrower, RelationHandler.ignore, this)
+  def addPointFact: Endpoint[IO, Unit] = post(path("fact") :: path("add") :: path("point") :: jsonBody[UnityPoint]) { (point: UnityPoint) =>
+    factTheory.add(FrameIT.Annotations.PointAnnotation(point, factTheory.toTerm))
 
-    // controller.simplifier.apply()
+    Ok(())
+  }
 
-    Ok(
-      factTheory
-        .getDeclarations
-        .flatMap(d => MMTConstantUnityFactBridge.unapply(d))
-    )
+  def addDistanceFact: Endpoint[IO, Unit] = post(path("fact") :: path("add") :: path("distance") :: jsonBody[UnityDistance]) { distance: UnityDistance =>
+    factTheory.add(FrameIT.Annotations.DistanceAnnotation(distance, factTheory.toTerm))
+
+    Ok(())
+  }
+
+  def getAllFacts: Endpoint[IO, List[Fact]] = get(path("fact") :: path("list") :: paramOption[Boolean]("simplified").withDefault(false)) { simplified: Boolean => {
+    // TODO How to get solver out of controller?
+    val simplificationUnit = SimplificationUnit(factTheory.getInnerContext, expandDefinitions = true, fullRecursion = true, solverO = None)
+
+    def simplify(obj: Obj): obj.ThisType = controller.simplifier.apply(obj, simplificationUnit)
+
+    val declarations: Seq[Declaration] = {
+      if (!simplified) {
+        factTheory.getDeclarations
+      } else {
+        // Simplify type and definiens components of constant declarations
+        factTheory.getDeclarations.map {
+          case c: Constant =>
+            new FinalConstant(
+              home = c.home,
+              name = c.name,
+              alias = c.alias,
+              tpC = TermContainer.asParsed(c.tp.map(simplify(_))),
+              dfC = TermContainer.asParsed(c.df.map(simplify(_))),
+              rl = c.rl,
+              notC = c.notC,
+              vs = c.vs
+            )
+          case x => x
+        }
+      }
+    }
+
+    val reconstructedFacts: Seq[Fact] = declarations.collect {
+        case FrameIT.Annotations.PointAnnotation(point) => point
+        case FrameIT.Annotations.DistanceAnnotation(distance) => distance
+        case d => UnknownFact(d)
+    }
+
+    Ok(reconstructedFacts.toList)
   }
   }
 
@@ -99,39 +141,4 @@ class FrameitServerExtension extends Extension with Logger with Endpoint.Module[
     ret
   }*/
   /*implicit lazy val ce : CheckingEnvironment = new CheckingEnvironment(controller.simplifier,ErrorThrower,RelationHandler.ignore, this)*/
-  /*
-    def apply(request: ServerRequest): ServerResponse = try {
-      request.path match {
-        case pathPrefix :: "init" :: Nil =>
-          // controller.handleLine("build FrameIT/FrameIT mmt-omdoc")
-          ServerResponse.TextResponse("Success")
-
-
-        case pathPrefix :: "facts" :: "add" :: factToAdd :: Nil => factToAdd match {
-          case "point" =>
-            request.body.asXML match {
-              case pointsXml@<ints></points> =>
-                val points = pointsXml.child.map {
-                  case pointXml@<point/> =>
-                    val malformedError = () => throw MalformedCommandError("<point> must have x, y, z attributes")
-                    List(
-                      pointXml.attribute("x").getOrElse(malformedError).toString.toFloat,
-                      pointXml.attribute("y").getOrElse(malformedError).toString.toFloat,
-                      pointXml.attribute("z").getOrElse(malformedError).toString.toFloat
-                    )
-
-                  case _ => throw MalformedCommandError("<points> tag must have <point> in it")
-                }
-                ServerResponse.TextResponse(points.mkString("\n"))
-              case _ => throw MalformedCommandError("Must have <points> tag")
-            }
-          case _ => throw UnknownCommandError()
-        }
-
-        case _ => throw UnknownCommandError()
-      }
-    } catch {
-      case _: UnknownCommandError => ServerResponse.errorResponse(s"Unknown request path and/or command: ${request.path.mkString("/")}")
-      case e: Exception => ServerResponse.errorResponse("Error while processing command: " + e.getMessage)
-    }*/
 }
--- src/frameit-mmt/src/info/kwarc/mmt/frameit/datastructures.scala
+++ src/frameit-mmt/src/info/kwarc/mmt/frameit/datastructures.scala
@@ -1,30 +1,51 @@
 package info.kwarc.mmt.frameit
 
 import info.kwarc.mmt.api._
-import info.kwarc.mmt.api.metadata.MetaDatum
 import info.kwarc.mmt.api.modules.{Module, Theory}
-import info.kwarc.mmt.api.notations.NotationContainer
-import info.kwarc.mmt.api.objects.{OMID, OMLIT, OMMOD, Term}
-import info.kwarc.mmt.api.symbols.{Constant, Declaration, FinalConstant, TermContainer}
-import info.kwarc.mmt.api.uom.{RealizedType, StandardString}
-import info.kwarc.mmt.lf.ApplySpine
+import info.kwarc.mmt.api.presentation.MMTSyntaxPresenter
+import info.kwarc.mmt.api.symbols.Declaration
+import io.circe.Json
 import io.circe.generic.auto._
 import io.circe.syntax._
 
-import scala.collection.immutable.{::, List, Nil}
+sealed abstract class Fact(label: String)
 
-sealed abstract class UnityFact(label: String, unityIdentifier: String)
+/**
+  * Facts of which the Unity side is aware of.
+  */
+sealed abstract class UnityFact(label: String, unityIdentifier: String) extends Fact(label)
 
 sealed case class UnityPoint(label: String, unityIdentifier: String, x: Double, y: Double, z: Double) extends UnityFact(label, unityIdentifier)
 
 sealed case class UnityDistance(label: String, unityIdentifier: String, start: UnityPoint, end: UnityPoint) extends UnityFact(label, unityIdentifier)
 
+sealed case class UnknownFact(d: Declaration) extends Fact(d.name.toString)
+
 // Need a wrapper to be able to import the inner implicit encoder
 object FactEnconderWrapper {
-  implicit val factEncoder: io.circe.Encoder[UnityFact] = fact => {
+  /**
+    * Get an encoder for [[Fact facts]].
+    *
+    * @param presenter A function returning an [[MMTSyntaxPresenter]].
+    *                  We need this indirection because the encoder has to be present as an implicit value
+    *                  "at compile time" for [[io.circe.generic]] to work, however, we can only construct
+    *                  the presenter later on in [[FrameitServerExtension]].
+    *
+    *                  Ideally this function is as cheap as just returning a variable.
+    * @return
+    */
+  def getFactEncoder(presenter: () => MMTSyntaxPresenter): io.circe.Encoder[Fact] = fact => {
     val (underlyingJson, underlyingType) = fact match {
       case point: UnityPoint => (point.asJson, "point")
       case distance: UnityDistance => (distance.asJson, "distance")
+      case UnknownFact(decl) =>
+        val declAsString = {
+          val stringRenderingHandler = new info.kwarc.mmt.api.presentation.StringBuilder
+          presenter()(decl)(stringRenderingHandler)
+          stringRenderingHandler.get
+        }
+
+        (Json.obj(("str", declAsString.asJson)), "unknown")
     }
 
     underlyingJson.asJson.asObject.get.+:("type", underlyingType.asJson).asJson
@@ -47,84 +68,6 @@ object Scroll {
     modules
       .filter(_.isInstanceOf[Theory])
       .map(_.asInstanceOf[Theory])
-      .flatMap(TheoryMetaBridgeToScroll.unapply)
-  }
-
-  /*implicit val scrollEncoder: io.circe.Encoder[Scroll] = scroll => Json.obj(
-    ("label",  scroll.label),
-    ("description", scroll.description),
-    ("problemTheory", scroll.pr)
-  )
-      case point: UnityPoint =>
-        Json.obj(
-          ("unityIdentifier", point.unityIdentifier.asJson),
-          ("x", point.x.asJson),
-          ("y", point.y.asJson),
-          ("z", point.z.asJson)
-        )
-      case _ => ???
-  }*/
-}
-
-object MMTConstantUnityFactBridge {
-  def apply(point: UnityPoint, home: Term): Constant = {
-    new FinalConstant(
-      home = home,
-      name = LocalName(point.label),
-      alias = Nil,
-      tpC = TermContainer.asParsed(None),
-      dfC = TermContainer.asParsed(None),
-      rl = None,
-      notC = new NotationContainer,
-      vs = symbols.Visibility.public
-    )
-
-    // TODO: Add unity identifier to constant
-    //       pointConstant.metadata.add()
-  }
-
-  def apply(distance: UnityDistance, home: Term): Constant = {
-    ???
-  }
-
-  def unapply(declaration: Declaration): Option[UnityFact] = declaration match {
-    case c: FinalConstant => unapply(c)
-    case _ => None
-  }
-
-  def unapply(constant: Constant): Option[UnityFact] = {
-    Some(UnityPoint("A", "1", 4, 5, 6))
-  }
-}
-
-/**
-  * Unapply method for MMT/urtheories string literals as [[Term]]s.
-  *
-  * @todo Ask Florian where this should go. Ideally either in mmt-lf or into the archive MMT/urtheories
-  */
-object UrtheoriesString {
-  private val urtheoriesStringType: GlobalName = Path.parseS("http://cds.omdoc.org/urtheories?Strins?string", NamespaceMap.empty)
-
-  def unapply(term: Term): Option[String] = term match {
-    case OMLIT(str: String, RealizedType(OMID(urtheoriesStringType), StandardString)) => Some(str)
-    case _ => None
-  }
-}
-
-object TheoryMetaBridgeToScroll {
-  def unapply(theory: Theory): List[Scroll] = {
-    val hasScrollProblemTheory: GlobalName = Path.parseS("https://example.com/frameit/annotation?ScrollMeta?hasScrollProblemTheory", NamespaceMap.empty)
-    val scrollProblem: GlobalName = Path.parseS("https://example.com/frameit/annotation?ScrollMeta?scrollProblem", NamespaceMap.empty)
-
-    theory.metadata.get(hasScrollProblemTheory).flatMap {
-      case MetaDatum(
-      _,
-      ApplySpine(OMID(scrollProblem), OMMOD(problemTheory) :: UrtheoriesString(label) :: UrtheoriesString(description) :: Nil)
-      ) => Some(Scroll(label, description, problemTheory, theory.path))
-
-      case _ =>
-        // Log error: s"Theory ${theory.path} has malformed metadata"
-        None
-    }
+      .flatMap(FrameIT.Annotations.getScrollsFor)
   }
 }
